a97cf7e0faf3a6dd8a56acb23e09cc86b8987d6e
Philipp Hancke <fippo@andyet.net>
2014-04-10 10:33:53 +0200

    default to generation 0

--- libs/strophe/strophe.jingle.sdp.util.js
+++ libs/strophe/strophe.jingle.sdp.util.js
@@ -292,6 +292,8 @@ SDPUtil = {
         candidate.port = elems[5];
         // elems[6] => "typ"
         candidate.type = elems[7];
+
+        candidate.generation = '0'; // default, may be overwritten below
         for (i = 8; i < elems.length; i += 2) {
             switch (elems[i]) {
                 case 'raddr':
