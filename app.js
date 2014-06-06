679e5bc3d5710031ea3e63cb7c9f7cf8cb43b9db
Philipp Hancke <fippo@andyet.net>
2014-06-06 06:14:20 +0200

    less label

--- app.js
+++ app.js
@@ -265,7 +265,7 @@ $(document).bind('remotestreamadded.jingle', function (event, data, sid) {
     if (data.stream.id.indexOf('mixedmslabel') === -1) {
         var ssrclines = SDPUtil.find_lines(sess.peerconnection.remoteDescription.sdp, 'a=ssrc');
         ssrclines = ssrclines.filter(function (line) {
-            return line.indexOf('msid:' + data.stream.label) !== -1;
+            return line.indexOf('msid:' + data.stream.id) !== -1;
         });
         if (ssrclines.length) {
             thessrc = ssrclines[0].substring(7).split(' ')[0];
