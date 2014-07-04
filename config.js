0c495b3d2835aae0d091ed636da83d58390c5285
turint <yasen@lindeas.com>
2014-07-04 22:25:44 +0300

    jitsi-meet.example.com domain references in the example config

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 var config = {
     hosts: {
-        domain: 'guest.jit.si',
-        muc: 'meet.jit.si', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.lambada.jitsi.net' // FIXME: use XEP-0030
+        domain: 'jitsi-meet.example.com',
+        muc: 'conference.jitsi-meet.example.com', // FIXME: use XEP-0030
+        bridge: 'jitsi-videobridge.jitsi-meet.example.com' // FIXME: use XEP-0030
     },
 //  getroomnode: function (path) { return 'someprefixpossiblybasedonpath'; },
 //  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
 //  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//lambada.jitsi.net/http-bind', // FIXME: use xep-0156 for that
+    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
     desktopSharing: 'ext', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
     chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
     minChromeExtVersion: '0.1', // Required version of Chrome extension
