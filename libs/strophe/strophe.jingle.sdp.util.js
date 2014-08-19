2c499626b3c19dd71140cc6272f9d592aacb776a
Philipp Hancke <fippo@andyet.net>
2014-08-19 16:31:59 +0200

    map tcptype for tcp candidates

--- libs/strophe/strophe.jingle.sdp.util.js
+++ libs/strophe/strophe.jingle.sdp.util.js
@@ -208,6 +208,7 @@ SDPUtil = {
             line += 'tcptype';
             line += ' ';
             line += cand.tcptype;
+            line += ' ';
         }
         line += 'generation';
         line += ' ';
