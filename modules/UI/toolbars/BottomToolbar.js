2c573d76ea0543d878e88756035664e388ad4822
Issac Gerges <igerges@atlassian.com>
2015-09-02 12:10:04 -0500

    Add analytics for bottom toolbar events

--- modules/UI/toolbars/BottomToolbar.js
+++ modules/UI/toolbars/BottomToolbar.js
@@ -1,14 +1,18 @@
 /* global $ */
 var PanelToggler = require("../side_pannels/SidePanelToggler");
+var AnalyticsAdapter = require("../../statistics/AnalyticsAdapter");
 
 var buttonHandlers = {
     "bottom_toolbar_contact_list": function () {
+        AnalyticsAdapter.sendEvent('bottomtoolbar.contacts.toggled');
         BottomToolbar.toggleContactList();
     },
     "bottom_toolbar_film_strip": function () {
+        AnalyticsAdapter.sendEvent('bottomtoolbar.filmstrip.toggled');
         BottomToolbar.toggleFilmStrip();
     },
     "bottom_toolbar_chat": function () {
+        AnalyticsAdapter.sendEvent('bottomtoolbar.chat.toggled');
         BottomToolbar.toggleChat();
     }
 };
