40aacec769ed1867cd51ad99ae623649dc3b31c9
Boris Grozev <boris@jitsi.org>
2015-09-09 14:09:58 -0500

    Removes misleading documentation. Formatting fixes.

--- modules/xmpp/TraceablePeerConnection.js
+++ modules/xmpp/TraceablePeerConnection.js
@@ -1,3 +1,4 @@
+/* global $ */
 var RTC = require('../RTC/RTC');
 
 var logger = require("jitsi-meet-logger").getLogger(__filename);
@@ -177,7 +178,8 @@ var normalizePlanB = function(desc) {
             }
 
             if (typeof mLine.ssrcs !== 'undefined' && Array.isArray(mLine.ssrcs)) {
-                for (var i = 0; i<mLine.ssrcs.length; i++){
+                var i;
+                for (i = 0; i<mLine.ssrcs.length; i++){
                     if (typeof mLine.ssrcs[i] === 'object'
                         && typeof mLine.ssrcs[i].id !== 'undefined'
                         && $.inArray(mLine.ssrcs[i].id, firstSsrcs) == 0) {
@@ -186,7 +188,7 @@ var normalizePlanB = function(desc) {
                     }
                 }
 
-                for (var i = 0; i<mLine.ssrcs.length; i++){
+                for (i = 0; i<mLine.ssrcs.length; i++){
                     if (typeof mLine.ssrcs[i] !== 'undefined') {
                         newSsrcLines.push(mLine.ssrcs[i]);
                     }
