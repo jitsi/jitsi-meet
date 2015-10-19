657a3fb2e17fc6fa3d04c82c769ec5323f3aed6a
Boris Grozev <boris@jitsi.org>
2015-10-19 15:59:16 -0500

    Removes comments (fixes warnings).

--- modules/UI/authentication/LoginDialog.js
+++ modules/UI/authentication/LoginDialog.js
@@ -24,7 +24,6 @@ function Dialog(callback, obtainSession) {
 
     var message = '<h2 data-i18n="dialog.passwordRequired">';
     message += APP.translation.translateString("dialog.passwordRequired");
-    // ++__authdomain__
     message += '</h2>' +
         '<input name="username" type="text" ';
     if (config.hosts.authdomain) {
@@ -35,7 +34,6 @@ function Dialog(callback, obtainSession) {
     message += '<input name="password" ' +
         'type="password" data-i18n="[placeholder]dialog.userPassword"' +
         ' placeholder="user password">';
-    // --__authdomain__
 
     var okButton = APP.translation.generateTranslationHTML("dialog.Ok");
 
@@ -56,7 +54,6 @@ function Dialog(callback, obtainSession) {
                     var password = f.password;
                     if (jid && password) {
                         stop = false;
-                        //++__authdomain__ complete jid by appending @<domain> if no @ was found in jid
                         if (jid.indexOf("@") < 0) {
                           jid = jid.concat('@');
                           if (config.hosts.authdomain) {
@@ -65,7 +62,6 @@ function Dialog(callback, obtainSession) {
                             jid += config.hosts.domain;
                           }
                         }
-                        // --__authdomain__
                         connection.reset();
                         connDialog.goToState('connecting');
                         connection.connect(jid, password, stateHandler);
