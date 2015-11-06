78d46e1bd0d87239d9d286861e2b1efb100a3a34
Boris Grozev <boris@jitsi.org>
2015-11-06 15:45:34 -0600

    Exposes functions in RandomUtils.

--- modules/util/RandomUtil.js
+++ modules/util/RandomUtil.js
@@ -65,7 +65,9 @@ var RandomUtil = {
             ret += this.randomHexDigit();
         }
         return ret;
-    }
+    },
+    randomElement: randomElement,
+    randomAlphanumStr: randomAlphanumStr
 };
 
 module.exports = RandomUtil;
