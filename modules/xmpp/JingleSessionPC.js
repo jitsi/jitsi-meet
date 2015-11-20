INKLUSIVACall/inklusiva-meet

Adds additional logs, including timestamps relative to the initial request.

modules/xmpp/JingleSessionPC.js
@@ -142,7 +142,8 @@ JingleSessionPC.prototype.doInitialize = function () {
      */
     this.peerconnection.oniceconnectionstatechange = function (event) {
         if (!(self && self.peerconnection)) return;
-        console.info("Ice: " + self.peerconnection.iceConnectionState);
+        console.log("(TIME) ICE " + self.peerconnection.iceConnectionState +
+                    ":\t", window.performance.now());
         self.updateModifySourcesQueue();
         switch (self.peerconnection.iceConnectionState) {
             case 'connected':

modules/xmpp/xmpp.js
@@ -80,8 +80,9 @@ function connect(jid, password) {
         var connectionFailed = false;
         var lastErrorMsg;
         connection.connect(jid, password, function (status, msg) {
-            console.log('Strophe status changed to',
-                Strophe.getStatusString(status), msg);
+            console.log("(TIME) Strophe " + Strophe.getStatusString(status) +
+                (msg ? "[" + msg + "]" : "") +
+                "\t:" + window.performance.now());
             if (status === Strophe.Status.CONNECTED) {
                 if (config.useStunTurn) {
                     connection.jingle.getStunAndTurnCredentials();

jitsi/jitsi-meet

modules/xmpp/JingleSessionPC.js
@@ -146,6 +146,8 @@ JingleSessionPC.prototype.doInitialize = function () {
      */
     this.peerconnection.oniceconnectionstatechange = function (event) {
         if (!(self && self.peerconnection)) return;
+        logger.log("(TIME) ICE " + self.peerconnection.iceConnectionState +
+                    ":\t", window.performance.now());
         self.updateModifySourcesQueue();
         switch (self.peerconnection.iceConnectionState) {
             case 'connected':

modules/xmpp/xmpp.js
@@ -103,8 +103,8 @@ XMPP.prototype._connect = function (jid, password) {
     var connectionFailed = false;
     var lastErrorMsg;
     this.connection.connect(jid, password, function (status, msg) {
-        logger.log('Strophe status changed to',
-            Strophe.getStatusString(status), msg);
+        logger.log("(TIME) Strophe " + Strophe.getStatusString(status) +
+            (msg ? "[" + msg + "]" : "") + "\t:" + window.performance.now());
         if (status === Strophe.Status.CONNECTED) {
             if (self.options.useStunTurn) {
                 self.connection.jingle.getStunAndTurnCredentials();