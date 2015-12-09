ae36f52b660d3544b7b98de7a1753749d3c4ec40
Robert Smallwood <robert.smallwood@readytalk.com>
2015-12-09 12:44:08 -0700

    Fixing missed merge conflict

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function init() {
 
     APP.desktopsharing.init();
     APP.RTC.start();
-    APP.xmpp.start(APP.settings.getJid(), null);
+    APP.xmpp.start();
     APP.statistics.start();
     APP.connectionquality.init();
     APP.keyboardshortcut.init();
