eefdbd4fe59589b369d9d68abf39a1048278db8f
damencho <damencho@jitsi.org>
2016-03-17 21:58:40 -0500

    Renames EtherpadContainerType to ETHERPAD_CONTAINER_TYPE.

--- modules/UI/etherpad/Etherpad.js
+++ modules/UI/etherpad/Etherpad.js
@@ -52,7 +52,7 @@ const DEFAULT_WIDTH = 640;
  */
 const DEFAULT_HEIGHT = 480;
 
-const EtherpadContainerType = "etherpad";
+const ETHERPAD_CONTAINER_TYPE = "etherpad";
 
 /**
  * Container for Etherpad iframe.
@@ -159,7 +159,7 @@ export default class EtherpadManager {
     openEtherpad () {
         this.etherpad = new Etherpad(this.domain, this.name);
         VideoLayout.addLargeVideoContainer(
-            EtherpadContainerType,
+            ETHERPAD_CONTAINER_TYPE,
             this.etherpad
         );
     }
@@ -174,9 +174,10 @@ export default class EtherpadManager {
         }
 
         let isVisible = VideoLayout.isLargeContainerTypeVisible(
-            EtherpadContainerType
+            ETHERPAD_CONTAINER_TYPE
         );
 
-        VideoLayout.showLargeVideoContainer(EtherpadContainerType, !isVisible);
+        VideoLayout.showLargeVideoContainer(
+            ETHERPAD_CONTAINER_TYPE, !isVisible);
     }
 }
