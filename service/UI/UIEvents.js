586b8401aef94c26b2f9e178700656fec9682a3a
damencho <damencho@jitsi.org>
2016-03-22 15:04:52 -0500

    Renames event name.

--- service/UI/UIEvents.js
+++ service/UI/UIEvents.js
@@ -21,7 +21,7 @@ export default {
     AUDIO_MUTED: "UI.audio_muted",
     VIDEO_MUTED: "UI.video_muted",
     ETHERPAD_CLICKED: "UI.etherpad_clicked",
-    SHARED_VIDEO_CLICKED: "UI.start-shared-video",
+    SHARED_VIDEO_CLICKED: "UI.start_shared_video",
     /**
      * Updates shared video with params: url, state, time(optional)
      * Where url is the video link, state is stop/start/pause and time is the
