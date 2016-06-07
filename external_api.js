98919e09964234bdffb3183bb73e86f83d3a7e64
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2016-06-07 14:08:02 -0500

    Changed variable from ssl to noSsl
    Defaults to SSL, only if the noSsl flag is true will it use http

--- external_api.js
+++ external_api.js
@@ -25,11 +25,11 @@ var JitsiMeetExternalAPI = (function()
      * @param parent_node the node that will contain the iframe
      * @param filmStripOnly if the value is true only the small videos will be
      * visible.
-     * @param ssl if the value is true https will be used on the iframe
+     * @param noSsl if the value is true https won't be used
      * @constructor
      */
     function JitsiMeetExternalAPI(domain, room_name, width, height, parentNode,
-        configOverwrite, interfaceConfigOverwrite, ssl) {
+        configOverwrite, interfaceConfigOverwrite, noSsl) {
         if (!width || width < MIN_WIDTH)
             width = MIN_WIDTH;
         if (!height || height < MIN_HEIGHT)
@@ -51,7 +51,7 @@ var JitsiMeetExternalAPI = (function()
         if(height)
             this.iframeHolder.style.height = height + "px";
         this.frameName = "jitsiConferenceFrame" + JitsiMeetExternalAPI.id;
-        this.url = (ssl) ? "https:" : "" +"//" + domain + "/";
+        this.url = (noSsl) ? "http" : "https" +"://" + domain + "/";
         if(room_name)
             this.url += room_name;
         this.url += "#external=true";
