8c58d6e919525a3ccba9bc46b480a0fb44d19a66
nikvaessen <nikvaes@gmail.com>
2016-07-28 16:56:51 +0200

    changed to transcription module

--- conference.js
+++ conference.js
@@ -533,9 +533,9 @@ export default {
                 if (config.iAmRecorder)
                     this.recorder = new Recorder();
 
-                //add the local audio track to the audio recording //fixme
+                //add the local audio track to the audio recording
                 tracks.forEach(function(track) {
-                    APP.audioRecording.giveTrack(track);
+                    APP.audioRecorder.addTrack(track);
                 });
 
                 // XXX The API will take care of disconnecting from the XMPP
@@ -1043,7 +1043,7 @@ export default {
             });
             APP.UI.addRemoteStream(track);
             //give the track to the audio recording module
-            APP.audioRecording.giveTrack(track);
+            APP.audioRecorder.addTrack(track);
         });
 
         room.on(ConferenceEvents.TRACK_REMOVED, (track) => {
@@ -1051,6 +1051,9 @@ export default {
                 return;
 
             APP.UI.removeRemoteStream(track);
+
+            //tell the audio recording module that track got removed
+            APP.audioRecorder.removeTrack(track);
         });
 
         room.on(ConferenceEvents.TRACK_MUTE_CHANGED, (track) => {
