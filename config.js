49c0c36f662e2c02c80ca417a13326e04f0fae20
nikvaessen <nikvaes@gmail.com>
2016-08-02 16:04:48 +0200

    added methods to prototype

--- config.js
+++ config.js
@@ -72,5 +72,5 @@ var config = {
     minHDHeight: 540,
     //disable lipsync so video does not get recorder
     //enableLipSync:false
-    sphinxURL: "localhost"
+    sphinxURL: "http://localhost:8081/recognise"
 };
