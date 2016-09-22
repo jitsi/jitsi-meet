8b79c1b974d99a17f264ddcc9738e9941052af0b
Ilya Daynatovich <shupuercha@gmail.com>
2016-09-22 17:43:53 +0300

    Implement copy button

--- modules/UI/util/MessageHandler.js
+++ modules/UI/util/MessageHandler.js
@@ -93,6 +93,7 @@ var messageHandler = {
             loadedFunction,
             closeFunction,
             focus,
+            size,
             defaultButton
         } = options;
 
@@ -123,7 +124,7 @@ var messageHandler = {
             focus: focus,
             loaded: loadedFunction,
             promptspeed: 0,
-            classes: this._getDialogClasses(),
+            classes: this._getDialogClasses(size),
             submit: function (e, v, m, f) {
                 twoButtonDialog = null;
                 if (submitFunction)
@@ -187,11 +188,11 @@ var messageHandler = {
         return titleString;
     },
 
-    _getDialogClasses() {
+    _getDialogClasses(size = 'small') {
         return {
             box: '',
             form: '',
-            prompt: 'dialog aui-layer aui-dialog2 aui-dialog2-small',
+            prompt: `dialog aui-layer aui-dialog2 aui-dialog2-${size}`,
             close: 'aui-icon aui-icon-small aui-iconfont-close-dialog',
             fade: 'aui-blanket',
             button: 'button-control',
