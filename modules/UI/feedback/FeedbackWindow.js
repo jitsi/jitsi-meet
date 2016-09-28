e004638ab066ffe8d616a2b0f33eadbf4cc3f8d1
Kostiantyn Pashura <pkostyan2007@gmail.com>
2016-09-28 14:53:03 +0300

    rebase fixes

--- modules/UI/feedback/FeedbackWindow.js
+++ modules/UI/feedback/FeedbackWindow.js
@@ -1,7 +1,4 @@
 /* global $, APP, interfaceConfig, AJS */
-/* jshint -W101 */
-
-const selector = '#aui-feedback-dialog';
 
 /**
  * Toggles the appropriate css class for the given number of stars, to
@@ -23,20 +20,20 @@ let toggleStars = function(starCount) {
  *
  * @returns {string} the contructed html string
  */
-let createRateFeedbackHTML = function (Feedback) {
+let createRateFeedbackHTML = function () {
     let rateExperience
             = APP.translation.translateString('dialog.rateExperience'),
         feedbackHelp = APP.translation.translateString('dialog.feedbackHelp');
 
     let starClassName = (interfaceConfig.ENABLE_FEEDBACK_ANIMATION)
-                            ? "icon-star shake-rotate"
-                            : "icon-star";
+        ? "icon-star shake-rotate"
+        : "icon-star";
 
     return `
         <div class="aui-dialog2-content feedback__content">
-            <form action="javascript:false;" onsubmit="return false;">
+            <form id="feedbackForm" 
+                action="javascript:false;" onsubmit="return false;">
                 <div class="feedback__rating">
-                    <h2>${ rateExperience }</h2>
                     <p class="star-label">&nbsp;</p>
                     <div id="stars" class="feedback-stars">
                         <a class="star-btn">
@@ -58,16 +55,10 @@ let createRateFeedbackHTML = function (Feedback) {
                     <p>&nbsp;</p>
                     <p>${ feedbackHelp }</p>
                 </div>
-                <textarea id="feedbackTextArea" rows="10" cols="40" autofocus></textarea>
+                <textarea id="feedbackTextArea" 
+                    rows="10" cols="40" autofocus></textarea>
             </form>
-            <footer class="aui-dialog2-footer feedback__footer">
-                <div class="aui-dialog2-footer-actions">
-                    <button id="dialog-close-button" class="aui-button aui-button_close">Close</button>
-                    <button id="dialog-submit-button" class="aui-button aui-button_submit">Submit</button>
-                </div>
-            </footer>
-        </div>
-`;
+        </div>`;
 };
 
 /**
@@ -93,27 +84,28 @@ let onLoadRateFunction = function (Feedback) {
         toggleStars(Feedback.feedbackScore - 1);
     }
 
-    if (Feedback.feedbackText && Feedback.feedbackText.length > 0)
-        $('#feedbackTextArea').text(Feedback.feedbackText);
-
-    let submitBtn = Feedback.$el.find('#dialog-submit-button');
-    let closeBtn = Feedback.$el.find('#dialog-close-button');
-
-    if (submitBtn && submitBtn.length) {
-        submitBtn.on('click', (e) => {
-            e.preventDefault();
-            Feedback.onFeedbackSubmitted();
-        });
-    }
-    if (closeBtn && closeBtn.length) {
-        closeBtn.on('click', (e) => {
-            e.preventDefault();
-            Feedback.hide();
-        });
-    }
+    if (Feedback.feedbackMessage && Feedback.feedbackMessage.length > 0)
+        $('#feedbackTextArea').text(Feedback.feedbackMessage);
 
     $('#feedbackTextArea').focus();
 };
+
+function onFeedbackSubmitted(Feedback) {
+    let form = $('#feedbackForm');
+    let message = form.find('textarea').val();
+
+    APP.conference.sendFeedback(
+        Feedback.feedbackScore,
+        message);
+
+    // TODO: make sendFeedback return true or false. (done in Kostya's PR)
+    Feedback.submitted = true;
+
+    //Remove history is submitted
+    //Feedback.feedbackScore = -1;
+    //Feedback.feedbackMessage ='';
+    Feedback.hide();
+}
 
 /**
  * @class Dialog
@@ -121,73 +113,43 @@ let onLoadRateFunction = function (Feedback) {
  */
 export default class Dialog {
 
-    constructor(options) {
+    constructor() {
         this.feedbackScore = -1;
-        this.feedbackText = null;
+        this.feedbackMessage = '';
         this.submitted = false;
         this.onCloseCallback = null;
-
-        this.states = {
-            rate_feedback: {
-                getHtml: createRateFeedbackHTML,
-                onLoad: onLoadRateFunction
-            }
-        };
-        this.state = options.state || 'rate_feedback';
-
-        this.window = AJS.dialog2(selector, {
-            closeOnOutsideClick: true
-        });
-        this.$el = this.window.$el;
-
-        AJS.dialog2(selector).on("hide", function() {
-            if (this.onCloseCallback) {
-                this.onCloseCallback();
-                this.onCloseCallback = null;
-            }
-        }.bind(this));
-
-        this.setState();
     }
 
-    setState(state) {
-        let newState = state || this.state;
+    setFeedbackMessage() {
+        let message = $('#feedbackTextArea').val();
 
-        let htmlStr = this.states[newState].getHtml(this);
-
-        this.$el.html(htmlStr);
-
-        this.states[newState].onLoad(this);
+        this.feedbackMessage = message;
     }
 
     show(cb) {
-        this.setState('rate_feedback');
-        if (typeof cb == 'function') {
-            this.onCloseCallback = cb;
+        if (typeof cb !== 'function') {
+            cb = function() { };
         }
+        this.onCloseCallback = cb;
+        let title = APP.translation.translateString('dialog.rateExperience');
+        let self = this;
 
-        this.window.show();
-
+        this.window = APP.UI.messageHandler.openDialog(
+            title,
+            createRateFeedbackHTML(),
+            true,
+            {'close': true, 'submit': true},
+            () => {onFeedbackSubmitted(self);},
+            () => {onLoadRateFunction(self);},
+            () => {
+                self.hide();
+                cb();
+            }
+        );
     }
 
     hide() {
-        this.window.hide();
-    }
-
-    onFeedbackSubmitted() {
-        let message = this.$el.find('textarea').val();
-        let self = this;
-
-        if (message && message.length > 0) {
-            self.feedbackText = message;
-        }
-
-        APP.conference.sendFeedback(self.feedbackScore,
-                                    self.feedbackText);
-
-        // TO DO: make sendFeedback return true or false.
-        self.submitted = true;
-
-        this.hide();
+        this.setFeedbackMessage();
+        this.window.close();
     }
 }
