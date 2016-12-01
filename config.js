cf83b4142ac1acb94375531cabb3dc3bc6300196
paweldomas <pawel.domas@jitsi.org>
2016-12-01 16:25:55 -0600

    ref(config): move loggingConfig to config.js

--- config.js
+++ config.js
@@ -78,3 +78,13 @@ var config = { // eslint-disable-line no-unused-vars
     // edit their profile.
     enableUserRolesBasedOnToken: false
 };
+
+// Logging configuration
+var loggingConfig = { // eslint-disable-line no-unused-vars
+    //default log level for the app and lib-jitsi-meet
+    defaultLogLevel: 'trace',
+    // Option to disable LogCollector (which stores the logs on CallStats)
+    //disableLogCollector: true,
+    // Logging level adjustments for verbose modules:
+    'modules/xmpp/strophe.util.js': 'log'
+};
