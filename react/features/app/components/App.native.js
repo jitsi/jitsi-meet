7a0deff00d4a208c16927f6ea47af6b340c93598
Saúl Ibarra Corretgé <saghul@gmail.com>
2017-01-11 16:05:59 -0600

    [RN] Keep device screen on while in a conference

--- react/features/app/components/App.native.js
+++ react/features/app/components/App.native.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux';
 import { _getRouteToRender } from '../functions';
 import { AbstractApp } from './AbstractApp';
 
+import '../../wake-lock';
+
 /**
  * Root application component.
  *
