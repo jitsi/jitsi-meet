b51dd59e070ade00268620c74b8d07e19ea0afff
Ilya Daynatovich <shupuercha@gmail.com>
2017-02-09 16:22:10 +0200

    Create web equivalent of App registry

---
+++ react/features/base/react/AppRegistry.native.js
@@ -0,0 +1,6 @@
+/* @flow */
+
+// Re-export react-native's AppRegistry because we want to provide a minimal
+// equivalent on Web.
+import { AppRegistry } from 'react-native';
+export default AppRegistry;
