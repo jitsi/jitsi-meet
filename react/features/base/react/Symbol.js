acd83ede2ff3a4ded12b4aeb01565a6bbb059bc7
Lyubo Marinov <lmarinov@atlassian.com>
2017-03-01 21:31:43 -0600

    [RN] Third-party ES6 Symbol ponyfill

--- react/features/base/react/Symbol.js
+++ react/features/base/react/Symbol.js
@@ -1,24 +1,5 @@
-// FIXME React Native does not polyfill Symbol at versions 0.39.2 or earlier.
-export default (global => {
-    let clazz = global.Symbol;
-
-    if (typeof clazz === 'undefined') {
-        // XXX At the time of this writing we use Symbol only as a way to
-        // prevent collisions in Redux action types. Consequently, the Symbol
-        // implementation provided bellow is minimal and specific to our
-        // purpose.
-        const toString = function() {
-            return this.join(''); // eslint-disable-line no-invalid-this
-        };
-
-        clazz = description => {
-            const thiz = (description || '').split('');
-
-            thiz.toString = toString;
-
-            return thiz;
-        };
-    }
-
-    return clazz;
-})(global || window || this); // eslint-disable-line no-invalid-this
+// XXX React Native 0.41.2 does not polyfill Symbol. The React source code of
+// jitsi/jitsi-meet does utilize Symbol though. However, it is satisfied with a
+// ponyfill.
+import Symbol from 'es6-symbol';
+export { Symbol as default };
