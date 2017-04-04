684572bd05f09fbd5c6f0de3c7fa1da3bf8a84ec
Lyubo Marinov <lmarinov@atlassian.com>
2017-04-04 17:51:22 -0500

    Comply w/ coding style

--- react/features/base/media/actionTypes.js
+++ react/features/base/media/actionTypes.js
@@ -1,31 +1,32 @@
 import { Symbol } from '../react';
 
 /**
- * Action to set the muted state of the local audio.
+ * The type of (redux) action to set the muted state of the local audio.
  *
  * {
- *      type: SET_AUDIO_MUTED,
- *      muted: boolean
+ *     type: SET_AUDIO_MUTED,
+ *     muted: boolean
  * }
  */
 export const SET_AUDIO_MUTED = Symbol('SET_AUDIO_MUTED');
 
 /**
- * Action to set the facing mode of the local video camera.
+ * The type of (redux) action to set the facing mode of the local video camera
+ * to a specific value.
  *
  * {
- *      type: SET_CAMERA_FACING_MODE,
- *      cameraFacingMode: CAMERA_FACING_MODE
+ *     type: SET_CAMERA_FACING_MODE,
+ *     cameraFacingMode: CAMERA_FACING_MODE
  * }
  */
 export const SET_CAMERA_FACING_MODE = Symbol('SET_CAMERA_FACING_MODE');
 
 /**
- * Action to set the muted state of the local video.
+ * The type of (redux) action to set the muted state of the local video.
  *
  * {
- *      type: SET_VIDEO_MUTED,
- *      muted: boolean
+ *     type: SET_VIDEO_MUTED,
+ *     muted: boolean
  * }
  */
 export const SET_VIDEO_MUTED = Symbol('SET_VIDEO_MUTED');
