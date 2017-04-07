739298c7826ea6109b8a2632d54c8d867f7d03cc
Saúl Ibarra Corretgé <saghul@gmail.com>
2017-04-07 17:26:51 +0200

    [RN] Use rounded avatars in the film strip

--- react/features/base/participants/components/styles.js
+++ react/features/base/participants/components/styles.js
@@ -8,8 +8,14 @@ export const styles = createStyleSheet({
      * Avatar style.
      */
     avatar: {
+        alignSelf: 'center',
+
+        // FIXME I don't understand how a 100 border radius of a 50x50 square
+        // results in a circle.
+        borderRadius: 100,
         flex: 1,
-        width: '100%'
+        height: 50,
+        width: 50
     },
 
     /**
