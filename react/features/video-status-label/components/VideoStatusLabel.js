90d5235408e2603b7a8c2fcda029f2c2d3a1a586
Leonard Kim <leonard.h.kim@gmail.com>
2017-05-15 11:55:08 -0700

    feat(video-label): Add dropdown for toggling audio only
    Add a menu that displays when hovering over VideoStatusLabel. The menu's
    display is controlled by CSS. As the existing AudioOnlyLabel no longer needs
    needs its own tooltip, it has been removed and label display logic has been
    moved into VideoStatusLabel.

--- react/features/video-status-label/components/VideoStatusLabel.js
+++ react/features/video-status-label/components/VideoStatusLabel.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import AudioOnlyLabel from './AudioOnlyLabel';
-import HDVideoLabel from './HDVideoLabel';
+import { toggleAudioOnly } from '../../base/conference';
+import { translate } from '../../base/i18n';
 
 /**
  * React {@code Component} responsible for displaying a label that indicates
@@ -24,25 +24,117 @@ export class VideoStatusLabel extends Component {
         _audioOnly: React.PropTypes.bool,
 
         /**
+         * Whether or not a connection to a conference has been established.
+         */
+        _conferenceStarted: React.PropTypes.bool,
+
+        /**
          * Whether or not a high-definition large video is displayed.
          */
-        _largeVideoHD: React.PropTypes.bool
+        _largeVideoHD: React.PropTypes.bool,
+
+        /**
+         * Invoked to request toggling of audio only mode.
+         */
+        dispatch: React.PropTypes.func,
+
+        /**
+         * Invoked to obtain translated strings.
+         */
+        t: React.PropTypes.func
+    }
+
+    /**
+     * Initializes a new {@code VideoStatusLabel} instance.
+     *
+     * @param {Object} props - The read-only React Component props with which
+     * the new instance is to be initialized.
+     */
+    constructor(props) {
+        super(props);
+
+        // Bind event handler so it is only bound once for every instance.
+        this._toggleAudioOnly = this._toggleAudioOnly.bind(this);
     }
 
     /**
      * Implements React's {@link Component#render()}.
      *
      * @inheritdoc
-     * @returns {ReactElement|null}
+     * @returns {ReactElement}
      */
     render() {
-        if (this.props._audioOnly) {
-            return <AudioOnlyLabel />;
-        } else if (this.props._largeVideoHD) {
-            return <HDVideoLabel />;
+        const { _audioOnly, _conferenceStarted, _largeVideoHD, t } = this.props;
+
+        // FIXME These truthy checks should not be necessary. The
+        // _conferenceStarted check is used to be defensive against toggling
+        // audio only mode while there is no conference and hides the need for
+        // error handling around audio only mode toggling. The _largeVideoHD
+        // check is used to prevent the label from displaying while the video
+        // resolution status is unknown but ties this component to the
+        // LargeVideoManager.
+        if (!_conferenceStarted || _largeVideoHD === undefined) {
+            return null;
         }
 
-        return null;
+        let displayedLabel;
+
+        if (_audioOnly) {
+            displayedLabel = <i className = 'icon-visibility-off' />;
+        } else {
+            displayedLabel = _largeVideoHD
+                ? t('videoStatus.hd') : t('videoStatus.sd');
+        }
+
+        return (
+            <div
+                className = 'video-state-indicator moveToCorner'
+                id = 'videoResolutionLabel' >
+                { displayedLabel }
+                { this._renderVideonMenu() }
+            </div>
+        );
+    }
+
+    /**
+     * Renders a dropdown menu for changing video modes.
+     *
+     * @private
+     * @returns {ReactElement}
+     */
+    _renderVideonMenu() {
+        const { _audioOnly, t } = this.props;
+        const audioOnlyAttributes = _audioOnly ? { className: 'active' }
+            : { onClick: this._toggleAudioOnly };
+        const videoAttributes = _audioOnly ? { onClick: this._toggleAudioOnly }
+            : { className: 'active' };
+
+        return (
+            <div className = 'video-state-indicator-menu'>
+                <div className = 'video-state-indicator-menu-options'>
+                    <div { ...audioOnlyAttributes }>
+                        <i className = 'icon-visibility' />
+                        { t('audioOnly.audioOnly') }
+                    </div>
+                    <div { ...videoAttributes }>
+                        <i className = 'icon-camera' />
+                        { this.props._largeVideoHD
+                            ? t('videoStatus.hdVideo')
+                            : t('videoStatus.sdVideo') }
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    /**
+     * Dispatches an action to toggle the state of audio only mode.
+     *
+     * @private
+     * @returns {void}
+     */
+    _toggleAudioOnly() {
+        this.props.dispatch(toggleAudioOnly());
     }
 }
 
@@ -54,16 +146,22 @@ export class VideoStatusLabel extends Component {
  * @private
  * @returns {{
  *     _audioOnly: boolean,
- *     _largeVideoHD: boolean
+ *     _conferenceStarted: boolean,
+ *     _largeVideoHD: (boolean|undefined)
  * }}
  */
 function _mapStateToProps(state) {
-    const { audioOnly, isLargeVideoHD } = state['features/base/conference'];
+    const {
+        audioOnly,
+        conference,
+        isLargeVideoHD
+    } = state['features/base/conference'];
 
     return {
         _audioOnly: audioOnly,
+        _conferenceStarted: Boolean(conference),
         _largeVideoHD: isLargeVideoHD
     };
 }
 
-export default connect(_mapStateToProps)(VideoStatusLabel);
+export default translate(connect(_mapStateToProps)(VideoStatusLabel));
