ddea60efe9606f59e243228a661184185bc518e7
Saúl Ibarra Corretgé <saghul@jitsi.org>
2017-05-22 15:33:42 +0200

    [RN] Add initial Jitsi Meet SDK for Android
    Dames en heren, welcome to Jitsi Meet SDK for Android, the Jitsi Meet library
    for Android.
    
    The Jitsi Meet SDK encapsulates React Native and all the dependencies Jitsi
    Meet has so other aopplications can integrate it easily.
    
    Unlike iOS, creating "fat" libraries is not allways (if at all) possible on
    Android, however, effort was put into making the integration as easy as
    possible.
    
    While React Native can be embedded in native applications, I don't think it was
    designed to be embedded as part of an Android library, hidden away from the
    application using it. This surfaced as a number of issues which had to be
    addressed specifically due to our use-case:
    
    - Activity lifecycle methods must be linked with the React Native engine, so the
      library provides wrapper methods.
    - Custom fonts have to be manually added as assets, since the provided gradle
      script doesn't work properly in a library target.
    - The RN packager has to be manually triggered since the gradle script will no
      longer do it for us.
    
    At this stage, the Jitsi Meet application is just a small single activity
    application which uses the Jitsi Meet SDK to create a single activity which
    represents the entire application. Events and external conference handling are
    forthcoming.
    
    PS: Yours truly would like to add that it was a lot more fun to work on the iOS
    side of things.

---
+++ android/sdk/src/main/java/org/jitsi/meet/sdk/JitsiMeetView.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright @ 2017-present Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.meet.sdk;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.FrameLayout;
+
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+
+import java.net.URL;
+
+
+public class JitsiMeetView extends FrameLayout {
+    /**
+     * React Native root view.
+     */
+    private ReactRootView mReactRootView;
+
+    /**
+     * React Native bridge. The instance manager allows embedding applications to create multiple
+     * root views off the same JavaScript bundle.
+     */
+    private static ReactInstanceManager mReactInstanceManager;
+
+    /**
+     * Background color used by this view and the React Native root view.
+     */
+    private static final int BACKGROUND_COLOR = 0xFF111111;
+
+    public JitsiMeetView(@NonNull Context context) {
+        super(context);
+
+        setBackgroundColor(BACKGROUND_COLOR);
+
+        if (mReactInstanceManager == null) {
+            initReactInstanceManager(((Activity)context).getApplication());
+        }
+    }
+
+    /**
+     * Loads the given URL and displays the conference. If the specified URL is null, the welcome
+     * page is displayed instead.
+     *
+     * @param url - The conference URL.
+     */
+    public void loadURL(@Nullable URL url) {
+        Bundle props = new Bundle();
+
+        if (url != null) {
+            props.putString("url", url.toString());
+        }
+
+        if (mReactRootView == null) {
+            mReactRootView = new ReactRootView(getContext());
+            mReactRootView.startReactApplication(mReactInstanceManager, "App", props);
+            mReactRootView.setBackgroundColor(BACKGROUND_COLOR);
+            addView(mReactRootView);
+        } else {
+            // TODO: ReactRootView#setAppProperties is only available on React Native 0.45.
+            throw new RuntimeException("Not yet supported");
+        }
+    }
+
+    /**
+     * Internal method to initialize the React Native instance manager. We create a single instance
+     * in order to load the JavaScript bundle a single time. All <tt>ReactRootView</tt> instances
+     * will be tied to the one and only <tt>ReactInstanceManager</tt>.
+     *
+     * @param application - <tt>Application</tt> instance which is running.
+     */
+    private static void initReactInstanceManager(Application application) {
+        mReactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModuleName("index.android")
+            .addPackage(new com.corbt.keepawake.KCKeepAwakePackage())
+            .addPackage(new com.facebook.react.shell.MainReactPackage())
+            .addPackage(new com.oblador.vectoricons.VectorIconsPackage())
+            .addPackage(new com.ocetnik.timer.BackgroundTimerPackage())
+            .addPackage(new com.oney.WebRTCModule.WebRTCModulePackage())
+            .addPackage(new com.rnimmersive.RNImmersivePackage())
+            .addPackage(new org.jitsi.meet.sdk.audiomode.AudioModePackage())
+            .addPackage(new org.jitsi.meet.sdk.proximity.ProximityPackage())
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build();
+    }
+
+    /**
+     * Activity lifecycle method which should be called from <tt>Activity.onBackPressed</tt> so
+     * we can do the required internal processing.
+     *
+     * @return - true if the back-press was processed, false otherwise. In case false is returned
+     * the application should call the parent's implementation.
+     */
+    public static boolean onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onBackPressed();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Activity lifecycle method which should be called from <tt>Activity.onDestroy</tt> so
+     * we can do the required internal processing.
+     *
+     * @param activity - <tt>Activity</tt> being destroyed.
+     */
+    public static void onHostDestroy(Activity activity) {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostDestroy(activity);
+        }
+    }
+
+    /**
+     * Activity lifecycle method which should be called from <tt>Activity.onPause</tt> so
+     * we can do the required internal processing.
+     *
+     * @param activity - <tt>Activity</tt> being paused.
+     */
+    public static void onHostPause(Activity activity) {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause(activity);
+        }
+    }
+
+    /**
+     * Activity lifecycle method which should be called from <tt>Activity.onResume</tt> so
+     * we can do the required internal processing.
+     *
+     * @param activity - <tt>Activity</tt> being resumed.
+     */
+    public static void onHostResume(Activity activity) {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(activity, null);
+        }
+    }
+
+    /**
+     * Activity lifecycle method which should be called from <tt>Activity.onNewIntent</tt> so
+     * we can do the required internal processing. Note that this is only needed if the activity's
+     * "launchMode" was set to "singleTask". This is required for deep linking to work once the
+     * application is already running.
+     *
+     * @param intent - <tt>Intent</tt> instance which was received.
+     */
+    public static void onNewIntent(Intent intent) {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onNewIntent(intent);
+        }
+    }
+}
--- android/sdk/src/main/java/org/jitsi/meet/sdk/proximity/ProximityPackage.java
+++ android/sdk/src/main/java/org/jitsi/meet/sdk/proximity/ProximityPackage.java
@@ -1,4 +1,20 @@
-package org.jitsi.meet.audiomode;
+/*
+ * Copyright @ 2017-present Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.meet.sdk.proximity;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
@@ -11,9 +27,9 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Implements {@link ReactPackage} for {@link AudioModeModule}.
+ * Implements {@link ReactPackage} for {@link ProximityModule}.
  */
-public class AudioModePackage implements ReactPackage {
+public class ProximityPackage implements ReactPackage {
     /**
      * {@inheritDoc}
      */
@@ -32,7 +48,7 @@ public class AudioModePackage implements ReactPackage {
             ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
 
-        modules.add(new AudioModeModule(reactContext));
+        modules.add(new ProximityModule(reactContext));
 
         return modules;
     }
