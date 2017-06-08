jitsi/jitsi-meet

commit 96e83989a54974499decf6985fd17c8990984db8

feat(device_selection): Implement popup

react/features/toolbox/components/ToolbarButton.native.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TouchableHighlight } from 'react-native';
+import { connect } from 'react-redux';
 
 import { Icon } from '../../base/font-icons';
 
@@ -10,13 +11,20 @@ import AbstractToolbarButton from './AbstractToolbarButton';
  *
  * @extends AbstractToolbarButton
  */
-export default class ToolbarButton extends AbstractToolbarButton {
+class ToolbarButton extends AbstractToolbarButton {
     /**
      * ToolbarButton component's property types.
      *
      * @static
      */
-    static propTypes = AbstractToolbarButton.propTypes
+    static propTypes = {
+        ...AbstractToolbarButton.propTypes,
+
+        /**
+         * Used to dispatch an action when the button is clicked.
+         */
+        dispatch: React.PropTypes.func
+    };
 
     /**
      * Renders the button of this Toolbar button.
@@ -29,7 +37,13 @@ export default class ToolbarButton extends AbstractToolbarButton {
     _renderButton(children) {
         const props = {};
 
-        'onClick' in this.props && (props.onPress = this.props.onClick);
+        'onClick' in this.props && (props.onPress = () => {
+            const action = this.props.onClick(event);
+
+            if (action) {
+                this.props.dispatch(action);
+            }
+        });
         'style' in this.props && (props.style = this.props.style);
         'underlayColor' in this.props
             && (props.underlayColor = this.props.underlayColor);
@@ -45,3 +59,5 @@ export default class ToolbarButton extends AbstractToolbarButton {
         return super._renderIcon(Icon);
     }
 }
+
+export default connect()(ToolbarButton);

Makefile
@@ -31,6 +31,8 @@ deploy-appbundle:
 		$(BUILD_DIR)/do_external_connect.min.map \
 		$(BUILD_DIR)/external_api.min.js \
 		$(BUILD_DIR)/external_api.min.map \
+		$(BUILD_DIR)/device_selection_popup_bundle.min.js \
+		$(BUILD_DIR)/device_selection_popup_bundle.min.map \
 		$(OUTPUT_DIR)/analytics.js \
 		$(DEPLOY_DIR)
 

css/_filmstrip.scss
@@ -136,7 +136,7 @@
     &__videos-filmstripOnly {
         margin-top: auto;
         margin-bottom: auto;
-        padding-right: $defaultToolbarSize;
+        padding-right: $defaultFilmStripOnlyToolbarSize;
     }
 
     .remote-videos-container {

css/_toolbars.scss
@@ -200,7 +200,13 @@
         height: auto;
         position: absolute;
         right: 0;
-        width: $defaultToolbarSize;
+        width: $defaultFilmStripOnlyToolbarSize;
+
+        .button {
+            height: 37px;
+            line-height: 37px !important;
+            width: 37px;
+        }
 
         .button:first-child {
             border-top-left-radius: 3px;

css/_variables.scss
@@ -34,6 +34,7 @@ $tooltipBg: rgba(0,0,0, 0.7);
  * Toolbar
  */
 $defaultToolbarSize: 50px;
+$defaultFilmStripOnlyToolbarSize: 37px;
 $splitterToolbarButtonMargin: 18px;
 $toolbarBackground: rgba(0, 0, 0, 0.5);
 $toolbarBadgeBackground: #165ECC;

interface_config.js
@@ -36,14 +36,14 @@ var interfaceConfig = { // eslint-disable-line no-unused-vars
      */
     TOOLBAR_BUTTONS: [
         //main toolbar
-        'microphone', 'camera', 'desktop', 'invite', 'fullscreen', 'hangup',
+        'microphone', 'camera', 'desktop', 'invite', 'fullscreen', 'hangup', 'deviceselection', // jshint ignore:line
         //extended toolbar
         'profile', 'contacts', 'chat', 'recording', 'etherpad', 'sharedvideo', 'dialout', 'settings', 'raisehand', 'filmstrip'], // jshint ignore:line
     /**
      * Main Toolbar Buttons
      * All of them should be in TOOLBAR_BUTTONS
      */
-    MAIN_TOOLBAR_BUTTONS: ['microphone', 'camera', 'desktop', 'invite', 'fullscreen', 'hangup'], // jshint ignore:line
+    MAIN_TOOLBAR_BUTTONS: ['microphone', 'camera', 'desktop', 'invite', 'fullscreen', 'hangup', 'deviceselection'], // jshint ignore:line
     SETTINGS_SECTIONS: ['language', 'devices', 'moderator'],
     // Determines how the video would fit the screen. 'both' would fit the whole
     // screen, 'height' would fit the original video height to the height of the

react/features/base/dialog/components/AbstractDialog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { hideDialog } from '../actions';
+import { dialogPropTypes } from '../constants';
 
 /**
  * Abstract dialog to display dialogs.
@@ -13,57 +14,12 @@ export default class AbstractDialog extends Component {
      * @static
      */
     static propTypes = {
-        /**
-         * Whether cancel button is disabled. Enabled by default.
-         */
-        cancelDisabled: React.PropTypes.bool,
-
-        /**
-         * Optional i18n key to change the cancel button title.
-         */
-        cancelTitleKey: React.PropTypes.string,
+        ...dialogPropTypes,
 
         /**
          * Used to show/hide the dialog on cancel.
          */
-        dispatch: React.PropTypes.func,
-
-        /**
-         * Is ok button enabled/disabled. Enabled by default.
-         */
-        okDisabled: React.PropTypes.bool,
-
-        /**
-         * Optional i18n key to change the ok button title.
-         */
-        okTitleKey: React.PropTypes.string,
-
-        /**
-         * The handler for onCancel event.
-         */
-        onCancel: React.PropTypes.func,
-
-        /**
-         * The handler for the event when submitting the dialog.
-         */
-        onSubmit: React.PropTypes.func,
-
-        /**
-         * Used to obtain translations in children classes.
-         */
-        t: React.PropTypes.func,
-
-        /**
-         * Key to use for showing a title.
-         */
-        titleKey: React.PropTypes.string,
-
-        /**
-         * The string to use as a title instead of {@code titleKey}. If a truthy
-         * value is specified, it takes precedence over {@code titleKey} i.e.
-         * the latter is unused.
-         */
-        titleString: React.PropTypes.string
+        dispatch: React.PropTypes.func
     };
 
     /**

react/features/base/dialog/components/Dialog.web.js
@@ -1,12 +1,8 @@
-import AKButton from '@atlaskit/button';
-import AKButtonGroup from '@atlaskit/button-group';
-import ModalDialog from '@atlaskit/modal-dialog';
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { translate } from '../../i18n';
-
 import AbstractDialog from './AbstractDialog';
+import StatelessDialog from './StatelessDialog';
 
 /**
  * Web dialog that uses atlaskit modal-dialog to display dialogs.
@@ -19,6 +15,8 @@ class Dialog extends AbstractDialog {
      * @static
      */
     static propTypes = {
+        ...AbstractDialog.propTypes,
+
         /**
          * This is the body of the dialog, the component children.
          */
@@ -31,6 +29,11 @@ class Dialog extends AbstractDialog {
         isModal: React.PropTypes.bool,
 
         /**
+         * Disables rendering of the submit button.
+         */
+        submitDisabled: React.PropTypes.bool,
+
+        /**
          * Width of the dialog, can be:
          * - 'small' (400px), 'medium' (600px), 'large' (800px),
          * 'x-large' (968px)
@@ -41,107 +44,34 @@ class Dialog extends AbstractDialog {
     };
 
     /**
+     * Initializes a new Dialog instance.
+     *
+     * @param {Object} props - The read-only properties with which the new
+     * instance is to be initialized.
+     */
+    constructor(props) {
+        super(props);
+
+        this._onCancel = this._onCancel.bind(this);
+        this._onSubmit = this._onSubmit.bind(this);
+    }
+
+    /**
      * Implements React's {@link Component#render()}.
      *
      * @inheritdoc
      * @returns {ReactElement}
      */
     render() {
-        return (
-            <ModalDialog
-                footer = { this._renderFooter() }
-                header = { this._renderHeader() }
-                isOpen = { true }
-                onDialogDismissed = { this._onCancel }
-                width = { this.props.width || 'medium' }>
-                <div>
-                    <form
-                        className = 'modal-dialog-form'
-                        id = 'modal-dialog-form'
-                        onSubmit = { this._onSubmit }>
-                        { this.props.children }
-                    </form>
-                </div>
-            </ModalDialog>);
-    }
+        const props = {
+            ...this.props,
+            onSubmit: this._onSubmit,
+            onCancel: this._onCancel
+        };
 
-    /**
-     * Render cancel button.
-     *
-     * @returns {*} The cancel button if enabled and dialog is not modal.
-     * @private
-     */
-    _renderCancelButton() {
-        if (this.props.cancelDisabled || this.props.isModal) {
-            return null;
-        }
+        delete props.dispatch;
 
-        return (
-            <AKButton
-                appearance = 'subtle'
-                id = 'modal-dialog-cancel-button'
-                onClick = { this._onCancel }>
-                { this.props.t(this.props.cancelTitleKey || 'dialog.Cancel') }
-            </AKButton>
-        );
-    }
-
-    /**
-     * Render component in dialog footer.
-     *
-     * @returns {ReactElement}
-     * @private
-     */
-    _renderFooter() {
-        return (
-            <footer className = 'modal-dialog-footer'>
-                <AKButtonGroup>
-                    { this._renderCancelButton() }
-                    { this._renderOKButton() }
-                </AKButtonGroup>
-            </footer>
-        );
-    }
-
-    /**
-     * Render component in dialog header.
-     *
-     * @returns {ReactElement}
-     * @private
-     */
-    _renderHeader() {
-        const { t } = this.props;
-
-        return (
-            <header>
-                <h2>
-                    { this.props.titleString || t(this.props.titleKey) }
-                </h2>
-            </header>
-        );
-    }
-
-    /**
-     * Render ok button.
-     *
-     * @returns {*} The ok button if enabled.
-     * @private
-     */
-    _renderOKButton() {
-        if (this.props.submitDisabled) {
-            return null;
-        }
-
-        return (
-            <AKButton
-                appearance = 'primary'
-                form = 'modal-dialog-form'
-                id = 'modal-dialog-ok-button'
-                isDisabled = { this.props.okDisabled }
-                onClick = { this._onSubmit }>
-                { this.props.t(this.props.okTitleKey || 'dialog.Ok') }
-            </AKButton>
-        );
+        return <StatelessDialog { ...props } />;
     }
 
     /**
@@ -158,4 +88,4 @@ class Dialog extends AbstractDialog {
     }
 }
 
-export default translate(connect()(Dialog));
+export default connect()(Dialog);

commit e7db1aa0b460dd4a05379ad7f2daf4d089cd8f87

[RN] Fix regression handling ToolbarButton onPress
Introduced in
https://github.com/jitsi/jitsi-meet/commit/96e83989a54974499decf6985fd17c8990984db8
as part of a refactor + feature.

react/features/toolbox/components/ToolbarButton.native.js
@@ -37,7 +37,7 @@ class ToolbarButton extends AbstractToolbarButton {
     _renderButton(children) {
         const props = {};
 
-        'onClick' in this.props && (props.onPress = () => {
+        'onClick' in this.props && (props.onPress = event => {
             const action = this.props.onClick(event);
 
             if (action) {