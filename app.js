c8c44d62eddd41f7ee18674493db3de04f3c429e
hristoterezov <hristo@jitsi.org>
2017-06-14 15:13:25 -0500

    fix: some exports to make it work with webpack 2

--- app.js
+++ app.js
@@ -1,64 +1,43 @@
 /* application specific logic */
 
-import "jquery";
-import "jquery-contextmenu";
-import "jquery-ui";
-import "strophe";
-import "strophe-disco";
-import "jQuery-Impromptu";
-import "autosize";
+import 'jquery';
+import 'jquery-contextmenu';
+import 'jquery-ui';
+import 'strophe';
+import 'strophe-disco';
+import 'jQuery-Impromptu';
+import 'autosize';
 
 import 'aui';
 import 'aui-experimental';
 import 'aui-css';
 import 'aui-experimental-css';
 
-window.toastr = require("toastr");
+window.toastr = require('toastr');
 
 export conference from './conference';
 export API from './modules/API';
 export keyboardshortcut from './modules/keyboardshortcut/keyboardshortcut';
-export remoteControl from "./modules/remotecontrol/RemoteControl";
-export settings from "./modules/settings/Settings";
-export translation from "./modules/translation/translation";
-export UI from "./modules/UI/UI";
+export remoteControl from './modules/remotecontrol/RemoteControl';
+export settings from './modules/settings/Settings';
+export translation from './modules/translation/translation';
+export UI from './modules/UI/UI';
 
-/**
- * After the APP has been initialized provides utility methods for dealing
- * with the conference room URL(address).
- * @type ConferenceUrl
- */
-export let ConferenceUrl = null;
-
-// Used by do_external_connect.js if we receive the attach data after
-// connect was already executed. status property can be "initialized",
-// "ready" or "connecting". We are interested in "ready" status only which
-// means that connect was executed but we have to wait for the attach data.
-// In status "ready" handler property will be set to a function that will
-// finish the connect process when the attach data or error is received.
+// Used by do_external_connect.js if we receive the attach data after connect
+// was already executed. status property can be 'initialized', 'ready', or
+// 'connecting'. We are interested in 'ready' status only which means that
+// connect was executed but we have to wait for the attach data. In status
+// 'ready' handler property will be set to a function that will finish the
+// connect process when the attach data or error is received.
 export const connect = {
-    status: "initialized",
-    handler: null
+    handler: null,
+    status: 'initialized'
 };
-
-export let connection = null;
 
 // Used for automated performance tests
 export const connectionTimes = {
-    "index.loaded": window.indexLoadedTime
+    'index.loaded': window.indexLoadedTime
 };
-
-/**
- * The log collector which captures JS console logs for this app.
- * @type {LogCollector}
- */
-export let logCollector = null;
-
-/**
- * Indicates if the log collector has been started (it will not be started
- * if the welcome page is displayed).
- */
-export let logCollectorStarted = false;
 
 // TODO The execution of the mobile app starts from react/index.native.js.
 // Similarly, the execution of the Web app should start from react/index.web.js
