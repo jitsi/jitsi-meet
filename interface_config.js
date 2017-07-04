16e972d077c7bdb0e6a1176671d9ed0cbc1ce371
Saúl Ibarra Corretgé <saghul@jitsi.org>
2017-07-04 08:38:06 +0200

    feat(chore): remove unused interfaceConfig options

--- interface_config.js
+++ interface_config.js
@@ -1,7 +1,4 @@
 var interfaceConfig = { // eslint-disable-line no-unused-vars
-    CANVAS_EXTRA: 104,
-    CANVAS_RADIUS: 0,
-    SHADOW_COLOR: '#ffffff',
     // TO FIX: this needs to be handled from SASS variables. There are some
     // methods allowing to use variables both in css and js.
     DEFAULT_BACKGROUND: '#474747',
