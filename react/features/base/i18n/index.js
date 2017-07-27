e4cc9f2c8673b4e36e4eb61cf098f34242bab5ce
damencho <damencho@jitsi.org>
2017-07-27 10:19:48 -0500

    Uses languages.json to obtain supported languages.

--- react/features/base/i18n/index.js
+++ react/features/base/i18n/index.js
@@ -1,8 +1,8 @@
-export * from './constants';
+
 export * from './functions';
 
 // TODO Eventually (e.g. when the non-React Web app is rewritten into React), it
 // should not be necessary to export i18next.
-export { default as i18next } from './i18next';
+export { default as i18next, DEFAULT_LANGUAGE, LANGUAGES } from './i18next';
 
 import './middleware';
