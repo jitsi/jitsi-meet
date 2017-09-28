da5f1081f2924613675b4673fd126b06c4c5aaa8
paweldomas <pawel.domas@jitsi.org>
2017-09-28 13:47:06 -0500

    ongoing complication

--- ios/app/watchos/extension/ComplicationController.swift
+++ ios/app/watchos/extension/ComplicationController.swift
@@ -18,47 +18,61 @@ import ClockKit
 
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
-    
+
     // MARK: - Timeline Configuration
-    
+
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.forward, .backward])
+        //handler([.forward, .backward])
+        handler([])
     }
-    
+
     func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
         handler(nil)
     }
-    
+
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
         handler(nil)
     }
-    
+
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
         handler(.showOnLockScreen)
     }
-    
+
     // MARK: - Timeline Population
-    
+
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
         handler(nil)
     }
-    
+
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         // Call the handler with the timeline entries prior to the given date
         handler(nil)
     }
-    
+
     func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         // Call the handler with the timeline entries after to the given date
         handler(nil)
     }
-    
+
     // MARK: - Placeholder Templates
-    
+
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Complication/jitsi")!)
+        if complication.family == .circularSmall {
+            let small = CLKComplicationTemplateCircularSmallRingImage()
+            small.imageProvider = imageProvider
+            // This method will be called once per supported complication, and the results will be cached
+            handler(small)
+        } else if complication.family == .utilitarianSmall {
+            let utilitarian = CLKComplicationTemplateUtilitarianSmallRingImage()
+            utilitarian.imageProvider = imageProvider
+            handler(utilitarian)
+        } else if complication.family == .modularSmall {
+            let modular = CLKComplicationTemplateModularSmallRingImage()
+            modular.imageProvider = imageProvider
+            handler(modular)
+        }
     }
-    
+
 }
