da03b49754b2ac962681f771d02a370c4cffdb60
Lyubo Marinov <lmarinov@atlassian.com>
2017-10-12 14:59:11 -0500

    Flow, coding style

--- react/features/base/jwt/reducer.js
+++ react/features/base/jwt/reducer.js
@@ -1,3 +1,5 @@
+// @flow
+
 import { equals, set, ReducerRegistry } from '../redux';
 
 import { SET_CALL_OVERLAY_VISIBLE, SET_JWT } from './actionTypes';
@@ -7,6 +9,7 @@ import { SET_CALL_OVERLAY_VISIBLE, SET_JWT } from './actionTypes';
  *
  * @private
  * @type {{
+ *     callOverlayVisible: ?boolean
  *     isGuest: boolean
  * }}
  */
@@ -38,22 +41,23 @@ const _INITIAL_STATE = {
  * specified {@code action}.
  */
 ReducerRegistry.register(
-'features/base/jwt', (state = _INITIAL_STATE, action) => {
-    switch (action.type) {
-    case SET_CALL_OVERLAY_VISIBLE:
-        return set(state, 'callOverlayVisible', action.callOverlayVisible);
+    'features/base/jwt',
+    (state = _INITIAL_STATE, action) => {
+        switch (action.type) {
+        case SET_CALL_OVERLAY_VISIBLE:
+            return set(state, 'callOverlayVisible', action.callOverlayVisible);
 
-    case SET_JWT: {
-        // eslint-disable-next-line no-unused-vars
-        const { type, ...payload } = action;
-        const nextState = {
-            ..._INITIAL_STATE,
-            ...payload
-        };
+        case SET_JWT: {
+            // eslint-disable-next-line no-unused-vars
+            const { type, ...payload } = action;
+            const nextState = {
+                ..._INITIAL_STATE,
+                ...payload
+            };
 
-        return equals(state, nextState) ? state : nextState;
-    }
-    }
+            return equals(state, nextState) ? state : nextState;
+        }
+        }
 
-    return state;
-});
+        return state;
+    });
