6f0fe06ba67a06fcc493dfe86003d8d89113322e
Lyubo Marinov <lmarinov@atlassian.com>
2017-10-19 13:29:25 -0500

    [RN] No CallKit failure on recoverable errors

--- react/features/mobile/callkit/middleware.js
+++ react/features/mobile/callkit/middleware.js
@@ -135,10 +135,15 @@ function _appWillMount({ dispatch, getState }, next, action) {
 function _conferenceFailed(store, next, action) {
     const result = next(action);
 
-    const { callUUID } = action.conference;
+    // XXX Certain CONFERENCE_FAILED errors are recoverable i.e. they have
+    // prevented the user from joining a specific conference but the app may be
+    // able to eventually join the conference.
+    if (!action.error.recoverable) {
+        const { callUUID } = action.conference;
 
-    if (callUUID) {
-        CallKit.reportCallFailed(callUUID);
+        if (callUUID) {
+            CallKit.reportCallFailed(callUUID);
+        }
     }
 
     return result;
