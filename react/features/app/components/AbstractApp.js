jitsi/jitsi-meet

commit e09949be9f4c56af909bb6359a1e1f3f98ea6ef5

feat(dark-theme): Applies dark theme on inline-dialogs, tooltips, flags and more

react/features/app/components/AbstractApp.js
@@ -4,6 +4,7 @@ import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { compose, createStore } from 'redux';
 import Thunk from 'redux-thunk';
+import { AtlasKitThemeProvider } from '@atlaskit/theme';
 
 import { i18next } from '../../base/i18n';
 import {
@@ -201,9 +202,11 @@ export class AbstractApp extends Component {
             return (
                 <I18nextProvider i18n = { i18next }>
                     <Provider store = { this._getStore() }>
-                        {
-                            this._createElement(component)
-                        }
+                        <AtlasKitThemeProvider mode = 'dark'>
+                            {
+                                this._createElement(component)
+                            }
+                        </AtlasKitThemeProvider>
                     </Provider>
                 </I18nextProvider>
             );

css/_connection-info.scss
@@ -1,7 +1,6 @@
 %connection-info {
     font-size: 12px;
     font-weight: 400;
-    color: $modalTextColor;
 
     td {
         padding: 2px 0;
@@ -34,7 +33,6 @@
     &__download
     {
         @extend .connection-info__icon;
-        color: $downloadConnectionIconColor;
     }
 
     &__status
@@ -45,7 +43,6 @@
     &__upload
     {
         @extend .connection-info__icon;
-        color: $uploadConnectionIconColor;
     }
 
     .showmore {

css/_filmstrip.scss
@@ -86,25 +86,15 @@
                 width: 100%;
 
                 & button {
-                    background: $toolbarBackground;
                     flex-grow: 1;
                     flex-shrink: 1;
                     overflow: hidden;
                 }
 
                 & > * {
-                    color: $toolbarButtonColor;
                     flex-grow: 0;
                     flex-shrink: 0;
                     margin-left: 2px;
-                }
-
-                /**
-                 * Making sure any svg-s in an invite button group will be
-                 * colored the way we want.
-                 */
-                & path {
-                    fill: $toolbarButtonColor;
                 }
             }
         }

css/_popup_menu.scss
@@ -19,7 +19,6 @@
     // Link Appearance
     &__link,
     &__contents {
-        color: $modalTextColor;
         display: block;
         box-sizing: border-box;
         text-decoration: none;
@@ -30,7 +29,6 @@
         padding: 0 5px;
 
         &.disabled {
-            color: gray !important;
             pointer-events: none;
         }
     }

css/_toolbars.scss
@@ -36,7 +36,6 @@
  * Common toolbar styles.
  */
 .toolbar {
-    color: $modalTextColor;
     height: 100%;
     pointer-events: auto;
     position: relative;
@@ -82,18 +81,19 @@
         vertical-align: middle;
         width: $defaultToolbarSize;
 
-        &_hangup {
-            color: $hangupColor;
-            font-size: $hangupFontSize;
-        }
-
         &[disabled] {
             opacity: 0.5;
         }
 
         &:hover, &:active {
+            color: $toolbarButtonColor;
             cursor: pointer;
             text-decoration: none;
+        }
+
+        &_hangup, &_hangup:hover {
+            color: $hangupColor;
+            font-size: $hangupFontSize;
         }
 
         &:not(.toggled) {

css/_variables.scss
@@ -64,6 +64,7 @@ $connectionIndicatorBg: #165ecc;
 $audioLevelShadow: rgba(9, 36, 77, 0.9);
 $videoStateIndicatorColor: $defaultColor;
 $videoStateIndicatorBackground: $toolbarBackground;
+$videoStateIndicatorSize: 40px;
 
 /**
  * Feedback Modal

css/_videolayout_default.scss
@@ -65,6 +65,15 @@
             float: left;
             pointer-events: all;
         }
+
+        /**
+         * Need to overwrite the background for the top toolbar dark theme div
+         * wrapper needed before we're able to move all top toolbar indicators
+         * creation to react.
+         */
+        .ckAJgx {
+            background: none;
+        }
     }
 
     &__toolbar {

css/modals/invite/_invite.scss
@@ -26,17 +26,6 @@
             user-select: text;
         }
 
-        .dial-in-numbers-trigger {
-            position: relative;
-            width: 100%;
-
-            .dial-in-numbers-trigger-icon {
-                position: absolute;
-                right: 10px;
-                top: 4px;
-            }
-        }
-
         .is-disabled,
         .is-loading {
             .dial-in-numbers-trigger-icon {
@@ -62,6 +51,10 @@
         &__input-container {
             flex: 1;
             margin-right: 10px;
+
+            .dropdown-button-trigger {
+                text-align: left;
+            }
         }
     }
 

commit 934cfcbcb46385bd549c7f6bbbe852621974492e

feat(dark-theme): fix breakage on mobile
e09949be9f4c56af909bb6359a1e1f3f98ea6ef5 introduced the dark theme globally, but
it only applies to web, so move the logic to App.web.js

react/features/app/components/AbstractApp.js
@@ -4,7 +4,6 @@ import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { compose, createStore } from 'redux';
 import Thunk from 'redux-thunk';
-import { AtlasKitThemeProvider } from '@atlaskit/theme';
 
 import { i18next } from '../../base/i18n';
 import {
@@ -202,11 +201,7 @@ export class AbstractApp extends Component {
             return (
                 <I18nextProvider i18n = { i18next }>
                     <Provider store = { this._getStore() }>
-                        <AtlasKitThemeProvider mode = 'dark'>
-                            {
-                                this._createElement(component)
-                            }
-                        </AtlasKitThemeProvider>
+                        { this._createElement(component) }
                     </Provider>
                 </I18nextProvider>
             );

react/features/app/components/App.web.js
@@ -1,3 +1,6 @@
+import { AtlasKitThemeProvider } from '@atlaskit/theme';
+import React from 'react';
+
 import { getLocationContextRoot } from '../../base/util';
 import '../../room-lock';
 
@@ -35,6 +38,20 @@ export class App extends AbstractApp {
              */
             windowLocationContextRoot: this._getWindowLocationContextRoot()
         };
+    }
+
+    /**
+     * Overrides the parent method to inject {@link AtlasKitThemeProvider} as
+     * the top most component.
+     *
+     * @override
+     */
+    _createElement(component, props) {
+        return (
+            <AtlasKitThemeProvider mode = 'dark'>
+                { super._createElement(component, props) }
+            </AtlasKitThemeProvider>
+        );
     }
 
     /**