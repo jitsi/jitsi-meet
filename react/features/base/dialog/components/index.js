8198e52b93a8660693a91e036430f1bc3b98c160
Saúl Ibarra Corretgé <saghul@jitsi.org>
2017-11-03 11:10:58 +0100

    [RN] Add SimpleBottomSheet component
    It emulates Android's BottomSheet in pure JavaScript. It's implemented as
    another Dialog, so it can be used instead of one.
    
    The implementation only supports text options with an associated icon, and an
    optional 'selected' marker.

--- react/features/base/dialog/components/index.js
+++ react/features/base/dialog/components/index.js
@@ -1,3 +1,4 @@
 export { default as DialogContainer } from './DialogContainer';
 export { default as Dialog } from './Dialog';
+export { default as SimpleBottomSheet } from './SimpleBottomSheet';
 export { default as StatelessDialog } from './StatelessDialog';
--- react/features/base/dialog/components/styles.js
+++ react/features/base/dialog/components/styles.js
@@ -19,3 +19,84 @@ export default createStyleSheet({
         color: ColorPalette.darkGrey
     }
 });
+
+/**
+ *  The React {@code Component} styles for {@code SimpleBottomSheet}.
+ *  These styles have been implemented as per the Material Design guidelines:
+ *  https://material.io/guidelines/components/bottom-sheets.html
+ */
+export const bottomSheet = createStyleSheet({
+    /**
+     * Style for the container of the sheet.
+     */
+    container: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+
+    /**
+     * Style for a backdrop overlay covering the screen while the
+     */
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+        right: 0,
+        top: 0
+    },
+
+    /**
+     * Base style for each row.
+     */
+    row: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        height: 48
+    },
+
+    /**
+     * Style for the {@code Icon} element in a row.
+     */
+    rowIcon: {
+        fontSize: 24
+    },
+
+    /**
+     * Helper for adding some padding between the icon and text in a row.
+     */
+    rowPadding: {
+        width: 32
+    },
+
+    /**
+     * Style for a row which is marked as selected.
+     */
+    rowSelectedText: {
+        color: ColorPalette.blue
+    },
+
+    /**
+     * Style for the {@code Text} element in a row.
+     */
+    rowText: {
+        fontSize: 16
+    },
+
+    /**
+     * Wrapper for all rows, it adds a margin to the sheet container.
+     */
+    rowsWrapper: {
+        marginHorizontal: 16,
+        marginVertical: 8
+    },
+
+    /**
+     * Bottom sheet's base style.
+     */
+    sheet: {
+        alignSelf: 'flex-end',
+        backgroundColor: ColorPalette.white,
+        flex: 1
+    }
+});
