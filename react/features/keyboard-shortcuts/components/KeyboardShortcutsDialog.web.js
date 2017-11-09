36085b65245e0be004713cde0ab4e8d83678879e
Leonard Kim <leonard.h.kim@gmail.com>
2017-11-09 11:10:20 -0800

    feat(keyboard-shortcuts): show help in a react dialog
    - Move shortcut help dom declaration to a react component
    - Let open/close logic be handled by AtlasKit Dialog
    - Remove static html for help from index.html
    - Consolidate keyboard shortcut css

---
+++ react/features/keyboard-shortcuts/components/KeyboardShortcutsDialog.web.js
@@ -0,0 +1,90 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import { Dialog } from '../../base/dialog';
+import { translate } from '../../base/i18n';
+
+/**
+ * Implements a React {@link Component} which displays a dialog describing
+ * registered keyboard shortcuts.
+ *
+ * @extends Component
+ */
+class KeyboardShortcutsDialog extends Component {
+    /**
+     * {@code KeyboardShortcutsDialog} component's property types.
+     *
+     * @static
+     */
+    static propTypes = {
+        /**
+         * A Map with keyboard keys as keys and translation keys as values.
+         */
+        shortcutDescriptions: PropTypes.object,
+
+        /**
+         * Invoked to obtain translated strings.
+         */
+        t: PropTypes.func
+    };
+
+    /**
+     * Implements React's {@link Component#render()}.
+     *
+     * @inheritdoc
+     * @returns {ReactElement}
+     */
+    render() {
+        const { shortcutDescriptions } = this.props;
+        const shortcuts = [];
+
+        for (const [ keyboardKey, translationKey ] of shortcutDescriptions) {
+            shortcuts.push(
+                this._renderShortcutsListItem(keyboardKey, translationKey));
+        }
+
+        return (
+            <Dialog
+                cancelTitleKey = { 'dialog.close' }
+                submitDisabled = { true }
+                titleKey = 'keyboardShortcuts.keyboardShortcuts'
+                width = 'small'>
+                <div
+                    id = 'keyboard-shortcuts'>
+                    <ul
+                        className = 'shortcuts-list'
+                        id = 'keyboard-shortcuts-list'>
+                        { shortcuts }
+                    </ul>
+                </div>
+            </Dialog>
+        );
+    }
+
+    /**
+     * Creates a {@code ReactElement} for describing a single keyboard shortcut.
+     *
+     * @param {string} keyboardKey - The keyboard key that triggers an action.
+     * @param {string} translationKey - A description of what the action does.
+     * @private
+     * @returns {ReactElement}
+     */
+    _renderShortcutsListItem(keyboardKey, translationKey) {
+        return (
+            <li
+                className = 'shortcuts-list__item'
+                key = { keyboardKey }>
+                <span className = 'item-action'>
+                    <kbd className = 'aui-label regular-key'>
+                        { keyboardKey }
+                    </kbd>
+                </span>
+                <span className = 'shortcuts-list__description'>
+                    { this.props.t(translationKey) }
+                </span>
+            </li>
+        );
+    }
+}
+
+export default translate(KeyboardShortcutsDialog);
