ef813fbf71cd765da4a66a039f347e37bc768a33
Leonard Kim <leonard.h.kim@gmail.com>
2017-11-16 09:25:04 -0800

    ref(notifications): replace NotificationWithToggle with Notification
    It was decided along with the mute participant dialog reactification
    that these types of warning messages should not be toggleable--that
    they should simply always display because there is no undo action.
    As such, the component NotificationWithToggle is no longer needed.

--- react/features/notifications/components/index.js
+++ react/features/notifications/components/index.js
@@ -1,3 +1,2 @@
 export { default as Notification } from './Notification';
 export { default as NotificationsContainer } from './NotificationsContainer';
-export { default as NotificationWithToggle } from './NotificationWithToggle';
