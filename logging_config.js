7ad875e735fa1fccb36f6fa80f2d2c0cd8b2f57e
Paweł Domas <pawel.domas@jitsi.org>
2018-01-17 12:59:48 -0600

    fix(RN logging): sync with logging_config.js (#2382)

--- logging_config.js
+++ logging_config.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars, no-var */
 // Logging configuration
+// XXX When making any changes to this file make sure to also update it's React
+// version at ./react/features/base/logging/reducer.js !!!
 var loggingConfig = {
     // default log level for the app and lib-jitsi-meet
     defaultLogLevel: 'trace',
@@ -7,9 +9,10 @@ var loggingConfig = {
     // Option to disable LogCollector (which stores the logs on CallStats)
     // disableLogCollector: true,
 
-    // Logging level adjustments for verbose modules:
-    'modules/xmpp/strophe.util.js': 'log',
+    // The following are too verbose in their logging with the
+    // {@link #defaultLogLevel}:
     'modules/statistics/CallStats.js': 'info',
+    'modules/xmpp/strophe.util.js': 'log',
     'modules/RTC/TraceablePeerConnection.js': 'info'
 };
 
