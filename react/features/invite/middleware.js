9f2d47e4e1913df658fc0f63b7985b2f1dd4f6ab
Leonard Kim <leonard.h.kim@gmail.com>
2018-03-15 16:18:19 -0700

    feat(info): show only during a lonely call

--- react/features/invite/middleware.js
+++ react/features/invite/middleware.js
@@ -1,7 +1,5 @@
-import { CONFERENCE_JOINED } from '../base/conference';
 import { MiddlewareRegistry } from '../base/redux';
 
-import { setInfoDialogVisibility } from './actions';
 import { UPDATE_DIAL_IN_NUMBERS_FAILED } from './actionTypes';
 
 const logger = require('jitsi-meet-logger').getLogger(__filename);
@@ -17,13 +15,6 @@ MiddlewareRegistry.register(store => next => action => {
     const result = next(action);
 
     switch (action.type) {
-    case CONFERENCE_JOINED:
-        // we do not want to show call info in iAmRecorder mode
-        if (store.getState()['features/base/config'].iAmRecorder) {
-            return result;
-        }
-        store.dispatch(setInfoDialogVisibility(true, true));
-        break;
 
     case UPDATE_DIAL_IN_NUMBERS_FAILED:
         logger.error(
