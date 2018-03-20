6504acd2da186d38dc8599bd6f2dd348bfc933fb
brian baldino <bbaldino@gmail.com>
2018-03-20 11:33:51 -0700

    refactor to modify presence message in place

--- resources/prosody-plugins/mod_presence_identity.lua
+++ resources/prosody-plugins/mod_presence_identity.lua
@@ -1,38 +1,35 @@
 local stanza = require "util.stanza";
-local core_post_stanza = prosody.core_post_stanza;
 
 -- For all received presence messages, if the jitsi_meet_context_(user|group)
 -- values are set in the session, then insert them into the presence messages
 -- for that session.
 function on_message(event)
-    if event["stanza"] then
-      if event.origin["jitsi_meet_context_user"] then
-          local new_stanza = stanza.clone(event.stanza)
+    if event and event["stanza"] then
+      if event.origin and event.origin.jitsi_meet_context_user and event.origin.jitsi_meet_context_group then
           -- First remove any 'identity' element if it already
           -- exists
-          new_stanza:maptags(
+          event.stanza:maptags(
               function(tag)
                   for k, v in pairs(tag) do
                       if k == "name" and v == "identity" then
-                          module:log("debug", "filtering out 'identity' field")
                           return nil
                       end
                   end
                   return tag
               end
           )
-          new_stanza:tag("identity"):tag("user")
-          for k, v in pairs(event.origin["jitsi_meet_context_user"]) do
-              new_stanza:tag(k):text(v):up()
-          end
-          new_stanza:up()
-          new_stanza:tag("group"):text(event.origin["jitsi_meet_context_group"])
+          module:log("debug", "Presence after previous identity stripped: %s", tostring(event.stanza))
 
-          module:log("debug", "Sending presence with identity inserted %s", tostring(new_stanza))
-          core_post_stanza(event.origin, new_stanza)
+          event.stanza:tag("identity"):tag("user")
+          for k, v in pairs(event.origin["jitsi_meet_context_user"]) do
+              event.stanza:tag(k):text(v):up()
+          end
+          event.stanza:up()
+          event.stanza:tag("group"):text(event.origin["jitsi_meet_context_group"])
+
+          module:log("debug", "Sending presence with identity inserted %s", tostring(event.stanza))
       end
     end
-    return true
 end
 
 module:hook("pre-presence/bare", on_message);
