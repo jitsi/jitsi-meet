9fa1b210f5e6d44a61c0e6f9c5a04b3bd0c1c212
hristoterezov <hristo@jitsi.org>
2018-03-22 13:15:14 -0500

    feat(loadScript): Use the function from js-utils

--- react/features/base/util/loadScript.web.js
+++ react/features/base/util/loadScript.web.js
@@ -1,17 +1,17 @@
 // @flow
 
-declare var JitsiMeetJS: Object;
+import { loadScript as loadScriptF } from 'js-utils';
 
 /**
  * Loads a script from a specific URL. The script will be interpreted upon load.
  *
  * @param {string} url - The url to be loaded.
  * @returns {Promise} Resolved with no arguments when the script is loaded and
- * rejected with the error from JitsiMeetJS.ScriptUtil.loadScript method.
+ * rejected with the error from loadScriptF method.
  */
 export function loadScript(url: string): Promise<void> {
     return new Promise((resolve, reject) =>
-        JitsiMeetJS.util.ScriptUtil.loadScript(
+        loadScriptF(
             url,
             /* async */ true,
             /* prepend */ false,
