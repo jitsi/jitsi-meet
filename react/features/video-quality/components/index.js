5708aee2a2366478ad2d7d16a4b8f106619d8ae7
Leonard Kim <leonard.h.kim@gmail.com>
2018-04-06 12:29:03 -0700

    fix(toolbar): video quality button shows current video quality

--- react/features/video-quality/components/index.js
+++ react/features/video-quality/components/index.js
@@ -1,2 +1,5 @@
+export {
+    default as OverflowMenuVideoQualityItem
+} from './OverflowMenuVideoQualityItem';
 export { default as VideoQualityDialog } from './VideoQualityDialog';
 export { default as VideoQualityLabel } from './VideoQualityLabel';
