a783939f12bcf9b66709660da2621f9e3f103d7b
Leonard Kim <leonard.h.kim@gmail.com>
2018-04-09 20:28:09 -0700

    ref(toolbar): remove old InviteButton

--- react/features/invite/components/index.js
+++ react/features/invite/components/index.js
@@ -1,4 +1,3 @@
 export { default as AddPeopleDialog } from './AddPeopleDialog';
 export { default as InfoDialogButton } from './InfoDialogButton';
-export { default as InviteButton } from './InviteButton';
 export { DialInSummary } from './dial-in-summary';
