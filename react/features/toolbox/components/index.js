c39b471b76d995143101446b3e41e3dbb28a6ce0
Leonard Kim <leonard.h.kim@gmail.com>
2018-04-09 20:07:06 -0700

    ref(toolbar): remove Video Quality Button

--- react/features/toolbox/components/index.js
+++ react/features/toolbox/components/index.js
@@ -1,6 +1,4 @@
 export { default as ToolbarButton } from './ToolbarButton';
 export { default as ToolbarButtonV2 } from './ToolbarButtonV2';
-export { default as ToolbarButtonWithDialog }
-    from './ToolbarButtonWithDialog';
 export { default as ToolboxFilmstrip } from './ToolboxFilmstrip';
 export { default as Toolbox } from './Toolbox';
--- react/features/video-quality/components/index.js
+++ react/features/video-quality/components/index.js
@@ -1,3 +1,2 @@
-export { default as VideoQualityButton } from './VideoQualityButton';
 export { default as VideoQualityDialog } from './VideoQualityDialog';
 export { default as VideoQualityLabel } from './VideoQualityLabel';
