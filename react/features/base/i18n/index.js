994bfe2a5438eb73f92eb86cfdcaba69a7ac3f97
hristoterezov <hristo@jitsi.org>
2018-05-17 16:09:32 -0500

    ref(i18next): Remove the base/config dependancy.

--- react/features/base/i18n/index.js
+++ react/features/base/i18n/index.js
@@ -4,5 +4,3 @@ export * from './functions';
 // TODO Eventually (e.g. when the non-React Web app is rewritten into React), it
 // should not be necessary to export i18next.
 export { default as i18next, DEFAULT_LANGUAGE, LANGUAGES } from './i18next';
-
-import './middleware';
