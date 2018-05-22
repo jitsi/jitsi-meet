569251037d91ccfca22d2819b29b105920324916
Bettenbuk Zoltan <zoltan.bettenbuk@gmail.com>
2018-05-22 21:47:10 +0200

    [RN] Add VideoQualityLabel indicator for mobile

---
+++ react/features/video-quality/components/styles.js
@@ -0,0 +1,16 @@
+// @flow
+
+import { ColorPalette, createStyleSheet } from '../../base/styles';
+
+/**
+ * The styles of the React {@code Components} of the feature video-quality.
+ */
+export default createStyleSheet({
+
+    /**
+     * Style for the audio-only indicator.
+     */
+    indicatorAudioOnly: {
+        backgroundColor: ColorPalette.green
+    }
+});
