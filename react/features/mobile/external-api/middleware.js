01c2786c95fc0cc7c84870ed9a378934387e10f1
paweldomas <pawel.domas@jitsi.org>
2018-07-05 13:33:06 -0500

    ref(base/util): move getSymbolDescription to util

--- react/features/mobile/external-api/middleware.js
+++ react/features/mobile/external-api/middleware.js
@@ -17,7 +17,7 @@ import {
 import { LOAD_CONFIG_ERROR } from '../../base/config';
 import { CONNECTION_FAILED } from '../../base/connection';
 import { MiddlewareRegistry } from '../../base/redux';
-import { toURLString } from '../../base/util';
+import { getSymbolDescription, toURLString } from '../../base/util';
 import { ENTER_PICTURE_IN_PICTURE } from '../picture-in-picture';
 
 /**
@@ -66,7 +66,7 @@ MiddlewareRegistry.register(store => next => action => {
         break;
 
     case ENTER_PICTURE_IN_PICTURE:
-        _sendEvent(store, _getSymbolDescription(type), /* data */ {});
+        _sendEvent(store, getSymbolDescription(type), /* data */ {});
         break;
 
     case LOAD_CONFIG_ERROR: {
@@ -74,7 +74,7 @@ MiddlewareRegistry.register(store => next => action => {
 
         _sendEvent(
             store,
-            _getSymbolDescription(type),
+            getSymbolDescription(type),
             /* data */ {
                 error: _toErrorString(error),
                 url: toURLString(locationURL)
@@ -111,29 +111,6 @@ function _toErrorString(
 }
 
 /**
- * Gets the description of a specific {@code Symbol}.
- *
- * @param {Symbol} symbol - The {@code Symbol} to retrieve the description of.
- * @private
- * @returns {string} The description of {@code symbol}.
- */
-function _getSymbolDescription(symbol: Symbol) {
-    let description = symbol.toString();
-
-    if (description.startsWith('Symbol(') && description.endsWith(')')) {
-        description = description.slice(7, -1);
-    }
-
-    // The polyfill es6-symbol that we use does not appear to comply with the
-    // Symbol standard and, merely, adds @@ at the beginning of the description.
-    if (description.startsWith('@@')) {
-        description = description.slice(2);
-    }
-
-    return description;
-}
-
-/**
  * If {@link SET_ROOM} action happens for a valid conference room this method
  * will emit an early {@link CONFERENCE_WILL_JOIN} event to let the external API
  * know that a conference is being joined. Before that happens a connection must
@@ -153,7 +130,7 @@ function _maybeTriggerEarlyConferenceWillJoin(store, action) {
 
     isRoomValid(room) && locationURL && _sendEvent(
         store,
-        _getSymbolDescription(CONFERENCE_WILL_JOIN),
+        getSymbolDescription(CONFERENCE_WILL_JOIN),
         /* data */ {
             url: toURLString(locationURL)
         });
@@ -184,7 +161,7 @@ function _sendConferenceEvent(
     }
 
     _swallowEvent(store, action, data)
-        || _sendEvent(store, _getSymbolDescription(type), data);
+        || _sendEvent(store, getSymbolDescription(type), data);
 }
 
 /**
@@ -210,7 +187,7 @@ function _sendConferenceFailedOnConnectionError(store, action) {
             conference => conference.getConnection() !== connection)
         && _sendEvent(
         store,
-        _getSymbolDescription(CONFERENCE_FAILED),
+        getSymbolDescription(CONFERENCE_FAILED),
         /* data */ {
             url: toURLString(locationURL),
             error: action.error.name
