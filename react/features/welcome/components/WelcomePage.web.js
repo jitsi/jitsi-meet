50ea84790569200026f8998b0fa00de48e2e70c8
virtuacoplenny <leonard.h.kim@gmail.com>
2018-07-24 12:26:17 -0700

    Refactor welcome page in prep for branding (#3230)
    * fix(welcome-page): css tweaks in prep for branded welcome page
    
    - Watermarks should no longer depend on toolbar size. The left watermark made
      room for the toolbar when the toolbar was on the left side of the screen, but
      the toolbar has been moved to the bottom. The right watermark...well it'll
      clash with the vertical filmstrip but at least the margins will be consistent
      with the left watermark.
    - Apply new font-family so fonts are more likely to be consistent across the
      app. Design likes SF UI and keeps requesting it so use it by default.
    - Change sizings of welcome page header to be more responsive. This will help
      the header be scrollable when there is no additional content and the header
      overflows.
    - Change colors of the welcome page header and remove background image that
      was in the header. Leave in the dom for the background image in case other
      deployments need to continue showing an image.
    - Add a period to the title of the welcome page.
    - Move watermarks dom location as it is not part of the header; it's part of the
      whole page.
    
    * [squash] Size and font adjustments. Renaming.

--- react/features/welcome/components/WelcomePage.web.js
+++ react/features/welcome/components/WelcomePage.web.js
@@ -35,7 +35,7 @@ class WelcomePage extends AbstractWelcomePage {
 
         /**
          * The HTML Element used as the container for additional content. Used
-         * for directly appending the additional content template to the dom
+         * for directly appending the additional content template to the dom.
          *
          * @private
          * @type {HTMLTemplateElement|null}
@@ -107,10 +107,12 @@ class WelcomePage extends AbstractWelcomePage {
                 <div
                     className = { `welcome ${showAdditionalContent
                         ? 'with-content' : 'without-content'}` }
-                    id = 'new_welcome_page'>
+                    id = 'welcome_page'>
+                    <div className = 'welcome-watermark'>
+                        <Watermarks />
+                    </div>
                     <div className = 'header'>
                         <div className = 'header-image' />
-                        <Watermarks />
                         <div className = 'header-text'>
                             <h1 className = 'header-text-title'>
                                 { t('welcomepage.title') }
@@ -120,7 +122,7 @@ class WelcomePage extends AbstractWelcomePage {
                                     { app: APP_NAME }) }
                             </p>
                         </div>
-                        <div id = 'new_enter_room'>
+                        <div id = 'enter_room'>
                             <form
                                 className = 'enter-room-input'
                                 onSubmit = { this._onFormSubmit }>
@@ -156,7 +158,7 @@ class WelcomePage extends AbstractWelcomePage {
     }
 
     /**
-     * Prevents submission of the form and delagates join logic.
+     * Prevents submission of the form and delegates join logic.
      *
      * @param {Event} event - The HTML Event which details the form submission.
      * @private
@@ -196,7 +198,7 @@ class WelcomePage extends AbstractWelcomePage {
     }
 
     /**
-     * Returns whether or not additional content should be displayed belowed
+     * Returns whether or not additional content should be displayed below
      * the welcome page's header for entering a room name.
      *
      * @private
