f1901f83d8665c3a85506dd6d19e42b5bd6081e0
Bettenbuk Zoltan <zoltan.bettenbuk@gmail.com>
2018-08-03 09:35:35 -0500

    Decrease logging verbosity

--- logging_config.js
+++ logging_config.js
@@ -3,16 +3,18 @@
 // Logging configuration
 var loggingConfig = {
     // default log level for the app and lib-jitsi-meet
-    defaultLogLevel: 'trace',
+    defaultLogLevel: 'warn',
 
     // Option to disable LogCollector (which stores the logs on CallStats)
     // disableLogCollector: true,
 
-    // The following are too verbose in their logging with the
-    // {@link #defaultLogLevel}:
-    'modules/RTC/TraceablePeerConnection.js': 'info',
-    'modules/statistics/CallStats.js': 'info',
-    'modules/xmpp/strophe.util.js': 'log'
+    // Although these are on the same level as the {@code defaultLogLevel} atm,
+    // we keep them here, as if anyone wants to change the
+    // {@code defaultLogLevel} to something more verbose, keeping these 3 on a
+    // less verbose level is to be considered.
+    'modules/RTC/TraceablePeerConnection.js': 'warn',
+    'modules/statistics/CallStats.js': 'warn',
+    'modules/xmpp/strophe.util.js': 'warn'
 };
 
 /* eslint-enable no-unused-vars, no-var */
