1516c99768fe2775613b0756b3a73fe2095a588a
Leonard Kim <leonard.h.kim@gmail.com>
2018-09-07 13:35:56 -0700

    the magical incantation to make flow shut up

--- react/features/base/react/components/web/Container.js
+++ react/features/base/react/components/web/Container.js
@@ -1,13 +1,14 @@
 /* @flow */
 
 import AbstractContainer from '../AbstractContainer';
+import type { Props } from '../AbstractContainer';
 
 /**
  * Represents a container of React/Web {@link Component} children with a style.
  *
  * @extends AbstractContainer
  */
-export default class Container extends AbstractContainer {
+export default class Container<P: Props> extends AbstractContainer<P> {
     /**
      * {@code Container} component's property types.
      *
