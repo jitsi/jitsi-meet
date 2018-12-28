148d4ebb9072a87e5f0d19f17a901b540131d4a8
Saúl Ibarra Corretgé <saghul@jitsi.org>
2018-12-28 10:38:56 +0100

    rn: add Firebase integration
    This is done at the app level, not the SDK.
    
    Currently 2 Firebase services are used:
    
      - Crashlytics
      - Dynamic Links
    
    They are enabled in tandem, if the appropriate Google services file
    (GoogleService-Info.plist on iOS or google-services.json on Android) is found.
    
    Each service needs to be individually enabled in the Firebase console.

--- ios/app/src/AppDelegate.m
+++ ios/app/src/AppDelegate.m
@@ -1,5 +1,6 @@
 /*
- * Copyright @ 2017-present Atlassian Pty Ltd
+ * Copyright @ 2018-present 8x8, Inc.
+ * Copyright @ 2017-2018 Atlassian Pty Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,13 +16,27 @@
  */
 
 #import "AppDelegate.h"
+#import "FIRUtilities.h"
 
 #import <JitsiMeet/JitsiMeet.h>
+
+@import Crashlytics;
+@import Fabric;
+@import Firebase;
+
 
 @implementation AppDelegate
 
 -             (BOOL)application:(UIApplication *)application
   didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    // Initialize Crashlytics and Firebase if a valid GoogleService-Info.plist file was provided.
+    if ([FIRUtilities appContainsRealServiceInfoPlist]) {
+        NSLog(@"Enablign Crashlytics and Firebase");
+        [FIRApp configure];
+        [Fabric with:@[[Crashlytics class]]];
+    }
+
     return [JitsiMeetView application:application
         didFinishLaunchingWithOptions:launchOptions];
 }
@@ -31,6 +46,30 @@
 -    (BOOL)application:(UIApplication *)application
   continueUserActivity:(NSUserActivity *)userActivity
     restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> *restorableObjects))restorationHandler {
+
+    if ([FIRUtilities appContainsRealServiceInfoPlist]) {
+        // 1. Attempt to handle Universal Links through Firebase in order to support
+        //    its Dynamic Links (which we utilize for the purposes of deferred deep
+        //    linking).
+        BOOL handled
+          = [[FIRDynamicLinks dynamicLinks]
+                handleUniversalLink:userActivity.webpageURL
+                         completion:^(FIRDynamicLink * _Nullable dynamicLink, NSError * _Nullable error) {
+           NSURL *dynamicLinkURL = dynamicLink.url;
+           if (dynamicLinkURL) {
+             userActivity.webpageURL = dynamicLinkURL;
+             [JitsiMeetView application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
+           }
+        }];
+
+        if (handled) {
+          return handled;
+        }
+    }
+
+    // 2. Default to plain old, non-Firebase-assisted Universal Links.
     return [JitsiMeetView application:application
                  continueUserActivity:userActivity
                    restorationHandler:restorationHandler];
@@ -39,8 +78,25 @@
 - (BOOL)application:(UIApplication *)app
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+
+    NSURL *openUrl = url;
+
+    if ([FIRUtilities appContainsRealServiceInfoPlist]) {
+        // Process Firebase Dynamic Links
+        FIRDynamicLink *dynamicLink = [[FIRDynamicLinks dynamicLinks] dynamicLinkFromCustomSchemeURL:url];
+        if (dynamicLink != nil) {
+            NSURL *dynamicLinkURL = dynamicLink.url;
+            if (dynamicLinkURL != nil
+                    && (dynamicLink.matchType == FIRDLMatchTypeUnique
+                        || dynamicLink.matchType == FIRDLMatchTypeDefault)) {
+                // Strong match, process it.
+                openUrl = dynamicLinkURL;
+            }
+        }
+    }
+
     return [JitsiMeetView application:app
-                              openURL:url
+                              openURL:openUrl
                               options:options];
 }
 
