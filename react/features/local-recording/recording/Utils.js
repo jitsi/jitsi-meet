2d8d7618787a7f1e5d4325b38956d61abf0bb253
zjf <450788018@qq.com>
2019-04-28 14:46:00 +0800

    增加本地下载提示框

--- react/features/local-recording/recording/Utils.js
+++ react/features/local-recording/recording/Utils.js
@@ -18,3 +18,16 @@ export function downloadBlob(blob, fileName = 'recording.ogg') {
     a.click();
     document.body.removeChild(a);
 }
+
+export function downloadUrl(url, fileName = 'recording.ogg') {
+
+    // fake a anchor tag
+    const a = document.createElement('a');
+
+    a.style = 'display: none';
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
