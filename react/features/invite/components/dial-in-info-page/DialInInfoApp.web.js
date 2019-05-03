0ec4e6a8050b3936f830578a42cb1a57f5f15c01
damencho <damencho@jitsi.org>
2019-05-03 10:49:50 +0100

    Makes numbers clickable on the more numbers page when opened on mobile.

--- react/features/invite/components/dial-in-info-page/DialInInfoApp.web.js
+++ react/features/invite/components/dial-in-info-page/DialInInfoApp.web.js
@@ -8,16 +8,19 @@ import { i18next } from '../../../base/i18n';
 import { DialInSummary } from '../dial-in-summary';
 
 import NoRoomError from './NoRoomError';
+import { Platform } from '../../../base/react';
 
 document.addEventListener('DOMContentLoaded', () => {
     const { room } = parseURLParams(window.location, true, 'search');
+    const OS = Platform.OS;
+    const isUsingMobileBrowser = OS === 'android' || OS === 'ios';
 
     ReactDOM.render(
         <I18nextProvider i18n = { i18next }>
             { room
                 ? <DialInSummary
                     className = 'dial-in-page'
-                    clickableNumbers = { false }
+                    clickableNumbers = { isUsingMobileBrowser }
                     room = { room } />
                 : <NoRoomError className = 'dial-in-page' /> }
         </I18nextProvider>,
