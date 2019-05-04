a9637f93c317b91b98d279a8c7e01f555da8e890
Leonard Kim <leonard.kim@8x8.com>
2019-05-04 14:37:30 -0700

    ref(chat): create AbstractMessageContainer
    So mobile and web can share logic for grouping chat
    messages by sender.

---
+++ react/features/chat/components/AbstractMessageContainer.js
@@ -0,0 +1,53 @@
+// @flow
+
+import { PureComponent } from 'react';
+
+export type Props = {
+
+    /**
+     * The messages array to render.
+     */
+    messages: Array<Object>
+}
+
+/**
+ * Abstract component to display a list of chat messages, grouped by sender.
+ *
+ * @extends PureComponent
+ */
+export default class AbstractMessageContainer<P: Props> extends PureComponent<P> {
+    static defaultProps = {
+        messages: []
+    };
+
+    /**
+     * Iterates over all the messages and creates nested arrays which hold
+     * consecutive messages sent by the same participant.
+     *
+     * @private
+     * @returns {Array<Array<Object>>}
+     */
+    _getMessagesGroupedBySender() {
+        const messagesCount = this.props.messages.length;
+        const groups = [];
+        let currentGrouping = [];
+        let currentGroupParticipantId;
+
+        for (let i = 0; i < messagesCount; i++) {
+            const message = this.props.messages[i];
+
+            if (message.id === currentGroupParticipantId) {
+                currentGrouping.push(message);
+            } else {
+                groups.push(currentGrouping);
+
+                currentGrouping = [ message ];
+                currentGroupParticipantId = message.id;
+            }
+        }
+
+        groups.push(currentGrouping);
+
+        return groups;
+    }
+}
