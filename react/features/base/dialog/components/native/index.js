7e9df74e6098520af18f24f269fcae5b20f0c39a
Bettenbuk Zoltan <zoltan.bettenbuk@gmail.com>
2019-05-07 16:47:33 +0200

    rn: add generic alert dialog

--- react/features/base/dialog/components/native/index.js
+++ react/features/base/dialog/components/native/index.js
@@ -4,6 +4,7 @@ export { default as BottomSheet } from './BottomSheet';
 export { default as ConfirmDialog } from './ConfirmDialog';
 export { default as CustomDialog } from './CustomDialog';
 export { default as DialogContainer } from './DialogContainer';
+export { default as AlertDialog } from './AlertDialog';
 export { default as InputDialog } from './InputDialog';
 export { default as CustomSubmitDialog } from './CustomSubmitDialog';
 
