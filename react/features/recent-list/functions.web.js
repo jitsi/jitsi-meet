c4e0408d9e7c54fd3daf4a87f1105f11ddded101
zjf <450788018@qq.com>
2019-05-17 15:54:05 +0800

    支持中文会议室名称

--- react/features/recent-list/functions.web.js
+++ react/features/recent-list/functions.web.js
@@ -1,6 +1,6 @@
 /* global interfaceConfig */
 
-import { parseURIString } from '../base/util';
+import { parseURIString,convertForTrans } from '../base/util';
 
 
 /**
@@ -18,7 +18,7 @@ export function toDisplayableList(recentList) {
                     date: item.date,
                     duration: item.duration,
                     time: [ item.date ],
-                    title: parseURIString(item.conference).room,
+                    title: convertForTrans(parseURIString(item.conference).room),
                     url: item.conference
                 };
             }));
