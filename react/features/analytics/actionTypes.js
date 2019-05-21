70921bb6ef4409cf3eb7f6def0210a4ffd35e836
Hristo Terezov <hristo@jitsi.org>
2019-05-21 11:32:20 +0100

    feat(analytics): local tracks duration event.

---
+++ react/features/analytics/actionTypes.js
@@ -0,0 +1,11 @@
+// @flow
+
+/**
+ * The type of (redux) action which signals that local media duration has changed.
+ *
+ * {
+ *     type: UPDATE_LOCAL_TRACKS_DURATION,
+ *     localTracksDuration: Object
+ * }
+ */
+export const UPDATE_LOCAL_TRACKS_DURATION = 'UPDATE_LOCAL_TRACKS_DURATION';
