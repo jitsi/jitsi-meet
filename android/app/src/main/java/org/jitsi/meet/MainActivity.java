a9e2b3153d2b0ebb394139fc6ae1233aaf0e4301
zjf <450788018@qq.com>
2019-05-22 21:41:19 +0800

    改为i.vmeeting.top

--- android/app/src/main/java/org/jitsi/meet/MainActivity.java
+++ android/app/src/main/java/org/jitsi/meet/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends JitsiMeetActivity {
         JitsiMeetConferenceOptions defaultOptions
             = new JitsiMeetConferenceOptions.Builder()
                 .setWelcomePageEnabled(true)
-                .setServerURL(buildURL("https://sz.vmeeting.top"))
+                .setServerURL(buildURL("https://i.vmeeting.top"))
                 .build();
         JitsiMeet.setDefaultConferenceOptions(defaultOptions);
 
