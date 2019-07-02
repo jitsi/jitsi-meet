1e0591dbf0a84e91c2ba2ab789babfb0a6f99e62
damencho <damencho@jitsi.org>
2019-07-02 13:09:38 +0100

    Updates formatting of the pin.

--- react/features/invite/_utils.js
+++ react/features/invite/_utils.js
@@ -8,8 +8,8 @@
 /**
  * Formats the conference pin in readable way for UI to display it.
  * Formats the pin in 3 groups of digits:
- * XXX XXX XXXX or XXXX XXXX XXXXX.
- * The group of lenght is pin.length / 2
+ * XXXX XXXX XX or XXXXX XXXXX XXX.
+ * The length of first and second group is Math.ceil(pin.length / 3)
  *
  * @param {Object} conferenceID - The conference id to format, string or number.
  * @returns {string} - The formatted conference pin.
@@ -19,7 +19,7 @@ export function _formatConferenceIDPin(conferenceID: Object) {
     const conferenceIDStr = conferenceID.toString();
 
     // let's split the conferenceID in 3 parts, to be easier to read
-    const partLen = Math.floor(conferenceIDStr.length / 3);
+    const partLen = Math.ceil(conferenceIDStr.length / 3);
 
     return `${
         conferenceIDStr.substring(0, partLen)} ${
