3f1f8059e2967a99cabdc7daa8f4393e274486ee
Leonard Kim <leonard.kim@8x8.com>
2019-07-11 12:53:33 -0700

    ref(user-interaction): remove storing of listener

--- react/features/base/user-interaction/actionTypes.js
+++ react/features/base/user-interaction/actionTypes.js
@@ -1,15 +1,4 @@
 /**
- * The type of (redux) action which signals that an event listener has been
- * added to listen for any user interaction with the page.
- *
- * {
- *     type: SET_USER_INTERACTION_LISTENER,
- *     userInteractionListener: Function
- * }
- */
-export const SET_USER_INTERACTION_LISTENER = 'SET_USER_INTERACTION_LISTENER';
-
-/**
  * The type of (redux) action which signals the user has interacted with the
  * page.
  *
