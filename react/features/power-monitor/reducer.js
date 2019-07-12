7abd6d9e7929cf673d57117d1f21588478d8f5f3
damencho <damencho@jitsi.org>
2019-07-12 11:24:41 +0100

    Moves suspendDetected state from overlay to power-monitor feature.

--- react/features/power-monitor/reducer.js
+++ react/features/power-monitor/reducer.js
@@ -3,7 +3,10 @@
 import { ReducerRegistry, set } from '../base/redux';
 import { Transport } from '../../../modules/transport';
 
-import { SET_TRANSPORT } from './actionTypes';
+import {
+    SET_TRANSPORT,
+    SUSPEND_DETECTED
+} from './actionTypes';
 
 /**
  * Reduces the redux actions of the feature power monitor.
@@ -12,6 +15,10 @@ ReducerRegistry.register('features/power-monitor', (state = { }, action) => {
     switch (action.type) {
     case SET_TRANSPORT:
         return _setTransport(state, action.transport);
+
+    case SUSPEND_DETECTED:
+        return _suspendDetected(state);
+
     }
 
     return state;
@@ -29,3 +36,16 @@ ReducerRegistry.register('features/power-monitor', (state = { }, action) => {
 function _setTransport(state, transport: ?Transport) {
     return set(state, 'transport', transport);
 }
+
+/**
+ * Reduces a specific redux action SUSPEND_DETECTED of the feature overlay.
+ *
+ * @param {Object} state - The redux state of the feature overlay.
+ * @private
+ * @returns {Object} The new state of the feature overlay after the reduction of
+ * the specified action.
+ */
+function _suspendDetected(state) {
+    return set(state, 'suspendDetected', true);
+}
+
