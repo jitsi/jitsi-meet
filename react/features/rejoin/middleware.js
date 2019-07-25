1c27f567ee66bfa9197ca286981f9ad20d62a1a8
paweldomas <pawel.domas@jitsi.org>
2019-07-25 13:23:48 +0200

    feat: send analytics event when conference is rejoined

---
+++ react/features/rejoin/middleware.js
@@ -0,0 +1,27 @@
+import { createRejoinedEvent, sendAnalytics } from '../analytics';
+
+import { StateListenerRegistry } from '../base/redux';
+
+StateListenerRegistry.register(
+    /* selector */ state => {
+        const recentList = state['features/recent-list'];
+
+        // Return the most recent conference entry
+        return recentList && recentList.length && recentList[recentList.length - 1];
+    },
+    // eslint-disable-next-line no-empty-pattern
+    /* listener */ (newMostRecent, { }, prevMostRecent) => {
+        if (prevMostRecent && newMostRecent) {
+
+            // Send the rejoined event just before the duration is reset on the most recent entry
+            if (prevMostRecent.conference === newMostRecent.conference && newMostRecent.duration === 0) {
+                sendAnalytics(
+                    createRejoinedEvent({
+                        lastConferenceDuration: prevMostRecent.duration / 1000,
+                        timeSinceLeft: (Date.now() - (prevMostRecent.date + prevMostRecent.duration)) / 1000,
+                        url: prevMostRecent.conference
+                    })
+                );
+            }
+        }
+    });
