1c1e8a942b786fc48f185412a869493e2e69d6ea
Saúl Ibarra Corretgé <saghul@jitsi.org>
2019-08-09 12:41:52 +0200

    audio-mode: refactor device handling
    This commit refactors device selection (more heavily on iOS) to make it
    consistent across platforms.
    
    Due to its complexity I couldn't break out each step into separate commits,
    apologies to the reviewer.
    
    Changes made to device handling:
    
    - speaker is always the default, regardless of the mode
    - "Phone" shows as a selectable option, even in video call mode
    - "Phone" is not displayed when wired headphones are present
    - Shared device picker between iOS and Android
    - Runtime device updates while the picker is open

--- react/features/mobile/audio-mode/components/AudioRouteButton.js
+++ react/features/mobile/audio-mode/components/AudioRouteButton.js
@@ -1,13 +1,5 @@
 // @flow
 
-import React from 'react';
-import {
-    findNodeHandle,
-    NativeModules,
-    requireNativeComponent,
-    View
-} from 'react-native';
-
 import { openDialog } from '../../../base/dialog';
 import { translate } from '../../../base/i18n';
 import { connect } from '../../../base/redux';
@@ -16,19 +8,6 @@ import type { AbstractButtonProps } from '../../../base/toolbox';
 
 import AudioRoutePickerDialog from './AudioRoutePickerDialog';
 
-/**
- * The {@code MPVolumeView} React {@code Component}. It will only be available
- * on iOS.
- */
-const MPVolumeView
-    = NativeModules.MPVolumeViewManager
-        && requireNativeComponent('MPVolumeView');
-
-/**
- * The style required to hide the {@code MPVolumeView}, since it's displayed
- * programmatically.
- */
-const HIDE_VIEW_STYLE = { display: 'none' };
 
 type Props = AbstractButtonProps & {
 
@@ -47,30 +26,6 @@ class AudioRouteButton extends AbstractButton<Props, *> {
     iconName = 'icon-volume';
     label = 'toolbar.audioRoute';
 
-    _volumeComponent: ?Object;
-
-    /**
-     * Initializes a new {@code AudioRouteButton} instance.
-     *
-     * @param {Props} props - The React {@code Component} props to initialize
-     * the new {@code AudioRouteButton} instance with.
-     */
-    constructor(props: Props) {
-        super(props);
-
-        /**
-         * The internal reference to the React {@code MPVolumeView} for
-         * showing the volume control view.
-         *
-         * @private
-         * @type {ReactElement}
-         */
-        this._volumeComponent = null;
-
-        // Bind event handlers so they are only bound once per instance.
-        this._setVolumeComponent = this._setVolumeComponent.bind(this);
-    }
-
     /**
      * Handles clicking / pressing the button, and opens the appropriate dialog.
      *
@@ -78,52 +33,7 @@ class AudioRouteButton extends AbstractButton<Props, *> {
      * @returns {void}
      */
     _handleClick() {
-        if (MPVolumeView) {
-            NativeModules.MPVolumeViewManager.show(
-                findNodeHandle(this._volumeComponent));
-        } else if (AudioRoutePickerDialog) {
-            this.props.dispatch(openDialog(AudioRoutePickerDialog));
-        }
-    }
-
-    _setVolumeComponent: (?Object) => void;
-
-    /**
-     * Sets the internal reference to the React Component wrapping the
-     * {@code MPVolumeView} component.
-     *
-     * @param {ReactElement} component - React Component.
-     * @private
-     * @returns {void}
-     */
-    _setVolumeComponent(component) {
-        this._volumeComponent = component;
-    }
-
-    /**
-     * Implements React's {@link Component#render()}.
-     *
-     * @inheritdoc
-     * @returns {React$Node}
-     */
-    render() {
-        if (!MPVolumeView && !AudioRoutePickerDialog) {
-            return null;
-        }
-
-        const element = super.render();
-
-        return (
-            <View>
-                { element }
-                {
-                    MPVolumeView
-                        && <MPVolumeView
-                            ref = { this._setVolumeComponent }
-                            style = { HIDE_VIEW_STYLE } />
-                }
-            </View>
-        );
+        this.props.dispatch(openDialog(AudioRoutePickerDialog));
     }
 }
 
--- react/features/mobile/audio-mode/middleware.js
+++ react/features/mobile/audio-mode/middleware.js
@@ -1,9 +1,9 @@
 // @flow
 
-import { NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 
 import { SET_AUDIO_ONLY } from '../../base/audio-only';
-import { APP_WILL_MOUNT } from '../../base/app';
+import { APP_WILL_MOUNT, APP_WILL_UNMOUNT } from '../../base/app';
 import {
     CONFERENCE_FAILED,
     CONFERENCE_LEFT,
@@ -12,7 +12,10 @@ import {
 } from '../../base/conference';
 import { MiddlewareRegistry } from '../../base/redux';
 
+import { _SET_AUDIOMODE_DEVICES, _SET_AUDIOMODE_SUBSCRIPTIONS } from './actionTypes';
+
 const { AudioMode } = NativeModules;
+const AudioModeEmitter = new NativeEventEmitter(AudioMode);
 const logger = require('jitsi-meet-logger').getLogger(__filename);
 
 /**
@@ -23,55 +26,127 @@ const logger = require('jitsi-meet-logger').getLogger(__filename);
  * @param {Store} store - The redux store.
  * @returns {Function}
  */
-MiddlewareRegistry.register(({ getState }) => next => action => {
-    const result = next(action);
+MiddlewareRegistry.register(store => next => action => {
+    /* eslint-disable no-fallthrough */
 
-    if (AudioMode) {
-        let mode;
+    switch (action.type) {
+    case _SET_AUDIOMODE_SUBSCRIPTIONS:
+        _setSubscriptions(store);
+        break;
+    case APP_WILL_UNMOUNT: {
+        store.dispatch({
+            type: _SET_AUDIOMODE_SUBSCRIPTIONS,
+            subscriptions: undefined
+        });
+        break;
+    }
+    case APP_WILL_MOUNT:
+        _appWillMount(store);
+    case CONFERENCE_FAILED: // eslint-disable-line no-fallthrough
+    case CONFERENCE_LEFT:
 
-        switch (action.type) {
-        case APP_WILL_MOUNT:
-        case CONFERENCE_FAILED:
-        case CONFERENCE_LEFT: {
-            const conference = getCurrentConference(getState());
+    /*
+    * NOTE: We moved the audio mode setting from CONFERENCE_WILL_JOIN to
+    * CONFERENCE_JOINED because in case of a locked room, the app goes
+    * through CONFERENCE_FAILED state and gets to CONFERENCE_JOINED only
+    * after a correct password, so we want to make sure we have the correct
+    * audio mode set up when we finally get to the conf, but also make sure
+    * that the app is in the right audio mode if the user leaves the
+    * conference after the password prompt appears.
+    */
+    case CONFERENCE_JOINED:
+    case SET_AUDIO_ONLY:
+        return _updateAudioMode(store, next, action);
 
-            if (typeof conference === 'undefined') {
-                mode = AudioMode.DEFAULT;
-            }
-
-            break;
-        }
-
-        /*
-         * NOTE: We moved the audio mode setting from CONFERENCE_WILL_JOIN to
-         * CONFERENCE_JOINED because in case of a locked room, the app goes
-         * through CONFERENCE_FAILED state and gets to CONFERENCE_JOINED only
-         * after a correct password, so we want to make sure we have the correct
-         * audio mode set up when we finally get to the conf, but also make sure
-         * that the app is in the right audio mode if the user leaves the
-         * conference after the password prompt appears.
-         */
-        case CONFERENCE_JOINED:
-        case SET_AUDIO_ONLY: {
-            const state = getState();
-            const { conference } = state['features/base/conference'];
-            const { enabled: audioOnly } = state['features/base/audio-only'];
-
-            conference
-                && (mode = audioOnly
-                    ? AudioMode.AUDIO_CALL
-                    : AudioMode.VIDEO_CALL);
-            break;
-        }
-        }
-
-        if (typeof mode !== 'undefined') {
-            AudioMode.setMode(mode)
-                .catch(err =>
-                    logger.error(
-                        `Failed to set audio mode ${String(mode)}: ${err}`));
-        }
     }
 
-    return result;
+    /* eslint-enable no-fallthrough */
+
+    return next(action);
 });
+
+/**
+ * Notifies this feature that the action {@link APP_WILL_MOUNT} is being
+ * dispatched within a specific redux {@code store}.
+ *
+ * @param {Store} store - The redux store in which the specified {@code action}
+ * is being dispatched.
+ * @private
+ * @returns {void}
+ */
+function _appWillMount(store) {
+    const subscriptions = [
+        AudioModeEmitter.addListener(AudioMode.DEVICE_CHANGE_EVENT, _onDevicesUpdate, store)
+    ];
+
+    store.dispatch({
+        type: _SET_AUDIOMODE_SUBSCRIPTIONS,
+        subscriptions
+    });
+}
+
+/**
+ * Handles audio device changes. The list will be stored on the redux store.
+ *
+ * @param {Object} devices - The current list of devices.
+ * @private
+ * @returns {void}
+ */
+function _onDevicesUpdate(devices) {
+    const { dispatch } = this; // eslint-disable-line no-invalid-this
+
+    dispatch({
+        type: _SET_AUDIOMODE_DEVICES,
+        devices
+    });
+}
+
+/**
+ * Notifies this feature that the action
+ * {@link _SET_AUDIOMODE_SUBSCRIPTIONS} is being dispatched within
+ * a specific redux {@code store}.
+ *
+ * @param {Store} store - The redux store in which the specified {@code action}
+ * is being dispatched.
+ * @private
+ * @returns {void}
+ */
+function _setSubscriptions({ getState }) {
+    const { subscriptions } = getState()['features/mobile/audio-mode'];
+
+    if (subscriptions) {
+        for (const subscription of subscriptions) {
+            subscription.remove();
+        }
+    }
+}
+
+/**
+ * Updates the audio mode based on the current (redux) state.
+ *
+ * @param {Store} store - The redux store in which the specified {@code action}
+ * is being dispatched.
+ * @param {Dispatch} next - The redux {@code dispatch} function to dispatch the
+ * specified {@code action} in the specified {@code store}.
+ * @param {Action} action - The redux action which is
+ * being dispatched in the specified {@code store}.
+ * @private
+ * @returns {*} The value returned by {@code next(action)}.
+ */
+function _updateAudioMode({ getState }, next, action) {
+    const result = next(action);
+    const state = getState();
+    const conference = getCurrentConference(state);
+    const { enabled: audioOnly } = state['features/base/audio-only'];
+    let mode;
+
+    if (conference) {
+        mode = audioOnly ? AudioMode.AUDIO_CALL : AudioMode.VIDEO_CALL;
+    } else {
+        mode = AudioMode.DEFAULT;
+    }
+
+    AudioMode.setMode(mode).catch(err => logger.error(`Failed to set audio mode ${String(mode)}: ${err}`));
+
+    return result;
+}
