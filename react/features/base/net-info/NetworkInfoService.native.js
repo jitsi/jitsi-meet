6ae9bbe0c548e28ee69f5e78c3023a82dbf42ebb
paweldomas <pawel.domas@jitsi.org>
2019-08-22 10:08:34 -0500

    feat: report analytics for the network connection
    Will emit new 'network.info' action with the online/offline status and
    extra details for native like the network type and
    'isConnectionExpensive' flag.

---
+++ react/features/base/net-info/NetworkInfoService.native.js
@@ -0,0 +1,66 @@
+// @flow
+import EventEmitter from 'events';
+import NetInfo from '@react-native-community/netinfo';
+import type { NetInfoState, NetInfoSubscription } from '@react-native-community/netinfo';
+
+import { ONLINE_STATE_CHANGED_EVENT } from './events';
+
+import type { NetworkInfo } from './types';
+
+/**
+ * The network info service implementation for iOS and Android. 'react-native-netinfo' seems to support windows as well,
+ * but that has not been tested and is nto used by jitsi-meet.
+ */
+export default class NetworkInfoService extends EventEmitter {
+    /**
+     * Stores the native subscription for future cleanup.
+     */
+    _subscription: NetInfoSubscription;
+
+    /**
+     * Converts library's structure to {@link NetworkInfo} used by jitsi-meet.
+     *
+     * @param {NetInfoState} netInfoState - The new state given by the native library.
+     * @private
+     * @returns {NetworkInfo}
+     */
+    static _convertNetInfoState(netInfoState: NetInfoState): NetworkInfo {
+        return {
+            isOnline: netInfoState.isInternetReachable,
+            details: netInfoState.details,
+            networkType: netInfoState.type
+        };
+    }
+
+    /**
+     * Checks for support.
+     *
+     * @returns {boolean}
+     */
+    static isSupported() {
+        return Boolean(NetInfo);
+    }
+
+    /**
+     * Starts the service.
+     *
+     * @returns {void}
+     */
+    start() {
+        this._subscription = NetInfo.addEventListener(netInfoState => {
+            this.emit(ONLINE_STATE_CHANGED_EVENT, NetworkInfoService._convertNetInfoState(netInfoState));
+        });
+    }
+
+    /**
+     * Stops the service.
+     *
+     * @returns {void}
+     */
+    stop() {
+        if (this._subscription) {
+            this._subscription();
+            this._subscription = undefined;
+        }
+    }
+}
---
+++ react/features/base/net-info/actions.js
@@ -0,0 +1,42 @@
+// @flow
+
+import { SET_NETWORK_INFO, _STORE_NETWORK_INFO_CLEANUP } from './actionTypes';
+
+import type { NetworkInfo } from './types';
+
+/**
+ * Up[dates the network info state.
+ *
+ * @param {NetworkInfo} networkInfo - The new network state to be set.
+ * @returns {{
+ *     type: SET_NETWORK_INFO,
+ *     isOnline: boolean,
+ *     networkType: string,
+ *     details: Object
+ * }}
+ */
+export function setNetworkInfo({ isOnline, networkType, details }: NetworkInfo): Object {
+    return {
+        type: SET_NETWORK_INFO,
+        isOnline,
+        networkType,
+        details
+    };
+}
+
+/**
+ * Stored the cleanup function used to shutdown the {@code NetworkInfoService}.
+ *
+ * @param {Function} cleanup - The cleanup function to be called on {@code APP_WILL_UNMOUNT}.
+ * @returns {{
+ *     type: _STORE_NETWORK_INFO_CLEANUP,
+ *     cleanup: Function
+ * }}
+ * @private
+ */
+export function _storeNetworkInfoCleanup(cleanup: Function): Object {
+    return {
+        type: _STORE_NETWORK_INFO_CLEANUP,
+        cleanup
+    };
+}
