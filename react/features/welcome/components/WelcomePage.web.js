dd200d41a725a4c9a1aba70b52e2b63d995a2018
Saúl Ibarra Corretgé <saghul@jitsi.org>
2019-09-13 11:40:23 +0200

    welcome: tame the linter

--- react/features/welcome/components/WelcomePage.web.js
+++ react/features/welcome/components/WelcomePage.web.js
@@ -147,12 +147,11 @@ class WelcomePage extends AbstractWelcomePage {
                                 <input
                                     autoFocus = { true }
                                     className = 'enter-room-input'
-                                    pattern = '^[a-zA-Z0-9=\?]+$'
-                                    title = { t('welcomepage.onlyAsciiAllowed') }
                                     id = 'enter_room_field'
                                     onChange = { this._onRoomChange }
-                                    placeholder
-                                        = { this.state.roomPlaceholder }
+                                    pattern = '^[a-zA-Z0-9=\?]+$'
+                                    placeholder = { this.state.roomPlaceholder }
+                                    title = { t('welcomepage.onlyAsciiAllowed') }
                                     type = 'text'
                                     value = { this.state.room } />
                             </form>
