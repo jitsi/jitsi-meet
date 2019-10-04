16381173cc59c5b59f96d59bda8cbdc5412b0217
Saúl Ibarra Corretgé <saghul@jitsi.org>
2019-10-04 17:10:19 +0200

    rn: add shared document support using Etherpad

--- react/features/etherpad/actionTypes.js
+++ react/features/etherpad/actionTypes.js
@@ -15,8 +15,16 @@ export const ETHERPAD_INITIALIZED = 'ETHERPAD_INITIALIZED';
  *     type: SET_DOCUMENT_EDITING_STATUS
  * }
  */
-export const SET_DOCUMENT_EDITING_STATUS
-    = 'SET_DOCUMENT_EDITING_STATUS';
+export const SET_DOCUMENT_EDITING_STATUS = 'SET_DOCUMENT_EDITING_STATUS';
+
+/**
+ * The type of the action which updates the shared document URL.
+ *
+ * {
+ *     type: SET_DOCUMENT_URL
+ * }
+ */
+export const SET_DOCUMENT_URL = 'SET_DOCUMENT_URL';
 
 /**
  * The type of the action which signals to start or stop editing a shared
