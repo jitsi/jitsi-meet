f1614b467ecbfcbc79e7475ecd86af8a24d24984
damencho <damencho@jitsi.org>
2019-10-08 13:05:41 +0100

    Adds copy icon next to the meeting url in info dialog.

--- react/features/base/icons/svg/index.js
+++ react/features/base/icons/svg/index.js
@@ -18,6 +18,7 @@ export { default as IconClose } from './close.svg';
 export { default as IconClosedCaption } from './closed_caption.svg';
 export { default as IconConnectionActive } from './gsm-bars.svg';
 export { default as IconConnectionInactive } from './ninja.svg';
+export { default as IconCopy } from './copy.svg';
 export { default as IconDeviceBluetooth } from './bluetooth.svg';
 export { default as IconDeviceEarpiece } from './phone-talk.svg';
 export { default as IconDeviceHeadphone } from './headset.svg';
