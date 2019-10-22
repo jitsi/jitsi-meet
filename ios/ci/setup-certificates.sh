c0a7d7c2556023a3a92e9c38c56c7a60b4b58195
paweldomas <pawel.domas@jitsi.org>
2019-10-22 10:58:40 -0500

    revert workaround removal

--- ios/ci/setup-certificates.sh
+++ ios/ci/setup-certificates.sh
@@ -89,6 +89,11 @@ security import ${CERT_DIR}/dev-cert.cer -k ios-build.keychain -A
 echo "importing dev-key.p12"
 security import ${CERT_DIR}/dev-key.p12 -k ios-build.keychain -P $IOS_SIGNING_CERT_PASSWORD -A
 
+echo "will set-key-partition-list"
+# Fix for OS X Sierra that hungs in the codesign step
+security set-key-partition-list -S apple-tool:,apple: -s -k $ENCRYPTION_PASSWORD ios-build.keychain > /dev/null
+echo "done set-key-partition-list"
+
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp "${CERT_DIR}/dev-profile.mobileprovision"  ~/Library/MobileDevice/Provisioning\ Profiles/
