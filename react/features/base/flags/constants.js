ed7e2440446b8c1d8c675441e1bfaabc21ea0d12
Bettenbuk Zoltan <zoltan.bettenbuk@gmail.com>
2019-10-24 17:41:31 +0200

    feat: feature flag for invite functionalities

--- react/features/base/flags/constants.js
+++ react/features/base/flags/constants.js
@@ -13,6 +13,12 @@ export const CALENDAR_ENABLED = 'calendar.enabled';
 export const CHAT_ENABLED = 'chat.enabled';
 
 /**
+ * Flag indicating if invite functionality should be enabled.
+ * Default: enabled (true).
+ */
+export const INVITE_ENABLED = 'invite.enabled';
+
+/**
  * Flag indicating if recording should be enabled in iOS.
  * Default: disabled (false).
  */
