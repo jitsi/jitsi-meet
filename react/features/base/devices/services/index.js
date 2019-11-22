191da551e38c1e0d6747fff43d7fc0c95576e535
Andrei Gavrilescu <andrei.gavrilescu@8x8.com>
2019-11-22 16:02:59 +0200

    refactor / address code review

--- react/features/base/devices/services/index.js
+++ react/features/base/devices/services/index.js
@@ -1,4 +1,2 @@
-export * from './device-detect/ActiveDeviceDetector';
-export * from './device-detect/Events';
 export * from './vad-reporter/Events';
 export * from './vad-reporter/VADReportingService';
--- react/features/no-audio-signal/reducer.js
+++ react/features/no-audio-signal/reducer.js
@@ -2,14 +2,14 @@
 
 import { ReducerRegistry, set } from '../base/redux';
 
-import { SET_NO_AUDIO_SIGNAL_NOTI_UID } from './actionTypes';
+import { SET_NO_AUDIO_SIGNAL_NOTIFICATION_UID } from './actionTypes';
 
 /**
  * Reduces the redux actions of the feature no audio signal
  */
 ReducerRegistry.register('features/no-audio-signal', (state = {}, action) => {
     switch (action.type) {
-    case SET_NO_AUDIO_SIGNAL_NOTI_UID:
+    case SET_NO_AUDIO_SIGNAL_NOTIFICATION_UID:
         return set(state, 'noAudioSignalNotificationUid', action.uid);
     }
 
