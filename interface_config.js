aff73f051a6282f91f77cb70511dd7c6490aa5cb
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-01-23 15:15:27 +0100

    lint: fix eslint error

--- interface_config.js
+++ interface_config.js
@@ -200,7 +200,7 @@ var interfaceConfig = {
 
     // If true, notifications regarding joining/leaving are no longer displayed
     DISABLE_JOIN_LEAVE_NOTIFICATIONS: false,
-  
+
     /**
     * Decides whether the chrome extension banner should be rendered on the landing page and during the meeting.
     * If this is set to false, the banner will not be rendered at all. If set to true, the check for extension(s)
