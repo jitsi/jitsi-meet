b3cf7febc14a2bf332024fdfefe320c04a3e20b9
Aaron van Meerten <aaron.van.meerten@gmail.com>
2020-01-24 08:09:50 -0600

    hook on websocket events

--- resources/prosody-plugins/mod_auth_token.lua
+++ resources/prosody-plugins/mod_auth_token.lua
@@ -32,6 +32,21 @@ module:hook("bosh-session", function(event)
     end
 end);
 
+-- Extract 'token' param from Websocket URL when session is created
+module:hook("websocket-session", function(event)
+	local session, request = event.session, event.request;
+	local query = request.url.query;
+
+	if query ~= nil then
+        local params = formdecode(query);
+        session.auth_token = query and params.token or nil;
+
+        -- The room name and optional prefix from the bosh query
+        session.jitsi_bosh_query_room = params.room;
+        session.jitsi_bosh_query_prefix = params.prefix or "";
+    end
+end);
+
 function provider.test_password(username, password)
 	return nil, "Password based auth not supported";
 end
