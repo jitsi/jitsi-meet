35d242ede58d22288d03a51955b6382c9b234084
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-02-20 15:52:41 +0100

    rn,overflow-menu: add a "more options" button

--- react/features/toolbox/components/native/styles.js
+++ react/features/toolbox/components/native/styles.js
@@ -55,16 +55,6 @@ const whiteToolbarButtonIcon = {
  */
 const styles = {
 
-    expandMenuContainer: {
-        alignItems: 'center',
-        flexDirection: 'column'
-    },
-
-    sheetGestureRecognizer: {
-        alignItems: 'stretch',
-        flexDirection: 'column'
-    },
-
     /**
      * The style of the toolbar.
      */
