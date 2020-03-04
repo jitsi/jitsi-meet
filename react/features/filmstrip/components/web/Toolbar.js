24e4101e2b3fc944912ea4794480f2c9d19c5000
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-03-04 12:01:54 +0100

    fix(optimise): cope with URL interface config overrides
    Regresssion from bd8a7edbd2f9e3058b67d39fdc1c78156902f995.
    
    When the toolbar buttons are overridden with URL parameters, our computed set of
    buttons will be wrong. Thus, compute it every time and check for the
    differences.

--- react/features/filmstrip/components/web/Toolbar.js
+++ react/features/filmstrip/components/web/Toolbar.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 
-import { connect } from '../../../base/redux';
+import { connect, equals } from '../../../base/redux';
 import { SettingsButton } from '../../../settings';
 import {
     AudioMuteButton,
@@ -88,9 +88,13 @@ class Toolbar extends Component<Props> {
 function _mapStateToProps(state): Object { // eslint-disable-line no-unused-vars
     // XXX: We are not currently using state here, but in the future, when
     // interfaceConfig is part of redux we will.
+    //
+    // NB: We compute the buttons again here because if URL parameters were used to
+    // override them we'd miss it.
+    const buttons = new Set(interfaceConfig.TOOLBAR_BUTTONS);
 
     return {
-        _visibleButtons: visibleButtons
+        _visibleButtons: equals(visibleButtons, buttons) ? visibleButtons : buttons
     };
 }
 
