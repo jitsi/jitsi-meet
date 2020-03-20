38a534e446ed6a69b9fe9b21fdf860a50b3186f8
James E. Blair <corvus@inaugust.com>
2020-03-19 18:34:50 -0700

    Add option to open Etherpad on join
    For sites that focus on collaborative editing during meetings, add
    an option which, when set, will automatically open etherpad when a
    participant joins.

--- config.js
+++ config.js
@@ -635,6 +635,7 @@ var config = {
      disableRemoteControl
      displayJids
      etherpad_base
+     openSharedDocumentOnJoin
      externalConnectUrl
      firefox_fake_device
      googleApiApplicationClientID
--- modules/UI/UI.js
+++ modules/UI/UI.js
@@ -229,6 +229,10 @@ UI.initEtherpad = name => {
     const url = new URL(name, config.etherpad_base);
 
     APP.store.dispatch(setDocumentUrl(url.toString()));
+
+    if (config.openSharedDocumentOnJoin) {
+        etherpadManager.toggleEtherpad();
+    }
 };
 
 /**
