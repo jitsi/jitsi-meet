b6fcf80fdd9e387f123b8829b4bdd5f461ef549f
Ivan Jiang <>
2020-03-23 15:20:26 -0700

    switch "DEFAULT_SERVER_URL" to jwt server url for React native. and stop sending beacon temporarily.

--- react/features/base/settings/constants.js
+++ react/features/base/settings/constants.js
@@ -3,4 +3,4 @@
 /**
  * The default server URL to open if no other was specified.
  */
-export const DEFAULT_SERVER_URL = 'https://meet.jit.si';
+export const DEFAULT_SERVER_URL = 'https://videochat-jwt.jane.qa';
