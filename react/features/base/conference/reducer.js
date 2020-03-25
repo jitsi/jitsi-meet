768b53a3e419f505c16f0cf71c72b45cbef3cbf6
zain <zain@jane.app>
2020-03-25 09:34:52 -0700

    send started_at back to Jane

--- react/features/base/conference/reducer.js
+++ react/features/base/conference/reducer.js
@@ -307,7 +307,8 @@ function _conferenceLeftOrWillLeave(state, { conference, type }) {
 function _conferenceWillJoin(state, { conference }) {
     return assign(state, {
         error: undefined,
-        joining: conference
+        joining: conference,
+        start: new Date().toISOString(),
     });
 }
 
