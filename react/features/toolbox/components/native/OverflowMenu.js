50c2701e1dcb231cf671269619687d5a29346549
gerbus <christopher.x.gerber@gmail.com>
2020-03-27 09:36:48 -0700

    Remove options from in-call menu
    * Permanently hide the More Options button and show all options immediately (left in code in case we want it back down the road)
    * Remove "Add Meeting Password" button
    * Remove "Start Live Stream" button
    * Remove "Invite" button (doesn't show by default but removed just in case)
    * Remove "Meeting Info" button
    * Remove "Raise Hand" button

--- react/features/toolbox/components/native/OverflowMenu.js
+++ react/features/toolbox/components/native/OverflowMenu.js
@@ -1,32 +1,43 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import { Divider } from 'react-native-paper';
+
+// import { Divider } from 'react-native-paper';
 
 import { ColorSchemeRegistry } from '../../../base/color-scheme';
 import { BottomSheet, hideDialog, isDialogOpen } from '../../../base/dialog';
 import { connect } from '../../../base/redux';
 import { StyleType } from '../../../base/styles';
-import { SharedDocumentButton } from '../../../etherpad';
 import { AudioRouteButton } from '../../../mobile/audio-mode';
 import { ParticipantsPaneButton } from '../../../participants-pane/components/native';
 import { ReactionMenu } from '../../../reactions/components';
 import { isReactionsEnabled } from '../../../reactions/functions.any';
-import { LiveStreamButton, RecordButton } from '../../../recording';
-import SecurityDialogButton from '../../../security/components/security-dialog/SecurityDialogButton';
-import { SharedVideoButton } from '../../../shared-video/components';
-import { ClosedCaptionButton } from '../../../subtitles';
-import { TileViewButton } from '../../../video-layout';
-import styles from '../../../video-menu/components/native/styles';
 import { getMovableButtons } from '../../functions.native';
-import HelpButton from '../HelpButton';
-import MuteEveryoneButton from '../MuteEveryoneButton';
-import MuteEveryonesVideoButton from '../MuteEveryonesVideoButton';
+
+// import { SharedDocumentButton } from '../../../etherpad';
+// import { LiveStreamButton, RecordButton } from '../../../recording';
+// import SecurityDialogButton from '../../../security/components/security-dialog/SecurityDialogButton';
+// import { SharedVideoButton } from '../../../shared-video/components';
+// import { ClosedCaptionButton } from '../../../subtitles';
+// import { TileViewButton } from '../../../video-layout';
+// import styles from '../../../video-menu/components/native/styles';
+
+// import HelpButton from '../HelpButton';
+// import MuteEveryoneButton from '../MuteEveryoneButton';
+// import MuteEveryonesVideoButton from '../MuteEveryonesVideoButton';
 
 import AudioOnlyButton from './AudioOnlyButton';
-import RaiseHandButton from './RaiseHandButton';
-import ScreenSharingButton from './ScreenSharingButton.js';
-import ToggleCameraButton from './ToggleCameraButton';
+
+// import RaiseHandButton from './RaiseHandButton';
+// import ScreenSharingButton from './ScreenSharingButton.js';
+// import ToggleCameraButton from './ToggleCameraButton';
+
+// import { LobbyModeButton } from '../../../lobby/components/native';
+// import { LiveStreamButton, RecordButton } from '../../../recording';
+// import { RoomLockButton } from '../../../room-lock';
+// import { VideoShareButton } from '../../../youtube-player/components';
+// import RaiseHandButton from './RaiseHandButton';
+// import ToggleCameraButton from './ToggleCameraButton';
 
 /**
  * The type of the React {@code Component} props of {@link OverflowMenu}.
@@ -141,22 +152,23 @@ class OverflowMenu extends PureComponent<Props, State> {
                 <AudioRouteButton { ...topButtonProps } />
                 <ParticipantsPaneButton { ...buttonProps } />
                 <AudioOnlyButton { ...buttonProps } />
-                {!_reactionsEnabled && !toolbarButtons.has('raisehand') && <RaiseHandButton { ...buttonProps } />}
-                <Divider style = { styles.divider } />
-                <SecurityDialogButton { ...buttonProps } />
-                <RecordButton { ...buttonProps } />
-                <LiveStreamButton { ...buttonProps } />
-                <MuteEveryoneButton { ...buttonProps } />
-                <MuteEveryonesVideoButton { ...buttonProps } />
-                <Divider style = { styles.divider } />
-                <SharedVideoButton { ...buttonProps } />
-                <ScreenSharingButton { ...buttonProps } />
-                {!toolbarButtons.has('togglecamera') && <ToggleCameraButton { ...buttonProps } />}
-                {!toolbarButtons.has('tileview') && <TileViewButton { ...buttonProps } />}
-                <Divider style = { styles.divider } />
-                <ClosedCaptionButton { ...buttonProps } />
-                <SharedDocumentButton { ...buttonProps } />
-                <HelpButton { ...buttonProps } />
+                {/* eslint-disable-next-line max-len */}
+                {/* {!_reactionsEnabled && !toolbarButtons.has('raisehand') && <RaiseHandButton { ...buttonProps } />}*/}
+                {/* <Divider style = { styles.divider } />*/}
+                {/* <SecurityDialogButton { ...buttonProps } />*/}
+                {/* <RecordButton { ...buttonProps } />*/}
+                {/* <LiveStreamButton { ...buttonProps } />*/}
+                {/* <MuteEveryoneButton { ...buttonProps } />*/}
+                {/* <MuteEveryonesVideoButton { ...buttonProps } />*/}
+                {/* <Divider style = { styles.divider } />*/}
+                {/* <SharedVideoButton { ...buttonProps } />*/}
+                {/* <ScreenSharingButton { ...buttonProps } />*/}
+                {/* {!toolbarButtons.has('togglecamera') && <ToggleCameraButton { ...buttonProps } />}*/}
+                {/* {!toolbarButtons.has('tileview') && <TileViewButton { ...buttonProps } />}*/}
+                {/* <Divider style = { styles.divider } />*/}
+                {/* <ClosedCaptionButton { ...buttonProps } />*/}
+                {/* <SharedDocumentButton { ...buttonProps } />*/}
+                {/* <HelpButton { ...buttonProps } />*/}
             </BottomSheet>
         );
     }
