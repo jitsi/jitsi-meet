622d6b97f977cd0b8826aaea5cd6b2e6070e5a25
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-04-01 13:08:02 +0200

    rn,cc: add feature-flag to disable close captions

--- react/features/subtitles/components/ClosedCaptionButton.native.js
+++ react/features/subtitles/components/ClosedCaptionButton.native.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { getFeatureFlag, CLOSE_CAPTIONS_ENABLED } from '../../base/flags';
 import { translate } from '../../base/i18n';
 import { IconClosedCaption } from '../../base/icons';
 import { connect } from '../../base/redux';
@@ -20,5 +21,24 @@ class ClosedCaptionButton
     toggledLabel = 'transcribing.stop';
 }
 
-export default translate(connect(_abstractMapStateToProps)(
-    ClosedCaptionButton));
+/**
+ * Maps (parts of) the redux state to the associated props for this component.
+ *
+ * @param {Object} state - The redux state.
+ * @param {Object} ownProps - The properties explicitly passed to the component
+ * instance.
+ * @private
+ * @returns {Props}
+ */
+export function mapStateToProps(state: Object, ownProps: Object) {
+    const { transcribingEnabled } = state['features/base/config'];
+    const enabled = getFeatureFlag(state, CLOSE_CAPTIONS_ENABLED, true) && transcribingEnabled;
+    const { visible = enabled } = ownProps;
+
+    return {
+        ..._abstractMapStateToProps(state, ownProps),
+        visible
+    };
+}
+
+export default translate(connect(mapStateToProps)(ClosedCaptionButton));
