1ec5b9026145f21995929336319d1949cba45a72
Ivan Jiang <>
2020-04-03 11:18:22 -0700

    Add bugsnag to bundle and change the app store redirecting link url.

--- react/features/deep-linking/components/NoMobileApp.web.js
+++ react/features/deep-linking/components/NoMobileApp.web.js
@@ -21,6 +21,7 @@ export default class NoMobileApp extends Component<*> {
         sendAnalytics(
             createDeepLinkingPageEvent(
                 'displayed', 'noMobileApp', { isMobileBrowser: true }));
+        window.location = "https://apps.apple.com/us/app/jane-online-appointments/id1505867614?ls=1";
     }
 
     /**
@@ -33,13 +34,16 @@ export default class NoMobileApp extends Component<*> {
 
         return (
             <div className = { ns }>
-                <h2 className = { `${ns}__title` }>
-                    Video chat isn't available on mobile.
+                {/*<h2 className = { `${ns}__title` }>*/}
+                {/*    Video chat isn't available on mobile.*/}
+                {/*</h2>*/}
+                <h2>
+                    Redirecting to the app store…
                 </h2>
-                <p className = { `${ns}__description` }>
-                    Please use { interfaceConfig.NATIVE_APP_NAME } on desktop to
-                    join calls.
-                </p>
+                {/*<p className = { `${ns}__description` }>*/}
+                {/*    Please use { interfaceConfig.NATIVE_APP_NAME } on desktop to*/}
+                {/*    join calls.*/}
+                {/*</p>*/}
             </div>
         );
     }
