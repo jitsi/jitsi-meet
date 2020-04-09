4b3d4b9a30fa28ccca708156c6ad033e36e0410d
setpill <37372069+setpill@users.noreply.github.com>
2020-04-09 17:56:09 +0200

    Maximize usage of available space in tile view

--- react/features/video-layout/functions.js
+++ react/features/video-layout/functions.js
@@ -35,30 +35,49 @@ export function getMaxColumnCount() {
 }
 
 /**
- * Returns the cell count dimensions for tile view. Tile view tries to uphold
- * equal count of tiles for height and width, until maxColumn is reached in
- * which rows will be added but no more columns.
+ * Returns the cell count dimensions for tile view. Tile view tries to fill
+ * the available space optimally.
  *
  * @param {Object} state - The redux store state.
- * @param {number} maxColumns - The maximum number of columns that can be
- * displayed.
  * @returns {Object} An object is return with the desired number of columns,
  * rows, and visible rows (the rest should overflow) for the tile view layout.
  */
-export function getTileViewGridDimensions(state: Object, maxColumns: number = getMaxColumnCount()) {
+export function getTileViewGridDimensions(state: Object) {
     // When in tile view mode, we must discount ourselves (the local participant) because our
     // tile is not visible.
     const { iAmRecorder } = state['features/base/config'];
     const numberOfParticipants = state['features/base/participants'].length - (iAmRecorder ? 1 : 0);
+    const { clientHeight, clientWidth } = state['features/base/responsive-ui'];
+    const aspectRatio = state['features/filmstrip'].TILE_ASPECT_RATIO;
 
-    const columnsToMaintainASquare = Math.ceil(Math.sqrt(numberOfParticipants));
-    const columns = Math.min(columnsToMaintainASquare, maxColumns);
-    const rows = Math.ceil(numberOfParticipants / columns);
-    const visibleRows = Math.min(maxColumns, rows);
+    let bestColumns = 0;
+    let bestRows = 0;
+    let bestTileWidth = 0;
+    let bestFound = false;
+
+    while (!bestFound) {
+        const columns = bestColumns + 1;
+        const straightTileWidth = Math.floor(clientWidth / columns);
+
+        const rows = Math.ceil(numberOfParticipants / columns);
+        const straightTileHeight = Math.floor(clientHeight / rows);
+
+        const constrainedTileWidth = Math.ceil(straightTileHeight * aspectRatio);
+
+        const tileWidth = Math.min(straightTileWidth, constrainedTileWidth);
+
+        if (tileWidth > bestTileWidth) {
+            bestColumns = columns;
+            bestRows = rows;
+            bestTileWidth = tileWidth;
+        } else {
+            bestFound = true;
+        }
+    }
 
     return {
-        columns,
-        visibleRows
+        columns: bestColumns,
+        visibleRows: bestRows
     };
 }
 
