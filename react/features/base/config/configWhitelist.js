85fe1720ab14012cac141c66fdd3cf6add56ce22
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-04-11 10:10:16 +0200

    config: whitelist disableSimulcast

--- react/features/base/config/configWhitelist.js
+++ react/features/base/config/configWhitelist.js
@@ -93,6 +93,7 @@ export default [
     'disableRemoteControl',
     'disableRemoteMute',
     'disableRtx',
+    'disableSimulcast',
     'disableSuspendVideo',
     'disableThirdPartyRequests',
     'displayJids',
