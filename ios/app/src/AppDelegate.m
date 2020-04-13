248617960df86b909ae2673bb6d72ec371a01c3e
Ivan Jiang <>
2020-04-13 10:21:22 -0700

    update deeplink domains.

--- ios/app/src/AppDelegate.m
+++ ios/app/src/AppDelegate.m
@@ -41,7 +41,7 @@
 
     jitsiMeet.conferenceActivityType = JitsiMeetConferenceActivityType;
     jitsiMeet.customUrlScheme = @"janeoa";
-    jitsiMeet.universalLinkDomains = @[@"videochat-jwt.jane.qa",@"videochat.jane.qa",@"videochat-us.janeapp.com",@"videochat-ca.janeapp.com",@"videochat-au.janeapp.com",@"videochat-uk.janeapp.com"];
+    jitsiMeet.universalLinkDomains = @[@"videochat-jwt.jane.qa",@"videochat.jane.qa",@"videochat-us.janeapp.com",@"videochat-ca.janeapp.com",@"videochat.janeapp.com.au",@"videochat.janeapp.co.uk"];
 
     jitsiMeet.defaultConferenceOptions = [JitsiMeetConferenceOptions fromBuilder:^(JitsiMeetConferenceOptionsBuilder *builder) {
         builder.serverURL = [NSURL URLWithString:@"https://videochat.jane.qa"];
