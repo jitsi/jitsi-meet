925cc2f474507b776c8ba063a7007aab9c12cfd7
Ivan Jiang <>
2020-04-13 16:50:32 -0700

    update RefreshButton component
    RefreshButton will only show if there is only one participant and no remote tracks from RTC.

--- react/features/conference/components/web/RefreshButton.js
+++ react/features/conference/components/web/RefreshButton.js
@@ -1,17 +1,27 @@
 /* @flow */
 
 import React, {Component} from 'react';
+import {getParticipantCount} from '../../../base/participants';
+import {connect} from '../../../base/redux';
+import {getRemoteTracks} from '../../../base/tracks';
+import {isToolboxVisible} from '../../../toolbox';
 
 /**
  * The type of the React {@code Component} props of {@link Subject}.
  */
+type Props = {
+    /**
+     * Whether the component should be hide.
+     */
+    hide: boolean,
+};
 
 /**
  * RefreshButton react component.
  *
  * @class RefreshButton
  */
-export default class RefreshButton extends Component {
+class RefreshButton extends Component<Props> {
 
     constructor(props) {
         super(props);
@@ -23,14 +33,17 @@ export default class RefreshButton extends Component {
     }
 
     render() {
+        if (this.props.hide) {
+            return null;
+        }
         return (
             <div className={`refreshButtonWrapper`}>
                 <div className='refreshButtonContainer'>
-                    <div className={`refreshButton `}>
+                    <div className={`refreshButton`}
+                         style={{cursor: 'pointer'}}
+                         onClick={this.reload}>
                 <span>
-                    Trouble Connecting? <strong
-                    style={{cursor: 'pointer'}}
-                    onClick={this.reload}>Re-connect</strong>
+                    Trouble Connecting? <strong>Reconnect</strong>
                 </span>
                     </div>
                 </div>
@@ -38,3 +51,14 @@ export default class RefreshButton extends Component {
         );
     }
 }
+
+function _mapStateToProps(state) {
+    const participantCount = getParticipantCount(state);
+    const remoteTracks = getRemoteTracks(state['features/base/tracks']);
+    const toolBoxVisible = isToolboxVisible(state);
+    return {
+        hide: participantCount > 1 && remoteTracks.length > 0
+    };
+}
+
+export default connect(_mapStateToProps)(RefreshButton);
