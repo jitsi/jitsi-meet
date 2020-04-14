445fa6327f6946af6cdfe4bb0c1a548314371b5a
adrmzz <adrmzz@users.noreply.github.com>
2020-04-14 11:37:07 +0200

    Update BuiltinLanguages.native.js
    adding Sardinian

--- react/features/base/i18n/BuiltinLanguages.native.js
+++ react/features/base/i18n/BuiltinLanguages.native.js
@@ -127,6 +127,12 @@ const _LANGUAGES = {
         main: require('../../../../lang/main-ptBR')
     },
 
+    // Sardinian (Sardinia)
+    'sc': {
+        languages: require('../../../../lang/languages-sc'),
+        main: require('../../../../lang/main-sc')
+    },
+    
     // Russian
     'ru': {
         languages: require('../../../../lang/languages-ru'),
