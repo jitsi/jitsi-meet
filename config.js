49a9c77442c2bcc2bf574275beb91a0027f97ec7
anshad <anshad.musafir@gmail.com>
2020-04-21 17:30:13 +0530

    Update config

--- config.js
+++ config.js
@@ -189,14 +189,15 @@ var config = {
     // by enabling fileRecordingsServiceEnabled, we show both the integrations
     // and the generic recording service (its configuration and storage type
     // depends on jibri configuration)
-    // fileRecordingsServiceEnabled: false,
+    fileRecordingsServiceEnabled: true,
     // Whether to show the possibility to share file recording with other people
     // (e.g. meeting participants), based on the actual implementation
     // on the backend.
     // fileRecordingsServiceSharingEnabled: false,
 
     // Whether to enable live streaming or not.
-    // liveStreamingEnabled: false,
+    liveStreamingEnabled: true,
+    hiddenDomain: "recorder.meet.roundesk.io",
 
     // Transcription (in interface_config,
     // subtitles and buttons can be configured)
