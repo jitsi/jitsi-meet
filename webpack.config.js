0355e454f99c6bf11a13908c19e8e0c63b328283
Ivan Jiang <>
2020-05-21 08:49:38 -0700

    update index.html & webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ const minimize
  */
 function getPerformanceHints(size) {
     return {
-        hints: minimize ? 'error' : false,
+        // hints: minimize ? 'error' : false,
+        hints: minimize ? 'warning' : false,
         maxAssetSize: size,
         maxEntrypointSize: size
     };
@@ -163,7 +164,7 @@ const config = {
             appBundle: `libs/app.bundle.min.js?v=${cacheVersionNumber}`,
             css: `css/all.css?v=${cacheVersionNumber}`,
             template: 'index.html',
-            minify: true,
+            minify: false,
             inject: false,
         })
     ].filter(Boolean),
