e0e574285081253ed0f54d9cb21ea58768541f3e
Ivan Jiang <>
2020-05-21 08:49:38 -0700

    update index.html & webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -194,7 +194,7 @@ const config = {
             appBundle: `libs/app.bundle.min.js?v=${cacheVersionNumber}`,
             css: `css/all.css?v=${cacheVersionNumber}`,
             template: 'index.html',
-            minify: true,
+            minify: false,
             inject: false
         })
     ].filter(Boolean),
