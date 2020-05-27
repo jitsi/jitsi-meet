e8404dd101a87d33deaec28b3aa4a9f344fb0dd6
damencho <damencho@jitsi.org>
2020-05-27 09:49:36 -0500

    fix: Fixes using is_healthcheck_room.

--- resources/prosody-plugins/mod_muc_lobby_rooms.lua
+++ resources/prosody-plugins/mod_muc_lobby_rooms.lua
@@ -208,7 +208,7 @@ process_host_module(main_muc_component_config, function(host_module, host)
     host_module:hook('muc-occupant-pre-join', function (event)
         local room, stanza = event.room, event.stanza;
 
-        if is_healthcheck_room(room) or not room:get_members_only() then
+        if is_healthcheck_room(room.jid) or not room:get_members_only() then
             return;
         end
 
