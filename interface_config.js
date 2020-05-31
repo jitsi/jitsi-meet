d3aa3ec1a5dd2ec6e5998d11a4263c9f9ebc28b2
Marshall Rose <mrose17@gmail.com>
2020-05-31 12:17:14 -0500

    Update interface_config.js
    disable recording and live-streaming...

--- interface_config.js
+++ interface_config.js
@@ -190,10 +190,12 @@ var interfaceConfig = {
      */
     TOOLBAR_BUTTONS: [
         'microphone', 'camera', 'closedcaptions', 'desktop', 'fullscreen',
-        'fodeviceselection', 'hangup', 'profile', 'chat', 'recording',
+        'fodeviceselection', 'hangup', 'profile', 'chat', 
+//                                                        'recording',
         'livestreaming', 'etherpad', 'sharedvideo', 'settings', 'raisehand',
         'videoquality', 'filmstrip', 'invite', 'feedback', 'stats', 'shortcuts',
-        'tileview', 'videobackgroundblur', 'download', 'help', 'mute-everyone', 'security'
+        'tileview',                        'download', 'help', 'mute-everyone', 'security'
+//                  'videobackgroundblur',
     ],
 
     TOOLBAR_TIMEOUT: 4000,
