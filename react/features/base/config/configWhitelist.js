a49f04f25b753bd9b27bd86d95ac4aff90504327
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-06-11 10:16:26 +0200

    config: document enableInsecureRoomNameWarning and whitelist it

--- react/features/base/config/configWhitelist.js
+++ react/features/base/config/configWhitelist.js
@@ -102,6 +102,7 @@ export default [
     'enableDisplayNameInStats',
     'enableEmailInStats',
     'enableIceRestart',
+    'enableInsecureRoomNameWarning',
     'enableLayerSuspension',
     'enableLipSync',
     'enableRemb',
