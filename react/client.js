908658b9e300e0992ee616d26b2f35c18c11f271
Yury Shapkarin <yury@shapkarin.me>
2020-06-19 07:40:02 +0300

    rename RpcAepp

--- react/client.js
+++ react/client.js
@@ -9,7 +9,7 @@ export const initClient = async () => {
 
     // eslint-disable-next-line new-cap
     client = await RpcAepp({
-        name: 'Superhero',
+        name: 'Superhero-league',
         nodes: [ {
             name: 'mainnet',
             instance: await Node({
