87b14c37119b689694de77a9de9e9b2efed5a40b
Vlad Piersec <vlad.piersec@8x8.com>
2020-06-24 10:34:00 +0300

    fix(prejoin): copy meeting info

--- react/features/invite/components/add-people-dialog/web/LiveStreamSection.js
+++ react/features/invite/components/add-people-dialog/web/LiveStreamSection.js
@@ -4,8 +4,7 @@ import React, { useState } from 'react';
 
 import { translate } from '../../../../base/i18n';
 import { Icon, IconCheck, IconCopy } from '../../../../base/icons';
-
-import { copyText } from './utils';
+import { copyText } from '../../../../base/util';
 
 type Props = {
 
