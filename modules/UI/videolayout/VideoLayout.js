30b262ba63319218003e593da2f68e19ed894ddc
aripjanov.sh <Aripjanov.sh@gmail.com>
2020-06-25 16:48:49 +0500

    sorting logs

--- modules/UI/videolayout/VideoLayout.js
+++ modules/UI/videolayout/VideoLayout.js
@@ -74,6 +74,8 @@ function getSortedParticipants() {
         const isModerator = Boolean(participant && participant.role === "moderator");
         const isVideoMuted = participantThumb.isVideoMuted;
 
+        logger.info(`Sort issue 77`, {isModerator, isVideoMuted, participant});
+
         if (isModerator) {
             moderators.push(participant);
         } else {
@@ -92,9 +94,11 @@ function getSortedParticipants() {
             sortedParticipants.push(participant);
         }
     }
-
+    logger.info(`Sort issue 95`, {sortedParticipants});
     sortedParticipants = _.sortBy(sortedParticipants, "sortWeight");
     otherParticipant = sortedParticipants.shift();
+
+    logger.info(`Sort issue 101`, {sortedParticipants, otherParticipant});
 
     return [otherParticipant, ...moderators, ...sortedParticipants]
             .filter(Boolean);
@@ -857,6 +861,8 @@ const VideoLayout = {
 
     sortParticipants() {
         const sortedParticipants = getSortedParticipants();
+        logger.log('Sort issue 864', {sortedParticipants})
+
         $("#localVideoTileViewContainer").css({order: -1})
         if(!_.isEmpty(sortedParticipants)) {
             _.map(sortedParticipants, (participant, orderKey) => {
