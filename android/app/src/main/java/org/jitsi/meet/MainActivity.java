4b1743bb2f7723b98cb63e580c4e38ebef305861
Roman <romanhawk@gmail.com>
2020-06-26 12:47:48 +0300

    android: add serverURL configuration for MDM/EMM environments
    Android for Enterprise provides special feature for applications to obtain configuration through RestrictionManager remotely by some MDM solution.
    
    Jitsi Meet can be remotely installed and provisioned with a proper URL (making URL not editable by the user) inside the Work Profile or Fully managed device.

--- android/app/src/main/java/org/jitsi/meet/MainActivity.java
+++ android/app/src/main/java/org/jitsi/meet/MainActivity.java
@@ -16,9 +16,15 @@
 
 package org.jitsi.meet;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.RestrictionEntry;
+import android.content.RestrictionsManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -31,6 +37,7 @@ import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.Map;
 
 /**
@@ -47,6 +54,27 @@ public class MainActivity extends JitsiMeetActivity {
      */
     private static final int OVERLAY_PERMISSION_REQUEST_CODE
         = (int) (Math.random() * Short.MAX_VALUE);
+
+    /**
+     * ServerURL configuration key for restriction configuration using {@link android.content.RestrictionsManager}
+     */
+    public static final String RESTRICTION_SERVER_URL = "SERVER_URL";
+
+    /**
+     * Broadcast receiver for restrictions handling
+     */
+    private BroadcastReceiver broadcastReceiver;
+
+    /**
+     * Flag if configuration is provided by RestrictionManager
+     */
+    private boolean configurationByRestrictions = false;
+
+    /**
+     * Default URL as could be obtained from RestrictionManager
+     */
+    private String defaultURL;
+
 
     // JitsiMeetActivity overrides
     //
@@ -85,16 +113,66 @@ public class MainActivity extends JitsiMeetActivity {
 
     @Override
     protected void initialize() {
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // As new restrictions including server URL are received,
+                // conference should be restarted with new configuration.
+                leave();
+                recreate();
+            }
+        };
+        registerReceiver(broadcastReceiver,
+            new IntentFilter(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED));
+
+        resolveRestrictions();
+        setJitsiMeetConferenceDefaultOptions();
+        super.initialize();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (broadcastReceiver != null) {
+            unregisterReceiver(broadcastReceiver);
+            broadcastReceiver = null;
+        }
+
+        super.onDestroy();
+    }
+
+    private void setJitsiMeetConferenceDefaultOptions() {
         // Set default options
         JitsiMeetConferenceOptions defaultOptions
             = new JitsiMeetConferenceOptions.Builder()
-                .setWelcomePageEnabled(true)
-                .setServerURL(buildURL("https://meet.jit.si"))
-                .setFeatureFlag("call-integration.enabled", false)
-                .build();
+            .setWelcomePageEnabled(true)
+            .setServerURL(buildURL(defaultURL))
+            .setFeatureFlag("call-integration.enabled", false)
+            .setFeatureFlag("server-url-change.enabled", !configurationByRestrictions)
+            .build();
         JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+    }
 
-        super.initialize();
+    private void resolveRestrictions() {
+        RestrictionsManager manager =
+            (RestrictionsManager) getSystemService(Context.RESTRICTIONS_SERVICE);
+        Bundle restrictions = manager.getApplicationRestrictions();
+        Collection<RestrictionEntry> entries = manager.getManifestRestrictions(
+            getApplicationContext().getPackageName());
+        for (RestrictionEntry restrictionEntry : entries) {
+            String key = restrictionEntry.getKey();
+            if (RESTRICTION_SERVER_URL.equals(key)) {
+                // If restrictions are passed to the application.
+                if (restrictions != null &&
+                    restrictions.containsKey(RESTRICTION_SERVER_URL)) {
+                    defaultURL = restrictions.getString(RESTRICTION_SERVER_URL);
+                    configurationByRestrictions = true;
+                // Otherwise use default URL from app-restrictions.xml.
+                } else {
+                    defaultURL = restrictionEntry.getSelectedString();
+                    configurationByRestrictions = false;
+                }
+            }
+        }
     }
 
     @Override
--- react/features/settings/functions.js
+++ react/features/settings/functions.js
@@ -1,4 +1,6 @@
 // @flow
+
+import { SERVER_URL_CHANGE_ENABLED, getFeatureFlag } from '../base/flags';
 import { i18next, DEFAULT_LANGUAGE, LANGUAGES } from '../base/i18n';
 import { createLocalTrack } from '../base/lib-jitsi-meet/functions';
 import {
@@ -21,6 +23,20 @@ declare var interfaceConfig: Object;
  */
 export function isSettingEnabled(settingName: string) {
     return interfaceConfig.SETTINGS_SECTIONS.includes(settingName);
+}
+
+/**
+ * Returns true if user is allowed to change Server URL.
+ *
+ * @param {(Function|Object)} stateful - The (whole) redux state, or redux's
+ * {@code getState} function to be used to retrieve the state.
+ * @returns {boolean} True to indicate that user can change Server URL, false otherwise.
+ */
+export function isServerURLChangeEnabled(stateful: Object | Function) {
+    const state = toState(stateful);
+    const flag = getFeatureFlag(state, SERVER_URL_CHANGE_ENABLED, true);
+
+    return flag;
 }
 
 /**
