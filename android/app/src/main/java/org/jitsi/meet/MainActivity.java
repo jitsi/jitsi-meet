f32140c4b7f4bc9654067cb68882118bbdfae952
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-07-07 22:35:39 +0200

    rn: set default resolution to 360p (experiment)

--- android/app/src/main/java/org/jitsi/meet/MainActivity.java
+++ android/app/src/main/java/org/jitsi/meet/MainActivity.java
@@ -147,6 +147,7 @@ public class MainActivity extends JitsiMeetActivity {
             .setWelcomePageEnabled(true)
             .setServerURL(buildURL(defaultURL))
             .setFeatureFlag("call-integration.enabled", false)
+            .setFeatureFlag("resolution", 360)
             .setFeatureFlag("server-url-change.enabled", !configurationByRestrictions)
             .build();
         JitsiMeet.setDefaultConferenceOptions(defaultOptions);
