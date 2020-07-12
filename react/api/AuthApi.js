c0b8d08da707bb20a3b682ffa8c013051e2ec192
오수경 <osugyeong@osugyeong-ui-MacBookPro.local>
2020-07-13 03:51:54 +0900

    Add api functions, add assets and change editorConfig

---
+++ react/api/AuthApi.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const apiRoot = process.env.REACT_APP_API_BASE;
+
+export function fetchCurrentUser() {
+  return axios.get(`${apiRoot}/current-user`, { withCredentials: true });
+}
+
+export function login(form) {
+  return axios.post(`${apiRoot}/login`, form);
+}
+
+export function logout() {
+  return axios.get(`${apiRoot}/logout`);
+}
+
+export function signup(form) {
+  return axios.post(`${apiRoot}/signup`, form);
+}
+
+export function passwordReset(form) {
+  return axios.post(`${apiRoot}/password-reset`, form);
+}
+
+export function updateAccount(form) {
+  return axios.patch(`${apiRoot}/account`, form);
+}
+
+export function passwordResetConfirm(form) {
+  return axios.post(`${apiRoot}/password-reset-confirm`, form);
+}
+
+export function updatePassword(form) {
+  return axios.patch(`${apiRoot}/password-reset-confirm`, form);
+}
---
+++ react/assets/index.js
@@ -0,0 +1,2 @@
+export const postech_logo_mini = require("./pologo.png");
+export const postech_logo = require("./postech_logo.png")
