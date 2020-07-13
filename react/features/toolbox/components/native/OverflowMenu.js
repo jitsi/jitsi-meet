79a13ac8fff5263771a21688371c7c868c233584
Ali Karpuzoglu <talik01@gmail.com>
2020-07-13 12:27:06 +0200

    Missing files for the kick everyone else button

--- react/features/toolbox/components/native/OverflowMenu.js
+++ react/features/toolbox/components/native/OverflowMenu.js
@@ -26,6 +26,7 @@ import RaiseHandButton from './RaiseHandButton';
 import ToggleCameraButton from './ToggleCameraButton';
 import styles from './styles';
 import MuteEveryoneElseButton from './MuteEveryoneElseButton';
+import KickEveryoneElseButton from './KickEveryoneElseButton';
 
 import { jitsiLocalStorage } from 'js-utils';
 
@@ -180,7 +181,10 @@ class OverflowMenu extends PureComponent<Props, State> {
 
         if(isModerator){
             return(
+                <View>
                 <MuteEveryoneElseButton { ...buttonProps } />
+                <KickEveryoneElseButton { ...buttonProps } />
+                </View>
             );
         }
         return
