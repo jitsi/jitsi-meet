e39c8d8ed61e31ee65b397826b1f349ec01e64fa
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-07-16 17:41:15 +0200

    rn,tile-view: render a larger avatar in tile view mode

--- react/features/filmstrip/components/native/Thumbnail.js
+++ react/features/filmstrip/components/native/Thumbnail.js
@@ -149,7 +149,7 @@ function Thumbnail(props: Props) {
             touchFeedback = { false }>
 
             <ParticipantView
-                avatarSize = { AVATAR_SIZE }
+                avatarSize = { tileView ? AVATAR_SIZE * 1.5 : AVATAR_SIZE }
                 disableVideo = { isScreenShare || participant.isFakeParticipant }
                 participantId = { participantId }
                 style = { _styles.participantViewStyle }
