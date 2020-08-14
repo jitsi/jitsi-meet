8f2a083ec5f100868dc01fdc3c4dce9463f17c1d
Aaron van Meerten <aaron.van.meerten@gmail.com>
2020-08-14 16:01:36 -0500

    FEAT: filter iq jibri supports audience/issuers FIX: reload configure hook

--- resources/prosody-plugins/mod_filter_iq_jibri.lua
+++ resources/prosody-plugins/mod_filter_iq_jibri.lua
@@ -2,7 +2,31 @@ local st = require "util.stanza";
 local is_feature_allowed = module:require "util".is_feature_allowed;
 local token_util = module:require "token/util".new(module);
 
-local accepted_rayo_iq_token_issuers = module:get_option_array("accepted_rayo_iq_token_issuers");
+local jibri_iq_token_accepted_issuers;
+local jibri_iq_token_accepted_audiences;
+local jibri_iq_token_asap_key_server;
+
+local function load_config()
+    jibri_iq_token_accepted_issuers = module:get_option_array("jibri_iq_token_accepted_issuers");
+    token_util:set_asap_accepted_issuers(jibri_iq_token_accepted_issuers);
+
+    jibri_iq_token_accepted_audiences = module:get_option_array("jibri_iq_token_accepted_audiences", {"jitsi"});
+    token_util:set_asap_accepted_audiences(jibri_iq_token_accepted_audiences);
+
+    jibri_iq_token_asap_key_server = module:get_option_string("jibri_iq_token_asap_key_server");
+    if jibri_iq_token_asap_key_server then
+        token_util:set_asap_key_server(jibri_iq_token_asap_key_server);
+    end
+end
+
+local function reload_config()
+    load_config();
+
+    -- clear ASAP accepted key cache on reload
+    token_util:clear_asap_cache();
+end
+
+load_config();
 
 -- filters jibri iq in case of requested from jwt authenticated session that
 -- has features in the user context, but without feature for recording
@@ -16,13 +40,12 @@ module:hook("pre-iq/full", function(event)
 
             if jibri.attr.action == 'start' then
                 local errorReason;
-                if accepted_rayo_iq_token_issuers then
+                if jibri_iq_token_accepted_issuers then
                     local iq_token = jibri.attr.token;
                     if iq_token then
                         local session = {};
                         session.auth_token = iq_token;
-                        local verified, reason = token_util:process_and_verify_token(
-                            session, accepted_rayo_iq_token_issuers);
+                        local verified, reason = token_util:process_and_verify_token(session);
                         if verified then
                             return nil; -- this will proceed with dispatching the stanza
                         end
@@ -49,3 +72,5 @@ module:hook("pre-iq/full", function(event)
         end
     end
 end);
+
+module:hook_global('config-reloaded', reload_config);
