c4e66e5a4c96e97426353d013a187a5b246ba5e9
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-09-09 09:28:48 +0200

    config: fix missing comma
    Fixes: https://github.com/jitsi/jitsi-meet/issues/7665

--- config.js
+++ config.js
@@ -128,7 +128,7 @@ var config = {
 
     // How many participants while in the tile view mode, before the receiving video quality is reduced from HD to SD.
     // Use -1 to disable.
-    // maxFullResolutionParticipants: 2
+    // maxFullResolutionParticipants: 2,
 
     // w3c spec-compliant video constraints to use for video capture. Currently
     // used by browsers that return true from lib-jitsi-meet's
