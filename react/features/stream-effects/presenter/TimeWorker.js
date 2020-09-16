2e63313c5ef1c799b08a99138cd6ca88e646f4ce
jbis9051 <josh9051@gmail.com>
2020-09-16 16:19:33 -0400

    Oops

--- react/features/stream-effects/presenter/TimeWorker.js
+++ react/features/stream-effects/presenter/TimeWorker.js
@@ -1,33 +1,33 @@
 // @flow
 
 /**
- * SET_TIMEOUT constant is used to set interval and it is set in
+ * SET_INTERVAL constant is used to set interval and it is set in
  * the id property of the request.data property. timeMs property must
  * also be set. request.data example:
  *
  * {
- *      id: SET_TIMEOUT,
+ *      id: SET_INTERVAL,
  *      timeMs: 33
  * }
  */
 export const SET_INTERVAL = 1;
 
 /**
- * CLEAR_TIMEOUT constant is used to clear the interval and it is set in
+ * CLEAR_INTERVAL constant is used to clear the interval and it is set in
  * the id property of the request.data property.
  *
  * {
- *      id: CLEAR_TIMEOUT
+ *      id: CLEAR_INTERVAL
  * }
  */
 export const CLEAR_INTERVAL = 2;
 
 /**
- * TIMEOUT_TICK constant is used as response and it is set in the id
+ * INTERVAL_TIMEOUT constant is used as response and it is set in the id
  * property.
  *
  * {
- *      id: TIMEOUT_TICK
+ *      id: INTERVAL_TIMEOUT
  * }
  */
 export const INTERVAL_TIMEOUT = 3;
