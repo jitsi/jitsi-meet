1010d50846b4a7d677372626edf02122000b1441
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2020-09-16 19:16:32 -0400

    feat(iFrame): Add a method for resizing large video container from iFrame

--- modules/UI/videolayout/LargeVideoManager.js
+++ modules/UI/videolayout/LargeVideoManager.js
@@ -21,7 +21,6 @@ import { PresenceLabel } from '../../../react/features/presence-status';
 import UIEvents from '../../../service/UI/UIEvents';
 import { createDeferred } from '../../util/helpers';
 import AudioLevels from '../audio_levels/AudioLevels';
-import UIUtil from '../util/UIUtil';
 
 import { VideoContainer, VIDEO_CONTAINER_TYPE } from './VideoContainer';
 
@@ -323,20 +322,25 @@ export default class LargeVideoManager {
     /**
      * Update container size.
      */
-    updateContainerSize() {
-        let widthToUse = UIUtil.getAvailableVideoWidth();
+    updateContainerSize(width, height) {
+        let widthToUse = width ?? (this.width > 0 ? this.width : window.innerWidth);
         const { isOpen } = APP.store.getState()['features/chat'];
 
+        /**
+         * If chat state is open, we re-compute the container width by subtracting the default width of
+         * the chat. We re-compute the width again after the chat window is closed. This is needed when
+         * custom styling is configured on the large video container through the iFrame API.
+         */
         if (isOpen) {
-            /**
-             * If chat state is open, we re-compute the container width
-             * by subtracting the default width of the chat.
-             */
             widthToUse -= CHAT_SIZE;
+            this.resizedForChat = true;
+        } else if (this.resizedForChat) {
+            this.resizedForChat = false;
+            widthToUse += CHAT_SIZE;
         }
 
         this.width = widthToUse;
-        this.height = window.innerHeight;
+        this.height = height ?? (this.height > 0 ? this.height : window.innerHeight);
     }
 
     /**
