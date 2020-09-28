f53ea3579025e061126c2ba5148252d62c807536
George Politis <gp@jitsi.org>
2020-09-28 11:05:32 +0200

    wip: Removes bogus comment.

--- react/features/connection-indicator/actions.js
+++ react/features/connection-indicator/actions.js
@@ -9,8 +9,7 @@ import { downloadJSON } from '../base/util/downloadJSON';
  */
 export function saveLogs() {
     return (dispatch, getState) => {
-        // this can be called from console and will not have reference to this
-        // that's why we reference the global var
+
         const logs = getState()['features/base/connection'].connection.getLogs();
         const roomName = getRoomName() || '';
 
