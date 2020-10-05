5e2ee3bdcd48f3fff14d50e1bc0c6b40a93648ec
Mihai Uscat <mihai.uscat@8x8.com>
2020-10-05 14:23:13 +0300

    fix: Show focus indicator only when navigating via keyboard

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import 'jQuery-Impromptu';
 
 import 'olm';
 
+import 'focus-visible';
+
 // We need to setup the jitsi-local-storage as early as possible so that we can start using it.
 // NOTE: If jitsi-local-storage is used before the initial setup is performed this will break the use case when we use
 // the  local storage from the parent page when the localStorage is disabled. Also the setup is relying that
