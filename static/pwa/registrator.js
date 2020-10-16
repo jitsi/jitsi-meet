d854b2cd3d62647c32324eb0ca472dcab09fd217
Tudor-Ovidiu Avram <tudor.potecaru@8x8.com>
2020-10-16 12:04:53 +0300

    fix(pwa) remove window.load event handler for pwa registrator

--- static/pwa/registrator.js
+++ static/pwa/registrator.js
@@ -1,12 +1,10 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-      .register('pwa-worker.js')
-      .then(reg => {
-          console.log('Service worker registered.', reg);
-      })
-      .catch(err => {
-          console.log(err);
-      });
-    });
+    navigator.serviceWorker
+        .register('pwa-worker.js')
+        .then(reg => {
+            console.log('Service worker registered.', reg);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
