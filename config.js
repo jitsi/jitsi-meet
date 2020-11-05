9ecdc27010cfa98e91d566e312453e9d8b587c91
damencho <damencho@jitsi.org>
2020-11-05 14:30:06 -0600

    feat: Drop enableUserRolesBasedOnToken and isGuest.

--- config.js
+++ config.js
@@ -362,11 +362,6 @@ var config = {
     // Disables profile and the edit of all fields from the profile settings (display name and email)
     // disableProfile: false,
 
-    // If true all users without a token will be considered guests and all users
-    // with token will be considered non-guests. Only guests will be allowed to
-    // edit their profile.
-    enableUserRolesBasedOnToken: false,
-
     // Whether or not some features are checked based on token.
     // enableFeaturesBasedOnToken: false,
 
