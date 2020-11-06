8577369558c166d82af3a37c55d1a515c8f31334
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-11-06 10:11:59 +0100

    feat(BrowserCapabilities) drop supportsVideo
    It has been `true` for a very long time.

--- react/features/video-quality/components/VideoQualitySlider.web.js
+++ react/features/video-quality/components/VideoQualitySlider.web.js
@@ -1,13 +1,11 @@
 // @flow
 
-import InlineMessage from '@atlaskit/inline-message';
 import React, { Component } from 'react';
 import type { Dispatch } from 'redux';
 
 import { createToolbarEvent, sendAnalytics } from '../../analytics';
 import { setAudioOnly } from '../../base/audio-only';
 import { translate } from '../../base/i18n';
-import JitsiMeetJS from '../../base/lib-jitsi-meet';
 import { connect } from '../../base/redux';
 import { setPreferredVideoQuality } from '../actions';
 import { VIDEO_QUALITY_LEVELS } from '../constants';
@@ -56,12 +54,6 @@ type Props = {
      * received from the remote participants.
      */
     _sendrecvVideoQuality: Number,
-
-    /**
-     * Whether or not displaying video is supported in the current
-     * environment. If false, the slider will be disabled.
-     */
-    _videoSupported: Boolean,
 
     /**
      * Invoked to request toggling of audio only mode.
@@ -141,25 +133,14 @@ class VideoQualitySlider extends Component<Props> {
      * @returns {ReactElement}
      */
     render() {
-        const { _videoSupported, t } = this.props;
+        const { t } = this.props;
         const activeSliderOption = this._mapCurrentQualityToSliderValue();
 
-        let classNames = 'video-quality-dialog';
-        let warning = null;
-
-        if (!_videoSupported) {
-            classNames += ' video-not-supported';
-            warning = this._renderAudioOnlyLockedMessage();
-        }
-
         return (
-            <div className = { classNames }>
+            <div className = { 'video-quality-dialog' }>
                 <h3 className = 'video-quality-dialog-title'>
                     { t('videoStatus.callQuality') }
                 </h3>
-                <div className = { warning ? '' : 'hide-warning' }>
-                    { warning }
-                </div>
                 <div className = 'video-quality-dialog-contents'>
                     <div className = 'video-quality-dialog-slider-container'>
                         { /* FIXME: onChange and onMouseUp are both used for
@@ -168,7 +149,6 @@ class VideoQualitySlider extends Component<Props> {
                            */ }
                         <input
                             className = 'video-quality-dialog-slider'
-                            disabled = { !_videoSupported }
                             max = { this._sliderOptions.length - 1 }
                             min = '0'
                             onChange = { this._onSliderChange }
@@ -184,24 +164,6 @@ class VideoQualitySlider extends Component<Props> {
                     </div>
                 </div>
             </div>
-        );
-    }
-
-    /**
-     * Creates a React Element for notifying that the browser is in audio only
-     * and cannot be changed.
-     *
-     * @private
-     * @returns {ReactElement}
-     */
-    _renderAudioOnlyLockedMessage() {
-        const { t } = this.props;
-
-        return (
-            <InlineMessage
-                title = { t('videoStatus.onlyAudioAvailable') }>
-                { t('videoStatus.onlyAudioSupported') }
-            </InlineMessage>
         );
     }
 
@@ -393,11 +355,7 @@ class VideoQualitySlider extends Component<Props> {
  *
  * @param {Object} state - The Redux state.
  * @private
- * @returns {{
- *     _audioOnly: boolean,
- *     _p2p: boolean,
- *     _sendrecvVideoQuality: number
- * }}
+ * @returns {Props}
  */
 function _mapStateToProps(state) {
     const { enabled: audioOnly } = state['features/base/audio-only'];
@@ -407,8 +365,7 @@ function _mapStateToProps(state) {
     return {
         _audioOnly: audioOnly,
         _p2p: p2p,
-        _sendrecvVideoQuality: preferredVideoQuality,
-        _videoSupported: JitsiMeetJS.mediaDevices.supportsVideo()
+        _sendrecvVideoQuality: preferredVideoQuality
     };
 }
 
