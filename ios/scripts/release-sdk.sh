a3e886f2de90b408154ff8d21f4b9525aef02284
Saúl Ibarra Corretgé <saghul@jitsi.org>
2020-11-17 14:42:05 +0100

    fix(ios) try to make BUILD_LIBRARY_FOR_DISTRIBUTION work
    Configuring it in the Xcode project doesn't seem to work.

--- ios/scripts/release-sdk.sh
+++ ios/scripts/release-sdk.sh
@@ -25,7 +25,14 @@ popd
 # Build the SDK
 pushd ${PROJECT_REPO}
 rm -rf ios/sdk/JitsiMeet.framework
-xcodebuild -workspace ios/jitsi-meet.xcworkspace -scheme JitsiMeet -destination='generic/platform=iOS' -configuration Release ENABLE_BITCODE=NO clean archive
+xcodebuild \
+    -workspace ios/jitsi-meet.xcworkspace \
+    -scheme JitsiMeet \
+    -destination='generic/platform=iOS' \
+    -configuration Release \
+    ENABLE_BITCODE=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+    clean archive
 if [[ $DO_GIT_TAG == 1 ]]; then
     git tag ios-sdk-${SDK_VERSION}
 fi
