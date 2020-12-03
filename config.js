fbbd568c322d27a027b12ed248eec7f343872096
amatic <amatic@nanocosmos.de>
2020-12-03 13:38:29 +0100

    Update.

--- config.js
+++ config.js
@@ -488,7 +488,7 @@ var config = {
 
     // If true, any checks to handoff to another application will be prevented
     // and instead the app will continue to display in the current browser.
-    // disableDeepLinking: false,
+    disableDeepLinking: true,
 
     // A property to disable the right click context menu for localVideo
     // the menu has option to flip the locally seen video for local presentations
