76eb834ff41279ff5a9f3341f39417dacdb0c1ac
damencho <damencho@jitsi.org>
2021-01-05 13:02:11 -0600

    fix: Fixes showing phone icon for jigasi participants.

--- react/features/base/participants/functions.js
+++ react/features/base/participants/functions.js
@@ -16,7 +16,6 @@ import {
 } from './constants';
 import { preloadImage } from './preloadImage';
 
-declare var config: Object;
 declare var interfaceConfig: Object;
 
 /**
@@ -299,7 +298,7 @@ export function isEveryoneModerator(stateful: Object | Function) {
  * @returns {boolean}
  */
 export function isIconUrl(icon: ?string | ?Object) {
-    return Boolean(icon) && typeof icon === 'object';
+    return Boolean(icon) && (typeof icon === 'object' || typeof icon === 'function');
 }
 
 /**
