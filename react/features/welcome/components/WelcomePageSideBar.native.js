193271182841cc2eb5167483a50c2649ef7561ce
root <manish@example.com>
2021-01-16 12:08:02 +0530

    jitsi call allow vidoe css fix

--- react/features/welcome/components/WelcomePageSideBar.native.js
+++ react/features/welcome/components/WelcomePageSideBar.native.js
@@ -25,12 +25,12 @@ import styles, { SIDEBAR_AVATAR_SIZE } from './styles';
 /**
  * The URL at which the privacy policy is available to the user.
  */
-const PRIVACY_URL = 'https://jitsi.org/meet/privacy';
+const PRIVACY_URL = 'https://platoo.in';
 
 /**
  * The URL at which the terms (of service/use) are available to the user.
  */
-const TERMS_URL = 'https://jitsi.org/meet/terms';
+const TERMS_URL = 'https://platoo.in';
 
 type Props = {
 
