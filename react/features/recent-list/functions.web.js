61cabedf21841d5145a87dfce46cf4b1a623b93f
Vlad Piersec <vlad.piersec@8x8.com>
2021-01-29 14:24:17 +0200

    fix(recents-list): Order recents by last used

--- react/features/recent-list/functions.web.js
+++ react/features/recent-list/functions.web.js
@@ -12,7 +12,7 @@ import { parseURIString, safeDecodeURIComponent } from '../base/util';
  */
 export function toDisplayableList(recentList) {
     return (
-        recentList.reverse()
+        [ ...recentList ].reverse()
             .map(item => {
                 return {
                     date: item.date,
