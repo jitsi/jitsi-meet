7a64bf006ea027b77564d8847570e1ac46ff0ec0
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-02-05 11:51:34 +0100

    misc(tools) add script for updating mobile apps versions

---
+++ resources/update-mobile-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e -u
+
+if [[ $# -ne 1 ]]; then
+    echo "Please specify a version"
+    exit 1
+fi
+
+THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
+VERSION=$1
+
+# iOS
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${VERSION}" ${THIS_DIR}/../ios/app/src/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${VERSION}" ${THIS_DIR}/../ios/app/watchos/app/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${VERSION}" ${THIS_DIR}/../ios/app/watchos/extension/Info.plist
+
+# Android
+sed -i "" -e "s/appVersion=.*/appVersion=${VERSION}/" ${THIS_DIR}/../android/gradle.properties
