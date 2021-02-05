d8f6d578d4be8c2c1bc61f356e50ff44642f38b3
Vlad Piersec <vlad.piersec@8x8.com>
2021-02-05 12:36:55 +0200

    feat(vpaas): Send billing id to prosody

--- react/features/conference/functions.web.js
+++ react/features/conference/functions.web.js
@@ -4,6 +4,7 @@ import { translateToHTML } from '../base/i18n';
 import { getLocalParticipant } from '../base/participants';
 import { toState } from '../base/redux';
 import { getBackendSafePath, getJitsiMeetGlobalNS } from '../base/util';
+import { getVpaasBillingId } from '../billing-counter/functions';
 import { showWarningNotification } from '../notifications';
 import { createRnnoiseProcessorPromise } from '../rnnoise';
 
@@ -84,6 +85,7 @@ export function getConferenceOptions(stateful) {
     options.applicationName = getName();
     options.getWiFiStatsMethod = getWiFiStatsMethod;
     options.createVADProcessor = createRnnoiseProcessorPromise;
+    options.billingId = getVpaasBillingId(state);
 
     // Disable CallStats, if requessted.
     if (options.disableThirdPartyRequests) {
