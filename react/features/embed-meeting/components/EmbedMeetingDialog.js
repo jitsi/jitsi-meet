2fe88d4b204415c57a3c14c1b03f0ee8a8e85c5f
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-02-09 14:26:32 +0100

    feat(embed-meeting) add autoplay permission to iframe sample

--- react/features/embed-meeting/components/EmbedMeetingDialog.js
+++ react/features/embed-meeting/components/EmbedMeetingDialog.js
@@ -33,7 +33,7 @@ function EmbedMeeting({ t, url }: Props) {
      * @returns {string} The iframe embed code.
      */
     const getEmbedCode = () =>
-        `<iframe allow="camera; microphone; fullscreen; display-capture" src="${url}"`
+        `<iframe allow="camera; microphone; fullscreen; display-capture; autoplay" src="${url}"`
         + ' style="height: 100%; width: 100%; border: 0px;"></iframe>';
 
     return (
