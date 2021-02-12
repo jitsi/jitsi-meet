4bf383b4acfd72bbebecb13422421c9d77d8465e
tudordan7 <tudor.pop@decagon.tech>
2021-02-12 16:16:58 +0200

    Tflite blur effect.

--- react/features/stream-effects/blur/JitsiStreamBlurEffect.js
+++ react/features/stream-effects/blur/JitsiStreamBlurEffect.js
@@ -5,16 +5,15 @@ import {
     SET_TIMEOUT,
     timerWorkerScript
 } from './TimerWorker';
-
-const segmentationWidth = 160;
-const segmentationHeight = 96;
+const segmentationWidth = 256;
+const segmentationHeight = 144;
 const _segmentationPixelCount = segmentationWidth * segmentationHeight;
+
 /**
  * Represents a modified MediaStream that adds blur to video background.
  * <tt>JitsiStreamBlurEffect</tt> does the processing of the original
  * video stream.
  */
-
 export default class JitsiStreamBlurEffect {
     _bpModel: Object;
     _inputVideoElement: HTMLVideoElement;
@@ -37,6 +36,7 @@ export default class JitsiStreamBlurEffect {
      */
     constructor(bpModel: Object) {
         this._bpModel = bpModel;
+
         // Bind event handler so it is only bound once for every instance.
         this._onMaskFrameTimer = this._onMaskFrameTimer.bind(this);
 
@@ -47,7 +47,6 @@ export default class JitsiStreamBlurEffect {
         this._inputVideoCanvasElement = document.createElement('canvas');
     }
 
-
     /**
      * EventHandler onmessage for the maskFrameTimerWorker WebWorker.
      *
@@ -56,18 +55,17 @@ export default class JitsiStreamBlurEffect {
      * @returns {void}
      */
     async _onMaskFrameTimer(response: Object) {
-       if (response.data.id === TIMEOUT_TICK) {
+        if (response.data.id === TIMEOUT_TICK) {
             await this._renderMask();
-       }
+        }
     }
-
 
     runPostProcessing() {
 
-        this._outputCanvasCtx.globalCompositeOperation = 'copy'
+        this._outputCanvasCtx.globalCompositeOperation = 'copy';
 
-        //draw segmentation mask
-        this._outputCanvasCtx.filter = 'blur(25px)' // FIXME Does not work on Safari
+        // draw segmentation mask
+        this._outputCanvasCtx.filter = 'blur(25px)';
         this._outputCanvasCtx.drawImage(
             this._segmentationMaskCanvas,
             0,
@@ -78,34 +76,33 @@ export default class JitsiStreamBlurEffect {
             0,
             this._inputVideoElement.width,
             this._inputVideoElement.height
-        )
+        );
 
-        this._outputCanvasCtx.globalCompositeOperation = 'source-in'
-        this._outputCanvasCtx.filter = 'none'
-        this._outputCanvasCtx.drawImage(this._inputVideoElement, 0, 0)
+        this._outputCanvasCtx.globalCompositeOperation = 'source-in';
+        this._outputCanvasCtx.filter = 'none';
+        this._outputCanvasCtx.drawImage(this._inputVideoElement, 0, 0);
 
-        this._outputCanvasCtx.globalCompositeOperation = 'destination-over'
-        this._outputCanvasCtx.filter = 'blur(25px)' // FIXME Does not work on Safari
-        this._outputCanvasCtx.drawImage(this._inputVideoElement, 0, 0)
+        this._outputCanvasCtx.globalCompositeOperation = 'destination-over';
+        this._outputCanvasCtx.filter = 'blur(25px)'; // FIXME Does not work on Safari
+        this._outputCanvasCtx.drawImage(this._inputVideoElement, 0, 0);
 
     }
 
     runTFLiteInference() {
-        this._bpModel._runInference()
-        const outputMemoryOffset = this._bpModel._getOutputMemoryOffset() / 4
+        this._bpModel._runInference();
+        const outputMemoryOffset = this._bpModel._getOutputMemoryOffset() / 4;
 
         for (let i = 0; i < _segmentationPixelCount; i++) {
-            const background = this._bpModel.HEAPF32[outputMemoryOffset + i * 2]
-            const person = this._bpModel.HEAPF32[outputMemoryOffset + i * 2 + 1]
-            const shift = Math.max(background, person)
-            const backgroundExp = Math.exp(background - shift)
-            const personExp = Math.exp(person - shift)
+            const background = this._bpModel.HEAPF32[outputMemoryOffset + i * 2];
+            const person = this._bpModel.HEAPF32[outputMemoryOffset + i * 2 + 1];
+            const shift = Math.max(background, person);
+            const backgroundExp = Math.exp(background - shift);
+            const personExp = Math.exp(person - shift);
 
             // Sets only the alpha component of each pixel
-            this._segmentationMask.data[i * 4 + 3] =
-                (255 * personExp) / (backgroundExp + personExp) // softmax
+            this._segmentationMask.data[i * 4 + 3] = (255 * personExp) / (backgroundExp + personExp);
         }
-        this._segmentationMaskCtx.putImageData(this._segmentationMask, 0, 0)
+        this._segmentationMaskCtx.putImageData(this._segmentationMask, 0, 0);
     }
 
     /**
@@ -116,7 +113,7 @@ export default class JitsiStreamBlurEffect {
      */
 
     _renderMask() {
-        this.resizeSource()
+        this.resizeSource();
         this.runTFLiteInference();
         this.runPostProcessing();
 
@@ -137,21 +134,20 @@ export default class JitsiStreamBlurEffect {
             0,
             segmentationWidth,
             segmentationHeight
-        )
+        );
 
         const imageData = this._segmentationMaskCtx.getImageData(
             0,
             0,
             segmentationWidth,
             segmentationHeight
-        )
-        const inputMemoryOffset = this._bpModel._getInputMemoryOffset() / 4
+        );
+        const inputMemoryOffset = this._bpModel._getInputMemoryOffset() / 4;
+
         for (let i = 0; i < _segmentationPixelCount; i++) {
-            this._bpModel.HEAPF32[inputMemoryOffset + i * 3] = imageData.data[i * 4] / 255
-            this._bpModel.HEAPF32[inputMemoryOffset + i * 3 + 1] =
-                imageData.data[i * 4 + 1] / 255
-            this._bpModel.HEAPF32[inputMemoryOffset + i * 3 + 2] =
-                imageData.data[i * 4 + 2] / 255
+            this._bpModel.HEAPF32[inputMemoryOffset + i * 3] = imageData.data[i * 4] / 255;
+            this._bpModel.HEAPF32[inputMemoryOffset + i * 3 + 1] = imageData.data[i * 4 + 1] / 255;
+            this._bpModel.HEAPF32[inputMemoryOffset + i * 3 + 2] = imageData.data[i * 4 + 2] / 255;
         }
     }
 
@@ -178,6 +174,7 @@ export default class JitsiStreamBlurEffect {
         const firstVideoTrack = stream.getVideoTracks()[0];
         const { height, frameRate, width }
             = firstVideoTrack.getSettings ? firstVideoTrack.getSettings() : firstVideoTrack.getConstraints();
+
         this._segmentationMask = new ImageData(segmentationWidth, segmentationHeight);
         this._segmentationMaskCanvas = document.createElement('canvas');
         this._segmentationMaskCanvas.width = segmentationWidth;
@@ -185,7 +182,7 @@ export default class JitsiStreamBlurEffect {
         this._segmentationMaskCtx = this._segmentationMaskCanvas.getContext('2d');
         this._outputCanvasElement.width = parseInt(width, 10);
         this._outputCanvasElement.height = parseInt(height, 10);
-        this._outputCanvasCtx = this._outputCanvasElement.getContext('2d')
+        this._outputCanvasCtx = this._outputCanvasElement.getContext('2d');
         this._inputVideoCanvasElement.width = parseInt(width, 10);
         this._inputVideoCanvasElement.height = parseInt(height, 10);
         this._inputVideoElement.width = parseInt(width, 10);
@@ -198,7 +195,8 @@ export default class JitsiStreamBlurEffect {
                 timeMs: 1000 / 30
             });
         };
-        //final output
+
+        // final output
         return this._outputCanvasElement.captureStream(parseInt(frameRate, 10));
     }
 
--- react/features/stream-effects/blur/index.js
+++ react/features/stream-effects/blur/index.js
@@ -3,13 +3,14 @@
 import * as wasmCheck from 'wasm-check';
 
 import JitsiStreamBlurEffect from './JitsiStreamBlurEffect';
-import createTFLiteModule from './vendor/tflite/tflite'
-import createTFLiteSIMDModule from './vendor/tflite/tflite-simd'
+import createTFLiteModule from './vendor/tflite/tflite';
+import createTFLiteSIMDModule from './vendor/tflite/tflite-simd';
 
 const models = {
-    96:  `/libs/segm_lite_v681.tflite`,
+    96: '/libs/segm_lite_v681.tflite',
     144: '/libs/segm_full_v679.tflite'
-}
+};
+
 /**
  * Creates a new instance of JitsiStreamBlurEffect. This loads the bodyPix model that is used to
  * extract person segmentation.
@@ -17,31 +18,28 @@ const models = {
  * @returns {Promise<JitsiStreamBlurEffect>}
  */
 export async function createBlurEffect() {
-    console.log('are you here?')
-    //console.log(createTFLiteModule(), 'create tf lite model')
     if (!MediaStreamTrack.prototype.getSettings && !MediaStreamTrack.prototype.getConstraints) {
         throw new Error('JitsiStreamBlurEffect not supported!');
     }
     let tflite;
 
     if (wasmCheck.feature.simd) {
-      tflite = await createTFLiteSIMDModule();
-      console.log('SIMD load OK!');
-      console.log(tflite);
+        tflite = await createTFLiteSIMDModule();
     } else {
-      tflite = await createTFLiteModule();
+        tflite = await createTFLiteModule();
     }
 
-      console.log(tflite, 'tflite !!!')
-        const modelBufferOffset = tflite._getModelBufferMemoryOffset()
-        const modelResponse = await fetch(
-            models[96]
-          )
+    const modelBufferOffset = tflite._getModelBufferMemoryOffset();
+    const modelResponse = await fetch(
+        models[144]
+    );
 
-        const model = await modelResponse.arrayBuffer()
+    const model = await modelResponse.arrayBuffer();
+
+    tflite.HEAPU8.set(new Uint8Array(model), modelBufferOffset);
+
+    tflite._loadModel(model.byteLength);
     
-        tflite.HEAPU8.set(new Uint8Array(model), modelBufferOffset)
-        tflite._loadModel(model.byteLength)
-        return new JitsiStreamBlurEffect(tflite);
- 
+    return new JitsiStreamBlurEffect(tflite);
+
 }
