d5d98322d70f33bcf31176ce955787cbc01352c1
tudordan7 <tudor.pop@decagon.tech>
2021-02-26 15:45:50 +0200

    fix(disable blur) disable blur button if camera is off.

--- react/features/blur/components/VideoBlurButton.js
+++ react/features/blur/components/VideoBlurButton.js
@@ -6,6 +6,7 @@ import { IconBlurBackground } from '../../base/icons';
 import { connect } from '../../base/redux';
 import { AbstractButton } from '../../base/toolbox/components';
 import type { AbstractButtonProps } from '../../base/toolbox/components';
+import { getLocalVideoTrack } from '../../base/tracks';
 import { toggleBlurEffect } from '../actions';
 
 /**
@@ -17,6 +18,16 @@ type Props = AbstractButtonProps & {
      * True if the video background is blurred or false if it is not.
      */
     _isVideoBlurred: boolean,
+
+    /**
+     * Flag signaling the visibility of camera preview.
+     */
+    videoMuted: boolean,
+
+    /**
+     * The JitsiLocalTrack to display.
+     */
+    videoTrack: ?Object,
 
     /**
      * The redux {@code dispatch} function.
@@ -60,6 +71,25 @@ class VideoBlurButton extends AbstractButton<Props, *> {
     _isToggled() {
         return this.props._isVideoBlurred;
     }
+
+    /**
+     * Returns {@code boolean} value indicating if disabled state is
+     * enabled or not. This checks two cases of true value. The first one
+     * checks the case when user join meeting with disabled camera. The
+     * second case is when user join meeting with enabled camera, but then
+     * he disables it.
+     *
+     * @protected
+     * @returns {boolean}
+     */
+    _isDisabled() {
+        if (this.props.videoMuted) {
+            return true;
+        }
+        if (!this.props.videoTrack) {
+            return true;
+        }
+    }
 }
 
 /**
@@ -67,14 +97,17 @@ class VideoBlurButton extends AbstractButton<Props, *> {
  * {@code VideoBlurButton} component.
  *
  * @param {Object} state - The Redux state.
+ * @param {Props} ownProps - The own props of the component.
  * @private
  * @returns {{
  *     _isVideoBlurred: boolean
  * }}
  */
-function _mapStateToProps(state): Object {
+function _mapStateToProps(state, ownProps): Object {
+
     return {
-        _isVideoBlurred: Boolean(state['features/blur'].blurEnabled)
+        videoMuted: ownProps.videoTrack ? ownProps.videoMuted : state['features/base/media'].video.muted,
+        videoTrack: ownProps.videoTrack || (getLocalVideoTrack(state['features/base/tracks']) || {}).jitsiTrack
     };
 }
 
