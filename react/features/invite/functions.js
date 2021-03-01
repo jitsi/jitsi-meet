7dbaf640c8c602cf8bca93ad5174b0dac0939822
Steffen Kolmer <steffen@kolmer.net>
2021-03-01 12:17:36 +0100

    Fixed another linter error

--- react/features/invite/functions.js
+++ react/features/invite/functions.js
@@ -608,7 +608,7 @@ export function hasMultipleNumbers(dialInNumbers: ?Object) {
     // deprecated and will be removed
     const { numbers } = dialInNumbers;
 
-    return Boolean(numbers && Object.values(numbers).map(a => Array.isArray(a) ? a.length : 0)
+    return Boolean(numbers && Object.values(numbers).map(a => (Array.isArray(a) ? a.length : 0))
         .reduce((a, b) => a + b) > 1);
 }
 
