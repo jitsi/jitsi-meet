0c75d7f816c1349fcdc8be542e028b3feaec43d7
titus.moldovan <titus.moldovan@8x8.com>
2021-03-04 11:57:38 +0200

    feat(e2ee): adds sounds for e2ee enabling/disabling

---
+++ react/features/e2ee/constants.js
@@ -0,0 +1,15 @@
+// @flow
+
+/**
+ * The identifier of the sound to be played when e2ee is disabled.
+ *
+ * @type {string}
+ */
+export const E2EE_OFF_SOUND_ID = 'E2EE_OFF_SOUND';
+
+/**
+ * The identifier of the sound to be played when e2ee is enabled.
+ *
+ * @type {string}
+ */
+export const E2EE_ON_SOUND_ID = 'E2EE_ON_SOUND';
