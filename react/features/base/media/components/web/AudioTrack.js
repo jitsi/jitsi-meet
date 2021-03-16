eed54982e9fed441b6d01372c241c3ecf07b3c10
f0x <f0x@cthu.lu>
2021-03-17 00:50:43 +0100

    Set default audio volume to 0.7, to allow raising of relative volume

--- react/features/base/media/components/web/AudioTrack.js
+++ react/features/base/media/components/web/AudioTrack.js
@@ -210,6 +210,8 @@ export default class AudioTrack extends Component<Props> {
         this._ref = audioElement;
         const { onInitialVolumeSet } = this.props;
 
+        this._ref.volume = 0.7;
+
         if (this._ref && onInitialVolumeSet) {
             onInitialVolumeSet(this._ref.volume);
         }
