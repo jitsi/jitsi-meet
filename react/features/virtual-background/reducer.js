61bb4ef02dc24161f2b10edc2f908001b3d90379
tudordan7 <tudor.pop@decagon.tech>
2021-03-30 16:11:27 +0300

    feat(virtual-background-persistence): Set up persistence on virtual background.

--- react/features/virtual-background/reducer.js
+++ react/features/virtual-background/reducer.js
@@ -4,29 +4,11 @@ import { PersistenceRegistry, ReducerRegistry } from '../base/redux';
 
 import { BACKGROUND_ENABLED, SET_VIRTUAL_BACKGROUND } from './actionTypes';
 
-/**
- * The default/initial redux state of the feature {@code features/virtual-background}.
- *
- * @type Object
- */
-const DEFAULT_STATE = {
-    virtualSource: undefined,
-    isVirtualBackground: undefined,
-    backgroundEffectEnabled: undefined
-};
-
 const STORE_NAME = 'features/virtual-background';
 
 /**
- * Sets up the persistence of the feature {@code features/virtual-background}.
+ * Sets up the persistence of the feature {@code virtual-background}.
  */
-const virtualBackground = {};
-
-// start with the default state
-Object.keys(DEFAULT_STATE).forEach(key => {
-    virtualBackground[key] = true;
-});
-
 PersistenceRegistry.register(STORE_NAME, true);
 
 /**
@@ -45,7 +27,6 @@ ReducerRegistry.register(STORE_NAME, (state = {}, action) => {
 
     switch (action.type) {
     case SET_VIRTUAL_BACKGROUND: {
-
         return {
             ...state,
             virtualSource,
@@ -53,7 +34,6 @@ ReducerRegistry.register(STORE_NAME, (state = {}, action) => {
         };
     }
     case BACKGROUND_ENABLED: {
-
         return {
             ...state,
             backgroundEffectEnabled
