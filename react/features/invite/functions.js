86dd35b9275241c228730c4b66cec3c07e4cadab
Tudor-Ovidiu Avram <tudor.potecaru@8x8.com>
2021-04-07 15:46:01 +0300

    code review changes

--- react/features/invite/functions.js
+++ react/features/invite/functions.js
@@ -808,6 +808,7 @@ export function isSharingEnabled(sharingFeature: string) {
  * Sends a post request to an invite service.
  *
  * @param {Array} inviteItems - The list of the "sip" type items to invite.
+ * @param {URL} locationURL - The URL of the location.
  * @param {string} sipInviteUrl - The invite service that generates the invitation.
  * @param {string} jwt - The jwt token.
  * @param {string} roomName - The name to the conference.
@@ -816,6 +817,7 @@ export function isSharingEnabled(sharingFeature: string) {
  */
 export function inviteSipEndpoints( // eslint-disable-line max-params
         inviteItems: Array<Object>,
+        locationURL: URL,
         sipInviteUrl: string,
         jwt: string,
         roomName: string,
@@ -825,7 +827,7 @@ export function inviteSipEndpoints( // eslint-disable-line max-params
         return Promise.resolve();
     }
 
-    const baseUrl = window.location.href.toLowerCase().replace(`/${roomName}`, '');
+    const baseUrl = locationURL.href.toLowerCase().replace(`/${roomName}`, '');
 
     return fetch(
        sipInviteUrl,
