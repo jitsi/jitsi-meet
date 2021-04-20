1840d4d0bc4bd468c98ee9d7b52d41acba8cd334
damencho <damencho@jitsi.org>
2021-04-20 15:32:04 -0500

    feat: JiConOp2 pushes a message with some info to clients.

---
+++ resources/prosody-plugins/mod_jiconop.lua
@@ -0,0 +1,41 @@
+local st = require "util.stanza";
+
+-- Jitsi Connection Optimization
+-- gathers needed information and pushes it with a message to clients
+-- this way we skip 4 request responses during every client setup
+
+-- this is after xmpp-bind, the moment a client has resource and can be contacted
+module:hook("resource-bind", function (event)
+    local session = event.session;
+
+    -- disco info data / all identity and features
+    local query = st.stanza("query", { xmlns = "http://jabber.org/protocol/disco#info" });
+    local done = {};
+    for _,identity in ipairs(module:get_host_items("identity")) do
+        local identity_s = identity.category.."\0"..identity.type;
+        if not done[identity_s] then
+            query:tag("identity", identity):up();
+            done[identity_s] = true;
+        end
+    end
+
+    -- check whether room has lobby enabled and display name is required for those trying to join
+    local lobby_muc_component_config = module:get_option_string('lobby_muc');
+    module:context(lobby_muc_component_config):fire_event('host-disco-info-node',
+            {origin = session; reply = query; node = 'lobbyrooms';});
+
+    local stanza = st.message({
+            type = 'conference-info';
+            from = module.host;
+            to = session.full_jid; });
+    stanza:add_child(query):up();
+
+    --- get turnservers and credentials
+    local services = module:fire_event("external_service/get_services");
+    stanza:tag("services");
+    for _, srv in ipairs(services) do
+        stanza:tag("service", srv):up();
+    end
+
+    session.send(stanza);
+end);
