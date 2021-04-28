aebd9cbe63a3751f179cc2e51a0c2c67d2ec68f7
Fabien Zucchet <80532941+fabienzucchet@users.noreply.github.com>
2021-04-28 17:15:29 +0200

    feat(PollResults Component): Component to display poll results (#1)
    * feat(PollResults Component): fist version of the component
    
    * feat(detailed votes): Display the detailed results of a poll
    
    * feat(Poll results): Use display name instead of ids in detailed results mode
    
    * feat(Poll results): Use display name instead of ids in detailed results mode
    
    * feat(Poll results): Use display name instead of ids in detailed results mode
    
    * feat(Poll results): Use display name instead of ids in detailed results mode
    
    * fix(Poll): change title to question
    
    * fix(Poll type): import Poll type from types.js
    
    * fix(Poll): change title to question
    
    * fix(Poll): get participants out of the map
    
    * fix(Poll): replace filter with find
    
    Co-authored-by: Fabien Zucchet <fabien.zucchet@student-cs.fr>

---
+++ react/features/polls/components/web/PollResults.js
@@ -0,0 +1,81 @@
+// @flow
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { getParticipants } from '../../../base/participants';
+import type { Poll } from '../../types';
+
+
+type Props = {
+
+    /**
+     * Display or not detailed votes
+     */
+    detailedVotes: boolean,
+
+    /**
+     * Display or not the poll question
+     */
+    displayQuestion: boolean,
+
+    /**
+     * Details of the poll to display
+     */
+    pollDetails: Poll,
+};
+
+/**
+ * Component that renders the poll results.
+ *
+ * @returns {React$Element<any>}
+ */
+function PollResults({ detailedVotes, displayQuestion, pollDetails }: Props) {
+
+    const question = displayQuestion ? <strong>{ pollDetails.question }</strong> : null;
+
+    const participants = useSelector(state => getParticipants(state));
+
+    const totalVoters = pollDetails.answers.reduce((accumulator, answer) => accumulator + answer.voters.size, 0);
+
+    const answers = pollDetails.answers.map((answer, index) => {
+
+        const answerPercent = Math.round(answer.voters.size / totalVoters * 100);
+
+        const detailedAnswer = detailedVotes
+            ? [ ...answer.voters ].map(voterId => {
+                const participant = participants.find(part => part.id === voterId);
+
+                const name = participant || 'Fellow Jitser';
+
+                return <li key = { voterId }>{ name }</li>;
+            })
+            : null;
+
+        return (
+            <li key = { index }>
+                { answer.name } ({ answerPercent } %)
+                <div>
+                    <ul>
+                        { detailedAnswer }
+                    </ul>
+                </div>
+            </li>
+        );
+    });
+
+    return (
+        <div>
+            <div className = 'poll-question-field'>
+                { question }
+            </div>
+            <div>
+                <ol className = 'poll-answer-fields'>
+                    { answers }
+                </ol>
+            </div>
+        </div>
+    );
+}
+
+export default PollResults;
--- react/features/polls/components/web/index.js
+++ react/features/polls/components/web/index.js
@@ -1,3 +1,4 @@
 // @flow
 
 export { default as PollCreateButton } from './PollCreateButton';
+export { default as PollResults } from './PollResults';
