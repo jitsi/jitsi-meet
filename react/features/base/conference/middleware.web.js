7a49c3ece726e1d53e408c290d30671d97d17799
Andrei Gavrilescu <gavrilescu.andrei90@gmail.com>
2021-04-28 16:29:22 +0300

    remove unused imports

--- react/features/base/conference/middleware.web.js
+++ react/features/base/conference/middleware.web.js
@@ -1,10 +1,8 @@
 // @flow
 
-import UIEvents from '../../../../service/UI/UIEvents';
 import { setPrejoinPageVisibility, setSkipPrejoinOnReload } from '../../prejoin';
 import { JitsiConferenceErrors } from '../lib-jitsi-meet';
 import { MiddlewareRegistry } from '../redux';
-import { TOGGLE_SCREENSHARING } from '../tracks/actionTypes';
 
 import { CONFERENCE_FAILED, CONFERENCE_JOINED } from './actionTypes';
 import './middleware.any';
