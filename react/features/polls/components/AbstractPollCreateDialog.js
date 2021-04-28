5ea03143fd82689a1a043c7d8ae977d851a50290
Jade Guiton <guiton.jade@gmail.com>
2021-04-28 16:47:37 +0200

    feat(polls.create) Added fields for question and answers (#3)
    * feat(polls.create) Added fields for question and answers + keyboard navigation
    
    * feat(polls.create) Minor changes, added some comments

--- react/features/polls/components/AbstractPollCreateDialog.js
+++ react/features/polls/components/AbstractPollCreateDialog.js
@@ -1,34 +1,79 @@
 // @flow
 
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { COMMAND_NEW_POLL } from '../constants';
 
-export const AbstractPollCreateDialog = Component => props => {
-    const conference = useSelector(state => state['features/base/conference'].conference);
+/*
+ * Props that will be passed by the AbstractPollCreateDialog to its
+ * concrete implementations (web/native).
+ **/
+export type AbstractProps = {
+    question: string, setQuestion: string => void,
+    answers: Array<string>,
+    setAnswer: (number, string) => void,
+    addAnswer: ?number => void,
+    removeAnswer: number => void,
+    onSubmit: Function,
+};
 
+/*
+ * Higher Order Component taking in a concrete PollCreateDialog component and
+ * augmenting it with state/behavior common to both web and native implementations.
+ */
+export const AbstractPollCreateDialog = Component => props => {
+    const [ question, setQuestion ] = useState('');
+
+    const [ answers, setAnswers ] = useState([ '' ]);
+    const setAnswer = useCallback((index, answer) => {
+        const newAnswers = [ ...answers ];
+
+        newAnswers[index] = answer;
+        setAnswers(newAnswers);
+    });
+    const addAnswer = useCallback(i => {
+        const newAnswers = [ ...answers ];
+
+        newAnswers.splice(i || answers.length, 0, '');
+        setAnswers(newAnswers);
+    });
+    const removeAnswer = useCallback(i => {
+        if (answers.length === 1) {
+            return;
+        }
+        const newAnswers = [ ...answers ];
+
+        newAnswers.splice(i, 1);
+        setAnswers(newAnswers);
+    });
+
+    const conference = useSelector(state => state['features/base/conference'].conference);
     const onSubmit = useCallback(() => {
         conference.sendCommandOnce(COMMAND_NEW_POLL, {
             attributes: {
                 pollId: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
                 sender: conference.myUserId(),
-                title: 'Example poll'
+                title: question
             },
-            children: [
-                { tagName: 'answer',
-                    value: 'Answer 1' },
-                { tagName: 'answer',
-                    value: 'Answer 2' },
-                { tagName: 'answer',
-                    value: 'Answer 3' }
-            ]
+            children: answers
+                .filter(answer => answer.trim().length > 0)
+                .map(answer => {
+                    return { tagName: 'answer',
+                        value: answer };
+                })
         });
 
         return true;
-    }, [ conference ]);
+    }, [ conference, question, answers ]);
 
     return (<Component
         { ...props }
-        onSubmit = { onSubmit } />);
+        addAnswer = { addAnswer }
+        answers = { answers }
+        onSubmit = { onSubmit }
+        question = { question }
+        removeAnswer = { removeAnswer }
+        setAnswer = { setAnswer }
+        setQuestion = { setQuestion } />);
 };
--- react/features/polls/components/web/PollCreateDialog.js
+++ react/features/polls/components/web/PollCreateDialog.js
@@ -1,17 +1,13 @@
 // @flow
 
-import React from 'react';
+import { FieldTextStateless } from '@atlaskit/field-text';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import { Dialog } from '../../../base/dialog';
 import { translate } from '../../../base/i18n';
-import { AbstractPollCreateDialog } from '../AbstractPollCreateDialog';
+import { AbstractPollCreateDialog, AbstractProps } from '../AbstractPollCreateDialog';
 
-type Props = {
-
-    /**
-     * The dialog submit callback (from AbstractPollCreateDialog)
-     */
-    onSubmit: Function,
+type Props = AbstractProps & {
 
     /**
      * The i18n translate function.
@@ -19,15 +15,106 @@ type Props = {
     t: Function
 };
 
-const PollCreateDialog = ({ onSubmit, t }: Props) => (
-    <Dialog
+const PollCreateDialog = (props: Props) => {
+    const {
+        question, setQuestion,
+        answers, setAnswer, addAnswer, removeAnswer,
+        onSubmit,
+        t
+    } = props;
+
+    /*
+     * This ref stores the Array of answer input fields, allowing us to focus on them.
+     * This array is maintained by registerfieldRef and the useEffect below.
+     */
+    const answerInputs = useRef([]);
+    const registerFieldRef = useCallback((i, r) => {
+        if (r === null) {
+            return;
+        }
+        answerInputs.current[i] = r.input;
+    }, [ answerInputs ]);
+
+    useEffect(() => {
+        answerInputs.current = answerInputs.current.slice(0, answers.length);
+    }, [ answers ]);
+
+    /*
+     * This state allows us to requestFocus asynchronously, without having to worry
+     * about whether a newly created input field has been rendered yet or not.
+     */
+    const [ lastFocus, requestFocus ] = useState(null);
+
+    useEffect(() => {
+        if (lastFocus === null) {
+            return;
+        }
+        const input = answerInputs.current[lastFocus];
+
+        if (input === undefined) {
+            return;
+        }
+        input.focus();
+    }, [ lastFocus ]);
+
+    // Called on keypress in answer fields
+    const onAnswerKeyDown = useCallback((i, ev) => {
+        if (ev.key === 'Enter') {
+            addAnswer(i + 1);
+            requestFocus(i + 1);
+            ev.preventDefault();
+        } else if (ev.key === 'Backspace' && ev.target.value === '') {
+            removeAnswer(i);
+            requestFocus(i - 1);
+            ev.preventDefault();
+        } else if (ev.key === 'ArrowDown') {
+            requestFocus(i + 1);
+        } else if (ev.key === 'ArrowUp') {
+            requestFocus(i - 1);
+        }
+    }, [ addAnswer, removeAnswer ]);
+
+    return (<Dialog
         okKey = { t('polls.create.Send') }
         onSubmit = { onSubmit }
-        titleKey = 'polls.create.title'
+        titleKey = 'polls.create.dialogTitle'
         width = 'small'>
-        <p>Test</p>
-    </Dialog>
-);
+        <div className = 'poll-question-field'>
+            <FieldTextStateless
+                autoFocus = { true }
+                compact = { true }
+                isLabelHidden = { true }
+                // eslint-disable-next-line react/jsx-no-bind
+                onChange = { ev => setQuestion(ev.target.value) }
+                placeholder = { t('polls.create.questionPlaceholder') }
+                shouldFitContainer = { true }
+                type = 'text'
+                value = { question } />
+        </div>
+        <ol className = 'poll-answer-fields'>
+            {
+            /* eslint-disable react/jsx-no-bind */
+                answers.map((answer, i) => (<li key = { i }>
+                    <FieldTextStateless
+                        compact = { true }
+                        isLabelHidden = { true }
+                        onChange = { ev => setAnswer(i, ev.target.value) }
+                        onKeyDown = { ev => onAnswerKeyDown(i, ev) }
+                        placeholder = { t('polls.create.answerPlaceholder', { index: i + 1 }) }
+                        ref = { r => registerFieldRef(i, r) }
+                        shouldFitContainer = { true }
+                        type = 'text'
+                        value = { answer } />
+                </li>))
+            /* eslint-enable react/jsx-no-bind */
+            }
+        </ol>
+    </Dialog>);
+};
 
+/*
+ * We apply AbstractPollCreateDialog to fill in the AbstractProps common
+ * to both the web and native implementations.
+ */
 // eslint-disable-next-line new-cap
 export default translate(AbstractPollCreateDialog(PollCreateDialog));
