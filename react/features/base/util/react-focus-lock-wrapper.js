e33674fb2e2a128e96b42eb02dedef2e834665be
Mihai-Andrei Uscat <52234168+muscat1@users.noreply.github.com>
2021-05-18 09:04:42 +0300

    fix(FocusLock): Stop stealing focus when embedded

--- react/features/base/util/react-focus-lock-wrapper.js
+++ react/features/base/util/react-focus-lock-wrapper.js
@@ -6,31 +6,26 @@ import FocusLock, { MoveFocusInside } from 'react-focus-lock';
  * FocusLock wrapper that disable the FocusLock in the @atlaskit/modal-dialog. We need to disable it because if the
  * iframe API is used and a dialog is displayed it is impossible to click on fields outside of the iframe (FocusLock
  * will steal the focus from any element that is not part of the dialog).
+ *
+ * @param {Object} props - The props passed to the FocusLock.
+ * @returns {ReactElement}
  */
-export default class FocusLockWrapper extends FocusLock<*> {
-    /**
-     * Implements React's {@link Component#render()}.
-     *
-     * @inheritdoc
-     * @returns {ReactElement}
-     */
-    render() {
-        const { children, ...otherProps } = this.props;
+export default (props: Object) => {
+    const { children, ...otherProps } = props;
 
-        const props = {
-            ...otherProps,
-            crossFrame: false
-        };
+    const forwardProps = {
+        ...otherProps,
+        crossFrame: false
+    };
 
-        // MoveFocusInside is added in order to initially bring the focus on the dialog.
-        return (
-            <FocusLock
-                { ...props }
-                className = 'focus-lock'>
-                <MoveFocusInside>{children}</MoveFocusInside>
-            </FocusLock>
-        );
-    }
-}
+    // MoveFocusInside is added in order to initially bring the focus on the dialog.
+    return (
+        <FocusLock
+            { ...forwardProps }
+            className = 'focus-lock'>
+            <MoveFocusInside>{children}</MoveFocusInside>
+        </FocusLock>
+    );
+};
 
 export * from 'react-focus-lock';
