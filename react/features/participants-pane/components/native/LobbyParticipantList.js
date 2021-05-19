01c0113b0dca405f517cdd6c4a53c81d5ae8248b
Calin Chitu <calinte2002@yahoo.co.uk>
2021-05-19 14:05:28 +0300

    feat(native-participants-pane) fixed lint errors

--- react/features/participants-pane/components/native/LobbyParticipantList.js
+++ react/features/participants-pane/components/native/LobbyParticipantList.js
@@ -1,12 +1,36 @@
 // @flow
 
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { View } from 'react-native';
+import { Text } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 
-// import { getLobbyState } from '../../../lobby/functions';
+import { getLobbyState } from '../../../lobby/functions';
 
 import { LobbyParticipantItem } from './LobbyParticipantItem';
 import { participants } from './participants';
 
-export const LobbyParticipantList = () => null;
+export const LobbyParticipantList = () => {
+    const {
+        lobbyEnabled
+    } = useSelector(getLobbyState);
+    const { t } = useTranslation();
+
+    if (!lobbyEnabled || !participants.length) {
+        return null;
+    }
+
+    return (
+        <>
+            <Text>{t('participantsPane.headings.lobby', { count: participants.length })}</Text>
+            <View>
+                {participants.map(p => (
+                    <LobbyParticipantItem
+                        key = { p.id }
+                        participant = { p } />)
+                )}
+            </View>
+        </>
+    );
+};
