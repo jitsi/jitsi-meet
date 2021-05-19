f899781bfc6de3cdeea374b43d348c8b183f1367
titus.moldovan <titus.moldovan@8x8.com>
2021-05-19 17:32:08 +0300

    task(rn): hide screen share button when audioOnly mode

--- react/features/toolbox/components/native/ScreenSharingButton.js
+++ react/features/toolbox/components/native/ScreenSharingButton.js
@@ -1,18 +1,40 @@
+// @flow
+
 import React from 'react';
 import { Platform } from 'react-native';
+
+import { connect } from '../../../base/redux';
 
 import ScreenSharingAndroidButton from './ScreenSharingAndroidButton.js';
 import ScreenSharingIosButton from './ScreenSharingIosButton.js';
 
 const ScreenSharingButton = props => (
     <>
-        {Platform.OS === 'android'
+        {Platform.OS === 'android' && props.visible
             && <ScreenSharingAndroidButton { ...props } />
         }
-        {Platform.OS === 'ios'
+        {Platform.OS === 'ios' && props.visible
             && <ScreenSharingIosButton { ...props } />
         }
     </>
 );
 
-export default ScreenSharingButton;
+/**
+ * Maps (parts of) the redux state to the associated props for the
+ * {@code ScreenSharingButton} component.
+ *
+ * @param {Object} state - The Redux state.
+ * @private
+ * @returns {{
+ *     enabled: boolean,
+ * }}
+ */
+function _mapStateToProps(state): Object {
+    const visible = !state['features/base/audio-only'].enabled;
+
+    return {
+        visible
+    };
+}
+
+export default connect(_mapStateToProps)(ScreenSharingButton);
