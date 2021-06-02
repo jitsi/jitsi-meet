c4d7ba7ffc9ddef3be6ff88c2370750895d82d84
hmuresan <horatiu.muresan@8x8.com>
2021-06-02 12:27:15 +0300

    fix(feedback): don't wait for feedback submission on leave meeting

--- conference.js
+++ conference.js
@@ -1972,7 +1972,12 @@ export default {
 
         room.on(
             JitsiConferenceEvents.CONFERENCE_UNIQUE_ID_SET,
-            (...args) => APP.store.dispatch(conferenceUniqueIdSet(room, ...args)));
+            (...args) => {
+                // Preserve the sessionId so that the value is accessible even after room
+                // is disconnected.
+                room.sessionId = room.getMeetingUniqueId();
+                APP.store.dispatch(conferenceUniqueIdSet(room, ...args));
+            });
 
         room.on(
             JitsiConferenceEvents.AUTH_STATUS_CHANGED,
@@ -2792,15 +2797,11 @@ export default {
             requestFeedbackPromise = Promise.resolve(true);
         }
 
-        let feedbackResult;
-
-        requestFeedbackPromise
-        .then(res => {
-            feedbackResult = res;
-
-            return this.leaveRoomAndDisconnect();
-        })
-        .then(() => {
+        Promise.all([
+            requestFeedbackPromise,
+            this.leaveRoomAndDisconnect()
+        ])
+        .then(values => {
             this._room = undefined;
             room = undefined;
 
@@ -2812,7 +2813,7 @@ export default {
             if (!interfaceConfig.SHOW_PROMOTIONAL_CLOSE_PAGE) {
                 APP.API.notifyReadyToClose();
             }
-            APP.store.dispatch(maybeRedirectToWelcomePage(feedbackResult));
+            APP.store.dispatch(maybeRedirectToWelcomePage(values[0]));
         });
     },
 
