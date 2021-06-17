04432ab76974c017609f7fcc7d415116506824b2
hmuresan <horatiu.muresan@8x8.com>
2021-06-17 15:16:58 +0300

    feat(did-page) Handle SIP number on did info page

--- react/features/base/icons/svg/index.js
+++ react/features/base/icons/svg/index.js
@@ -130,3 +130,4 @@ export { default as IconWifi1Bar } from './wifi-1.svg';
 export { default as IconWifi2Bars } from './wifi-2.svg';
 export { default as IconWifi3Bars } from './wifi-3.svg';
 export { default as IconYahoo } from './yahoo.svg';
+export { default as IconSip } from './sip.svg';
