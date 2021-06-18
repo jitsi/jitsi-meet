655f79955d44f15965fc27f860872ca9d6b606e0
Calin Chitu <calinte2002@yahoo.co.uk>
2021-06-18 15:01:58 +0300

    fix(lobby) disable invite from lobby screen fix #9209

--- react/features/lobby/components/web/LobbyScreen.js
+++ react/features/lobby/components/web/LobbyScreen.js
@@ -21,7 +21,9 @@ class LobbyScreen extends AbstractLobbyScreen {
      */
     render() {
         return (
-            <PreMeetingScreen title = { this.props.t(this._getScreenTitleKey()) }>
+            <PreMeetingScreen
+                showCopyUrlButton = { this.props.showCopyUrlButton }
+                title = { this.props.t(this._getScreenTitleKey()) }>
                 { this._renderContent() }
             </PreMeetingScreen>
         );
