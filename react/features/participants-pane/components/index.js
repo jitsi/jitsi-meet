35c4b49068c68518ae9f92f66a2c1b1dfac2f78a
damencho <damencho@jitsi.org>
2021-07-06 13:05:47 +0300

    fix: Changes participants pane to be component.
    The functional component was always rendered:
    `prev props: {} !== {} :next props`.

--- react/features/participants-pane/components/index.js
+++ react/features/participants-pane/components/index.js
@@ -5,6 +5,6 @@ export * from './MeetingParticipantContextMenu';
 export * from './MeetingParticipantItem';
 export * from './MeetingParticipantList';
 export * from './ParticipantItem';
-export * from './ParticipantsPane';
+export { default as ParticipantsPane } from './ParticipantsPane';
 export * from './ParticipantsPaneButton';
 export * from './RaisedHandIndicator';
