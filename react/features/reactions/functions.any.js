ea0d953d1c9dca717919fbc1a56efd0c50300e25
robertpin <robert.pin9@gmail.com>
2021-07-15 15:23:48 +0300

    feat(reactions-webhook) Added reactions backend call for webhook (#9534)
    * Added backend call for reactions webhook
    
    * Updated webhook url
    
    * Fixed linting error
    
    * Code review fixes
    
    * Fixed linting errors

--- react/features/reactions/functions.any.js
+++ react/features/reactions/functions.any.js
@@ -1,5 +1,11 @@
 // @flow
 
+import { getLocalParticipant } from '../base/participants';
+import { extractFqnFromPath } from '../dynamic-branding/functions';
+
+import { REACTIONS } from './constants';
+import logger from './logger';
+
 /**
  * Returns the queue of reactions.
  *
@@ -8,4 +14,75 @@
  */
 export function getReactionsQueue(state: Object) {
     return state['features/reactions'].queue;
+}
+
+/**
+ * Returns reaction key from the reaction message.
+ *
+ * @param {string} message - The reaction message.
+ * @returns {string}
+ */
+export function getReactionKeyByMessage(message: string): ?string {
+    return Object.keys(REACTIONS).find(key => REACTIONS[key].message === `:${message}:`);
+}
+
+/**
+ * Gets reactions key array from concatenated message.
+ *
+ * @param {string} message - The reaction message.
+ * @returns {Array}
+ */
+export function messageToKeyArray(message: string) {
+    let formattedMessage = message.replace(/::/g, '-');
+
+    formattedMessage = formattedMessage.replace(/:/g, '');
+    const messageArray = formattedMessage.split('-');
+
+    return messageArray.map<?string>(getReactionKeyByMessage);
+}
+
+/**
+ * Sends reactions to the backend.
+ *
+ * @param {Object} state - The redux state object.
+ * @param {Array} reactions - Reactions array to be sent.
+ * @returns {void}
+ */
+export async function sendReactionsWebhook(state: Object, reactions: Array<?string>) {
+    const { webhookProxyUrl: url } = state['features/base/config'];
+    const { conference } = state['features/base/conference'];
+    const { jwt } = state['features/base/jwt'];
+    const { locationURL } = state['features/base/connection'];
+    const localParticipant = getLocalParticipant(state);
+
+    const headers = {
+        'Authorization': `Bearer ${jwt}`,
+        'Content-Type': 'application/json'
+    };
+
+
+    const reqBody = {
+        meetingFqn: extractFqnFromPath(locationURL.pathname),
+        sessionId: conference.sessionId,
+        submitted: Date.now(),
+        reactions,
+        participantId: localParticipant.id,
+        participantName: localParticipant.name
+    };
+
+    if (url) {
+        try {
+            const res = await fetch(`${url}/reactions`, {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(reqBody)
+            });
+
+            if (!res.ok) {
+                logger.error('Status error:', res.status);
+            }
+        } catch (err) {
+            logger.error('Could not send request', err);
+        }
+    }
 }
