09af88088ded3ab5e8343f6ebedeffc8d752f95f
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-07-28 21:38:35 +0200

    fix(logging) reduce overly vebose logging
    It huurts performance on mobile. It can still be enabled by setting the level to
    debug / trace.

--- logging_config.js
+++ logging_config.js
@@ -12,6 +12,9 @@ var loggingConfig = {
     // {@link #defaultLogLevel}:
     'modules/RTC/TraceablePeerConnection.js': 'info',
     'modules/statistics/CallStats.js': 'info',
+    'modules/sdp/SDPUtil.js': 'info',
+    'modules/xmpp/JingleSessionPC.js': 'info',
+    'modules/xmpp/strophe.jingle.js': 'info',
     'modules/xmpp/strophe.util.js': 'log'
 };
 
