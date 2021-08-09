ed98eca3267ceef41ac03b47a53c8255bd44d796
Дамян Минков <damencho@jitsi.org>
2021-08-09 11:08:02 -0500

    feat: Rises the stanza limit for unlimited jids (jicofo).
    https://github.com/jitsi/jitsi-meet/blob/c5c4449fbcdfc26b372539aeeabb3fcc882ad131/doc/debian/jitsi-meet-prosody/prosody.cfg.lua-jvb.example#L24

--- resources/prosody-plugins/mod_limits_exception.lua
+++ resources/prosody-plugins/mod_limits_exception.lua
@@ -7,6 +7,9 @@ end
 
 local unlimited_jids = module:get_option_inherited_set("unlimited_jids", {});
 
+-- rises the limit of the stanza size for the unlimited jids, default is 10MB
+local unlimited_stanza_size_limit = module:get_option_number("unlimited_size", 10*1024*1024);
+
 if unlimited_jids:empty() then
 	return;
 end
@@ -19,6 +22,11 @@ module:hook("authentication-success", function (event)
 			session.conn:setlimit(0);
 		elseif session.throttle then
 			session.throttle = nil;
+		end
+
+		if unlimited_stanza_size_limit then
+			module:log('info', 'Setting stanza size limits for %s to %s', jid, unlimited_stanza_size_limit)
+			session.stream:set_stanza_size_limit(unlimited_stanza_size_limit);
 		end
 	end
 end);
