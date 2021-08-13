af85b1d343f530de52c86b3103da591538399c07
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-08-13 10:23:14 +0200

    chore(deps) update eslint-config-jitsi
    Flow rules are now separate from the default ones.

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     'extends': [
         '../.eslintrc.js',
+        'eslint-config-jitsi/flow',
         'eslint-config-jitsi/jsdoc',
         'eslint-config-jitsi/react',
         '.eslintrc-react-native.js'
