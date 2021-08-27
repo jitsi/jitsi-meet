0fc9f6ffc2ff4fc17ef37ba90275b2ef8e7a1e22
robertpin <robert.pin9@gmail.com>
2021-08-27 15:04:35 +0300

    Disable reactions by default on native

--- react/features/chat/middleware.js
+++ react/features/chat/middleware.js
@@ -261,7 +261,7 @@ function _addChatMsgListener(conference, store) {
             const state = store.getState();
 
             if (navigator.product === 'ReactNative') {
-                featureFlag = getFeatureFlag(state, REACTIONS_ENABLED, true);
+                featureFlag = getFeatureFlag(state, REACTIONS_ENABLED, false);
             } else {
                 const { enableReactions } = state['features/base/config'];
 
