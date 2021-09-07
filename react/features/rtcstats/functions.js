418c75c5d3bbcf211484737c15f05a00fefa6cef
Andrei Gavrilescu <gavrilescu.andrei90@gmail.com>
2021-09-07 11:23:54 +0300

    send dominant speaker stats

--- react/features/rtcstats/functions.js
+++ react/features/rtcstats/functions.js
@@ -2,6 +2,8 @@
 
 import { toState } from '../base/redux';
 
+import RTCStats from './RTCStats';
+
 /**
  * Checks whether rtcstats is enabled or not.
  *
@@ -18,4 +20,15 @@ export function isRtcstatsEnabled(stateful: Function | Object) {
     const config = state['features/base/config'];
 
     return config?.analytics?.rtcstatsEnabled ?? false;
+}
+
+/**
+ * Can the rtcstats service send data.
+ *
+ * @param {Function|Object} stateful - The redux store or {@code getState} function.
+ * @returns {boolean}
+ */
+export function canSendRtcstatsData(stateful: Function | Object) {
+
+    return isRtcstatsEnabled(stateful) && RTCStats.isInitialized();
 }
