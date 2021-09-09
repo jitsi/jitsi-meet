150d5a65289229ac7450d55923b2f2ea5f9409e0
Scott Boone <scott.boone@8x8.com>
2021-09-09 11:00:54 -0700

    improved docs

--- resources/prosody-plugins/mod_muc_census.lua
+++ resources/prosody-plugins/mod_muc_census.lua
@@ -1,4 +1,21 @@
--- report list of rooms with # of members and created date, filtered on test rooms
+-- provides an http endpoint at /room-census that reports list of rooms with the
+-- number of members and created date in this JSON format:
+--
+--     {
+--         "room_census": [
+--             {
+--                 "room_name": "<muc name>",
+--                 "participants": <# participants>,
+--                 "created_time": <unix timestamp>,
+--             },
+--             ...
+--         ]
+--     }
+-- 
+-- to activate, add "muc_census" to the modules_enabled table in prosody.cfg.lua
+-- 
+-- warning: this module is unprotected. if you enable it, be sure to secure it
+-- with web server, firewall or ther rules
 
 local jid = require "util.jid";
 local json = require "util.json";
@@ -20,19 +37,7 @@ local tostring = tostring;
 local muc_domain_prefix = module:get_option_string("muc_mapper_domain_prefix", "conference");
 
 --- handles request to get number of participants in all rooms
--- @return GET response, containing json in this form:
---
---     {
---         "room_census": [
---             {
---                 "room_name": "<muc name>",
---                 "participants": <# participants>,
---                 "created_time": <unix timestamp>,
---             },
---             ...
---         ]
---     }
---
+-- @return GET response
 function handle_get_room_census(event)
     local host_session = prosody.hosts["conference." .. tostring(module.host)]
     if not host_session or not host_session.modules.muc then
