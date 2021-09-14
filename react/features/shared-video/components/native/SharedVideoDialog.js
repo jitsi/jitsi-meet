b92c1f52d55db569d7a299b07301a334d049041c
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-09-14 12:22:45 +0200

    fix(shared-video) make placeholder translatable
    Also fix capitalization.

--- react/features/shared-video/components/native/SharedVideoDialog.js
+++ react/features/shared-video/components/native/SharedVideoDialog.js
@@ -3,9 +3,9 @@
 import React from 'react';
 
 import { InputDialog } from '../../../base/dialog';
+import { translate } from '../../../base/i18n';
 import { connect } from '../../../base/redux';
 import { ColorPalette } from '../../../base/styles';
-import { defaultSharedVideoLink } from '../../constants';
 import AbstractSharedVideoDialog from '../AbstractSharedVideoDialog';
 
 /**
@@ -41,16 +41,20 @@ class SharedVideoDialog extends AbstractSharedVideoDialog<*> {
      * @inheritdoc
      */
     render() {
+        const { t } = this.props;
+
         return (
             <InputDialog
                 contentKey = 'dialog.shareVideoTitle'
                 onSubmit = { this._onSubmitValue }
                 textInputProps = {{
-                    placeholder: defaultSharedVideoLink,
+                    autoCapitalize: 'none',
+                    autoCorrect: false,
+                    placeholder: t('dialog.sharedVideoLinkPlaceholder'),
                     placeholderTextColor: ColorPalette.lightGrey
                 }} />
         );
     }
 }
 
-export default connect()(SharedVideoDialog);
+export default translate(connect()(SharedVideoDialog));
