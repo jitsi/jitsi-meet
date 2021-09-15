ae681fccb836837634e1213fd64836c5f50bc88a
Fly me to the moon <git@bigly.dog>
2021-09-15 12:16:17 -0700

    no hardcode trans name

--- _nuxaas/branding.py
+++ _nuxaas/branding.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from argparse import ArgumentParser, Namespace
 from collections.abc import ByteString
 from json import dumps, loads
 from os import linesep
@@ -14,17 +15,20 @@ _LANG = _TOP_LV / "lang"
 
 
 _FLAGS = RegexFlag.MULTILINE | RegexFlag.IGNORECASE
-_RE_1 = compile("jitsi meet", flags=_FLAGS)
-_RE_2 = compile("jitsi", flags=_FLAGS)
+_LNAME_RE = compile("jitsi meet", flags=_FLAGS)
+_SNAME_RE = compile("jitsi", flags=_FLAGS)
 
 
-def _simple_trans(val: Any) -> Any:
-    if not isinstance(val, str):
-        return val
-    else:
-        v1 = _RE_1.sub("kΛ Talk", val)
-        v2 = _RE_2.sub("kΛ", v1)
-        return v2
+def _simple_trans(l_name: str, s_name: str) -> Callable[[Any], Any]:
+    def cont(val: Any) -> Any:
+        if not isinstance(val, str):
+            return val
+        else:
+            v1 = _LNAME_RE.sub(l_name, val)
+            v2 = _SNAME_RE.sub(s_name, v1)
+            return v2
+
+    return cont
 
 
 def _map(fn: Callable[[Any], Any], x: Any) -> Any:
@@ -36,20 +40,31 @@ def _map(fn: Callable[[Any], Any], x: Any) -> Any:
         return fn(x)
 
 
+def _parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("--l-name", required=True)
+    parser.add_argument("--s-name", required=True)
+    return parser.parse_args()
+
+
 def main() -> None:
+    args = _parse_args()
+
     cols, _ = get_terminal_size()
     line = linesep + cols * "-" + linesep
+    trans = _simple_trans(args.l_name, args.s_name)
+
     for path in _LANG.glob("*.json"):
         raw = path.read_text()
         json = loads(raw)
-        xformed = _map(_simple_trans, x=json)
+        xformed = _map(trans, x=json)
         raw = dumps(xformed, check_circular=False, ensure_ascii=False, indent=4)
         path.write_text(raw)
         print(path, line, end="", file=stderr)
 
     for path in _TOP_LV.glob("*.html"):
         html = path.read_text()
-        html = _RE_1.sub("kΛ Talk", html)
+        html = _LNAME_RE.sub(args.l_name, html)
         path.write_text(html)
 
 
