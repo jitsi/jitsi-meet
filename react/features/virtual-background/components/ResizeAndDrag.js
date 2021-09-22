1f51ae161c84405d1782b7e03acdd7264083a835
tudordan7 <tudor.pop@decagon.tech>
2021-09-22 16:28:03 +0300

    feat(virtual-background): Resize and drag and drop on virtual background.

--- react/features/virtual-background/components/ResizeAndDrag.js
+++ react/features/virtual-background/components/ResizeAndDrag.js
@@ -40,6 +40,91 @@ type Props = {
  * @returns {ReactElement}
  */
 function ResizeAndDrag({ _currentCameraDeviceId, _virtualBackground, dispatch, updateTransparent }: Props) {
+
+    // Define several math function.
+
+    /**
+     * Get client rect.
+     *
+     * @param {Object} rotatedBox - Object that contains dimensions positions and rotation of resizable element.
+     * @returns {void}
+     */
+    function getClientRect(rotatedBox) {
+        const { x, y, width, height } = rotatedBox;
+        const rad = rotatedBox.rotation;
+
+        const p1 = getCorner(x, y, 0, 0, rad);
+        const p2 = getCorner(x, y, width, 0, rad);
+        const p3 = getCorner(x, y, width, height, rad);
+        const p4 = getCorner(x, y, 0, height, rad);
+
+        const minX = Math.min(p1.x, p2.x, p3.x, p4.x);
+        const minY = Math.min(p1.y, p2.y, p3.y, p4.y);
+        const maxX = Math.max(p1.x, p2.x, p3.x, p4.x);
+        const maxY = Math.max(p1.y, p2.y, p3.y, p4.y);
+
+        return {
+            x: minX,
+            y: minY,
+            width: maxX - minX,
+            height: maxY - minY
+        };
+    }
+
+    /**
+     * Get Corner.
+     *
+     * @param {number} pivotX - The min x position.
+     * @param {number} pivotY - The min y position.
+     * @param {number} diffX - The box width.
+     * @param {number} diffY - The box height.
+     * @param {number} angle - Rotation details.
+     * @returns {void}
+     */
+    function getCorner(pivotX, pivotY, diffX, diffY, angle) { // eslint-disable-line max-params
+        const distance = Math.sqrt((diffX * diffX) + (diffY * diffY));
+
+        // Find angle from pivot to corner.
+        // eslint-disable-next-line no-param-reassign
+        angle += Math.atan2(diffY, diffX);
+
+        // Get new x and y and round it off to integer.
+        const x = pivotX + (distance * Math.cos(angle));
+        const y = pivotY + (distance * Math.sin(angle));
+
+        return {
+            x,
+            y
+        };
+    }
+
+    /**
+     * Get total boxes on drag action.
+     *
+     * @param {Object} boxes - Object that contains dimensions and positions of draggable element.
+     * @returns {void}
+     */
+    function getTotalBox(boxes) {
+        let minX = Infinity;
+        let minY = Infinity;
+        let maxX = -Infinity;
+        let maxY = -Infinity;
+
+        boxes.forEach(box => {
+            minX = Math.min(minX, box.x);
+            minY = Math.min(minY, box.y);
+            maxX = Math.max(maxX, box.x + box.width);
+            maxY = Math.max(maxY, box.y + box.height);
+        });
+
+        return {
+            x: minX,
+            y: minY,
+            width: maxX - minX,
+            height: maxY - minY
+        };
+    }
+
     const dragAndResizeRef = useRef();
     const [ desktopShareType ]
     = useState((_virtualBackground.backgroundType === VIRTUAL_BACKGROUND_TYPE.TRANSPARENT_PREVIEW)
@@ -51,7 +136,7 @@ function ResizeAndDrag({ _currentCameraDeviceId, _virtualBackground, dispatch, u
     /**
      * Apply video preview loader.
      *
-     * @returns {Promise}
+     * @returns {void}
      */
     const loadVideoPreview = () =>
         (
@@ -169,7 +254,51 @@ function ResizeAndDrag({ _currentCameraDeviceId, _virtualBackground, dispatch, u
 
             // Create new transformer
             const personTr = new Konva.Transformer({
-                node: image
+                node: image,
+                boundBoxFunc: (oldBox, newBox) => {
+                    const box = getClientRect(newBox);
+                    const isOut = box.x < 0 || box.y < 0 || box.x + box.width > stage.width()
+                    || box.y + box.height > stage.height();
+
+                    // If new bounding box is out of visible viewport, let's just skip transforming
+                    // this logic can be improved by still allow some transforming if we have small available space.
+                    if (isOut) {
+                        return oldBox;
+                    }
+
+                    return newBox;
+                }
+            });
+
+            personTr.on('dragmove', () => {
+                const boxes = personTr.nodes().map(node => node.getClientRect());
+                const box = getTotalBox(boxes);
+
+                personTr.nodes().forEach(shape => {
+                    const absPos = shape.getAbsolutePosition();
+
+                    // Where are shapes inside bounding box of all shapes?
+                    const offsetX = box.x - absPos.x;
+
+                    const offsetY = box.y - absPos.y;
+
+                    // We total box goes outside of viewport, we need to move absolute position of shape.
+                    const newAbsPos = { ...absPos };
+
+                    if (box.x < 0) {
+                        newAbsPos.x = -offsetX;
+                    }
+                    if (box.y < 0) {
+                        newAbsPos.y = -offsetY;
+                    }
+                    if (box.x + box.width > stage.width()) {
+                        newAbsPos.x = stage.width() - box.width - offsetX;
+                    }
+                    if (box.y + box.height > stage.height()) {
+                        newAbsPos.y = stage.height() - box.height - offsetY;
+                    }
+                    shape.setAbsolutePosition(newAbsPos);
+                });
             });
 
             // Enable specific anchors.
