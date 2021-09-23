a6359e5d4c5a15cf217e4252c7d8d0e5677cc515
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-09-23 11:12:31 +0200

    fix(lobby) don't mix web and native actions on the same file
    USe actions.any for common actions.

--- react/features/lobby/actions.native.js
+++ react/features/lobby/actions.native.js
@@ -1,10 +1,24 @@
 // @flow
 
+import { type Dispatch } from 'redux';
+
+import { appNavigate } from '../app/actions';
 import { openDialog } from '../base/dialog';
 
 import { DisableLobbyModeDialog, EnableLobbyModeDialog } from './components/native';
 
-export * from './actions.web';
+export * from './actions.any';
+
+/**
+ * Cancels the ongoing knocking and abandons the join flow.
+ *
+ * @returns {Function}
+ */
+export function cancelKnocking() {
+    return async (dispatch: Dispatch<any>) => {
+        dispatch(appNavigate(undefined));
+    };
+}
 
 /**
  * Action to show the dialog to disable lobby mode.
