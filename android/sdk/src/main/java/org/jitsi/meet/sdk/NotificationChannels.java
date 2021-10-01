6124e72f215f40eb466d3c36094eb1000521e572
tmoldovan8x8 <62697631+tmoldovan8x8@users.noreply.github.com>
2021-10-01 10:17:23 +0300

    feat(android) extract notification channel name

--- android/sdk/src/main/java/org/jitsi/meet/sdk/NotificationChannels.java
+++ android/sdk/src/main/java/org/jitsi/meet/sdk/NotificationChannels.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class NotificationChannels {
     static final String ONGOING_CONFERENCE_CHANNEL_ID = "JitsiOngoingConferenceChannel";
-    static final String ONGOING_CONFERNCE_CHANNEL_NAME = "Ongoing Conference Notifications";
 
     public static List<String> allIds = new ArrayList<String>() {{ add(ONGOING_CONFERENCE_CHANNEL_ID); }};
 }
