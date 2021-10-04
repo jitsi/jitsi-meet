c9aeb756eec6fda266bcdadebcd9a8a2581776b0
titus.moldovan <titus.moldovan@8x8.com>
2021-10-04 14:30:23 +0300

    adds doc

--- android/sdk/src/main/java/org/jitsi/meet/sdk/JitsiInitializer.java
+++ android/sdk/src/main/java/org/jitsi/meet/sdk/JitsiInitializer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright @ 2021-present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jitsi.meet.sdk;
 
 import android.content.Context;
@@ -10,13 +25,13 @@ import com.facebook.soloader.SoLoader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JitsiInitializer implements Initializer<Void> {
+public class JitsiInitializer implements Initializer<Object> {
 
     @NonNull
     @Override
-    public Void create(@NonNull Context context) {
+    public Object create(@NonNull Context context) {
         SoLoader.init(context, /* native exopackage */ false);
-        return null;
+        return new Object();
     }
 
     @NonNull
