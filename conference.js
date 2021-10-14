7c342524e03e4cb8bd0550db286cbcdaf3aaa72e
robertpin <robert.pin9@gmail.com>
2021-10-14 13:17:56 +0300

    fix(screenshare) Add timestamp to desktop track
    Send screenshare duration to analytics

--- conference.js
+++ conference.js
@@ -107,6 +107,7 @@ import {
     getLocalJitsiAudioTrack,
     getLocalJitsiVideoTrack,
     getLocalTracks,
+    getLocalVideoTrack,
     isLocalCameraTrackMuted,
     isLocalTrackMuted,
     isUserInteractionRequiredForUnmute,
@@ -1548,6 +1549,8 @@ export default {
         if (config.enableScreenshotCapture) {
             APP.store.dispatch(toggleScreenshotCaptureSummary(false));
         }
+        const tracks = APP.store.getState()['features/base/tracks'];
+        const timestamp = getLocalVideoTrack(tracks)?.timestamp ?? 0;
 
         // It can happen that presenter GUM is in progress while screensharing is being turned off. Here it needs to
         // wait for that GUM to be resolved in order to prevent leaking the presenter track(this.localPresenterVideo
@@ -1609,7 +1612,8 @@ export default {
         return promise.then(
             () => {
                 this.videoSwitchInProgress = false;
-                sendAnalytics(createScreenSharingEvent('stopped'));
+                sendAnalytics(createScreenSharingEvent('stopped',
+                    timestamp === 0 ? null : (Date.now() / 1000) - timestamp));
                 logger.info('Screen sharing stopped.');
             },
             error => {
