66cd61fd70bbfa495859e3eab277e6491a7d6c14
Brent Keath <brent.keath@intulse.com>
2021-10-15 10:50:24 -0400

    Testing prejoin skip options

--- react/features/base/settings/reducer.js
+++ react/features/base/settings/reducer.js
@@ -41,7 +41,8 @@ const DEFAULT_STATE = {
     userSelectedAudioOutputDeviceLabel: undefined,
     userSelectedCameraDeviceLabel: undefined,
     userSelectedMicDeviceLabel: undefined,
-    userSelectedSkipPrejoin: undefined
+    // TODO: Brent testing
+    userSelectedSkipPrejoin: true
 };
 
 const STORE_NAME = 'features/base/settings';
