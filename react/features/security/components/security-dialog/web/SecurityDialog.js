ddb5d76113b95a40919d252b9189c98e400e7562
Brent Keath <brent.keath@intulse.com>
2021-10-19 09:39:05 -0400

    Added _lobbyEnabled as a prop in the security dialog

--- react/features/security/components/security-dialog/web/SecurityDialog.js
+++ react/features/security/components/security-dialog/web/SecurityDialog.js
@@ -14,6 +14,11 @@ import PasswordSection from './PasswordSection';
 type Props = {
 
     /**
+     * State of the lobby mode.
+     */
+    _lobbyEnabled: boolean,
+
+    /**
      * Whether or not the current user can modify the current password.
      */
     _canEditPassword: boolean,
@@ -57,6 +62,7 @@ type Props = {
  * @returns {React$Element<any>}
  */
 function SecurityDialog({
+    _lobbyEnabled,
     _canEditPassword,
     _conference,
     _locked,
