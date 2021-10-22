0715f85796150d79b10bf36d7dfe26f42adea0d1
robertpin <robert.pin9@gmail.com>
2021-10-22 16:50:07 +0300

    fix(video-thumbnail) Fixed name for remote participants
    Display name on thumbnail for remote participants as well
    Updated style

--- react/features/filmstrip/components/web/Thumbnail.js
+++ react/features/filmstrip/components/web/Thumbnail.js
@@ -812,7 +812,7 @@ class Thumbnail extends Component<Props, State> {
                 </span>
                 <div className = 'videocontainer__toolbar'>
                     <StatusIndicators participantID = { id } />
-                    <div>{ _participant.name }</div>
+                    <span className = 'videocontainer__participant-name'>{_participant.name}</span>
                 </div>
                 <div className = 'videocontainer__toptoolbar'>
                     { this._renderTopIndicators() }
@@ -960,6 +960,7 @@ class Thumbnail extends Component<Props, State> {
                 </div>
                 <div className = 'videocontainer__toolbar'>
                     <StatusIndicators participantID = { id } />
+                    <span className = 'videocontainer__participant-name'>{_participant.name}</span>
                 </div>
                 <div className = 'videocontainer__hoverOverlay' />
                 <div className = 'displayNameContainer'>
