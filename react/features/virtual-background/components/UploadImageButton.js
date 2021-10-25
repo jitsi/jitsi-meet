8174dcb00ec2ba447c74181e9e458a83da3b96a7
Pavol Cvengros <pavol.cvengros@securcom.me>
2021-10-25 12:04:51 +0200

    chore(deps) update uuid package to 8.3.2
    Co-authored-by: Saúl Ibarra Corretgé <saghul@jitsi.org>

--- react/features/virtual-background/components/UploadImageButton.js
+++ react/features/virtual-background/components/UploadImageButton.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { useCallback, useRef } from 'react';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 import { translate } from '../../base/i18n';
 import { Icon, IconPlusCircle } from '../../base/icons';
@@ -72,7 +72,7 @@ function UploadImageButton({
         reader.readAsDataURL(imageFile[0]);
         reader.onload = async () => {
             const url = await resizeImage(reader.result);
-            const uuId = uuid.v4();
+            const uuId = uuidv4();
 
             setStoredImages([
                 ...storedImages,
