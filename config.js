36e81569a4931ac59b043e8b0e5c9cb7aeac7306
Brent Keath <brent.keath@intulse.com>
2021-11-01 10:18:58 -0400

    Trying another config change I found in the Jitsi-Meet forums

--- config.js
+++ config.js
@@ -193,7 +193,7 @@ var config = {
     },
 
     // Enable / disable simulcast support.
-    // disableSimulcast: false,
+    disableSimulcast: true,
 
     // Enable / disable layer suspension.  If enabled, endpoints whose HD layers are not in use will be suspended
     // (no longer sent) until they are requested again. This is enabled by default. This must be enabled for screen
