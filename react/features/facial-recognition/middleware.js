048954d6bfdbb3d42325e6038f5f6fcd5b0d3936
Gabriel Borlea <gabriel.borlea@8x8.com>
2021-11-08 16:42:23 +0200

    refactor(facial-expression): middleware

--- react/features/facial-recognition/middleware.js
+++ react/features/facial-recognition/middleware.js
@@ -3,8 +3,7 @@
 import {
     CONFERENCE_JOINED,
     CONFERENCE_WILL_LEAVE,
-    getCurrentConference,
-    getIsConferenceJoined
+    getCurrentConference
 } from '../base/conference';
 import { getParticipantCount } from '../base/participants';
 import { MiddlewareRegistry } from '../base/redux';
@@ -22,37 +21,30 @@ import {
 import { sendFacialExpressionToParticipants, sendFacialExpressionToServer } from './functions';
 
 
-MiddlewareRegistry.register(store => next => action => {
-    const { enableFacialRecognition } = store.getState()['features/base/config'];
+MiddlewareRegistry.register(({ dispatch, getState }) => next => action => {
+    const { enableFacialRecognition } = getState()['features/base/config'];
 
     if (!enableFacialRecognition) {
         return next(action);
     }
-
     if (action.type === CONFERENCE_JOINED) {
-        const { dispatch } = store;
-
         dispatch(loadWorker());
         dispatch(startFacialRecognition());
 
         return next(action);
     }
+    if (!getCurrentConference(getState())) {
 
-    if (!getIsConferenceJoined(store.getState())) {
         return next(action);
     }
+
     switch (action.type) {
     case CONFERENCE_WILL_LEAVE : {
-        const { dispatch } = store;
-
         dispatch(stopFacialRecognition());
 
         return next(action);
     }
-
     case TRACK_UPDATED: {
-        const { dispatch } = store;
-
         const { videoType, type } = action.track.jitsiTrack;
 
         if (videoType === 'camera') {
@@ -74,7 +66,6 @@ MiddlewareRegistry.register(store => next => action => {
         return next(action);
     }
     case TRACK_ADDED: {
-        const { dispatch } = store;
         const { mediaType, videoType } = action.track;
 
         if (mediaType === 'presenter' && videoType === 'camera') {
@@ -84,9 +75,7 @@ MiddlewareRegistry.register(store => next => action => {
 
         return next(action);
     }
-
     case TRACK_REMOVED: {
-        const { dispatch } = store;
         const { videoType } = action.track.jitsiTrack;
 
         if ([ 'camera', 'desktop' ].includes(videoType)) {
@@ -95,17 +84,12 @@ MiddlewareRegistry.register(store => next => action => {
 
         return next(action);
     }
-
     case VIRTUAL_BACKGROUND_TRACK_CHANGED: {
-        const { dispatch } = store;
-
         dispatch(resetTrack());
 
         return next(action);
     }
-
     case ADD_FACIAL_EXPRESSION: {
-        const { getState } = store;
         const state = getState();
         const conference = getCurrentConference(state);
 
