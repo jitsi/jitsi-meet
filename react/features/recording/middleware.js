deca2e55cced817cf643be78b09c792d6abd5d86
Hristo Terezov <hristo@jitsi.org>
2021-11-09 12:58:49 -0600

    fix(recorder): "already started" notification

--- react/features/recording/middleware.js
+++ react/features/recording/middleware.js
@@ -27,6 +27,7 @@ import {
     showPendingRecordingNotification,
     showRecordingError,
     showRecordingLimitNotification,
+    showRecordingWarning,
     showStartedRecordingNotification,
     showStoppedRecordingNotification,
     updateRecordingSessionData
@@ -262,10 +263,11 @@ function _showRecordingErrorNotification(recorderSession, dispatch) {
         }));
         break;
     case JitsiMeetJS.constants.recording.error.UNEXPECTED_REQUEST:
-        dispatch(showRecordingError({
-            titleKey: isStreamMode
+        dispatch(showRecordingWarning({
+            descriptionKey: isStreamMode
                 ? 'liveStreaming.sessionAlreadyActive'
-                : 'recording.sessionAlreadyActive'
+                : 'recording.sessionAlreadyActive',
+            titleKey: isStreamMode ? 'liveStreaming.inProgress' : 'recording.inProgress'
         }));
         break;
     default:
