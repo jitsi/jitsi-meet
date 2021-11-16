1cb5de56dedbed1d869ac6d545aaae63c0f872cf
Calin Chitu <calinte2002@yahoo.co.uk>
2021-11-16 15:22:35 +0200

    feat(chat/native) fixed scroll inside chat room

--- react/features/chat/components/native/ChatMessageGroup.js
+++ react/features/chat/components/native/ChatMessageGroup.js
@@ -6,7 +6,6 @@ import { FlatList } from 'react-native';
 import { MESSAGE_TYPE_LOCAL, MESSAGE_TYPE_REMOTE } from '../../constants';
 
 import ChatMessage from './ChatMessage';
-import styles from './styles';
 
 type Props = {
 
@@ -43,8 +42,7 @@ export default class ChatMessageGroup extends Component<Props> {
                 data = { this.props.messages }
                 inverted = { true }
                 keyExtractor = { this._keyExtractor }
-                renderItem = { this._renderMessage }
-                style = { styles.messageContainer } />
+                renderItem = { this._renderMessage } />
         );
     }
 
