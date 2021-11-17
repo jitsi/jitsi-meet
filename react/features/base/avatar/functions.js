76a976b5d1bb8a596cfaa372ebce935f8ad29e50
Vlad Piersec <vlad.piersec@8x8.com>
2021-11-17 10:49:08 +0200

    fix(Avatar): Display correctly any emoji/special character in a avatar initials

--- react/features/base/avatar/functions.js
+++ react/features/base/avatar/functions.js
@@ -1,5 +1,6 @@
 // @flow
 
+import GraphemeSplitter from 'grapheme-splitter';
 import _ from 'lodash';
 
 const AVATAR_COLORS = [
@@ -13,6 +14,8 @@ const AVATAR_COLORS = [
     '#2AA076',
     '#00A8B3'
 ];
+const wordSplitRegex = (/\s+|\.+|_+|;+|-+|,+|\|+|\/+|\\+/);
+const splitter = new GraphemeSplitter();
 
 /**
  * Generates the background color of an initials based avatar.
@@ -41,6 +44,20 @@ export function getAvatarColor(initials: ?string, customAvatarBackgrounds: Array
 }
 
 /**
+ * Returns the first grapheme from a word, uppercased.
+ *
+ * @param {string} word - The string to get grapheme from.
+ * @returns {string}
+ */
+function getFirstGraphemeUpper(word) {
+    if (!word?.length) {
+        return '';
+    }
+
+    return splitter.splitGraphemes(word)[0].toUpperCase();
+}
+
+/**
  * Generates initials for a simple string.
  *
  * @param {string?} s - The string to generate initials for.
@@ -49,12 +66,7 @@ export function getAvatarColor(initials: ?string, customAvatarBackgrounds: Array
 export function getInitials(s: ?string) {
     // We don't want to use the domain part of an email address, if it is one
     const initialsBasis = _.split(s, '@')[0];
-    const words = _.words(initialsBasis);
-    let initials = '';
+    const [ firstWord, secondWord ] = initialsBasis.split(wordSplitRegex);
 
-    for (const w of words) {
-        (initials.length < 2) && (initials += String.fromCodePoint(w.codePointAt(0)).toUpperCase());
-    }
-
-    return initials;
+    return getFirstGraphemeUpper(firstWord) + getFirstGraphemeUpper(secondWord);
 }
