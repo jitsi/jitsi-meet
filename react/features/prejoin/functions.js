6520cc2496a4ee99be45e934f79571fdda06b9b0
Payetus <payet91@gmail.com>
2021-11-17 17:24:18 +0100

    feat(Lobby): Adding autoknock feature as a config  (#10366)
    * adding lobby localization to "ca" lang
    
    * feat(Lobby): Adding autoknock config
    
    feat(Lobby): Adding autoknock config
    
    Co-authored-by: Payetus <payet91@gmail.xom>

--- react/features/prejoin/functions.js
+++ react/features/prejoin/functions.js
@@ -180,8 +180,8 @@ export function isPrejoinPageVisible(state: Object): boolean {
  * @returns {boolean}
  */
 export function shouldAutoKnock(state: Object): boolean {
-    const { iAmRecorder, iAmSipGateway } = state['features/base/config'];
+    const { iAmRecorder, iAmSipGateway, autoKnockLobby } = state['features/base/config'];
 
-    return (isPrejoinPageEnabled(state) || (iAmRecorder && iAmSipGateway))
+    return (isPrejoinPageEnabled(state) || autoKnockLobby || (iAmRecorder && iAmSipGateway))
         && !state['features/lobby'].knocking;
 }
