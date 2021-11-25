4b3456ac0c072a8a92a327ba9bf6b94c854c39be
Calin Chitu <calinte2002@yahoo.co.uk>
2021-11-25 20:22:00 +0200

    feat(branding) added native extension to updateTheme helper

--- react/features/base/ui/components/BaseTheme.native.js
+++ react/features/base/ui/components/BaseTheme.native.js
@@ -3,7 +3,7 @@
 import { font, colors, colorMap, spacing, shape, typography } from '../Tokens';
 import { createNativeTheme } from '../functions.native';
 
-import updateTheme from './updateTheme';
+import updateTheme from './updateTheme.native';
 
 export default createNativeTheme(updateTheme({
     font,
