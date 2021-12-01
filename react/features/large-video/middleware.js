d9f71ee238e72b6c31841b1f093e971c466ecd9c
Saúl Ibarra Corretgé <saghul@jitsi.org>
2021-12-01 15:00:38 +0100

    fix(etherpad) fix Etherpad closing when dominant speaker changes
    Fixes: https://github.com/jitsi/jitsi-meet/issues/8109

--- react/features/large-video/middleware.js
+++ react/features/large-video/middleware.js
@@ -13,6 +13,7 @@ import {
     TRACK_ADDED,
     TRACK_REMOVED
 } from '../base/tracks';
+import { TOGGLE_DOCUMENT_EDITING } from '../etherpad/actionTypes';
 
 import { selectParticipantInLargeVideo } from './actions';
 import logger from './logger';
@@ -47,6 +48,7 @@ MiddlewareRegistry.register(store => next => action => {
     case PARTICIPANT_JOINED:
     case PARTICIPANT_LEFT:
     case PIN_PARTICIPANT:
+    case TOGGLE_DOCUMENT_EDITING:
     case TRACK_ADDED:
     case TRACK_REMOVED:
         store.dispatch(selectParticipantInLargeVideo());
