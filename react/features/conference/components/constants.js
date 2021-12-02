4088a0407341dfdb2b840b51b78e4b322204a335
Ivan Jiang <ivanjiang5628@gmail.com>
2021-12-02 09:39:25 -0800

    stop showing the room name and video quality display in conference info.
    (cherry picked from commit 6720b8ff5b922171fa3be48f2196ac368facd70e)

--- react/features/conference/components/constants.js
+++ react/features/conference/components/constants.js
@@ -1,12 +1,14 @@
 export const CONFERENCE_INFO = {
     alwaysVisible: [ 'recording', 'local-recording' ],
     autoHide: [
-        'subject',
-        'conference-timer',
-        'participants-count',
-        'e2ee',
-        'transcribing',
-        'video-quality',
-        'insecure-room'
+
+        // 'subject',
+        'conference-timer'
+
+        // 'participants-count',
+        // 'e2ee',
+        // 'transcribing',
+        // 'video-quality',
+        // 'insecure-room'
     ]
 };
