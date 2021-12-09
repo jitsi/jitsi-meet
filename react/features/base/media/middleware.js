9528cbad0edf90a09b736d2e4fc94062cb576ccd
Jaya Allamsetty <jaya.allamsetty@8x8.com>
2021-12-09 14:37:32 -0500

    fix(notifications) Adjust the timeout for unmute blocked notifications.

--- react/features/base/media/middleware.js
+++ react/features/base/media/middleware.js
@@ -94,7 +94,7 @@ MiddlewareRegistry.register(store => next => action => {
             store.dispatch(showWarningNotification({
                 descriptionKey: 'notify.audioUnmuteBlockedDescription',
                 titleKey: 'notify.audioUnmuteBlockedTitle'
-            }, NOTIFICATION_TIMEOUT_TYPE.LONG));
+            }, NOTIFICATION_TIMEOUT_TYPE.MEDIUM));
         }
         break;
     }
@@ -119,7 +119,7 @@ MiddlewareRegistry.register(store => next => action => {
             store.dispatch(showWarningNotification({
                 descriptionKey: 'notify.videoUnmuteBlockedDescription',
                 titleKey: 'notify.videoUnmuteBlockedTitle'
-            }, NOTIFICATION_TIMEOUT_TYPE.LONG));
+            }, NOTIFICATION_TIMEOUT_TYPE.MEDIUM));
         }
         break;
     }
