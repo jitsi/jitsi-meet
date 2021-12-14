fc4a9d2c42efe2ceb27ba202c7a49a780ae5d76f
Calin Chitu <calinte2002@yahoo.co.uk>
2021-12-14 10:53:18 +0200

    feat(detox) finished setup

---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,14 @@
+describe('Example', () => {
+    beforeAll(async () => {
+        await device.launchApp();
+        newInstance: true
+    });
+
+    beforeEach(async () => {
+        await device.reloadReactNative();
+    });
+
+    it('should have welcome screen', async () => {
+        await expect(element(by.id('welcome-screen'))).toBeVisible();
+    });
+});
