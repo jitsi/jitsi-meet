b1ce56c3c3979f785bcc585554a397bb9360eb23
Дамян Минков <damencho@jitsi.org>
2021-12-21 14:35:37 -0600

    feat: Drop unused constants.

--- react/features/breakout-rooms/constants.js
+++ react/features/breakout-rooms/constants.js
@@ -4,19 +4,3 @@
  * Key for this feature.
  */
 export const FEATURE_KEY = 'features/breakout-rooms';
-
-/**
-  * The type of json-message which indicates that json carries
-  * a request for a participant to move to a specified room.
-  */
-export const JSON_TYPE_MOVE_TO_ROOM_REQUEST = `${FEATURE_KEY}/move-to-room`;
-
-/**
-  * The type of json-message which indicates that json carries a request to remove a specified breakout room.
-  */
-export const JSON_TYPE_REMOVE_BREAKOUT_ROOM = `${FEATURE_KEY}/remove`;
-
-/**
-  * The type of json-message which indicates that json carries breakout rooms data.
-  */
-export const JSON_TYPE_UPDATE_BREAKOUT_ROOMS = `${FEATURE_KEY}/update`;
