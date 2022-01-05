f72268af3307bad98b2b33f5f396a81b7ad97e65
Andrei Bora <andrei.bora@8x8.com>
2022-01-05 16:52:21 +0200

    Console log settings error.

--- react/features/jaas/middleware.web.js
+++ react/features/jaas/middleware.web.js
@@ -1,9 +1,13 @@
 import { redirectToStaticPage } from '../app/actions';
+import {
+    JitsiConferenceErrors,
+    JitsiConferenceEvents
+} from '../base/lib-jitsi-meet';
 import { MiddlewareRegistry } from '../base/redux';
 
-
-import { SET_DETAILS } from './actionTypes';
+import { SET_DETAILS, CONFERENCE_JOINED } from './actionTypes';
 import { STATUSES } from './constants';
+import logger from './logger';
 
 /**
  * The redux middleware for jaas.
@@ -19,8 +23,35 @@ MiddlewareRegistry.register(store => next => async action => {
         if (status === STATUSES.BLOCKED) {
             store.dispatch(redirectToStaticPage('/static/planLimit.html'));
         }
+        break;
+    }
+
+    case CONFERENCE_JOINED:{
+        _addErrorMsgListener(action.conference, store);
     }
     }
 
     return next(action);
 });
+
+/** .........
+ * Registers listener for {@link JitsiConferenceEvents.CONFERENCE_ERROR} that
+ * will log the error message if the error type is {@link JitsiConferenceErrors.SETTINGS_ERROR}
+ *
+ * @param {JitsiConference} conference - The conference instance on which the
+ * new event listener will be registered.
+ * @param {Object} store - The redux store object.
+ * @private
+ * @returns {void}
+ */
+function _addErrorMsgListener(conference, store) {
+    if (store.getState()['features/base/config'].iAmRecorder) {
+        // We don't register anything on web if we are in iAmRecorder mode
+        return;
+    }
+
+    conference.on(
+        JitsiConferenceEvents.CONFERENCE_ERROR, (errorType, errorMsg) => {
+            errorType === JitsiConferenceErrors.SETTINGS_ERROR && logger.error(errorMsg);
+        });
+}
