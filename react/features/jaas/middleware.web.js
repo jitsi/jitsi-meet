5081706ebf901f72d9a6c154e4785714d011c717
Andrei Bora <andrei.bora@8x8.com>
2022-01-10 12:55:16 +0200

    Resolve code review

--- react/features/jaas/middleware.web.js
+++ react/features/jaas/middleware.web.js
@@ -18,6 +18,21 @@ import logger from './logger';
  */
 MiddlewareRegistry.register(store => next => async action => {
     switch (action.type) {
+    case CONFERENCE_JOINED: {
+        const { conference } = action;
+
+        if (store.getState()['features/base/config'].iAmRecorder) {
+            // We don't register anything on web if we are in iAmRecorder mode
+            return;
+        }
+
+        conference.on(
+            JitsiConferenceEvents.CONFERENCE_ERROR, (errorType, errorMsg) => {
+                errorType === JitsiConferenceErrors.SETTINGS_ERROR && logger.error(errorMsg);
+            });
+        break;
+    }
+
     case SET_DETAILS: {
         const { status } = action.payload;
 
@@ -26,33 +41,7 @@ MiddlewareRegistry.register(store => next => async action => {
         }
         break;
     }
-
-    case CONFERENCE_JOINED:{
-        _addErrorMsgListener(action.conference, store);
-    }
     }
 
     return next(action);
 });
-
-/** .........
- * Registers listener for {@link JitsiConferenceEvents.CONFERENCE_ERROR} that will
- * log the error message if the error type is {@link JitsiConferenceErrors.SETTINGS_ERROR} .
- *
- * @param {JitsiConference} conference - The conference instance on which the
- * new event listener will be registered.
- * @param {Object} store - The redux store object.
- * @private
- * @returns {void}
- */
-function _addErrorMsgListener(conference, store) {
-    if (store.getState()['features/base/config'].iAmRecorder) {
-        // We don't register anything on web if we are in iAmRecorder mode
-        return;
-    }
-
-    conference.on(
-        JitsiConferenceEvents.CONFERENCE_ERROR, (errorType, errorMsg) => {
-            errorType === JitsiConferenceErrors.SETTINGS_ERROR && logger.error(errorMsg);
-        });
-}
