89c6cc9042b43e48e424719e473ddc15850ff8d6
Calin Chitu <calinte2002@yahoo.co.uk>
2022-01-11 14:30:14 +0200

    feat(detox) review remarks

--- e2e/MuteAudioVideoTest.e2e.js
+++ e2e/MuteAudioVideoTest.e2e.js
@@ -1,5 +1,9 @@
 // @flow
 
+import { v4 as uuidv4 } from 'uuid';
+
+const sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
+
 describe('Jitsi Meet App', () => {
     beforeAll(async () => {
         await device.launchApp({
@@ -10,25 +14,31 @@ describe('Jitsi Meet App', () => {
             }
         });
 
-        await device.setURLBlacklist([
-            '.*meet\.jit\.si.*',
-            '.*api2\.amplitude\.com.*'
-        ]);
+        // await device.setURLBlacklist([
+        //     '.*meet\.jit\.si.*',
+        //     '.*api2\.amplitude\.com.*'
+        // ]);
+        await device.disableSynchronization();
 
         true;
     });
 
     beforeEach(async () => {
-        await device.reloadReactNative();
+        // await device.reloadReactNative();
     });
 
     it('is audio/video muted', async () => {
-        const roomName = 'TestMyRoomOrGoHome';
+        const roomName = uuidv4();
+
+        await sleep(4000);
 
         await expect(element(by.id('room-name-input'))).toBeVisible();
         await element(by.id('room-name-input')).tap();
         await element(by.id('room-name-input')).typeText(roomName);
         await element(by.id('join-room-button')).tap();
+
+        await sleep(4000);
+
         await expect(element(by.id('audio-mute-button'))).toBeVisible();
         await element(by.id('audio-mute-button')).tap();
     });
