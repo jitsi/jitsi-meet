a6718572f89df2b0ca4308d3e7782445eb93266f
Gabriel Borlea <gabriel.borlea@8x8.com>
2022-01-18 12:50:45 +0200

    fix(speaker-stats): reset hidden stats when clearing the criteria at unmount

--- react/features/speaker-stats/middleware.js
+++ react/features/speaker-stats/middleware.js
@@ -10,16 +10,16 @@ import { MiddlewareRegistry } from '../base/redux';
 
 import {
     INIT_SEARCH,
-    INIT_UPDATE_STATS
+    INIT_UPDATE_STATS,
+    RESET_SEARCH_CRITERIA
 } from './actionTypes';
 import { initReorderStats, updateStats } from './actions';
-import { filterBySearchCriteria, getSortedSpeakerStats, getPendingReorder } from './functions';
+import { filterBySearchCriteria, getSortedSpeakerStats, getPendingReorder, resetHiddenStats } from './functions';
 
 MiddlewareRegistry.register(({ dispatch, getState }) => next => action => {
     const result = next(action);
 
     switch (action.type) {
-
     case INIT_SEARCH: {
         const state = getState();
         const stats = filterBySearchCriteria(state);
@@ -38,6 +38,14 @@ MiddlewareRegistry.register(({ dispatch, getState }) => next => action => {
             dispatch(updateStats(pendingReorder ? getSortedSpeakerStats(state, stats) : stats));
         }
         break;
+
+    case RESET_SEARCH_CRITERIA: {
+        const state = getState();
+        const stats = resetHiddenStats(state);
+
+        dispatch(updateStats(stats));
+        break;
+    }
     case PARTICIPANT_JOINED:
     case PARTICIPANT_LEFT:
     case PARTICIPANT_KICKED:
