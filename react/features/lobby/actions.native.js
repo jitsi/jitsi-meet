bc4553bdc04a267680514525917a3e646e2bb88f
Calin Chitu <calinte2002@yahoo.co.uk>
2022-01-25 11:44:14 +0200

    feat(lobby) removed native lobby enable/disable dialogs

--- react/features/lobby/actions.native.js
+++ react/features/lobby/actions.native.js
@@ -3,9 +3,6 @@
 import { type Dispatch } from 'redux';
 
 import { appNavigate } from '../app/actions';
-import { openDialog } from '../base/dialog';
-
-import { DisableLobbyModeDialog, EnableLobbyModeDialog } from './components/native';
 
 export * from './actions.any';
 
@@ -20,20 +17,3 @@ export function cancelKnocking() {
     };
 }
 
-/**
- * Action to show the dialog to disable lobby mode.
- *
- * @returns {showNotification}
- */
-export function showDisableLobbyModeDialog() {
-    return openDialog(DisableLobbyModeDialog);
-}
-
-/**
- * Action to show the dialog to enable lobby mode.
- *
- * @returns {showNotification}
- */
-export function showEnableLobbyModeDialog() {
-    return openDialog(EnableLobbyModeDialog);
-}
