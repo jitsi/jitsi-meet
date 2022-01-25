5da276cb651253653e9b575a00e4ee3d1dbf28c6
Calin Chitu <calinte2002@yahoo.co.uk>
2022-01-25 11:44:14 +0200

    feat(lobby) removed native lobby enable/disable dialogs

--- react/features/lobby/components/native/index.js
+++ react/features/lobby/components/native/index.js
@@ -1,6 +1,4 @@
 // @flow
 
-export { default as DisableLobbyModeDialog } from './DisableLobbyModeDialog';
-export { default as EnableLobbyModeDialog } from './EnableLobbyModeDialog';
 export { default as KnockingParticipantList } from './KnockingParticipantList';
 export { default as LobbyScreen } from './LobbyScreen';
