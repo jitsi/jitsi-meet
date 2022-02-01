782e9322654aa307dd88c559ef06e013e7feaf38
Calinteodor <calinte2002@yahoo.co.uk>
2022-02-01 16:52:42 +0200

    Revert "add(speaker-stats): avatar in speaker stats item"
    This reverts commit 7bab3c2577a269a2b864fd47dcd307a8a7819cf1.

--- react/features/speaker-stats/components/web/SpeakerStatsItem.js
+++ react/features/speaker-stats/components/web/SpeakerStatsItem.js
@@ -3,8 +3,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 
-import { Avatar } from '../../../base/avatar';
-
 import TimeElapsed from './TimeElapsed';
 
 const useStyles = makeStyles(() => {
@@ -12,9 +10,6 @@ const useStyles = makeStyles(() => {
         speakerStatsItem: {
             height: 48,
             padding: '8px 0px'
-        },
-        speakerStatsAvatar: {
-            height: 32
         }
 
     };
@@ -88,11 +83,7 @@ const SpeakerStatsItem = (props: Props) => {
         <div
             className = { rowDisplayClass }
             key = { props.participantId } >
-            <div className = { `speaker-stats-item__avatar ${classes.speakerStatsAvatar}` }>
-                <Avatar
-                    className = 'userAvatar'
-                    participantId = { props.participantId } />
-            </div>
+            <div className = 'speaker-stats-item__avatar' />
             <div className = 'speaker-stats-item__name-time'>
                 <div
                     aria-label = { props.t('speakerStats.speakerStats') }>
