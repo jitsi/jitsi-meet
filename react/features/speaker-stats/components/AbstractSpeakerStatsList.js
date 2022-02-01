ff5a18dfcde36f6af017f790ea7f871a04859086
Calinteodor <calinte2002@yahoo.co.uk>
2022-02-01 16:52:42 +0200

    Revert "fix(speaker-stats): remove makeStyles import from abstract list"
    This reverts commit 4ec2b487c3dff428f4e8bb6f894a5271e7c83d3a.

--- react/features/speaker-stats/components/AbstractSpeakerStatsList.js
+++ react/features/speaker-stats/components/AbstractSpeakerStatsList.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { makeStyles } from '@material-ui/core/styles';
 import { useCallback, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
