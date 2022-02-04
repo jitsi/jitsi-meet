aaa26f8c7de946be4eeb5712d60a3f5c6d0ceb7e
Saúl Ibarra Corretgé <saghul@jitsi.org>
2022-02-04 10:55:34 +0100

    fix(toolbox) never show the toolbox for recorders

--- react/features/toolbox/functions.web.js
+++ react/features/toolbox/functions.web.js
@@ -41,7 +41,7 @@ export function isButtonEnabled(name: string, state: Object) {
  * otherwise.
  */
 export function isToolboxVisible(state: Object) {
-    const { iAmSipGateway, toolbarConfig } = state['features/base/config'];
+    const { iAmRecorder, iAmSipGateway, toolbarConfig } = state['features/base/config'];
     const { alwaysVisible } = toolbarConfig || {};
     const {
         timeoutID,
@@ -49,8 +49,8 @@ export function isToolboxVisible(state: Object) {
     } = state['features/toolbox'];
     const { audioSettingsVisible, videoSettingsVisible } = state['features/settings'];
 
-    return Boolean(!iAmSipGateway && (timeoutID || visible || alwaysVisible
-                                      || audioSettingsVisible || videoSettingsVisible));
+    return Boolean(!iAmRecorder && !iAmSipGateway
+            && (timeoutID || visible || alwaysVisible || audioSettingsVisible || videoSettingsVisible));
 }
 
 /**
