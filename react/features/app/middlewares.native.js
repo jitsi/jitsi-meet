c84e6eecddbe85c33892ac4360e6f7fb6fbfa82b
Saúl Ibarra Corretgé <saghul@jitsi.org>
2022-02-22 11:36:16 +0100

    feat(rn) drop incoming call handling
    This "feature" has been dead (and most likely buggy) for years. The
    recommended way is for apps to implement their own incoming call
    handling and then call into the JitsiMeetActivity. We did not have those
    APIs back then.

--- react/features/app/middlewares.native.js
+++ react/features/app/middlewares.native.js
@@ -7,7 +7,6 @@ import '../mobile/background/middleware';
 import '../mobile/call-integration/middleware';
 import '../mobile/external-api/middleware';
 import '../mobile/full-screen/middleware';
-import '../mobile/incoming-call/middleware';
 import '../mobile/navigation/middleware';
 import '../mobile/permissions/middleware';
 import '../mobile/proximity/middleware';
