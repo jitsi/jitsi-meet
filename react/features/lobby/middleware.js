39ef4c19b67219fcf28cd689376556ba3e120891
tudordan7 <tudor.pop@decagon.tech>
2022-03-01 17:09:54 +0200

    fix(lobby-notifications): Prevent lobby notification to remain on screen.

--- react/features/lobby/middleware.js
+++ react/features/lobby/middleware.js
@@ -104,6 +104,7 @@ StateListenerRegistry.register(
                     if (navigator.product === 'ReactNative' || isParticipantsPaneVisible) {
                         return;
                     }
+
                     _handleLobbyNotification({
                         dispatch,
                         getState
@@ -153,49 +154,51 @@ function _handleLobbyNotification(store) {
 
     if (knockingParticipants.length === 0) {
         dispatch(hideNotification(LOBBY_NOTIFICATION_ID));
-    } else {
-        let notificationTitle;
-        let customActionNameKey;
-        let customActionHandler;
-        let descriptionKey;
-        let icon;
 
-        if (knockingParticipants.length === 1) {
-            const firstParticipant = knockingParticipants[0];
-
-            descriptionKey = 'notify.participantWantsToJoin';
-            notificationTitle = firstParticipant.name;
-            icon = NOTIFICATION_ICON.PARTICIPANT;
-            customActionNameKey = [ 'lobby.admit', 'lobby.reject' ];
-            customActionHandler = [ () => batch(() => {
-                dispatch(hideNotification(LOBBY_NOTIFICATION_ID));
-                dispatch(approveKnockingParticipant(firstParticipant.id));
-            }),
-            () => batch(() => {
-                dispatch(hideNotification(LOBBY_NOTIFICATION_ID));
-                dispatch(rejectKnockingParticipant(firstParticipant.id));
-            }) ];
-        } else if (knockingParticipants.length > 1) {
-            descriptionKey = 'notify.participantsWantToJoin';
-            notificationTitle = i18n.t('notify.waitingParticipants', {
-                waitingParticipants: knockingParticipants.length
-            });
-            icon = NOTIFICATION_ICON.PARTICIPANTS;
-            customActionNameKey = [ 'notify.viewLobby' ];
-            customActionHandler = [ () => batch(() => {
-                dispatch(hideNotification(LOBBY_NOTIFICATION_ID));
-                dispatch(openParticipantsPane());
-            }) ];
-        }
-        dispatch(showNotification({
-            title: notificationTitle,
-            descriptionKey,
-            uid: LOBBY_NOTIFICATION_ID,
-            customActionNameKey,
-            customActionHandler,
-            icon
-        }, NOTIFICATION_TIMEOUT_TYPE.STICKY));
+        return;
     }
+
+    let notificationTitle;
+    let customActionNameKey;
+    let customActionHandler;
+    let descriptionKey;
+    let icon;
+
+    if (knockingParticipants.length === 1) {
+        const firstParticipant = knockingParticipants[0];
+
+        descriptionKey = 'notify.participantWantsToJoin';
+        notificationTitle = firstParticipant.name;
+        icon = NOTIFICATION_ICON.PARTICIPANT;
+        customActionNameKey = [ 'lobby.admit', 'lobby.reject' ];
+        customActionHandler = [ () => batch(() => {
+            dispatch(hideNotification(LOBBY_NOTIFICATION_ID));
+            dispatch(approveKnockingParticipant(firstParticipant.id));
+        }),
+        () => batch(() => {
+            dispatch(hideNotification(LOBBY_NOTIFICATION_ID));
+            dispatch(rejectKnockingParticipant(firstParticipant.id));
+        }) ];
+    } else {
+        descriptionKey = 'notify.participantsWantToJoin';
+        notificationTitle = i18n.t('notify.waitingParticipants', {
+            waitingParticipants: knockingParticipants.length
+        });
+        icon = NOTIFICATION_ICON.PARTICIPANTS;
+        customActionNameKey = [ 'notify.viewLobby' ];
+        customActionHandler = [ () => batch(() => {
+            dispatch(hideNotification(LOBBY_NOTIFICATION_ID));
+            dispatch(openParticipantsPane());
+        }) ];
+    }
+    dispatch(showNotification({
+        title: notificationTitle,
+        descriptionKey,
+        uid: LOBBY_NOTIFICATION_ID,
+        customActionNameKey,
+        customActionHandler,
+        icon
+    }, NOTIFICATION_TIMEOUT_TYPE.STICKY));
 }
 
 /**
