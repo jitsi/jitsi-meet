371f3ee199804530b9901f8632e1c18f39323743
Calin Chitu <calinte2002@yahoo.co.uk>
2022-03-03 12:50:25 +0200

    feat(display-name) remove DisplayNameLabel web

--- react/features/display-name/components/web/index.js
+++ react/features/display-name/components/web/index.js
@@ -1,6 +1,5 @@
 // @flow
 
 export { default as DisplayName } from './DisplayName';
-export { default as DisplayNameLabel } from './DisplayNameLabel';
 export { default as DisplayNamePrompt } from './DisplayNamePrompt';
 export { default as DominantSpeakerName } from './DominantSpeakerName';
