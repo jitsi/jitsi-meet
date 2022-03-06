8cb94e97a06ff5f41111a716580f297846cbe340
Shahab <shahab.saeeda@gmail.com>
2022-03-06 11:42:00 +0330

    refactor(premeeting): use jss instead of scss in ConnectionStatus

--- react/features/base/premeeting/components/web/ConnectionStatus.js
+++ react/features/base/premeeting/components/web/ConnectionStatus.js
@@ -1,5 +1,7 @@
 // @flow
 
+import { makeStyles } from '@material-ui/styles';
+import clsx from 'clsx';
 import React, { useCallback, useState } from 'react';
 
 import { translate } from '../../../i18n';
@@ -26,6 +28,84 @@ type Props = {
     t: Function
 }
 
+const useStyles = makeStyles(() => {
+    return {
+        root: {
+            '&.con-status': {
+                borderRadius: '6px',
+                color: '#fff',
+                fontSize: '12px',
+                letterSpacing: '0.16px',
+                lineHeight: '16px',
+                position: 'absolute',
+                width: '100%'
+            },
+
+            '& .con-status-header': {
+                backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                alignItems: 'center',
+                display: 'flex',
+                padding: '14px 16px'
+            },
+
+            '& .con-status-circle': {
+                borderRadius: '50%',
+                display: 'inline-block',
+                padding: '4px',
+                marginRight: '16px'
+            },
+
+            '& .con-status--good': {
+                background: '#31B76A'
+            },
+
+            '& .con-status--poor': {
+                background: '#E12D2D'
+            },
+
+            '& .con-status--non-optimal': {
+                background: '#E39623'
+            },
+
+            '& .con-status-arrow': {
+                marginLeft: 'auto',
+                transition: 'background-color 0.16s ease-out'
+            },
+
+            '& .con-status-arrow--up': {
+                transform: 'rotate(180deg)'
+            },
+
+            '& .con-status-arrow > svg': {
+                cursor: 'pointer'
+            },
+
+            '& .con-status-arrow:hover': {
+                backgroundColor: 'rgba(1, 1, 1, 0.1)'
+            },
+
+            '& .con-status-text': {
+                textAlign: 'center'
+            },
+
+            '& .con-status-details': {
+                backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                borderTop: '1px solid #5E6D7A',
+                padding: '16px',
+                transition: 'opacity 0.16s ease-out'
+            },
+
+            '& .con-status-details-visible': {
+                opacity: 1
+            },
+
+            '& .con-status-details-hidden': {
+                opacity: 0
+            }
+        }
+    };
+});
+
 const CONNECTION_TYPE_MAP = {
     [CONNECTION_TYPE.POOR]: {
         connectionClass: 'con-status--poor',
@@ -51,6 +131,8 @@ const CONNECTION_TYPE_MAP = {
  * @returns {ReactElement}
  */
 function ConnectionStatus({ connectionDetails, t, connectionType }: Props) {
+    const styles = useStyles();
+
     if (connectionType === CONNECTION_TYPE.NONE) {
         return null;
     }
@@ -78,7 +160,7 @@ function ConnectionStatus({ connectionDetails, t, connectionType }: Props) {
     }, [ showDetails, toggleDetails ]);
 
     return (
-        <div className = 'con-status'>
+        <div className = { clsx(styles.root, 'con-status') }>
             <div
                 aria-level = { 1 }
                 className = 'con-status-header'
