7225dffbc2d9b063f503507da74fc217d41caaa8
Shahab <shahab.saeeda@gmail.com>
2022-03-06 16:17:54 +0330

    refactor(calendar): use jss instead of sass for microsoft signin button

--- react/features/calendar-sync/components/MicrosoftSignInButton.web.js
+++ react/features/calendar-sync/components/MicrosoftSignInButton.web.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { withStyles } from '@material-ui/core/styles';
 import React, { Component } from 'react';
 
 import { translate } from '../../base/i18n';
@@ -10,6 +11,11 @@ import { translate } from '../../base/i18n';
  * {@link MicrosoftSignInButton}.
  */
 type Props = {
+
+    /**
+     * An object containing the CSS classes.
+     */
+    classes: Object,
 
     /**
      * The callback to invoke when {@code MicrosoftSignInButton} is clicked.
@@ -27,6 +33,36 @@ type Props = {
     t: Function
 };
 
+const styles = () => {
+    /**
+     * The Microsoft sign in button must follow Microsoft's brand guidelines.
+     * See: https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-branding-guidelines.
+     */
+    return {
+        root: {
+            alignItems: 'center',
+            background: '#FFFFFF',
+            border: '1px solid #8C8C8C',
+            boxSizing: 'border-box',
+            cursor: 'pointer',
+            display: 'inline-flex',
+            fontFamily: 'Segoe UI, Roboto, arial, sans-serif',
+            height: '41px',
+            padding: '12px'
+        },
+        cta: {
+            display: 'inline-block',
+            color: '#5E5E5E',
+            fontSize: '15px',
+            lineHeight: '41px'
+        },
+        logo: {
+            display: 'inline-block',
+            marginRight: '12px'
+        }
+    };
+};
+
 /**
  * A React Component showing a button to sign in with Microsoft.
  *
@@ -42,13 +78,13 @@ class MicrosoftSignInButton extends Component<Props> {
     render() {
         return (
             <div
-                className = 'microsoft-sign-in'
+                className = { this.props.classes.root }
                 onClick = { this.props.onClick }>
                 <img
                     alt = { this.props.t('welcomepage.logo.microsoftLogo') }
-                    className = 'microsoft-logo'
+                    className = { this.props.classes.logo }
                     src = 'images/microsoftLogo.svg' />
-                <div className = 'microsoft-cta'>
+                <div className = { this.props.classes.cta }>
                     { this.props.text }
                 </div>
             </div>
@@ -56,4 +92,4 @@ class MicrosoftSignInButton extends Component<Props> {
     }
 }
 
-export default translate(MicrosoftSignInButton);
+export default translate(withStyles(styles)(MicrosoftSignInButton));
