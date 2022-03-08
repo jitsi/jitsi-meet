4b95a5d6cb98b379895b8e0c16603ac052f4e708
Horatiu Muresan <39557534+horymury@users.noreply.github.com>
2022-03-08 16:57:53 +0200

    fix(language) Add lang API option
    - jwt from API overwrites any jwt sent as queryparam
    - `defaultLanguage` from configOverwrite converts to `lang` query param

--- react/features/base/config/configWhitelist.js
+++ react/features/base/config/configWhitelist.js
@@ -81,7 +81,6 @@ export default [
     'brandingRoomAlias',
     'debug',
     'debugAudioLevels',
-    'defaultLanguage',
     'defaultLocalDisplayName',
     'defaultRemoteDisplayName',
     'desktopSharingFrameRate',
