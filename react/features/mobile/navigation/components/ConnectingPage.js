d40ee5b4e2c9ba285058d4d2373bc128f404f96f
Calin Chitu <calinte2002@yahoo.co.uk>
2022-03-17 14:01:56 +0200

    fix(mobile/navigations) useTranslation hook

--- react/features/mobile/navigation/components/ConnectingPage.js
+++ react/features/mobile/navigation/components/ConnectingPage.js
@@ -1,36 +1,32 @@
 // @flow
 
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { SafeAreaView, Text, View } from 'react-native';
 
-import { translate } from '../../../base/i18n';
 import JitsiScreen from '../../../base/modal/components/JitsiScreen';
 import { LoadingIndicator } from '../../../base/react';
 
-
 import { navigationStyles, TEXT_COLOR } from './styles';
 
-type Props = {
+const ConnectingPage = () => {
+    const { t } = useTranslation();
 
-    /**
-     * The Function to be invoked to translate i18n keys.
-     */
-    t: Function
+    return (
+        <JitsiScreen style = { navigationStyles.connectingScreenContainer }>
+            <View style = { navigationStyles.connectingScreenContent }>
+                <SafeAreaView>
+                    <LoadingIndicator
+                        color = { TEXT_COLOR }
+                        size = 'large'
+                        style = { navigationStyles.connectingScreenIndicator } />
+                    <Text style = { navigationStyles.connectingScreenText }>
+                        { t('connectingOverlay.joiningRoom') }
+                    </Text>
+                </SafeAreaView>
+            </View>
+        </JitsiScreen>
+    );
 };
-const ConnectingPage = ({ t }: Props) => (
-    <JitsiScreen style = { navigationStyles.connectingScreenContainer }>
-        <View style = { navigationStyles.connectingScreenContent }>
-            <SafeAreaView>
-                <LoadingIndicator
-                    color = { TEXT_COLOR }
-                    size = 'large'
-                    style = { navigationStyles.connectingScreenIndicator } />
-                <Text style = { navigationStyles.connectingScreenText }>
-                    {t('connectingOverlay.joiningRoom')}
-                </Text>
-            </SafeAreaView>
-        </View>
-    </JitsiScreen>
-);
 
-export default translate(ConnectingPage);
+export default ConnectingPage;
