842755674a05d0733d5286d4d13f80f0a42e10ba
Avram Tudor <tudor.potecaru@8x8.com>
2022-03-18 10:19:20 +0200

    fix(highlight) display option to start recording (#11146)
    Fix incorrect handling of error case when highlighting moments
    Allow users to start recording when trying to highlight while recording not started

--- react/features/recording/components/Recording/AbstractHighlightButton.js
+++ react/features/recording/components/Recording/AbstractHighlightButton.js
@@ -1,10 +1,20 @@
 // @flow
 
 import { Component } from 'react';
+import { batch } from 'react-redux';
 
 import { getActiveSession, isHighlightMeetingMomentDisabled } from '../..';
+import { openDialog } from '../../../base/dialog';
 import { JitsiRecordingConstants } from '../../../base/lib-jitsi-meet';
+import {
+    hideNotification,
+    NOTIFICATION_TIMEOUT_TYPE,
+    NOTIFICATION_TYPE,
+    showNotification
+} from '../../../notifications';
 import { highlightMeetingMoment } from '../../actions.any';
+import { StartRecordingDialog } from '../../components';
+import { PROMPT_RECORDING_NOTIFICATION_ID } from '../../constants';
 
 export type Props = {
 
@@ -45,7 +55,21 @@ export default class AbstractHighlightButton<P: Props> extends Component<P> {
     _onClick() {
         const { _disabled, dispatch } = this.props;
 
-        if (!_disabled) {
+        if (_disabled) {
+            dispatch(showNotification({
+                descriptionKey: 'recording.highlightMomentDisabled',
+                titleKey: 'recording.highlightMoment',
+                uid: PROMPT_RECORDING_NOTIFICATION_ID,
+                customActionNameKey: [ 'localRecording.start' ],
+                customActionHandler: [ () => {
+                    batch(() => {
+                        dispatch(hideNotification(PROMPT_RECORDING_NOTIFICATION_ID));
+                        dispatch(openDialog(StartRecordingDialog));
+                    });
+                } ],
+                appearance: NOTIFICATION_TYPE.NORMAL
+            }, NOTIFICATION_TIMEOUT_TYPE.MEDIUM));
+        } else {
             dispatch(highlightMeetingMoment());
         }
     }
