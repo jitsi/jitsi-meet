28060ed7f5c5c5d8bf7d46af3c9af2a9f66323df
Calin Chitu <calinte2002@yahoo.co.uk>
2022-03-30 16:28:28 +0300

    feat(e2e) fixed package-lock conflict and created device variables

--- detoxrcFileGenerator.js
+++ detoxrcFileGenerator.js
@@ -1,23 +1,63 @@
+/* eslint-disable no-undef */
+
 const fs = require('file-system');
 
-const devices = require('./tests/mobile/constants.js');
+const device = {
+    android: process.env.android,
+    ios: process.env.ios
+};
+
+const androidEmulatorArray = [ 'Pixel_3a_API_30_x86' ];
+const iOSSimulatorArray = [ 'iPhone 13 Pro' ];
+
+// Returns a random emulator name from the array
+const emulatorName = emulatorArray => emulatorArray[Math.floor(Math.random() * emulatorArray.length)];
+
+const getAndroidDevices = () => {
+    if (device.android) {
+        return {
+            'phone': {
+                'type': 'android.phone',
+                'device': {
+                    'avdName': device.android
+                }
+            }
+        };
+    }
+
+    return {
+        'emulator': {
+            'type': 'android.emulator',
+            'device': {
+                'avdName': emulatorName(androidEmulatorArray)
+            }
+        }
+    };
+};
+
+const getAndroidConfiguration = () => {
+    if (device.android) {
+        return {
+            'device': 'phone'
+        };
+    }
+
+    return {
+        'device': 'emulator'
+    };
+};
 
 const data = {
     'testRunner': 'jest',
     'runnerConfig': 'tests/mobile/config.json',
     'skipLegacyWorkersInjection': true,
     'devices': {
-        'emulator': {
-            'type': 'android.emulator',
-            'device': {
-                'avdName': devices.config.android
-            }
-        },
+        ...getAndroidDevices(),
 
         'simulator': {
             'type': 'ios.simulator',
             'device': {
-                'type': devices.config.ios
+                'type': emulatorName(iOSSimulatorArray)
             }
         }
     },
@@ -37,7 +77,7 @@ const data = {
     },
     'configurations': {
         'android': {
-            'device': 'emulator',
+            ...getAndroidConfiguration(),
             'app': 'android.release'
         },
 
