33330fd1e7af0952bdcdecd6112af6e63e3537ed
robertpin <robert.pin9@gmail.com>
2022-03-31 11:51:25 +0300

    feat(rn, thumbnail) Updated indicators on native
    Remove tint for participant in large view
    Change pinned indicator from border to icon
    On stage view move screen sharing indicator from top to bottom
    On stage view show pinned indicator instead of moderator indicator

--- react/features/filmstrip/components/native/Thumbnail.js
+++ react/features/filmstrip/components/native/Thumbnail.js
@@ -4,7 +4,6 @@ import React, { PureComponent } from 'react';
 import { Image, View } from 'react-native';
 import type { Dispatch } from 'redux';
 
-import { ColorSchemeRegistry } from '../../../base/color-scheme';
 import { MEDIA_TYPE, VIDEO_TYPE } from '../../../base/media';
 import {
     PARTICIPANT_ROLE,
@@ -18,7 +17,6 @@ import {
 } from '../../../base/participants';
 import { Container } from '../../../base/react';
 import { connect } from '../../../base/redux';
-import { StyleType } from '../../../base/styles';
 import { getTrackByMediaTypeAndParticipant } from '../../../base/tracks';
 import { ConnectionIndicator } from '../../../connection-indicator';
 import { DisplayNameLabel } from '../../../display-name';
@@ -32,6 +30,7 @@ import { SQUARE_TILE_ASPECT_RATIO } from '../../constants';
 
 import AudioMutedIndicator from './AudioMutedIndicator';
 import ModeratorIndicator from './ModeratorIndicator';
+import PinnedIndicator from './PinnedIndicator';
 import RaisedHandIndicator from './RaisedHandIndicator';
 import ScreenShareIndicator from './ScreenShareIndicator';
 import styles, { AVATAR_SIZE } from './styles';
@@ -80,11 +79,6 @@ type Props = {
     _participantId: string,
 
     /**
-     * Indicates whether the participant is displayed on the large video.
-     */
-    _participantInLargeVideo: boolean,
-
-    /**
      * Indicates whether the participant is pinned or not.
      */
     _pinned: boolean,
@@ -103,18 +97,6 @@ type Props = {
      * Whether to show the moderator indicator or not.
      */
     _renderModeratorIndicator: boolean,
-
-    /**
-     * The color-schemed stylesheet of the feature.
-     */
-    _styles: StyleType,
-
-    /**
-     * If true, there will be no color overlay (tint) on the thumbnail
-     * indicating the participant associated with the thumbnail is displayed on
-     * large video. By default there will be a tint.
-     */
-    disableTint?: boolean,
 
     /**
      * Invoked to trigger state changes in Redux.
@@ -208,7 +190,9 @@ class Thumbnail extends PureComponent<Props> {
             _isFakeParticipant,
             _renderModeratorIndicator: renderModeratorIndicator,
             _participantId: participantId,
-            renderDisplayName
+            _pinned,
+            renderDisplayName,
+            tileView
         } = this.props;
         const indicators = [];
 
@@ -221,7 +205,7 @@ class Thumbnail extends PureComponent<Props> {
                 ] }>
                 <ConnectionIndicator participantId = { participantId } />
                 <RaisedHandIndicator participantId = { participantId } />
-                {isScreenShare && (
+                {tileView && isScreenShare && (
                     <View style = { styles.indicatorContainer }>
                         <ScreenShareIndicator />
                     </View>
@@ -232,7 +216,11 @@ class Thumbnail extends PureComponent<Props> {
                 style = { styles.thumbnailIndicatorContainer }>
                 <Container style = { (audioMuted || renderModeratorIndicator) && styles.bottomIndicatorsContainer }>
                     { audioMuted && <AudioMutedIndicator /> }
+                    { !tileView && _pinned && <PinnedIndicator />}
                     { renderModeratorIndicator && <ModeratorIndicator />}
+                    { !tileView && isScreenShare
+                        && <ScreenShareIndicator />
+                    }
                 </Container>
                 {
                     renderDisplayName && <DisplayNameLabel
@@ -257,12 +245,8 @@ class Thumbnail extends PureComponent<Props> {
             _isScreenShare: isScreenShare,
             _isFakeParticipant,
             _participantId: participantId,
-            _participantInLargeVideo: participantInLargeVideo,
-            _pinned,
             _raisedHand,
             _renderDominantSpeakerIndicator,
-            _styles,
-            disableTint,
             height,
             tileView
         } = this.props;
@@ -281,7 +265,6 @@ class Thumbnail extends PureComponent<Props> {
                 onLongPress = { this._onThumbnailLongPress }
                 style = { [
                     styles.thumbnail,
-                    _pinned && !tileView ? _styles.thumbnailPinned : null,
                     styleOverrides,
                     _raisedHand ? styles.thumbnailRaisedHand : null,
                     _renderDominantSpeakerIndicator ? styles.thumbnailDominantSpeaker : null
@@ -295,8 +278,6 @@ class Thumbnail extends PureComponent<Props> {
                             avatarSize = { tileView ? AVATAR_SIZE * 1.5 : AVATAR_SIZE }
                             disableVideo = { isScreenShare || _isFakeParticipant }
                             participantId = { participantId }
-                            tintEnabled = { participantInLargeVideo && !disableTint }
-                            tintStyle = { _styles.activeThumbnailTint }
                             zOrder = { 1 } />
                         {
                             this._renderIndicators()
@@ -316,13 +297,9 @@ class Thumbnail extends PureComponent<Props> {
  * @returns {Object}
  */
 function _mapStateToProps(state, ownProps) {
-    // We need read-only access to the state of features/large-video so that the
-    // filmstrip doesn't render the video of the participant who is rendered on
-    // the stage i.e. as a large video.
-    const largeVideo = state['features/large-video'];
     const { ownerId } = state['features/shared-video'];
     const tracks = state['features/base/tracks'];
-    const { participantID } = ownProps;
+    const { participantID, tileView } = ownProps;
     const participant = getParticipantByIdOrUndefined(state, participantID);
     const localParticipantId = getLocalParticipant(state).id;
     const id = participant?.id;
@@ -334,9 +311,8 @@ function _mapStateToProps(state, ownProps) {
     const participantCount = getParticipantCount(state);
     const renderDominantSpeakerIndicator = participant && participant.dominantSpeaker && participantCount > 2;
     const _isEveryoneModerator = isEveryoneModerator(state);
-    const renderModeratorIndicator = !_isEveryoneModerator
+    const renderModeratorIndicator = tileView && !_isEveryoneModerator
         && participant?.role === PARTICIPANT_ROLE.MODERATOR;
-    const participantInLargeVideo = id === largeVideo.participantId;
     const { gifUrl: gifSrc } = getGifForParticipant(state, id);
     const mode = getGifDisplayMode(state);
 
@@ -347,13 +323,11 @@ function _mapStateToProps(state, ownProps) {
         _isScreenShare: isScreenShare,
         _local: participant?.local,
         _localVideoOwner: Boolean(ownerId === localParticipantId),
-        _participantInLargeVideo: participantInLargeVideo,
         _participantId: id,
         _pinned: participant?.pinned,
         _raisedHand: hasRaisedHand(participant),
         _renderDominantSpeakerIndicator: renderDominantSpeakerIndicator,
-        _renderModeratorIndicator: renderModeratorIndicator,
-        _styles: ColorSchemeRegistry.get(state, 'Thumbnail')
+        _renderModeratorIndicator: renderModeratorIndicator
     };
 }
 
