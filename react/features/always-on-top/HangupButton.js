c2399deb5500d647fe73cb30e3666f4207bfef8d
Robert Pintilii <robert.pin9@gmail.com>
2022-04-04 13:38:49 +0300

    ref(AOT) Change buttons to not use abstract classes (#11302)
    This reduces the bundle size by about 100KB
    It also decouples the AOT buttons from the classes that are used to implement other features

--- react/features/always-on-top/HangupButton.js
+++ react/features/always-on-top/HangupButton.js
@@ -1,27 +1,59 @@
 // @flow
+import React, { Component } from 'react';
 
 // We need to reference these files directly to avoid loading things that are not available
 // in this environment (e.g. JitsiMeetJS or interfaceConfig)
+import { IconHangup } from '../base/icons';
 import type { Props } from '../base/toolbox/components/AbstractButton';
-import AbstractHangupButton from '../base/toolbox/components/AbstractHangupButton';
+
+import ToolbarButton from './ToolbarButton';
 
 const { api } = window.alwaysOnTop;
 
 /**
  * Stateless hangup button for the Always-on-Top windows.
  */
-export default class HangupButton extends AbstractHangupButton<Props, *> {
+export default class HangupButton extends Component<Props, *> {
 
     accessibilityLabel = 'Hangup';
+    icon = IconHangup;
 
     /**
-     * Helper function to perform the actual hangup action.
+     * Initializes a new {@code HangupButton} instance.
      *
-     * @override
+     * @param {Props} props - The React {@code Component} props to initialize
+     * the new {@code HangupButton} instance with.
+     */
+    constructor(props: Props) {
+        super(props);
+
+        // Bind event handlers so they are only bound once per instance.
+        this._onClick = this._onClick.bind(this);
+    }
+
+    _onClick: () => {};
+
+    /**
+     * Handles clicking / pressing the button, and disconnects the conference.
+     *
      * @protected
      * @returns {void}
      */
-    _doHangup() {
+    _onClick() {
         api.executeCommand('hangup');
+    }
+
+    /**
+     * Implements React's {@link Component#render()}.
+     *
+     * @inheritdoc
+     * @returns {ReactElement}
+     */
+    render() {
+        return (<ToolbarButton
+            accessibilityLabel = { this.accessibilityLabel }
+            customClass = 'hangup-button'
+            icon = { this.icon }
+            onClick = { this._onClick } />);
     }
 }
