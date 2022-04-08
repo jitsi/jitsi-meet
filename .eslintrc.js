676449420d032dbbee7b7117334330ececcd0f7d
Saúl Ibarra Corretgé <saghul@jitsi.org>
2022-04-08 13:09:25 +0200

    Remove unneded eslint rule override

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,5 @@
 module.exports = {
     'extends': [
         '@jitsi/eslint-config'
-    ],
-    'overrides': [
-        {
-            'files': [ '*.ts', '*.tsx' ],
-            'rules': {
-                'no-undef': 'off'
-            }
-        }
     ]
 };
