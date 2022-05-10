171c6c6ebf8432620078ac6e69c252853c794fe6
Calin Chitu <calinte2002@yahoo.co.uk>
2022-05-10 14:40:18 +0300

    fix(video-menu/native) wrong import path

--- react/features/video-menu/components/native/ConnectionStatusComponent.js
+++ react/features/video-menu/components/native/ConnectionStatusComponent.js
@@ -14,6 +14,7 @@ import { getParticipantDisplayName } from '../../../base/participants';
 import { BaseIndicator } from '../../../base/react';
 import { connect } from '../../../base/redux';
 import { StyleType } from '../../../base/styles';
+import { getSourceNameByParticipantId } from '../../../base/tracks';
 import statsEmitter from '../../../connection-indicator/statsEmitter';
 
 import styles from './styles';
