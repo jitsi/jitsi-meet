4f49cde73e7f07f75a87e42c9dc0eb5728b62834
Calin Chitu <calinte2002@yahoo.co.uk>
2022-05-18 13:15:43 +0300

    feat(react) removed unused native components

--- react/features/base/react/components/native/index.js
+++ react/features/base/react/components/native/index.js
@@ -1,12 +1,8 @@
 // @flow
 
 export { default as AvatarListItem } from './AvatarListItem';
-export { default as BackButton } from './BackButton';
 export { default as BaseIndicator } from './BaseIndicator';
-export { default as Button } from './Button';
 export { default as Container } from './Container';
-export { default as ForwardButton } from './ForwardButton';
-export { default as HeaderLabel } from './HeaderLabel';
 export { default as Image } from './Image';
 export { default as Link } from './Link';
 export { default as Linkify } from './Linkify';
