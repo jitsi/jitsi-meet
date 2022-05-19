0098091a37ec4715d39c41efaf394571a5210ad5
Saúl Ibarra Corretgé <saghul@jitsi.org>
2022-05-19 10:43:36 +0200

    fix(participants-pane,video-menu) fix incorrect selector usage

--- react/features/video-menu/components/web/ParticipantContextMenu.js
+++ react/features/video-menu/components/web/ParticipantContextMenu.js
@@ -145,7 +145,7 @@ const ParticipantContextMenu = ({
     const _volume = (participant?.local ?? true ? undefined
         : participant?.id ? participantsVolume[participant?.id] : undefined) ?? 1;
     const isBreakoutRoom = useSelector(isInBreakoutRoom);
-    const isModerationSupported = useSelector(isAvModerationSupported());
+    const isModerationSupported = useSelector(isAvModerationSupported);
     const stageFilmstrip = useSelector(isStageFilmstripEnabled);
 
     const _currentRoomId = useSelector(getCurrentRoomId);
