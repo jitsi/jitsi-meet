d7ad8f313ae1c3aec5415fd9b397d6159d1c7b71
Ramachandra Perla <ramachandra.perla@isowhiz.com>
2022-06-08 15:34:43 +0530

    Update interface_config.js

--- interface_config.js
+++ interface_config.js
@@ -125,7 +125,7 @@ var interfaceConfig = {
      */
     MOBILE_DOWNLOAD_LINK_IOS: 'https://itunes.apple.com/us/app/jitsi-meet/id1165103905',
 
-    NATIVE_APP_NAME: 'Jitsi Meet',
+    NATIVE_APP_NAME: 'Polytok',
 
     // Names of browsers which should show a warning stating the current browser
     // has a suboptimal experience. Browsers which are not listed as optimal or
