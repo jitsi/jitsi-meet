cad8d2279cf26c8924795d075061f2d7b8ba8d11
Calin-Teodor <calin.chitu@8x8.com>
2022-07-05 16:30:15 +0300

    feat(base): reverted eslint change

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -19,11 +19,5 @@ module.exports = {
         'react': {
             'version': 'detect'
         }
-    },
-    parserOptions: {
-        parser: '@babel/eslint-parser',
-        requireConfigFile: false, // <== ADD THIS
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module' // Allows for the use of imports
     }
 };
