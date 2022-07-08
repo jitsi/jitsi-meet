b22a167160e80cd644363db406d3aed0e865d55e
Дамян Минков <damencho@jitsi.org>
2022-07-08 14:49:27 +0300

    squash: Drops duplicate code and supports multi-shards.
    By adding room= parameter in query and tenant prefix for the api we add support for multi-shards setup.

--- resources/prosody-plugins/mod_muc_password_check.lua
+++ resources/prosody-plugins/mod_muc_password_check.lua
@@ -1,12 +1,12 @@
+local inspect = require "inspect";
+local formdecode = require "util.http".formdecode;
+local urlencode = require "util.http".urlencode;
 local jid = require "util.jid";
 local json = require "util.json";
-local async_handler_wrapper = module:require "util".async_handler_wrapper;
-local room_jid_match_rewrite = module:require "util".room_jid_match_rewrite;
-local starts_with = module:require "util".starts_with;
+local util = module:require "util";
+local async_handler_wrapper = util.async_handler_wrapper;
+local starts_with = util.starts_with;
 local token_util = module:require "token/util".new(module);
-local inspect = require "inspect";
-local parse = require "net.url".parseQuery;
-
 
 -- option to enable/disable room API token verifications
 local enableTokenVerification
@@ -21,7 +21,7 @@ local muc_domain_prefix = module:get_option_string("muc_mapper_domain_prefix", "
 
 local json_content_type = "application/json";
 
---- Verifies room name, domain name with the values in the token
+--- Verifies the token
 -- @param token the token we received
 -- @param room_address the full room address jid
 -- @return true if values are ok or false otherwise
@@ -35,7 +35,7 @@ function verify_token(token, room_address)
     -- (allowEmptyToken = true) and we will allow access to rooms info without
     -- a token
     if token == nil then
-        module:log("warn", "no token provided");
+        module:log("warn", "no token provided for %s", room_address);
         return false;
     end
 
@@ -43,137 +43,121 @@ function verify_token(token, room_address)
     session.auth_token = token;
     local verified, reason, msg = token_util:process_and_verify_token(session);
     if not verified then
-        module:log("warn", "not a valid token %s %s", tostring(reason), tostring(msg));
+        module:log("warn", "not a valid token %s %s for %s", tostring(reason), tostring(msg), room_address);
         return false;
     end
 
     return true;
 end
 
-function handle_validate_room_password (event)
-    local request = event.request;
-    module:log("info","Request for room password validate: reqid %s", request.headers["request_id"])
-
-    if request.headers.content_type ~= json_content_type
-            or (not request.body or #request.body == 0) then
-        module:log("warn", "Wrong content type: %s", request.headers.content_type);
-        return 400;
+-- Validates the request by checking for required url param room and
+-- validates the token provided with the request
+-- @param request - The request to validate.
+-- @return [error_code, room]
+local function validate_and_get_room(request)
+    if not request.url.query then
+        module:log("warn", "No query");
+        return 400, nil;
     end
 
-    local params = json.decode(event.request.body);
-    if not params then
-        module:log("warn", "Missing params");
-        return 400;
+    local params = formdecode(request.url.query);
+    local room_name = urlencode(params.room) or "";
+    local subdomain = urlencode(params.prefix) or "";
+
+    if not room_name then
+        module:log("warn", "Missing room param for %s", room_name);
+        return 400, nil;
     end
 
-    local conference = params["conferenceFullName"];
-    local passcode = params["passcode"];
+    local room_address = jid.join(room_name, muc_domain_prefix.."."..muc_domain_base);
 
-    if (not conference or not passcode) then
-        module:log("warn", "Missing conference or passcode param");
-        return 400;
+    if subdomain and subdomain ~= "" then
+        room_address = "["..subdomain.."]"..room_address;
     end
-
-    local room_address = room_jid_match_rewrite(conference)
 
     -- verify access
-    local token = event.request.headers["authorization"]
+    local token = request.headers["authorization"]
 
     if token and starts_with(token,'Bearer ') then
         token = token:sub(8,#token)
     end
 
     if not verify_token(token, room_address) then
-        return 403;
+        return 403, nil;
     end
 
     local room = get_room_from_jid(room_address);
 
-    response_data = {};
-    local PUT_response = {
-        headers = { content_type = "application/json"; };
-    };
-    if room then
-        response_data["valid"] = room:get_password() == passcode;
+    if not room then
+        module:log("warn", "No room found for %s", room_address);
+        return 404, nil;
     else
-        response_data["valid"] = false;
+        return 200, room;
+    end
+end
 
-        -- no room found but everything else was valid
-        PUT_response.status_code = 404;
+function handle_validate_room_password (event)
+    local request = event.request;
+
+    if request.headers.content_type ~= json_content_type
+            or (not request.body or #request.body == 0) then
+        module:log("warn", "Wrong content type: %s", request.headers.content_type);
+        return { status_code = 400; }
     end
 
-    PUT_response.body = json.encode(response_data);
+    local params = json.decode(request.body);
+    if not params then
+        module:log("warn", "Missing params");
+        return { status_code = 400; }
+    end
+
+    local passcode = params["passcode"];
+
+    if not passcode then
+        module:log("warn", "Missing passcode param");
+        return { status_code = 400; };
+    end
+
+    local error_code, room = validate_and_get_room(request);
+
+    if not room then
+        return { status_code = error_code; }
+    end
+
+    local PUT_response = {
+        headers = { content_type = "application/json"; };
+        body = json.encode({ valid = (room:get_password() == passcode) })
+    };
+
     module:log("debug","Sending response for room password validate: %s", inspect(PUT_response));
 
     return PUT_response;
-
 end
 
 --- Handles request for retrieving the room participants details
 -- @param event the http event, holds the request query
 -- @return GET response, containing a json with participants details
 function handle_get_room_password (event)
-    module:log("info","Request for room password received: reqid %s", event.request.headers["request_id"])
-    if (not event.request.url.query) then
-        module:log("warn", "No query");
-        return 400;
-    end
-    local params = parse(event.request.url.query);
-    local room_name = params["room"];
-    local domain_name = params["domain"];
-    local subdomain = params["subdomain"];
-    local conference = params["conferenceFullName"];
+    local error_code, room = validate_and_get_room(event.request);
 
-    local room_address;
-
-    if (not conference) and ((not room_name) or (not domain_name)) then
-        module:log("warn", "Missing param conference or room_name and domain_name");
-        return 400;
+    if not room then
+        return { status_code = error_code; }
     end
 
-    if conference then
-        room_address = room_jid_match_rewrite(conference)
-    else
-        room_address = jid.join(room_name, muc_domain_prefix.."."..domain_name);
+    room_details = {};
+    room_details["conference"] = room.jid;
+    room_details["passcodeProtected"] = room:get_password() ~= nil;
+    room_details["lobbyEnabled"] = room._data ~= nil and room._data.lobbyroom ~= nil;
 
-        if subdomain and subdomain ~= "" then
-            room_address = "["..subdomain.."]"..room_address;
-        end
-    end
-
-
-    -- verify access
-    local token = event.request.headers["authorization"]
-
-    if token and starts_with(token,'Bearer ') then
-        token = token:sub(8,#token)
-    end
-
-    if not verify_token(token, room_address) then
-        return 403;
-    end
-
-        local room = get_room_from_jid(room_address);
-
-    if room then
-        room_details = {};
-        room_details["conference"] = room_address;
-        room_details["passcodeProtected"] = room:get_password() ~= nil;
-        room_details["lobbyEnabled"] = room._data ~= nil and room._data.lobbyroom ~= nil;
-
-        local GET_response = {
-            headers = {
-                content_type = "application/json";
-            };
-            body = json.encode(room_details);
+    local GET_response = {
+        headers = {
+            content_type = "application/json";
         };
-        module:log("debug","Sending response for room password: %s", inspect(GET_response));
+        body = json.encode(room_details);
+    };
+    module:log("debug","Sending response for room password: %s", inspect(GET_response));
 
-        return GET_response;
-    end
-
-    -- default case, return 404
-    return 404;
+    return GET_response;
 end
 
 -- process a host module directly if loaded or hooks to wait for its load
