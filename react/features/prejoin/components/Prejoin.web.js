6d55275d030ce6ed96b01ed0a127975db88ba702
Avram Tudor <tudor.potecaru@8x8.com>
2022-08-22 15:51:17 +0300

    feat(prejoin) add room name to premeeting screen

--- react/features/prejoin/components/Prejoin.web.js
+++ react/features/prejoin/components/Prejoin.web.js
@@ -5,7 +5,6 @@ import React, { Component } from 'react';
 
 import { LoginDialog, WaitForOwnerDialog } from '../../authentication/components';
 import { Avatar } from '../../base/avatar';
-import { getRoomName } from '../../base/conference';
 import { isNameReadOnly } from '../../base/config';
 import { isDialogOpen } from '../../base/dialog/functions';
 import { translate } from '../../base/i18n';
@@ -90,11 +89,6 @@ type Props = {
      * Whether the name input should be read only or not.
      */
     readOnlyName: boolean,
-
-    /**
-     * The name of the meeting that is about to be joined.
-     */
-    roomName: string,
 
     /**
      * Sets visibility of the 'JoinByPhoneDialog'.
@@ -474,7 +468,6 @@ function mapStateToProps(state): Object {
         participantId,
         prejoinConfig: state['features/base/config'].prejoinConfig,
         readOnlyName: isNameReadOnly(state),
-        roomName: getRoomName(state),
         showCameraPreview: !isVideoMutedByUser(state),
         showDialog: isJoinByPhoneDialogVisible(state),
         showErrorOnJoin,
