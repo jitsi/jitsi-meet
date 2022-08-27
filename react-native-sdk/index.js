f36b9f98f3d135d05d42dc60c60bf6589d63a8ce
filiprejmus <rejmusfilip@gmail.com>
2022-08-27 08:50:36 -0700

    [RNSDK] Init index.js for React Native SDK

---
+++ react-native-sdk/index.js
@@ -0,0 +1,44 @@
+import 'react-native-gesture-handler';
+
+// Apply all necessary polyfills as early as possible to make sure anything imported henceforth
+// sees them.
+import 'react-native-get-random-values';
+import './react/features/mobile/polyfills';
+import { App } from './react/features/app/components/App.native';
+import JitsiThemePaperProvider from './react/features/base/ui/components/JitsiThemeProvider';
+
+import React, { useState, useEffect } from 'react';
+import { View, Button, TextInput } from 'react-native';
+
+
+/**
+ * @param {*} props
+ * @returns nothing
+ */
+export default function JitsiMeetView(props) {
+    const [ appProps, setAppProps ] = useState({});
+
+    /**
+     * Executes the onLeave callback passed from props as well as setting the props to an empty object
+     */
+    function onLeave() {
+        setAppProps({});
+        props.onLeave();
+    }
+    useEffect(
+        () => {
+            setAppProps({ 'url': props.url,
+                'onLeave': onLeave });
+        }, []
+    );
+
+    return (
+        <View style={{ width: '100%',
+            height: '100%' }}>
+            <JitsiThemePaperProvider>
+                <App
+                    {...appProps} />
+            </JitsiThemePaperProvider>
+        </View>
+    );
+}
