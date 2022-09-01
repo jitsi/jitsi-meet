cfda02ee1079c1a9c1bf09556f34439562799e78
Robert Pintilii <robert.pin9@gmail.com>
2022-09-01 11:32:01 +0300

    ref(shared-video) Merge web and native reducers (#12108)
    Convert reducer to TS

--- react/features/app/reducers.web.js
+++ react/features/app/reducers.web.js
@@ -13,7 +13,6 @@ import '../remote-control/reducer';
 import '../screen-share/reducer';
 import '../noise-suppression/reducer';
 import '../screenshot-capture/reducer';
-import '../shared-video/reducer';
 import '../talk-while-muted/reducer';
 import '../virtual-background/reducer';
 import './reducers.any';
