2c8b79053f45f89d6deff818beb8d83c39dc1ef3
James A <jqdrqgnq@users.noreply.github.com>
2022-09-14 12:28:07 -0400

    Fix merge.

--- react/features/base/config/functions.any.ts
+++ react/features/base/config/functions.any.ts
@@ -69,7 +69,7 @@ export function getMultipleVideoSendingSupportFeatureFlag(state: IReduxState) {
  * @param {Object} state - The global state.
  * @returns {boolean}
  */
-export function getSsrcRewritingFeatureFlag(state: Object) {
+export function getSsrcRewritingFeatureFlag(state: IState) {
     return getFeatureFlag(state, FEATURE_FLAGS.SSRC_REWRITING);
 }
 
