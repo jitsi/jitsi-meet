eb7246ee064549abeb4b24e4c063abade1d1b656
filiprejmus <rejmusfilip@gmail.com>
2022-09-30 15:40:45 +0200

    [RNSDK] Init Android site of SDK

---
+++ react-native-sdk/android/src/main/java/com/jitsimeetreactnative/JitsiMeetReactNativeModule.java
@@ -0,0 +1,33 @@
+package com.jitsimeetreactnative;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = JitsiMeetReactNativeModule.NAME)
+public class JitsiMeetReactNativeModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "JitsiMeetReactNative";
+
+    public JitsiMeetReactNativeModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(double a, double b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+}
---
+++ react-native-sdk/android/src/main/java/com/jitsimeetreactnative/JitsiMeetReactNativePackage.java
@@ -0,0 +1,28 @@
+package com.jitsimeetreactnative;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class JitsiMeetReactNativePackage implements ReactPackage {
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new JitsiMeetReactNativeModule(reactContext));
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
