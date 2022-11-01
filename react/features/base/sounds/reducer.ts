384e4cc8a2927104c8ae79cc7abc62a426081aeb
filiprejmus <rejmusfilip@gmail.com>
2022-11-01 16:44:27 +0100

    Remove logger import to sounds reducer

--- react/features/base/sounds/reducer.ts
+++ react/features/base/sounds/reducer.ts
@@ -9,7 +9,6 @@ import {
     _ADD_AUDIO_ELEMENT,
     _REMOVE_AUDIO_ELEMENT
 } from './actionTypes';
-import logger from './logger';
 
 /**
  * The structure use by this reducer to describe a sound.
