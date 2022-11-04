dcd8bbc8c9c2a2959aa5880543fed7e43e0ab0fc
isowhizmacbookpro2 <shiva.varimalla@isowhiz.in>
2022-11-04 12:06:48 +0530

    Jwt change

--- react/features/base/jwt/actions.js
+++ react/features/base/jwt/actions.js
@@ -14,11 +14,11 @@ import { SET_JWT } from './actionTypes';
 export function setJWT(jwt: ?string) {
     return {
         type: SET_JWT,
-        // jwt
+        jwt
         //video
         // jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6InBvbHl0b2siLCJzdWIiOiJwb2x5dG9rIiwicm9vbSI6IioiLCJzZWxmaWUiOiJWIiwibmJmIjoxNjYyMDk1MTQ2LCJleHAiOjE2NjIwOTg4NjZ9.ZmoPW-s44QaFV7LfezThl9NClDY3xFEf5IMlBb0nUjw'
         //audio
-        jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6InBvbHl0b2siLCJzdWIiOiJwb2x5dG9rIiwicm9vbSI6IioiLCJzZWxmaWUiOiJBIiwibmJmIjoxNjYyMDk1MTQ2LCJleHAiOjE2NjIwOTg4NjZ9.pFpcOG0DiSckpve-DSnrn9-PMld5iaTXATWllJoqESc'
+        // jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6InBvbHl0b2siLCJzdWIiOiJwb2x5dG9rIiwicm9vbSI6IioiLCJzZWxmaWUiOiJBIiwibmJmIjoxNjYyMDk1MTQ2LCJleHAiOjE2NjIwOTg4NjZ9.pFpcOG0DiSckpve-DSnrn9-PMld5iaTXATWllJoqESc'
         //picture
         // jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6InBvbHl0b2siLCJzdWIiOiJwb2x5dG9rIiwicm9vbSI6IioiLCJzZWxmaWUiOiJQIiwibmJmIjoxNjYyMDk1MTQ2LCJleHAiOjE2NjIwOTg4NjZ9.RBPk5Oi0t3MK1kzQtI03WPX8YnmoQjYjgDI9kfiqxFw'
         //N
