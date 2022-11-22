879b55fa9bee7a996d3ce759df816773a0ac218e
Calin-Teodor <calin.chitu@8x8.com>
2022-11-22 17:47:41 +0200

    feat(mobile/navigation): updated header navigation button styles

--- react/features/mobile/navigation/components/styles.js
+++ react/features/mobile/navigation/components/styles.js
@@ -4,6 +4,19 @@ import BaseTheme from '../../../base/ui/components/BaseTheme.native';
 
 export const TEXT_COLOR = BaseTheme.palette.text01;
 
+const HEADER_ACTION_BUTTON_SIZE = 17;
+
+const headerNavigationButton = {
+    alignContent: 'center',
+    height: '100%',
+    justifyItems: 'center'
+};
+
+const headerNavigationText = {
+    color: BaseTheme.palette.link01,
+    fontSize: HEADER_ACTION_BUTTON_SIZE
+};
+
 const unreadCounterDescription = {
     ...BaseTheme.typography.bodyShortBoldLarge,
     color: BaseTheme.palette.text03
@@ -32,6 +45,33 @@ export const navigationStyles = {
 
     connectingScreenText: {
         color: TEXT_COLOR
+    },
+
+    headerNavigationButtonIcon: {
+        ...headerNavigationButton,
+        paddingTop: 18,
+        width: BaseTheme.spacing[7]
+    },
+
+    headerNavigationButtonText: {
+        ...headerNavigationButton,
+        paddingTop: 10,
+        width: BaseTheme.spacing[10]
+    },
+
+    headerNavigationIcon: {
+        marginLeft: 12
+    },
+
+    headerNavigationText: {
+        ...headerNavigationText,
+        marginLeft: BaseTheme.spacing[3]
+    },
+
+    headerNavigationTextBold: {
+        ...headerNavigationText,
+        ...BaseTheme.typography.labelButton,
+        marginRight: BaseTheme.spacing[3]
     },
 
     unreadCounterContainer: {
