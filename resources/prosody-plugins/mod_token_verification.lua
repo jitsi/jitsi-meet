664dfdac9882c24311b5d2d9efeffd5e9d44a2b9
emrah <emrah.eryilmaz@nordeck.net>
2023-01-01 16:57:18 +0300

    refactor(prosody-plugins): convert tabs to spaces in mixed formatted file, mod_token_verification.lua

--- resources/prosody-plugins/mod_token_verification.lua
+++ resources/prosody-plugins/mod_token_verification.lua
@@ -13,16 +13,16 @@ end
 
 local parentHostName = string.gmatch(tostring(host), "%w+.(%w.+)")();
 if parentHostName == nil then
-	log("error", "Failed to start - unable to get parent hostname");
-	return;
+    log("error", "Failed to start - unable to get parent hostname");
+    return;
 end
 
 local parentCtx = module:context(parentHostName);
 if parentCtx == nil then
-	log("error",
-		"Failed to start - unable to get parent context for host: %s",
-		tostring(parentHostName));
-	return;
+    log("error",
+        "Failed to start - unable to get parent context for host: %s",
+        tostring(parentHostName));
+    return;
 end
 
 local token_util = module:require "token/util".new(parentCtx);
@@ -33,9 +33,9 @@ if token_util == nil then
 end
 
 log("debug",
-	"%s - starting MUC token verifier app_id: %s app_secret: %s allow empty: %s",
-	tostring(host), tostring(token_util.appId), tostring(token_util.appSecret),
-	tostring(token_util.allowEmptyToken));
+    "%s - starting MUC token verifier app_id: %s app_secret: %s allow empty: %s",
+    tostring(host), tostring(token_util.appId), tostring(token_util.appSecret),
+    tostring(token_util.allowEmptyToken));
 
 -- option to disable room modification (sending muc config form) for guest that do not provide token
 local require_token_for_moderation;
@@ -46,16 +46,16 @@ load_config();
 
 -- verify user and whether he is allowed to join a room based on the token information
 local function verify_user(session, stanza)
-	log("debug", "Session token: %s, session room: %s",
-		tostring(session.auth_token),
-		tostring(session.jitsi_meet_room));
+    log("debug", "Session token: %s, session room: %s",
+        tostring(session.auth_token),
+        tostring(session.jitsi_meet_room));
 
-	-- token not required for admin users
-	local user_jid = stanza.attr.from;
-	if is_admin(user_jid) then
-		log("debug", "Token not required from admin user: %s", user_jid);
-		return true;
-	end
+    -- token not required for admin users
+    local user_jid = stanza.attr.from;
+    if is_admin(user_jid) then
+        log("debug", "Token not required from admin user: %s", user_jid);
+        return true;
+    end
 
     log("debug",
         "Will verify token for user: %s, room: %s ", user_jid, stanza.attr.to);
@@ -67,22 +67,22 @@ local function verify_user(session, stanza)
                 stanza, "cancel", "not-allowed", "Room and token mismatched"));
         return false; -- we need to just return non nil
     end
-	log("debug",
+    log("debug",
         "allowed: %s to enter/create room: %s", user_jid, stanza.attr.to);
     return true;
 end
 
 module:hook("muc-room-pre-create", function(event)
-	local origin, stanza = event.origin, event.stanza;
-	log("debug", "pre create: %s %s", tostring(origin), tostring(stanza));
+    local origin, stanza = event.origin, event.stanza;
+    log("debug", "pre create: %s %s", tostring(origin), tostring(stanza));
     if not verify_user(origin, stanza) then
         return true; -- Returning any value other than nil will halt processing of the event
     end
 end, 99);
 
 module:hook("muc-occupant-pre-join", function(event)
-	local origin, room, stanza = event.origin, event.room, event.stanza;
-	log("debug", "pre join: %s %s", tostring(room), tostring(stanza));
+    local origin, room, stanza = event.origin, event.room, event.stanza;
+    log("debug", "pre join: %s %s", tostring(room), tostring(stanza));
     if not verify_user(origin, stanza) then
         return true; -- Returning any value other than nil will halt processing of the event
     end
