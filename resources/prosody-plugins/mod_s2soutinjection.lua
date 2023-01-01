0fb9fa9ec52405898f6416103b37d055de8b00c1
emrah <emrah.eryilmaz@nordeck.net>
2023-01-01 17:47:04 +0300

    refactor(prosody-plugins): convert tabs to spaces to match the common format, mod_s2soutinjection.lua

--- resources/prosody-plugins/mod_s2soutinjection.lua
+++ resources/prosody-plugins/mod_s2soutinjection.lua
@@ -19,72 +19,71 @@ local injected = module:get_option("s2s_connect_overrides");
 local proxy_listener = { default_port = nil, default_mode = "*a", default_interface = "*" };
 
 function proxy_listener.onconnect(conn)
-	local session = sessions[conn];
+    local session = sessions[conn];
 
-	-- Now the real s2s listener can take over the connection.
-	local listener = portmanager.get_service("s2s").listener;
+    -- Now the real s2s listener can take over the connection.
+    local listener = portmanager.get_service("s2s").listener;
 
-	local log = session.log;
+    local log = session.log;
 
-	local filter = initialize_filters(session);
+    local filter = initialize_filters(session);
 
-	session.version = 1;
+    session.version = 1;
 
-	session.sends2s = function (t)
-		log("debug", "sending (s2s over proxy): %s", (t.top_tag and t:top_tag()) or t:match("^[^>]*>?"));
-		if t.name then
-			t = filter("stanzas/out", t);
-		end
-		if t then
-			t = filter("bytes/out", tostring(t));
-			if t then
-				return conn:write(tostring(t));
-			end
-		end
-	end
+    session.sends2s = function (t)
+        log("debug", "sending (s2s over proxy): %s", (t.top_tag and t:top_tag()) or t:match("^[^>]*>?"));
+        if t.name then
+            t = filter("stanzas/out", t);
+        end
+        if t then
+            t = filter("bytes/out", tostring(t));
+            if t then
+                return conn:write(tostring(t));
+            end
+        end
+    end
 
-	session.open_stream = function ()
-		session.sends2s(st.stanza("stream:stream", {
-			xmlns='jabber:server', ["xmlns:db"]='jabber:server:dialback',
-			["xmlns:stream"]='http://etherx.jabber.org/streams',
-			from=session.from_host, to=session.to_host, version='1.0', ["xml:lang"]='en'}):top_tag());
-	end
+    session.open_stream = function ()
+        session.sends2s(st.stanza("stream:stream", {
+            xmlns='jabber:server', ["xmlns:db"]='jabber:server:dialback',
+            ["xmlns:stream"]='http://etherx.jabber.org/streams',
+            from=session.from_host, to=session.to_host, version='1.0', ["xml:lang"]='en'}):top_tag());
+    end
 
-	conn.setlistener(conn, listener);
+    conn.setlistener(conn, listener);
 
-	listener.register_outgoing(conn, session);
+    listener.register_outgoing(conn, session);
 
-	listener.onconnect(conn);
+    listener.onconnect(conn);
 end
 
 function proxy_listener.register_outgoing(conn, session)
-	session.direction = "outgoing";
-	sessions[conn] = session;
+    session.direction = "outgoing";
+    sessions[conn] = session;
 end
 
 function proxy_listener.ondisconnect(conn, err)
-	sessions[conn] = nil;
+    sessions[conn] = nil;
 end
 
 module:hook("route/remote", function(event)
-	local from_host, to_host, stanza = event.from_host, event.to_host, event.stanza;
-	local inject = injected and injected[to_host];
-	if not inject then return end
-	module:log("debug", "opening a new outgoing connection for this stanza");
-	local host_session = new_outgoing(from_host, to_host);
+    local from_host, to_host, stanza = event.from_host, event.to_host, event.stanza;
+    local inject = injected and injected[to_host];
+    if not inject then return end
+    module:log("debug", "opening a new outgoing connection for this stanza");
+    local host_session = new_outgoing(from_host, to_host);
 
-	-- Store in buffer
-	host_session.bounce_sendq = bounce_sendq;
-	host_session.sendq = { {tostring(stanza), stanza.attr.type ~= "error" and stanza.attr.type ~= "result" and st.reply(stanza)} };
-	host_session.log("debug", "stanza [%s] queued until connection complete", tostring(stanza.name));
+    -- Store in buffer
+    host_session.bounce_sendq = bounce_sendq;
+    host_session.sendq = { {tostring(stanza), stanza.attr.type ~= "error" and stanza.attr.type ~= "result" and st.reply(stanza)} };
+    host_session.log("debug", "stanza [%s] queued until connection complete", tostring(stanza.name));
 
-	local host, port = inject[1] or inject, tonumber(inject[2]) or 5269;
+    local host, port = inject[1] or inject, tonumber(inject[2]) or 5269;
 
-	local conn = addclient(host, port, proxy_listener, "*a");
+    local conn = addclient(host, port, proxy_listener, "*a");
 
-	proxy_listener.register_outgoing(conn, host_session);
+    proxy_listener.register_outgoing(conn, host_session);
 
-	host_session.conn = conn;
-	return true;
+    host_session.conn = conn;
+    return true;
 end, -2);
-
