0185f943009e14970f70099a768dbe6f57a8cda7
emrah <emrah.eryilmaz@nordeck.net>
2023-01-01 17:12:41 +0300

    refactor(prosody-plugins): convert tabs to spaces in mixed formatted file, mod_muc_max_occupants.lua

--- resources/prosody-plugins/mod_muc_max_occupants.lua
+++ resources/prosody-plugins/mod_muc_max_occupants.lua
@@ -13,60 +13,60 @@ local whitelist = module:get_option_set("muc_access_whitelist");
 local MAX_OCCUPANTS = module:get_option_number("muc_max_occupants", -1);
 
 local function count_keys(t)
-  return it.count(it.keys(t));
+    return it.count(it.keys(t));
 end
 
 local function check_for_max_occupants(event)
-  local room, origin, stanza = event.room, event.origin, event.stanza;
-  local user, domain, res = split_jid(stanza.attr.from);
+    local room, origin, stanza = event.room, event.origin, event.stanza;
+    local user, domain, res = split_jid(stanza.attr.from);
 
-  if is_healthcheck_room(room.jid) then
-    return;
-  end
+    if is_healthcheck_room(room.jid) then
+        return;
+    end
 
-  --no user object means no way to check for max occupants
-  if user == nil then
-    return
-  end
-  -- If we're a whitelisted user joining the room, don't bother checking the max
-  -- occupants.
-  if whitelist and whitelist:contains(domain) or whitelist:contains(user..'@'..domain) then
-    return;
-  end
+    --no user object means no way to check for max occupants
+    if user == nil then
+        return
+    end
+    -- If we're a whitelisted user joining the room, don't bother checking the max
+    -- occupants.
+    if whitelist and whitelist:contains(domain) or whitelist:contains(user..'@'..domain) then
+        return;
+    end
 
-	if room and not room._jid_nick[stanza.attr.from] then
+    if room and not room._jid_nick[stanza.attr.from] then
         local max_occupants_by_room = event.room._data.max_occupants;
-		local count = count_keys(room._occupants);
+        local count = count_keys(room._occupants);
         -- if no of occupants limit is set per room basis use
         -- that settings otherwise use the global one
         local slots = max_occupants_by_room or MAX_OCCUPANTS;
 
-		-- If there is no whitelist, just check the count.
-		if not whitelist and count >= slots then
-			module:log("info", "Attempt to enter a maxed out MUC");
-			origin.send(st.error_reply(stanza, "cancel", "service-unavailable"));
-			return true;
-		end
+        -- If there is no whitelist, just check the count.
+        if not whitelist and count >= slots then
+            module:log("info", "Attempt to enter a maxed out MUC");
+            origin.send(st.error_reply(stanza, "cancel", "service-unavailable"));
+            return true;
+        end
 
-		-- TODO: Are Prosody hooks atomic, or is this a race condition?
-		-- For each person in the room that's not on the whitelist, subtract one
-		-- from the count.
-		for _, occupant in room:each_occupant() do
-			user, domain, res = split_jid(occupant.bare_jid);
-			if not whitelist:contains(domain) and not whitelist:contains(user..'@'..domain) then
-				slots = slots - 1
-			end
-		end
+        -- TODO: Are Prosody hooks atomic, or is this a race condition?
+        -- For each person in the room that's not on the whitelist, subtract one
+        -- from the count.
+        for _, occupant in room:each_occupant() do
+            user, domain, res = split_jid(occupant.bare_jid);
+            if not whitelist:contains(domain) and not whitelist:contains(user..'@'..domain) then
+                slots = slots - 1
+            end
+        end
 
-		-- If the room is full (<0 slots left), error out.
-		if slots <= 0 then
-			module:log("info", "Attempt to enter a maxed out MUC");
-			origin.send(st.error_reply(stanza, "cancel", "service-unavailable"));
-			return true;
-		end
-	end
+        -- If the room is full (<0 slots left), error out.
+        if slots <= 0 then
+            module:log("info", "Attempt to enter a maxed out MUC");
+            origin.send(st.error_reply(stanza, "cancel", "service-unavailable"));
+            return true;
+        end
+    end
 end
 
 if MAX_OCCUPANTS > 0 then
-	module:hook("muc-occupant-pre-join", check_for_max_occupants, 10);
+    module:hook("muc-occupant-pre-join", check_for_max_occupants, 10);
 end
