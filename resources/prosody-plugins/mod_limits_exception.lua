08b29d54ea641ed765bc1697d695543ed720fce6
emrah <emrah.eryilmaz@nordeck.net>
2023-01-01 17:47:40 +0300

    refactor(prosody-plugins): convert tabs to spaces to match the common format, mod_limits_exception.lua

--- resources/prosody-plugins/mod_limits_exception.lua
+++ resources/prosody-plugins/mod_limits_exception.lua
@@ -2,7 +2,7 @@
 local have_async = pcall(require, 'util.async');
 
 if not have_async then
-	return;
+    return;
 end
 
 local unlimited_jids = module:get_option_inherited_set("unlimited_jids", {});
@@ -11,22 +11,22 @@ local unlimited_jids = module:get_option_inherited_set("unlimited_jids", {});
 local unlimited_stanza_size_limit = module:get_option_number("unlimited_size", 10*1024*1024);
 
 if unlimited_jids:empty() then
-	return;
+    return;
 end
 
 module:hook("authentication-success", function (event)
-	local session = event.session;
-	local jid = session.username .. "@" .. session.host;
-	if unlimited_jids:contains(jid) then
-		if session.conn and session.conn.setlimit then
-			session.conn:setlimit(0);
-		elseif session.throttle then
-			session.throttle = nil;
-		end
+    local session = event.session;
+    local jid = session.username .. "@" .. session.host;
+    if unlimited_jids:contains(jid) then
+        if session.conn and session.conn.setlimit then
+            session.conn:setlimit(0);
+        elseif session.throttle then
+            session.throttle = nil;
+        end
 
-		if unlimited_stanza_size_limit and session.stream.set_stanza_size_limit then
-			module:log('info', 'Setting stanza size limits for %s to %s', jid, unlimited_stanza_size_limit)
-			session.stream:set_stanza_size_limit(unlimited_stanza_size_limit);
-		end
-	end
+        if unlimited_stanza_size_limit and session.stream.set_stanza_size_limit then
+            module:log('info', 'Setting stanza size limits for %s to %s', jid, unlimited_stanza_size_limit)
+            session.stream:set_stanza_size_limit(unlimited_stanza_size_limit);
+        end
+    end
 end);
