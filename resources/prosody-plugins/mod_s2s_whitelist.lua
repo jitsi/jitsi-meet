83d4d3614cddcd033f822e5ca17425dd5e3b0afc
emrah <emrah.eryilmaz@nordeck.net>
2023-01-01 17:45:46 +0300

    refactor(prosody-plugins): convert tabs to spaces to match the common format, mod_s2s_whitelist.lua

--- resources/prosody-plugins/mod_s2s_whitelist.lua
+++ resources/prosody-plugins/mod_s2s_whitelist.lua
@@ -4,17 +4,17 @@ local st = require "util.stanza";
 local whitelist = module:get_option_inherited_set("s2s_whitelist", {});
 
 module:hook("route/remote", function (event)
-	if not whitelist:contains(event.to_host) then
-		module:send(st.error_reply(event.stanza, "cancel", "not-allowed", "Communication with this domain is restricted"));
-		return true;
-	end
+    if not whitelist:contains(event.to_host) then
+        module:send(st.error_reply(event.stanza, "cancel", "not-allowed", "Communication with this domain is restricted"));
+        return true;
+    end
 end, 100);
 
 module:hook("s2s-stream-features", function (event)
-	if not whitelist:contains(event.origin.from_host) then
-		event.origin:close({
-			condition = "policy-violation";
-			text = "Communication with this domain is restricted";
-		});
-	end
+    if not whitelist:contains(event.origin.from_host) then
+        event.origin:close({
+            condition = "policy-violation";
+            text = "Communication with this domain is restricted";
+        });
+    end
 end, 1000);
