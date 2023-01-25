767adc1f5ea28363eb1b6eb3cb654a567c891977
Calin-Teodor <calin.chitu@8x8.com>
2023-01-25 15:47:41 +0200

    feat(notifications): updated styles

--- react/features/notifications/components/native/styles.js
+++ react/features/notifications/components/native/styles.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { BoxModel, ColorPalette } from '../../../base/styles';
+import BaseTheme from '../../../base/ui/components/BaseTheme.native';
 
 /**
  * The styles of the React {@code Components} of the feature notifications.
@@ -14,34 +14,39 @@ export default {
         justifyContent: 'center',
         flex: 1,
         flexDirection: 'column',
-        paddingLeft: 1.5 * BoxModel.padding
+        marginLeft: BaseTheme.spacing[2]
     },
 
     /**
      * Test style of the notification.
      */
     contentText: {
-        alignSelf: 'flex-start',
-        color: ColorPalette.white
+        color: BaseTheme.palette.text04,
+        marginLeft: BaseTheme.spacing[6],
+        position: 'relative'
     },
 
     /**
      * Dismiss icon style.
      */
     dismissIcon: {
-        color: ColorPalette.white,
-        fontSize: 20,
-        padding: 1.5 * BoxModel.padding
+        color: BaseTheme.palette.icon04,
+        fontSize: 20
     },
 
     /**
      * Outermost view of a single notification.
      */
     notification: {
-        backgroundColor: '#768898',
+        alignItems: 'center',
+        display: 'flex',
+        backgroundColor: BaseTheme.palette.ui12,
+        borderRadius: BaseTheme.shape.borderRadius,
         flexDirection: 'row',
-        minHeight: 48,
-        marginTop: 0.5 * BoxModel.margin
+        maxHeight: 104,
+        height: 'auto',
+        marginBottom: BaseTheme.spacing[5],
+        marginHorizontal: BaseTheme.spacing[2]
     },
 
     /**
@@ -56,6 +61,44 @@ export default {
      * Wrapper for the message.
      */
     notificationContent: {
-        flexDirection: 'column'
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+
+    participantName: {
+        color: BaseTheme.palette.text04,
+        overflow: 'hidden'
+    },
+
+    iconContainer: {
+        position: 'absolute',
+        left: BaseTheme.spacing[2]
+    },
+
+    btnLabel: {
+        color: BaseTheme.palette.link01
+    },
+
+    btnContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginLeft: BaseTheme.spacing[4]
+    },
+
+    rejectBtnLabel: {
+        color: BaseTheme.palette.actionDanger
+    },
+
+    avatarContainer: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: BaseTheme.spacing[2]
+    },
+
+    avatarText: {
+        color: BaseTheme.palette.text04,
+        fontWeight: 'bold',
+        marginLeft: BaseTheme.spacing[2]
     }
 };
