5f53f4c627469afa2c701586912ea4c5914bd210
robertpin <robert.pin9@gmail.com>
2023-03-09 12:51:48 +0200

    Fix lint

--- react/features/settings/functions.native.ts
+++ react/features/settings/functions.native.ts
@@ -3,8 +3,13 @@ export * from './functions.any';
 /**
  * Used on web.
  *
- * @returns {void}
+ * @param {(Function|Object)} _stateful -The (whole) redux state, or redux's
+ * {@code getState} function to be used to retrieve the state.
+ * @param {boolean} _isDisplayedOnWelcomePage - Indicates whether the shortcuts dialog is displayed on the
+ * welcome page or not.
+ * @returns {Object} - The properties for the "Shortcuts" tab from settings
+ * dialog.
  */
-export function getShortcutsTabProps() {
+export function getShortcutsTabProps(_stateful: any, _isDisplayedOnWelcomePage?: boolean) {
     // needed to fix lint error.
 }
