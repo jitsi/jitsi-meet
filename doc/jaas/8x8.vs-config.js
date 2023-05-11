0dd75a1718fc6a86dfe335fca7d292ba11731dec
damencho <damencho@jitsi.org>
2023-05-11 12:40:16 -0500

    squash: Use local deployment UI with jaas, not 8x8.vc one.
    - We load config.js from 8x8.vc with the tenant, to allow release pinning to work.
    
    - We sed the vpass_cookie in the custom nginx conf as variables are not allowed in location matching.
    
    - The jaas-vars need to be global as it will overwrite config.js location and index html.

---
+++ doc/jaas/8x8.vs-config.js
@@ -0,0 +1,1 @@
+<script src="https://8x8.vc/<!--# echo var="subdir" default="" -->config.js"/>
--- doc/jaas/move-to-jaas.sh
+++ doc/jaas/move-to-jaas.sh
@@ -25,7 +25,10 @@ EOF
 apt install token-generator
 
 mkdir -p /etc/jitsi/meet/jaas
+
+VPASS_COOKIE=$(echo -n ${JAAS_KEY_ID}| cut -d/ -f1)
 cp /usr/share/jitsi-meet-web-config/nginx-jaas.conf /etc/jitsi/meet/jaas
+sed -i "s/jaas_magic_cookie/${VPASS_COOKIE}/g" /etc/jitsi/meet/jaas/nginx-jaas.conf
 
 # let's create the daily key now
 /usr/share/jitsi-meet/scripts/update-asap-daily.sh
@@ -37,8 +40,8 @@ else
   echo "No /etc/cron.daily. Please add to your cron jobs to execute as root daily the script: /usr/share/jitsi-meet/scripts/update-asap-daily.sh"
 fi
 
-VPASS_COOKIE=$(echo -n ${JAAS_KEY_ID}| cut -d/ -f1)
-echo "set \$jaas_magic_cookie ${VPASS_COOKIE};" > /etc/jitsi/meet/jaas/jaas-vars
+cp /usr/share/jitsi-meet-web-config/8x8.vs-config.js /etc/jitsi/meet/jaas/
+echo "set \$config_js_location /etc/jitsi/meet/jaas/8x8.vs-config.js;" >> /etc/jitsi/meet/jaas/jaas-vars
 echo "set \$custom_index index-jaas.html;" >> /etc/jitsi/meet/jaas/jaas-vars
 
 ln -s /usr/share/jitsi-meet-web-config/index-jaas.html /usr/share/jitsi-meet/index-jaas.html
