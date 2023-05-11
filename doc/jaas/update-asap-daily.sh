9f783a8b0ba75caf5df44cd44325c6e0afbed0f4
damencho <damencho@jitsi.org>
2023-05-11 12:34:52 -0500

    squash: Generates the daily asap token with expiration of 1 day.
    The default is 1 hour.

--- doc/jaas/update-asap-daily.sh
+++ doc/jaas/update-asap-daily.sh
@@ -3,7 +3,7 @@ JWT_DATE=$(echo -n $JWT_KID | cut -d/ -f2-)
 JWT_DATE=${JWT_DATE#jwt-}
 KEY_FILE=/etc/jitsi/token-generator/daily-key
 echo -n "set \$jaas_asap_key " > ${KEY_FILE}
-ASAP_KEY=$(ASAP_SIGNING_KEY_FILE=/etc/jitsi/token-generator/asap-${JWT_DATE}.key ASAP_JWT_KID="${JWT_KID}" node /usr/share/token-generator/jwt.js| tail -n1)
+ASAP_KEY=$(ASAP_SIGNING_KEY_FILE=/etc/jitsi/token-generator/asap-${JWT_DATE}.key ASAP_JWT_KID="${JWT_KID}" ASAP_EXPIRES_IN="1 day" node /usr/share/token-generator/jwt.js| tail -n1)
 echo -n "${ASAP_KEY};" >> ${KEY_FILE}
 
 service nginx reload
