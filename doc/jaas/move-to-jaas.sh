215094769a59422f372a96835110cbfebcbb1d3c
damencho <damencho@jitsi.org>
2023-05-15 13:07:48 -0500

    squash: More fixes of misspelled config file.

--- doc/jaas/move-to-jaas.sh
+++ doc/jaas/move-to-jaas.sh
@@ -42,8 +42,8 @@ VPASS_COOKIE=$(echo -n ${JAAS_KEY_ID}| cut -d/ -f1)
 cp /usr/share/jitsi-meet-web-config/nginx-jaas.conf /etc/jitsi/meet/jaas
 sed -i "s/jaas_magic_cookie/${VPASS_COOKIE}/g" /etc/jitsi/meet/jaas/nginx-jaas.conf
 
-cp /usr/share/jitsi-meet-web-config/8x8.vs-config.js /etc/jitsi/meet/jaas/
-echo "set \$config_js_location /etc/jitsi/meet/jaas/8x8.vs-config.js;" >> /etc/jitsi/meet/jaas/jaas-vars
+cp /usr/share/jitsi-meet-web-config/8x8.vc-config.js /etc/jitsi/meet/jaas/
+echo "set \$config_js_location /etc/jitsi/meet/jaas/8x8.vc-config.js;" >> /etc/jitsi/meet/jaas/jaas-vars
 echo "set \$custom_index index-jaas.html;" >> /etc/jitsi/meet/jaas/jaas-vars
 
 ln -s /usr/share/jitsi-meet-web-config/index-jaas.html /usr/share/jitsi-meet/index-jaas.html
