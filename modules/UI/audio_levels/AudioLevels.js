1d2458bfa5d1af2d6543b011646a400f7a54b167
vempatisaivishal <vempatisaivishal@gmail.com>
2023-05-28 17:47:08 +0530

    These optimizations help improve the code's readability, reduce unnecessary lines, and ensure consistent style and structure. They also provide a more concise implementation without changing the functionality or behavior of the original code

--- modules/UI/audio_levels/AudioLevels.js
+++ modules/UI/audio_levels/AudioLevels.js
@@ -1,16 +1,6 @@
-/* global interfaceConfig */
-
 import UIUtil from '../util/UIUtil';
 
-/**
- * Responsible for drawing audio levels.
- */
 const AudioLevels = {
-    /**
-     * Updates the audio level of the large video.
-     *
-     * @param audioLevel the new audio level to set.
-     */
     updateLargeVideoAudioLevel(elementId, audioLevel) {
         const element = document.getElementById(elementId);
 
@@ -18,48 +8,36 @@ const AudioLevels = {
             return;
         }
 
-        let level = parseFloat(audioLevel);
+        const level = parseFloat(audioLevel) || 0;
+        const shadowElement = element.querySelector('.dynamic-shadow');
 
-        level = isNaN(level) ? 0 : level;
-
-        let shadowElement = element.getElementsByClassName('dynamic-shadow');
-
-        if (shadowElement && shadowElement.length > 0) {
-            shadowElement = shadowElement[0];
+        if (shadowElement) {
+            shadowElement.style.boxShadow = this._updateLargeVideoShadow(level);
         }
-
-        shadowElement.style.boxShadow = this._updateLargeVideoShadow(level);
     },
 
-    /**
-     * Updates the large video shadow effect.
-     */
     _updateLargeVideoShadow(level) {
         const scale = 2;
-
-        // Internal circle audio level.
         const int = {
             level: level > 0.15 ? 20 : 0,
-            color: interfaceConfig.AUDIO_LEVEL_PRIMARY_COLOR
+            color: interfaceConfig.AUDIO_LEVEL_PRIMARY_COLOR,
+            blur: 0
         };
 
-        // External circle audio level.
         const ext = {
-            level: parseFloat(
-                ((int.level * scale * level) + int.level).toFixed(0)),
-            color: interfaceConfig.AUDIO_LEVEL_SECONDARY_COLOR
+            level: parseFloat(((int.level * scale * level) + int.level).toFixed(0)),
+            color: interfaceConfig.AUDIO_LEVEL_SECONDARY_COLOR,
+            blur: 0
         };
 
-        // Internal blur.
         int.blur = int.level ? 2 : 0;
-
-        // External blur.
         ext.blur = ext.level ? 6 : 0;
 
-        return [
-            `0 0 ${int.blur}px ${int.level}px ${int.color}`,
-            `0 0 ${ext.blur}px ${ext.level}px ${ext.color}`
-        ].join(', ');
+        return `${this._getShadowStyle(int)} ${this._getShadowStyle(ext)}`;
+    },
+
+    _getShadowStyle({ blur, level, color }) {
+        return `0 0 ${blur}px ${level}px ${color}`;
     }
 };
 
