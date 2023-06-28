3e060fd0f7e8b86e2ccca922643e078d36919f41
Robert Pintilii <robert.pin9@gmail.com>
2023-06-28 10:40:46 +0300

    v1 - move buttons to the right

--- react/features/base/ui/constants.web.ts
+++ react/features/base/ui/constants.web.ts
@@ -253,14 +253,17 @@ export const commonStyles = (theme: Theme) => {
             textAlign: 'center' as const
         },
 
-        '.toolbox-content-items': {
+        '.toolbox-content-wrapper': {
             background: theme.palette.ui01,
-            borderRadius: 6,
             padding: 6,
+            alignItems: 'flex-end',
+            boxShadow: '0px 2px 8px 4px rgba(0, 0, 0, 0.25), 0px 0px 0px 1px rgba(0, 0, 0, 0.15)'
+        },
+
+        '.toolbox-content-items': {
             textAlign: 'center' as const,
             pointerEvents: 'all' as const,
             display: 'flex',
-            boxShadow: '0px 2px 8px 4px rgba(0, 0, 0, 0.25), 0px 0px 0px 1px rgba(0, 0, 0, 0.15)',
 
             '& > div': {
                 marginRight: theme.spacing(2),
