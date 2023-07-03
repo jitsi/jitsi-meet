07a6e9ed12747bd5c0fef9af552f11d15a4a6e15
Robert Pintilii <robert.pin9@gmail.com>
2023-07-03 14:55:56 +0300

    ref(scss) Variables cleanup
    Remove some unused variables
    Replace variables that are only used once with their value

--- react/features/base/dialog/components/native/styles.ts
+++ react/features/base/dialog/components/native/styles.ts
@@ -192,7 +192,7 @@ export const brandedDialog = {
  */
 ColorSchemeRegistry.register('Dialog', {
     button: {
-        backgroundColor: schemeColor('buttonBackground'),
+        backgroundColor: '#44A5FF',
         flex: 1,
         padding: BoxModel.padding * 1.5
     },
