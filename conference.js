26a57bdd60d90f27fc8c6856cb62d1b85a11aa8f
damencho <damencho@jitsi.org>
2023-07-06 22:53:36 -0500

    squash: Fixes adding initial local tracks.

--- conference.js
+++ conference.js
@@ -739,7 +739,9 @@ export default {
 
         setGUMPendingStateOnFailedTracks(filteredTracks);
 
-        return APP.store.dispatch(connect());
+        return this._setLocalAudioVideoStreams(filteredTracks).then(
+            () => APP.store.dispatch(connect())
+        );
     },
 
     /**
