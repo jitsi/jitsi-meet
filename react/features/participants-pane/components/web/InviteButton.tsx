f3a5c654d737a264f253ccf4d5935b383237496f
Mihaela Dumitru <mihdmt@gmail.com>
2023-08-03 12:09:14 +0300

    fix(external-api) extend buttonsWithNotifyClick effect to participants pane invite button

--- react/features/participants-pane/components/web/InviteButton.tsx
+++ react/features/participants-pane/components/web/InviteButton.tsx
@@ -1,22 +1,41 @@
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { createToolbarEvent } from '../../../analytics/AnalyticsEvents';
 import { sendAnalytics } from '../../../analytics/functions';
+import { IReduxState } from '../../../app/types';
+import { getButtonNotifyMode, getButtonsWithNotifyClick } from '../../../base/config/functions.web';
 import { IconAddUser } from '../../../base/icons/svg';
 import Button from '../../../base/ui/components/web/Button';
 import { BUTTON_TYPES } from '../../../base/ui/constants.web';
 import { beginAddPeople } from '../../../invite/actions';
+import { NOTIFY_CLICK_MODE } from '../../../toolbox/constants';
+
+const INVITE_BUTTON_KEY = 'invite';
 
 export const InviteButton = () => {
     const dispatch = useDispatch();
     const { t } = useTranslation();
+    const notifyMode = useSelector((state: IReduxState) => getButtonNotifyMode(
+        INVITE_BUTTON_KEY,
+        getButtonsWithNotifyClick(state)
+    ));
 
     const onInvite = useCallback(() => {
-        sendAnalytics(createToolbarEvent('invite'));
+        if (notifyMode) {
+            APP.API.notifyToolbarButtonClicked(
+                INVITE_BUTTON_KEY, notifyMode === NOTIFY_CLICK_MODE.PREVENT_AND_NOTIFY
+            );
+        }
+
+        if (notifyMode === NOTIFY_CLICK_MODE.PREVENT_AND_NOTIFY) {
+            return;
+        }
+
+        sendAnalytics(createToolbarEvent(INVITE_BUTTON_KEY));
         dispatch(beginAddPeople());
-    }, [ dispatch ]);
+    }, [ dispatch, notifyMode ]);
 
     return (
         <Button
