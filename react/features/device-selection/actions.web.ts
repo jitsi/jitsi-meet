d19751cafe7cd18000a2a3ea7e8d97c2cda486b4
Gabriel Borlea <gabriel.borlea@8x8.com>
2023-09-01 16:49:57 +0300

    fix(video-select): remove video preview from device selection and fix video switch on android browsers

--- react/features/device-selection/actions.web.ts
+++ react/features/device-selection/actions.web.ts
@@ -6,7 +6,10 @@ import {
     setVideoInputDevice
 } from '../base/devices/actions';
 import { getDeviceLabelById, setAudioOutputDeviceId } from '../base/devices/functions';
+import { isAndroidMobileBrowser } from '../base/environment/utils';
+import { MEDIA_TYPE } from '../base/media/constants';
 import { updateSettings } from '../base/settings/actions';
+import { getLocalTrack } from '../base/tracks/functions.any';
 import { toggleNoiseSuppression } from '../noise-suppression/actions';
 import { setScreenshareFramerate } from '../screen-share/actions';
 
@@ -79,10 +82,17 @@ export function submitVideoDeviceSelectionTab(newState: any, isDisplayedOnWelcom
                 userSelectedCameraDeviceLabel:
                     getDeviceLabelById(getState(), newState.selectedVideoInputId, 'videoInput')
             }));
+            const localTrack = getLocalTrack(getState()['features/base/tracks'], MEDIA_TYPE.VIDEO);
 
-            dispatch(setVideoInputDevice(newState.selectedVideoInputId));
+            // on some android devices the current video stream has to be stopped
+            // before creating a new one to replace the old one with
+            if (isAndroidMobileBrowser() && localTrack && !localTrack.muted) {
+                localTrack.jitsiTrack.stopStream();
+                dispatch(setVideoInputDevice(newState.selectedVideoInputId));
+            } else {
+                dispatch(setVideoInputDevice(newState.selectedVideoInputId));
+            }
         }
-
         if (newState.localFlipX !== currentState.localFlipX) {
             dispatch(updateSettings({
                 localFlipX: newState.localFlipX
