eb6563bc09eaf3c50897b93084f8e9b385c75193
Calin-Teodor <calin.chitu@8x8.com>
2023-09-04 12:09:36 +0300

    sdk(react-native-sdk): fixed some linter issues

--- react-native-sdk/compare_dependencies.js
+++ react-native-sdk/compare_dependencies.js
@@ -11,21 +11,25 @@
  * - a positive integer if v1 > v2
  * - NaN if either version string is in the wrong format
  */
-
 function versionCompare(v1, v2, dependency) {
-    let v1parts = (v1 || "0").split('.'),
-        v2parts = (v2 || "0").split('.');
+    let v1parts = (v1 || '0').split('.'),
+        v2parts = (v2 || '0').split('.');
 
-    while (v1parts.length < v2parts.length) v1parts.push("0");
-    while (v2parts.length < v1parts.length) v2parts.push("0");
+    while (v1parts.length < v2parts.length) {
+        v1parts.push('0');
+    }
+
+    while (v2parts.length < v1parts.length) {
+        v2parts.push('0');
+    }
 
     v1parts = v1parts.map(function(x){
         const match = (/[A-Za-zαß]/).exec(x);
-        return Number(match ? x.replace(match[0], "." + x.charCodeAt(match.index)):x);
+        return Number(match ? x.replace(match[0], '.' + x.charCodeAt(match.index)) : x);
     });
     v2parts = v2parts.map(function(x){
         const match = (/[A-Za-zαß]/).exec(x);
-        return Number(match ? x.replace(match[0], "." + x.charCodeAt(match.index)):x);
+        return Number(match ? x.replace(match[0], '.' + x.charCodeAt(match.index)) : x);
     });
 
     for (let i = 0; i < v1parts.length; ++i) {
@@ -41,9 +45,9 @@ function versionCompare(v1, v2, dependency) {
             console.log(`⚠️We recommend you update ${dependency}: ${v1parts} to ${v2parts}`);
 
             return 1;
-        } else {
-            return -1;
         }
+
+        return -1;
     }
 
     if (v1parts.length !== v2parts.length) {
@@ -53,4 +57,4 @@ function versionCompare(v1, v2, dependency) {
     return 0;
 }
 
-module.exports = { versionCompare }
+module.exports = { versionCompare };
