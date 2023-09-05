f84a561d9e764eb8cb179cd80d5d116af1d9e691
Calinteodor <calin.chitu@8x8.com>
2023-09-05 14:09:13 +0300

    sdk(react-native-sdk): Update rnsdk peer deps (#13793)
    * sdk(react-native-sdk): prepare_sdk script updates regarding deps and peer deps

--- react-native-sdk/prepare_sdk.js
+++ react-native-sdk/prepare_sdk.js
@@ -58,11 +58,25 @@ function copyFolderRecursiveSync(source, target) {
  * Merges the dependency versions from the root package.json with the dependencies of the SDK package.json.
  */
 function mergeDependencyVersions() {
+
+    // Updates SDK dependencies to match project dependencies.
     for (const key in SDKPackageJSON.dependencies) {
         if (SDKPackageJSON.dependencies.hasOwnProperty(key)) {
             SDKPackageJSON.dependencies[key] = packageJSON.dependencies[key] || packageJSON.devDependencies[key];
         }
     }
+
+    // Updates SDK peer dependencies.
+    for (const key in packageJSON.dependencies) {
+        if (SDKPackageJSON.peerDependencies.hasOwnProperty(key)) {
+
+            // Updates all peer dependencies except react and react-native.
+            if (key !== 'react' && key !== 'react-native') {
+                SDKPackageJSON.peerDependencies[key] = packageJSON.dependencies[key];
+            }
+        }
+    }
+
     const data = JSON.stringify(SDKPackageJSON, null, 4);
 
     fs.writeFileSync('package.json', data);
