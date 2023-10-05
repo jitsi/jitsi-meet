e080ba1f2cdbb461261853a2ab9f186295500ebd
emrah <emrah.eryilmaz@nordeck.net>
2023-10-05 18:02:53 +0300

    feat(config): add notify.suboptimalExperienceTitle into list

--- config.js
+++ config.js
@@ -1590,8 +1590,9 @@ var config = {
     //     'notify.raisedHand', // shown when a partcipant used raise hand,
     //     'notify.screenShareNoAudio', // shown when the audio could not be shared for the selected screen
     //     'notify.screenSharingAudioOnlyTitle', // shown when the best performance mode has been affected by screen sharing
-    //     'notify.selfViewTitle', // shown "You can always un-hide the self-view from settings"
+    //     'notify.selfViewTitle', // show "You can always un-hide the self-view from settings"
     //     'notify.startSilentTitle', // shown when user joined with no audio
+    //     'notify.suboptimalExperienceTitle', // show the browser warning
     //     'notify.unmute', // shown to moderator when user raises hand during AV moderation
     //     'notify.videoMutedRemotelyTitle', // shown when user's video is muted by a remote party,
     //     'prejoin.errorDialOut',
