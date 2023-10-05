df0dad71405749832848c93b98895cb349f53330
emrah <emrah.eryilmaz@nordeck.net>
2023-10-05 17:54:31 +0300

    feat(config): add notify.screenSharingAudioOnlyTitle into list

--- config.js
+++ config.js
@@ -1589,6 +1589,7 @@ var config = {
     //     'notify.passwordSetRemotely', // shown when a password has been set remotely
     //     'notify.raisedHand', // shown when a partcipant used raise hand,
     //     'notify.screenShareNoAudio', // shown when the audio could not be shared for the selected screen
+    //     'notify.screenSharingAudioOnlyTitle', // shown when the best performance mode has been affected by screen sharing
     //     'notify.startSilentTitle', // shown when user joined with no audio
     //     'notify.unmute', // shown to moderator when user raises hand during AV moderation
     //     'notify.videoMutedRemotelyTitle', // shown when user's video is muted by a remote party,
