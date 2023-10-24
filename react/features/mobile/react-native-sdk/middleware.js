f37c7804b9565b43b133b52964cf2aa236943f13
Calin-Teodor <calin.chitu@8x8.com>
2023-10-24 12:30:25 +0300

    sdk(react-native-sdk): control notification through a state listener

--- react/features/mobile/react-native-sdk/middleware.js
+++ react/features/mobile/react-native-sdk/middleware.js
@@ -10,6 +10,7 @@ import {
 } from '../../base/conference/actionTypes';
 import { PARTICIPANT_JOINED } from '../../base/participants/actionTypes';
 import MiddlewareRegistry from '../../base/redux/MiddlewareRegistry';
+import StateListenerRegistry from '../../base/redux/StateListenerRegistry';
 import { READY_TO_CLOSE } from '../external-api/actionTypes';
 import { participantToParticipantInfo } from '../external-api/functions';
 import { ENTER_PICTURE_IN_PICTURE } from '../picture-in-picture/actionTypes';
@@ -21,7 +22,8 @@ const { JMOngoingConference } = NativeModules;
 
 
 /**
- * Check if native modules are being used or not. If not then the init of middleware doesn't happen.
+ * Check if native modules are being used or not.
+ * If not, then the init of middleware doesn't happen.
  */
 !externalAPIEnabled && MiddlewareRegistry.register(store => next => action => {
     const result = next(action);
@@ -36,15 +38,12 @@ const { JMOngoingConference } = NativeModules;
         rnSdkHandlers?.onConferenceFocused && rnSdkHandlers?.onConferenceFocused();
         break;
     case CONFERENCE_JOINED:
-        JMOngoingConference?.launchNotification();
         rnSdkHandlers?.onConferenceJoined && rnSdkHandlers?.onConferenceJoined();
         break;
     case CONFERENCE_LEFT:
-        JMOngoingConference?.destroyNotification();
         //  Props are torn down at this point, perhaps need to leave this one out
         break;
     case CONFERENCE_WILL_JOIN:
-        JMOngoingConference?.createNotification();
         rnSdkHandlers?.onConferenceWillJoin && rnSdkHandlers?.onConferenceWillJoin();
         break;
     case ENTER_PICTURE_IN_PICTURE:
@@ -58,11 +57,22 @@ const { JMOngoingConference } = NativeModules;
         break;
     }
     case READY_TO_CLOSE:
-        JMOngoingConference?.abortNotification();
         rnSdkHandlers?.onReadyToClose && rnSdkHandlers?.onReadyToClose();
         break;
     }
 
     return result;
-}
+});
+
+StateListenerRegistry.register(
+    state => state['features/base/conference'].conference,
+    (conference, { dispatch }, previousConference) => {
+        if (conference !== previousConference) {
+            JMOngoingConference.launchNotification();
+        } else if (conference && !previousConference) {
+            JMOngoingConference.launchNotification();
+        } else {
+            JMOngoingConference.abortNotification();
+        }
+    }
 );
