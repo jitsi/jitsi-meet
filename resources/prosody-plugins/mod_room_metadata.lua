bc0bf63ade377e7586d01fc7225ea486e86c7630
Erin Yuki Schlarb <erin-dev@ninetailed.ninja>
2023-11-01 19:54:02 +0000

    fix: Make room_metadata Prosody module depend on the required jitsi_session module
    Without this room_metadata will silently discard all room metadata client requests assuming that they didn’t come from Jitsi meet clients.
    
    Fixes #14001

--- resources/prosody-plugins/mod_room_metadata.lua
+++ resources/prosody-plugins/mod_room_metadata.lua
@@ -4,5 +4,7 @@
 local COMPONENT_IDENTITY_TYPE = 'room_metadata';
 local room_metadata_component_host = module:get_option_string('room_metadata_component', 'metadata.'..module.host);
 
+module:depends("jitsi_session");
+
 -- Advertise the component so clients can pick up the address and use it
 module:add_identity('component', COMPONENT_IDENTITY_TYPE, room_metadata_component_host);
