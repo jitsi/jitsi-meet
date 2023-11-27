dd50daa7d6367412676db59fa69ce5098493fcd3
Mihaela Dumitru <mihdmt@gmail.com>
2023-11-27 16:40:35 +0200

    feat(sounds) use audio files based on locale

--- react/features/e2ee/functions.ts
+++ react/features/e2ee/functions.ts
@@ -1,10 +1,23 @@
-import { IReduxState } from '../app/types';
+import i18next from 'i18next';
+
+import { IReduxState, IStore } from '../app/types';
 import { IStateful } from '../base/app/types';
+import { getSoundFileSrc } from '../base/media/functions';
 import { getParticipantById, getParticipantCount, getParticipantCountWithFake } from '../base/participants/functions';
 import { toState } from '../base/redux/functions';
+import { registerSound, unregisterSound } from '../base/sounds/actions';
 
+import {
+    E2EE_OFF_SOUND_ID,
+    E2EE_ON_SOUND_ID,
+    MAX_MODE_LIMIT,
+    MAX_MODE_THRESHOLD
+} from './constants';
+import {
+    E2EE_OFF_SOUND_FILE,
+    E2EE_ON_SOUND_FILE
+} from './sounds';
 
-import { MAX_MODE_LIMIT, MAX_MODE_THRESHOLD } from './constants';
 
 /**
  * Gets the value of a specific React {@code Component} prop of the currently
@@ -72,4 +85,36 @@ export function displayVerification(state: IReduxState, pId: string) {
     return Boolean(conference?.isE2EEEnabled()
         && participant?.e2eeVerificationAvailable
         && participant?.e2eeVerified === undefined);
+}
+
+/**
+ * Unregisters the audio files based on locale.
+ *
+ * @param {Dispatch<any>} dispatch - The redux dispatch function.
+ * @returns {void}
+ */
+export function unregisterE2eeAudioFiles(dispatch: IStore['dispatch']) {
+    dispatch(unregisterSound(E2EE_OFF_SOUND_ID));
+    dispatch(unregisterSound(E2EE_ON_SOUND_ID));
+}
+
+/**
+ * Registers the audio files based on locale.
+ *
+ * @param {Dispatch<any>} dispatch - The redux dispatch function.
+ * @param {boolean|undefined} shouldUnregister - Whether the sounds should be unregistered.
+ * @returns {void}
+ */
+export function registerE2eeAudioFiles(dispatch: IStore['dispatch'], shouldUnregister?: boolean) {
+    const language = i18next.language;
+
+    shouldUnregister && unregisterE2eeAudioFiles(dispatch);
+
+    dispatch(registerSound(
+        E2EE_OFF_SOUND_ID,
+        getSoundFileSrc(E2EE_OFF_SOUND_FILE, language)));
+
+    dispatch(registerSound(
+        E2EE_ON_SOUND_ID,
+        getSoundFileSrc(E2EE_ON_SOUND_FILE, language)));
 }
