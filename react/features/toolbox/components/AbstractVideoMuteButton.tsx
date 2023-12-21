998854a22e47acf2ffe1bdd7833fa024302e998a
nurjin jafar <nurjin.jafar@nordeck.net>
2023-12-21 14:46:02 +0100

    accessibility findings in Toolbar (#14161)
    feat(accessibility): update translations and toggled labels

--- react/features/toolbox/components/AbstractVideoMuteButton.tsx
+++ react/features/toolbox/components/AbstractVideoMuteButton.tsx
@@ -33,6 +33,7 @@ export default class AbstractVideoMuteButton<P extends IProps> extends BaseVideo
     accessibilityLabel = 'toolbar.accessibilityLabel.videomute';
     toggledAccessibilityLabel = 'toolbar.accessibilityLabel.videounmute';
     label = 'toolbar.videomute';
+    toggledLabel = 'toolbar.videounmute';
     tooltip = 'toolbar.videomute';
     toggledTooltip = 'toolbar.videounmute';
 
