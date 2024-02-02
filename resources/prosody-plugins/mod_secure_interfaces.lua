6666f6e982cbc8b181a12d5ecbbc6e6523ed3938
damencho <damencho@jitsi.org>
2024-02-02 10:28:44 -0600

    squash: Fix linter errors.

--- resources/prosody-plugins/mod_secure_interfaces.lua
+++ resources/prosody-plugins/mod_secure_interfaces.lua
@@ -14,7 +14,7 @@ module:hook("stream-features", function (event)
 		-- module:log("debug", "Marking session from %s to %s as secure", session.ip or "[?]", localip);
 		session.secure = true;
 		session.conn.starttls = false;
-	else
-		-- module:log("debug", "Not marking session from %s to %s as secure", session.ip or "[?]", localip);
+--	else
+--		module:log("debug", "Not marking session from %s to %s as secure", session.ip or "[?]", localip);
 	end
 end, 2500);
