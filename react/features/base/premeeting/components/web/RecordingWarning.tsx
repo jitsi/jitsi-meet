22809437e066e2614a950ed9f052722f58a1538e
Mihaela Dumitru <mihdmt@gmail.com>
2024-02-13 13:45:23 +0200

    fix(prejoin) adjust recording warning (#14361) (#14362)

--- react/features/base/premeeting/components/web/RecordingWarning.tsx
+++ react/features/base/premeeting/components/web/RecordingWarning.tsx
@@ -2,53 +2,37 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { makeStyles } from 'tss-react/mui';
 
-import Icon from '../../../icons/components/Icon';
-import { IconRecord } from '../../../icons/svg';
 import { withPixelLineHeight } from '../../../styles/functions.web';
 
 const useStyles = makeStyles()(theme => {
     return {
-        container: {
-            backgroundColor: theme.palette.warning01,
-            borderRadius: theme.shape.borderRadius,
-            color: theme.palette.text04,
+        warning: {
+            bottom: 0,
+            color: theme.palette.text03,
             display: 'flex',
             justifyContent: 'center',
             ...withPixelLineHeight(theme.typography.bodyShortRegular),
             marginBottom: theme.spacing(3),
             marginTop: theme.spacing(2),
-            paddingBottom: theme.spacing(2),
-            paddingTop: theme.spacing(2),
-            width: '100%'
-        },
-        warning: {
-            alignItems: 'center',
-            display: 'flex',
             paddingLeft: theme.spacing(3),
-            paddingRight: theme.spacing(3)
-        },
-        text: {
-            fontWeight: 600,
-            paddingLeft: theme.spacing(2)
+            paddingRight: theme.spacing(3),
+            position: 'absolute',
+            width: '100%',
+
+            '@media (max-width: 720px)': {
+                position: 'relative'
+            }
         }
     };
 });
 
 const RecordingWarning = () => {
     const { t } = useTranslation();
-    const { classes, theme } = useStyles();
-    const color = theme.palette.icon04;
+    const { classes } = useStyles();
 
     return (
-        <div className = { classes.container }>
-            <div className = { classes.warning }>
-                <Icon
-                    color = { color }
-                    src = { IconRecord } />
-                <span className = { classes.text }>
-                    {t('prejoin.recordingWarning')}
-                </span>
-            </div>
+        <div className = { classes.warning }>
+            {t('prejoin.recordingWarning')}
         </div>
     );
 };
