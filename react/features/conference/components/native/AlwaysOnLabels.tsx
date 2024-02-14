deb3b653dc307509c2444781cc338c4f96cdc34c
Calin-Teodor <calin.chitu@8x8.com>
2024-02-14 12:31:08 +0200

    feat(conference): condition live stream label visibility

--- react/features/conference/components/native/AlwaysOnLabels.tsx
+++ react/features/conference/components/native/AlwaysOnLabels.tsx
@@ -1,11 +1,13 @@
 import React, { useCallback } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { IReduxState } from '../../../app/types';
 import { JitsiRecordingConstants } from '../../../base/lib-jitsi-meet';
 import { openHighlightDialog } from '../../../recording/actions.native';
 import HighlightButton from '../../../recording/components/Recording/native/HighlightButton';
 import RecordingLabel from '../../../recording/components/native/RecordingLabel';
+import { getActiveSession } from '../../../recording/functions';
 import VisitorsCountLabel from '../../../visitors/components/native/VisitorsCountLabel';
 
 import RaisedHandsCountLabel from './RaisedHandsCountLabel';
@@ -28,35 +30,41 @@ interface IProps {
 
 const AlwaysOnLabels = ({ createOnPress }: IProps) => {
     const dispatch = useDispatch();
+    const isStreaming = useSelector((state: IReduxState) => Boolean(getActiveSession(state, JitsiRecordingConstants.mode.STREAM)));
     const openHighlightDialogCallback = useCallback(() => dispatch(openHighlightDialog()), [ dispatch ]);
 
-    return (<>
-        <TouchableOpacity
-            hitSlop = { LabelHitSlop }
-            onPress = { createOnPress(LABEL_ID_RECORDING) } >
-            <RecordingLabel mode = { JitsiRecordingConstants.mode.FILE } />
-        </TouchableOpacity>
-        <TouchableOpacity
-            hitSlop = { LabelHitSlop }
-            onPress = { createOnPress(LABEL_ID_STREAMING) } >
-            <RecordingLabel mode = { JitsiRecordingConstants.mode.STREAM } />
-        </TouchableOpacity>
-        <TouchableOpacity
-            hitSlop = { LabelHitSlop }
-            onPress = { openHighlightDialogCallback }>
-            <HighlightButton />
-        </TouchableOpacity>
-        <TouchableOpacity
-            hitSlop = { LabelHitSlop }
-            onPress = { createOnPress(LABEL_ID_RAISED_HANDS_COUNT) } >
-            <RaisedHandsCountLabel />
-        </TouchableOpacity>
-        <TouchableOpacity
-            hitSlop = { LabelHitSlop }
-            onPress = { createOnPress(LABEL_ID_VISITORS_COUNT) } >
-            <VisitorsCountLabel />
-        </TouchableOpacity>
-    </>);
+    return (
+        <>
+            <TouchableOpacity
+                hitSlop = { LabelHitSlop }
+                onPress = { createOnPress(LABEL_ID_RECORDING) } >
+                <RecordingLabel mode = { JitsiRecordingConstants.mode.FILE } />
+            </TouchableOpacity>
+            {
+                isStreaming
+                && <TouchableOpacity
+                    hitSlop = { LabelHitSlop }
+                    onPress = { createOnPress(LABEL_ID_STREAMING) } >
+                    <RecordingLabel mode = { JitsiRecordingConstants.mode.STREAM } />
+                </TouchableOpacity>
+            }
+            <TouchableOpacity
+                hitSlop = { LabelHitSlop }
+                onPress = { openHighlightDialogCallback }>
+                <HighlightButton />
+            </TouchableOpacity>
+            <TouchableOpacity
+                hitSlop = { LabelHitSlop }
+                onPress = { createOnPress(LABEL_ID_RAISED_HANDS_COUNT) } >
+                <RaisedHandsCountLabel />
+            </TouchableOpacity>
+            <TouchableOpacity
+                hitSlop = { LabelHitSlop }
+                onPress = { createOnPress(LABEL_ID_VISITORS_COUNT) } >
+                <VisitorsCountLabel />
+            </TouchableOpacity>
+        </>
+    );
 };
 
 export default AlwaysOnLabels;
