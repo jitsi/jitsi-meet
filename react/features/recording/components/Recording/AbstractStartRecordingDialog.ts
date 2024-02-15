9d90045918f64941185a1d84cf3c51346c17fdbb
Saúl Ibarra Corretgé <saghul@jitsi.org>
2024-02-15 15:42:04 +0100

    fix(recording) only show advanced section for cloud recordings

--- react/features/recording/components/Recording/AbstractStartRecordingDialog.ts
+++ react/features/recording/components/Recording/AbstractStartRecordingDialog.ts
@@ -397,7 +397,8 @@ class AbstractStartRecordingDialog extends Component<IProps, IState> {
             });
         }
 
-        if (this.state.shouldRecordTranscription) {
+        if (this.state.selectedRecordingService === RECORDING_TYPES.JITSI_REC_SERVICE
+                && this.state.shouldRecordTranscription) {
             dispatch(setRequestingSubtitles(true, false, null));
         }
 
