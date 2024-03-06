0c45d87d1a44ed26d695d3368c460e6b28d3210d
Calinteodor <calin.chitu@8x8.com>
2024-03-06 17:33:08 +0200

    react-native-sdk(android): screen share updates (#14440)
    * react-native-sdk(android): removed related modules, services to screen-share feature and updated peerDeps

--- react/features/mobile/react-native-sdk/middleware.js
+++ react/features/mobile/react-native-sdk/middleware.js
@@ -1,5 +1,3 @@
-import { NativeModules, Platform } from 'react-native';
-
 import { getAppProp } from '../../base/app/functions';
 import {
     CONFERENCE_BLURRED,
@@ -11,7 +9,6 @@ import {
 import { SET_AUDIO_MUTED, SET_VIDEO_MUTED } from '../../base/media/actionTypes';
 import { PARTICIPANT_JOINED, PARTICIPANT_LEFT } from '../../base/participants/actionTypes';
 import MiddlewareRegistry from '../../base/redux/MiddlewareRegistry';
-import StateListenerRegistry from '../../base/redux/StateListenerRegistry';
 import { READY_TO_CLOSE } from '../external-api/actionTypes';
 import { participantToParticipantInfo } from '../external-api/functions';
 import { ENTER_PICTURE_IN_PICTURE } from '../picture-in-picture/actionTypes';
@@ -19,7 +16,6 @@ import { ENTER_PICTURE_IN_PICTURE } from '../picture-in-picture/actionTypes';
 import { isExternalAPIAvailable } from './functions';
 
 const externalAPIEnabled = isExternalAPIAvailable();
-const { JMOngoingConference } = NativeModules;
 
 
 /**
@@ -78,21 +74,3 @@ const { JMOngoingConference } = NativeModules;
 
     return result;
 });
-
-/**
- * Before enabling media projection service control on Android,
- * we need to check if native modules are being used or not.
- */
-Platform.OS === 'android' && !externalAPIEnabled && StateListenerRegistry.register(
-    state => state['features/base/conference'].conference,
-    (conference, previousConference) => {
-        if (!conference) {
-            JMOngoingConference.abort();
-        } else if (conference && !previousConference) {
-            JMOngoingConference.launch();
-        } else if (conference !== previousConference) {
-            JMOngoingConference.abort();
-            JMOngoingConference.launch();
-        }
-    }
-);
