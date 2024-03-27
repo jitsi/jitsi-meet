4f81059714c586886786240cb9bc7e21e636d3fe
Calin-Teodor <calin.chitu@8x8.com>
2024-03-27 15:52:12 +0200

    feat(subtitles): fixed ts errors

--- react/features/subtitles/functions.native.ts
+++ react/features/subtitles/functions.native.ts
@@ -1,6 +1,5 @@
 /* eslint-disable max-params, max-len */
 
-import { IStore } from '../app/types';
 import { sendEvent } from '../mobile/external-api/functions';
 
 
@@ -16,10 +15,10 @@ const TRANSCRIPTION_CHUNK_RECEIVED = 'TRANSCRIPTION_CHUNK_RECEIVED';
  * @param {string} language - The language of the transcribed message.
  * @param {Object} participant - The participant who send the message.
  * @param {any} text - The message text.
- * @param {IStore} _store - The store.
+ * @param {any} _store - The store.
  * @returns {Event}
  */
-export const notifyTranscriptionChunkReceived = (transcriptMessageID: string, language: string, participant: Object, text: any, _store?: IStore) =>
+export const notifyTranscriptionChunkReceived = (transcriptMessageID: string, language: string, participant: Object, text: any, _store?: any) =>
     sendEvent(
         _store,
         TRANSCRIPTION_CHUNK_RECEIVED,
