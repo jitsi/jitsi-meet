3afb3e732ec63ba778d87d7de87ea58307348e70
Calin-Teodor <calin.chitu@8x8.com>
2024-04-10 11:17:26 +0300

    feat(toolbox): fixed icon size and updated default color to white

--- react/features/toolbox/components/native/CustomOptionButton.tsx
+++ react/features/toolbox/components/native/CustomOptionButton.tsx
@@ -8,6 +8,7 @@ import AbstractButton, { IProps as AbstractButtonProps }
 
 import styles from './styles';
 
+const ICON_COLOR = '#fff'
 
 interface IProps extends AbstractButtonProps {
     icon: any;
@@ -32,6 +33,7 @@ class CustomOptionButton extends AbstractButton<IProps> {
      */
     icon = () => (
         <Image
+            tintColor = { ICON_COLOR }
             source = {{ uri: this.iconSrc }}
             style = { styles.iconImageStyles } />
     );
