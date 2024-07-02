58a9e423988eb0ce385adafdf65e35065777ec94
Patrick He <77468352+he-patrick@users.noreply.github.com>
2024-07-02 07:32:42 -0400

    Add private message functionality

--- react/features/chat/components/web/KebabMenu.tsx
+++ react/features/chat/components/web/KebabMenu.tsx
@@ -5,12 +5,31 @@ import Button from "../../../base/ui/components/web/Button";
 import { BUTTON_TYPES } from "../../../base/ui/constants.any";
 import { IconDotsHorizontal } from "../../../base/icons/svg";
 import Popover from "@mui/material/Popover";
+import { useDispatch, useSelector } from 'react-redux';
+import { handleLobbyChatInitialized, openChat } from '../../actions.web';
+import { getParticipantById } from "../../../base/participants/functions";
+import { IReduxState } from "../../../app/types";
 
 export interface IProps {
     onReply: () => void;
     onPrivateMessage: () => void;
     onCopy: () => void;
     className?: string;
+
+    /**
+    * True if the message is a lobby chat message.
+    */
+    isLobbyMessage: boolean;
+
+    /**
+     * The ID of the participant that the message is to be sent.
+     */
+    participantID: string;
+
+    /**
+     * Whether the button should be visible or not.
+     */
+    visible?: boolean;
 }
 
 const useStyles = makeStyles()((theme) => ({
@@ -26,19 +45,44 @@ const useStyles = makeStyles()((theme) => ({
     menuItem: {
         padding: "8px 16px",
         cursor: "pointer",
+        color: "white", // Set text color to white
         "&:hover": {
             backgroundColor: theme.palette.action03,
         },
     },
+    menuPanel: {
+        backgroundColor: theme.palette.ui02,
+        borderRadius: theme.shape.borderRadius,
+        boxShadow: theme.shadows[3],
+    },
 }));
 
-const KebabMenu = ({ onReply, onPrivateMessage, onCopy, className }: IProps) => {
+const KebabMenu = ({ isLobbyMessage, participantID, onCopy, className }: IProps) => {
+    const dispatch = useDispatch();
     const { classes, cx } = useStyles();
     const { t } = useTranslation();
     const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
+    const participant = useSelector((state: IReduxState) => getParticipantById(state, participantID));
+
     const handleMenuClick = useCallback((event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
+    }, []);
+
+    const handleReplyClick = useCallback(() => {
+        // Actions
+    }, []);
+
+    const handlePrivateClick = useCallback(() => {
+        if (isLobbyMessage) {
+            dispatch(handleLobbyChatInitialized(participantID));
+        } else {
+            dispatch(openChat(participant));
+        }
+    }, [dispatch, isLobbyMessage, participant, participantID]);
+
+    const handleCopyClick = useCallback(() => {
+        // Actions
     }, []);
 
     const handleMenuItemClick = useCallback((action: () => void) => {
@@ -76,14 +120,14 @@ const KebabMenu = ({ onReply, onPrivateMessage, onCopy, className }: IProps) => 
                     horizontal: "center",
                 }}
             >
-                <div>
-                    <div className={classes.menuItem} onClick={() => handleMenuItemClick(onReply)}>
+                <div className={classes.menuPanel}>
+                    <div className={classes.menuItem} onClick={() => handleMenuItemClick(handleReplyClick)}>
                         {t("Reply")}
                     </div>
-                    <div className={classes.menuItem} onClick={() => handleMenuItemClick(onPrivateMessage)}>
+                    <div className={classes.menuItem} onClick={() => handleMenuItemClick(handlePrivateClick)}>
                         {t("Private Message")}
                     </div>
-                    <div className={classes.menuItem} onClick={() => handleMenuItemClick(onCopy)}>
+                    <div className={classes.menuItem} onClick={() => handleMenuItemClick(handleCopyClick)}>
                         {t("Copy")}
                     </div>
                 </div>
