98b6e1ebba9fd494258f0560bcee9e31134a562d
Saúl Ibarra Corretgé <saghul@jitsi.org>
2024-07-31 13:47:16 +0200

    feat(external-api) add "name" property to participant-kicked-out event

--- modules/API/API.js
+++ modules/API/API.js
@@ -1814,9 +1814,9 @@ class API {
      * Notify external application of a participant, remote or local, being
      * removed from the conference by another participant.
      *
-     * @param {string} kicked - The ID of the participant removed from the
+     * @param {Object} kicked - The participant removed from the
      * conference.
-     * @param {string} kicker - The ID of the participant that removed the
+     * @param {Object} kicker - The participant that removed the
      * other participant.
      * @returns {void}
      */
