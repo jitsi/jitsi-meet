1d00213bad9c30e2f6dd01d94c62b835e240c3eb
Patrick He <77468352+he-patrick@users.noreply.github.com>
2024-08-27 01:01:25 -0400

    Fix tsc errors

--- react/features/chat/components/web/EmojiSelector.tsx
+++ react/features/chat/components/web/EmojiSelector.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
+import { makeStyles } from 'tss-react/mui';
 
-const EmojiSelector = ({ onSelect }) => {
-    const emojiMap = {
+interface EmojiSelectorProps {
+    onSelect: (emoji: string) => void;
+}
+
+const useStyles = makeStyles()(theme => {
+    return {
+        emojiMap: {
+            display: 'flex',
+            flexDirection: 'row',
+            borderRadius: '4px',
+            backgroundColor: '#3D3D3D'
+        }
+    }
+})
+
+const EmojiSelector: React.FC<EmojiSelectorProps> = ({ onSelect }) => {
+    const { classes } = useStyles();
+
+    const emojiMap: Record<string, string> = {
         grin: '😀',
         smile: '😁',
         laugh: '😂',
         rofl: '🤣',
         happy: '😃'
     };
-
     const emojiNames = Object.keys(emojiMap);
 
     return (
-        <div
-            style = {{ display: 'flex',
-                flexDirection: 'row',
-                borderRadius: '4px',
-                backgroundColor: '#3D3D3D' }}>
-            {emojiNames.map(name => (
+        <div className = { classes.emojiMap }>
+            {emojiNames.map((name) => (
                 <span
-                    key = { name }
-                    onClick = { () => onSelect(emojiMap[name]) }
-                    style = {{ cursor: 'pointer',
+                    key={name}
+                    onClick={() => onSelect(emojiMap[name])}
+                    style={{
+                        cursor: 'pointer',
                         padding: '5px',
-                        fontSize: '1.5em' }}>
+                        fontSize: '1.5em'
+                    }}
+                >
                     {emojiMap[name]}
                 </span>
             ))}
