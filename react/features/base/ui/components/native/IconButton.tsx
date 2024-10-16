2b719743d38d31a8f20b0bf4c77f59d9e4262d7f
Calin-Teodor <calin.chitu@8x8.com>
2024-10-16 15:47:33 +0300

    feat(base): minor syntax and ts types fixes

--- react/features/base/ui/components/native/IconButton.tsx
+++ react/features/base/ui/components/native/IconButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableHighlight } from 'react-native';
+import { TouchableHighlight, ViewStyle } from 'react-native';
 
 import Icon from '../../../icons/components/Icon';
 import styles from '../../../react/components/native/styles';
@@ -58,7 +58,7 @@ const IconButton: React.FC<IIconButtonProps> = ({
             style = { [
                 iconButtonContainerStyles,
                 style
-            ] }
+            ] as ViewStyle }
             underlayColor = { underlayColor }>
             <Icon
                 color = { color }
