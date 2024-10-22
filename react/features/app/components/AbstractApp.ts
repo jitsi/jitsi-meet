5bb3ba71d0ff53bc6a4beb8fc0975ddfb03d1242
Saúl Ibarra Corretgé <saghul@jitsi.org>
2024-10-22 14:23:27 +0200

    chore(helpers) drop custom createDeferred() for Promise.withResolvers()

--- react/features/app/components/AbstractApp.ts
+++ react/features/app/components/AbstractApp.ts
@@ -27,11 +27,9 @@ export interface IProps {
  */
 export class AbstractApp<P extends IProps = IProps> extends BaseApp<P> {
     /**
-     * The deferred for the initialisation {{promise, resolve, reject}}.
+     * The deferred for the initialization {{promise, resolve, reject}}.
      */
-    _init: {
-        promise: Promise<any>;
-    };
+    _init: PromiseWithResolvers<any>;
 
     /**
      * Initializes the app.
