05a930e5f5ecab4c0ec4dc0b2881a7302d219d78
Mihaela Dumitru <mihdmt@gmail.com>
2024-12-04 16:39:01 +0200

    fix(whiteboard) simplify room hash generation to cover custom region shards (#15345)

--- react/features/whiteboard/functions.ts
+++ react/features/whiteboard/functions.ts
@@ -99,7 +99,7 @@ export const generateCollabServerUrl = (state: IReduxState): string | undefined 
     const inBreakoutRoom = isInBreakoutRoom(state);
     const roomId = getCurrentRoomId(state);
     const room = md5.hex(
-        `${locationURL?.origin}${getBackendSafePath(locationURL?.pathname)}${inBreakoutRoom ? `|${roomId}` : ''}`
+        `${getBackendSafePath(locationURL?.pathname)}${inBreakoutRoom ? `|${roomId}` : ''}`
     );
 
     return appendURLParam(collabServerBaseUrl, 'room', room);
