4ea24fa3d411cf056e526f1c3e43fb707cde6520
Calin-Teodor <calin.chitu@8x8.com>
2024-12-11 13:39:37 +0200

    dep(@notifee/react-native): rebase and linter fixes

--- index.android.js
+++ index.android.js
@@ -1,26 +1,23 @@
-import './react/index.native';
-import notifee, {EventType} from '@notifee/react-native';
+import notifee, { EventType } from '@notifee/react-native';
+
 import logger from './react/features/app/logger';
+import './react/index.native';
 
 // Needs to be registered outside any React components as early as possible
 await notifee.registerForegroundService(() => {
-    return new Promise(resolve => {
+    return new Promise(() => {
         logger.warn('Foreground service running');
 
         notifee.onBackgroundEvent(async event => {
             if (event.type === EventType.ACTION_PRESS && event.detail.pressAction.id === 'hang-up') {
                 console.log('HANG UP BUTTON PRESSED BACKGROUND');
             }
-
-            return resolve();
         });
 
         notifee.onForegroundEvent(async ({ type, detail }) => {
             if (type === EventType.ACTION_PRESS && detail.pressAction.id === 'hang-up') {
                 console.log('HANG UP BUTTON PRESSED');
             }
-
-            return resolve();
         });
     });
 });
