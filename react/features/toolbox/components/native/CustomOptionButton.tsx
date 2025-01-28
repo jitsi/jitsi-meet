01757281e2a8e3e42a59f53eb5f9ae21a3f30fca
Calin-Teodor <calin.chitu@8x8.com>
2025-01-28 10:27:35 +0200

    feat(toolbox): fixed linter

--- react/features/toolbox/components/native/CustomOptionButton.tsx
+++ react/features/toolbox/components/native/CustomOptionButton.tsx
@@ -3,9 +3,9 @@ import { Image, View, ViewStyle } from 'react-native';
 import { SvgCssUri } from 'react-native-svg';
 import { connect } from 'react-redux';
 
+import { translate } from '../../../base/i18n/functions';
 import AbstractButton, { IProps as AbstractButtonProps } from '../../../base/toolbox/components/AbstractButton';
 import BaseTheme from '../../../base/ui/components/BaseTheme.native';
-import { translate } from '../../../base/i18n/functions';
 
 import styles from './styles';
 
@@ -42,29 +42,29 @@ class CustomOptionButton extends AbstractButton<IProps> {
         }
 
         if (this.iconSrc?.includes('svg')) {
-            iconComponent
-                = (<View style = { this.props.isToolboxButton
-                && [ styles.toolboxButtonIconContainer,
-                    { backgroundColor: this.backgroundColor } ] as ViewStyle }>
+            iconComponent = (
                 <SvgCssUri
                     height = { BaseTheme.spacing[4] }
                     uri = { this.iconSrc }
                     width = { BaseTheme.spacing[4] } />
-                </View>);
+            );
         } else {
-            iconComponent
-                = (<View style = { this.props.isToolboxButton
-                && [ styles.toolboxButtonIconContainer,
-                    { backgroundColor: this.backgroundColor } ] as ViewStyle }>
+            iconComponent = (
                 <Image
                     height = { BaseTheme.spacing[4] }
                     resizeMode = { 'contain' }
                     source = {{ uri: this.iconSrc }}
                     width = { BaseTheme.spacing[4] } />
-                </View>);
+            );
         }
 
-        return iconComponent;
+        return(
+            <View style = { this.props.isToolboxButton && [
+                styles.toolboxButtonIconContainer,
+                { backgroundColor: this.backgroundColor } ] as ViewStyle }>
+                { iconComponent }
+            </View>
+        );
     };
 
     label = this.text || '';
