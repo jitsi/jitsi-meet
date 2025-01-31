810654833870ad824de973d410e5621dbd578d23
Calin-Teodor <calin.chitu@8x8.com>
2025-01-31 15:04:41 +0200

    feat(toolbox): moved getToolbarButtons from middleware to functions

--- react/features/toolbox/functions.native.ts
+++ react/features/toolbox/functions.native.ts
@@ -6,6 +6,9 @@ import { getFeatureFlag } from '../base/flags/functions';
 import { getParticipantCountWithFake } from '../base/participants/functions';
 import { toState } from '../base/redux/functions';
 import { isLocalVideoTrackDesktop } from '../base/tracks/functions.native';
+import { MAIN_TOOLBAR_BUTTONS_PRIORITY } from './constants';
+import { IGetVisibleNativeButtonsParams, IToolboxButton } from './types';
+import { isButtonEnabled } from './functions.any';
 
 export * from './functions.any';
 
@@ -28,21 +31,21 @@ export function getMovableButtons(width: number): Set<string> {
 
     switch (true) {
     case width >= WIDTH.FIT_9_ICONS: {
-        buttons = [ 'chat', 'togglecamera', 'screensharing', 'raisehand', 'tileview' ];
+        buttons = [ 'microphone', 'camera', 'chat', 'screensharing', 'raisehand', 'tileview' ];
         break;
     }
     case width >= WIDTH.FIT_8_ICONS: {
-        buttons = [ 'chat', 'togglecamera', 'raisehand', 'tileview' ];
+        buttons = [ 'microphone', 'camera', 'chat', 'raisehand', 'tileview' ];
         break;
     }
 
     case width >= WIDTH.FIT_7_ICONS: {
-        buttons = [ 'chat', 'togglecamera', 'raisehand' ];
+        buttons = [ 'microphone', 'camera', 'chat', 'raisehand' ];
         break;
     }
 
     case width >= WIDTH.FIT_6_ICONS: {
-        buttons = [ 'chat', 'togglecamera' ];
+        buttons = [ 'microphone', 'camera', 'chat', 'togglecamera' ];
         break;
     }
 
@@ -98,4 +101,56 @@ export function isVideoMuteButtonDisabled(state: IReduxState) {
 
     return !hasAvailableDevices(state, 'videoInput')
         || (unmuteBlocked && Boolean(muted));
+}
+
+
+/**
+ * Returns all buttons that need to be rendered.
+ *
+ * @param {IGetVisibleButtonsParams} params - The parameters needed to extract the visible buttons.
+ * @returns {Object} - The visible buttons arrays .
+ */
+export function getVisibleNativeButtons({
+                                      allButtons,
+                                      clientWidth,
+                                      mainToolbarButtonsThresholds,
+                                      toolbarButtons
+                                  }: IGetVisibleNativeButtonsParams) {
+    const filteredButtons = Object.keys(allButtons).filter(key =>
+        typeof key !== 'undefined' // filter invalid buttons that may be coming from config.mainToolbarButtons
+        // override
+        && isButtonEnabled(key, toolbarButtons));
+
+    const { order } = mainToolbarButtonsThresholds.find(({ width }) => clientWidth > width)
+    || mainToolbarButtonsThresholds[mainToolbarButtonsThresholds.length - 1];
+
+    const mainToolbarButtonKeysOrder = [
+        ...order.filter(key => filteredButtons.includes(key)),
+        ...MAIN_TOOLBAR_BUTTONS_PRIORITY.filter(key => !order.includes(key) && filteredButtons.includes(key)),
+        ...filteredButtons.filter(key => !order.includes(key) && !MAIN_TOOLBAR_BUTTONS_PRIORITY.includes(key))
+    ];
+
+    const mainButtonsKeys = mainToolbarButtonKeysOrder.slice(0, order.length);
+    const overflowMenuButtons = filteredButtons.reduce((acc, key) => {
+        if (!mainButtonsKeys.includes(key)) {
+            acc.push(allButtons[key]);
+        }
+
+        return acc;
+    }, [] as IToolboxButton[]);
+
+    // if we have 1 button in the overflow menu it is better to directly display it in the main toolbar by replacing
+    // the "More" menu button with it.
+    if (overflowMenuButtons.length === 1) {
+        const button = overflowMenuButtons.shift()?.key;
+
+        button && mainButtonsKeys.push(button);
+    }
+
+    const mainMenuButtons = mainButtonsKeys.map(key => allButtons[key]);
+
+    return {
+        mainMenuButtons,
+        overflowMenuButtons
+    };
 }
