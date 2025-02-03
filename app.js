cfe81bda88c505ad22a75991fbd35aaca43316aa
Saúl Ibarra Corretgé <saghul@jitsi.org>
2025-02-03 14:52:16 +0100

    feat(build) use core-js to polyfill modern JavaScript features
    This should prevent us accidentally breaking compatibility with older
    browsers because polyfilling happens automatically based on usage
    detection.

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 /* Jitsi Meet app main entrypoint. */
 
-// Polyfill Promise.withReolvers.
-// FIXME(saghul) webpack + core-js v3 should polyfill this.
-import 'promise.withresolvers/auto';
-
 // Re-export jQuery
 // FIXME: Remove this requirement from torture tests.
 import $ from 'jquery';
