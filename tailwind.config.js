57641eefa8f1d4fe79c5039c7a362b6005610dc5
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2025-02-14 13:31:54 +0100

    Modified tailwind content source

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const { config } = require("@internxt/css-config");
 module.exports = {
     ...config,
     important: true,
-    content: [...config.content, "./node_modules/@internxt/ui/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: [...config.content, "./node_modules/@internxt/ui/dist/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
     corePlugins: {
         preflight: false,
     },
