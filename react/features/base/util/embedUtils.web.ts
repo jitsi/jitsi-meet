2696d64256a40f035ef4fc3ad274d02afa158764
Saúl Ibarra Corretgé <saghul@jitsi.org>
2025-03-07 13:48:16 +0100

    feat(inIframe/isEmbedded) add ability to detect generic embedding
    On web we detect if we run on an iframe, and on mobile we detect if the
    app is one of ours.

--- react/features/base/util/embedUtils.web.ts
+++ react/features/base/util/embedUtils.web.ts
@@ -3,11 +3,7 @@
  *
  * @returns {boolean} Whether the current page is loaded in an iframe.
  */
-export function inIframe(): boolean {
-    if (navigator.product === 'ReactNative') {
-        return false;
-    }
-
+export function isEmbedded(): boolean {
     try {
         return window.self !== window.top;
     } catch (e) {
