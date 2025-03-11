59420ea1a8204201db0b3203818594ef4ac66462
Calin-Teodor <calin.chitu@8x8.com>
2025-03-11 14:13:35 +0200

    feat(ios): fixed data type for startRecording param

--- ios/sdk/src/JitsiMeetView.h
+++ ios/sdk/src/JitsiMeetView.h
@@ -54,7 +54,7 @@ typedef NS_ENUM(NSInteger, RecordingMode) {
 - (void)toggleCamera;
 - (void)showNotification:(NSString * _Nonnull)appearance :(NSString * _Nullable)description :(NSString * _Nullable)timeout :(NSString * _Nullable)title :(NSString * _Nullable)uid;
 - (void)hideNotification:(NSString * _Nullable)uid;
-- (void)startRecording:(RecordingMode)mode :(NSString * _Nullable)dropboxToken :(BOOL)shouldShare :(NSString * _Nullable)rtmpStreamKey :(NSString * _Nullable)rtmpBroadcastID :(NSString * _Nullable)youtubeStreamKey :(NSString * _Nullable)youtubeBroadcastID :(NSString * _Nullable)extraMetadata :(BOOL)transcription;
+- (void)startRecording:(RecordingMode)mode :(NSString * _Nullable)dropboxToken :(BOOL)shouldShare :(NSString * _Nullable)rtmpStreamKey :(NSString * _Nullable)rtmpBroadcastID :(NSString * _Nullable)youtubeStreamKey :(NSString * _Nullable)youtubeBroadcastID :(NSDictionary * _Nullable)extraMetadata :(BOOL)transcription;
 - (void)stopRecording:(RecordingMode)mode :(BOOL)transcription;
 - (void)overwriteConfig:(NSDictionary * _Nonnull)config;
 - (void)sendCameraFacingModeMessage:(NSString * _Nonnull)to :(NSString * _Nonnull)facingMode;
