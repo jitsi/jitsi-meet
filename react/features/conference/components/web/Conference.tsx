6e4d9b081227d1bae572463d98890def865be8aa
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2025-03-13 14:59:12 +0100

    Removed jitsi conference component changes

--- react/features/conference/components/web/Conference.tsx
+++ react/features/conference/components/web/Conference.tsx
@@ -1,22 +1,18 @@
-import { throttle } from "lodash";
+import _ from "lodash";
 import React from "react";
 import { WithTranslation } from "react-i18next";
 import { connect as reactReduxConnect } from "react-redux";
 
-// @ts-expect-error
+// @ts-ignore
 import VideoLayout from "../../../../../modules/UI/videolayout/VideoLayout";
-import { IReduxState, IStore } from "../../../app/types";
+import { IReduxState } from "../../../app/types";
 import { getConferenceNameForTitle } from "../../../base/conference/functions";
 import { hangup } from "../../../base/connection/actions.web";
 import { isMobileBrowser } from "../../../base/environment/utils";
 import { translate } from "../../../base/i18n/functions";
 import { setColorAlpha } from "../../../base/util/helpers";
 import Chat from "../../../chat/components/web/Chat";
-import MainFilmstrip from "../../../filmstrip/components/web/MainFilmstrip";
-import ScreenshareFilmstrip from "../../../filmstrip/components/web/ScreenshareFilmstrip";
-import StageFilmstrip from "../../../filmstrip/components/web/StageFilmstrip";
 import CalleeInfoContainer from "../../../invite/components/callee-info/CalleeInfoContainer";
-import LargeVideo from "../../../large-video/components/LargeVideo.web";
 import LobbyScreen from "../../../lobby/components/web/LobbyScreen";
 import { getIsLobbyVisible } from "../../../lobby/functions";
 import { getOverlayToRender } from "../../../overlay/functions.web";
@@ -27,18 +23,19 @@ import ReactionAnimations from "../../../reactions/components/web/ReactionsAnima
 import { toggleToolboxVisible } from "../../../toolbox/actions.any";
 import { fullScreenChanged, showToolbox } from "../../../toolbox/actions.web";
 import JitsiPortal from "../../../toolbox/components/web/JitsiPortal";
-import Toolbox from "../../../toolbox/components/web/Toolbox";
 import { LAYOUT_CLASSNAMES } from "../../../video-layout/constants";
 import { getCurrentLayout } from "../../../video-layout/functions.any";
-// import VisitorsQueue from "../../../visitors/components/web/VisitorsQueue";
-// import { showVisitorsQueue } from "../../../visitors/functions";
 import { init } from "../../actions.web";
 import { maybeShowSuboptimalExperienceNotification } from "../../functions.web";
 import type { AbstractProps } from "../AbstractConference";
 import { AbstractConference, abstractMapStateToProps } from "../AbstractConference";
 
+import Header, { Mode } from "../../../base/meet/views/Conference/components/Header";
 import ConferenceInfo from "./ConferenceInfo";
 import { default as Notice } from "./Notice";
+
+import ConferenceControlsWrapper from "../../../base/meet/views/Conference/containers/ConferenceControlsWrapper";
+import VideoGalleryWrapper from "../../../base/meet/views/Conference/containers/VideoGalleryWrapper";
 
 /**
  * DOM events for when full screen mode has changed. Different browsers need
@@ -48,6 +45,8 @@ import { default as Notice } from "./Notice";
  * @type {Array<string>}
  */
 const FULL_SCREEN_EVENTS = ["webkitfullscreenchange", "mozfullscreenchange", "fullscreenchange"];
+
+declare const APP: any;
 
 /**
  * The type of the React {@code Component} props of {@link Conference}.
@@ -94,17 +93,9 @@ interface IProps extends AbstractProps, WithTranslation {
      */
     _showPrejoin: boolean;
 
-    dispatch: IStore["dispatch"];
-}
+    dispatch: any;
 
-/**
- * Returns true if the prejoin screen should be displayed and false otherwise.
- *
- * @param {IProps} props - The props object.
- * @returns {boolean} - True if the prejoin screen should be displayed and false otherwise.
- */
-function shouldShowPrejoin({ _showLobby, _showPrejoin }: IProps) {
-    return _showPrejoin && !_showLobby;
+    isParticipantsPaneOpened: boolean;
 }
 
 /**
@@ -113,7 +104,13 @@ function shouldShowPrejoin({ _showLobby, _showPrejoin }: IProps) {
 class Conference extends AbstractConference<IProps, any> {
     _originalOnMouseMove: Function;
     _originalOnShowToolbar: Function;
+    state = {
+        videoMode: "gallery" as Mode,
+    };
 
+    _onSetVideoModeClicked = (newMode: Mode) => {
+        this.setState({ videoMode: newMode });
+    };
     /**
      * Initializes a new Conference instance.
      *
@@ -130,19 +127,19 @@ class Conference extends AbstractConference<IProps, any> {
         this._originalOnShowToolbar = this._onShowToolbar;
         this._originalOnMouseMove = this._onMouseMove;
 
-        this._onShowToolbar = throttle(() => this._originalOnShowToolbar(), 100, {
+        this._onShowToolbar = _.throttle(() => this._originalOnShowToolbar(), 100, {
             leading: true,
             trailing: false,
         });
 
-        this._onMouseMove = throttle((event) => this._originalOnMouseMove(event), _mouseMoveCallbackInterval, {
+        this._onMouseMove = _.throttle((event) => this._originalOnMouseMove(event), _mouseMoveCallbackInterval, {
             leading: true,
             trailing: false,
         });
 
         // Bind event handler so it is only bound once for every instance.
         this._onFullScreenChange = this._onFullScreenChange.bind(this);
-        this._onVideospaceTouchStart = this._onVideospaceTouchStart.bind(this);
+        this._onVidespaceTouchStart = this._onVidespaceTouchStart.bind(this);
         this._setBackground = this._setBackground.bind(this);
     }
 
@@ -152,7 +149,7 @@ class Conference extends AbstractConference<IProps, any> {
      * @inheritdoc
      */
     componentDidMount() {
-        document.title = `${this.props._roomName} | ${interfaceConfig.APP_NAME}`;
+        document.title = `${interfaceConfig.APP_NAME}`;
         this._start();
     }
 
@@ -202,9 +199,9 @@ class Conference extends AbstractConference<IProps, any> {
             _overflowDrawer,
             _showLobby,
             _showPrejoin,
-
             t,
         } = this.props;
+        const { videoMode } = this.state;
 
         return (
             <div
@@ -216,19 +213,26 @@ class Conference extends AbstractConference<IProps, any> {
             >
                 <Chat />
                 <div
-                    className={_layoutClassName}
+                    // _layoutClassName has the styles to manage the side bar
+                    className={_layoutClassName + " bg-gray-100"}
+                    // className={"bg-gray-100 relative flex"}
                     id="videoconference_page"
                     onMouseMove={isMobileBrowser() ? undefined : this._onShowToolbar}
                 >
-                    {_showPrejoin || _showLobby || <ConferenceInfo />}
+                    <ConferenceInfo />
                     <Notice />
-                    <div id="videospace" onTouchStart={this._onVideospaceTouchStart}>
-                        <LargeVideo />
+                    <div onTouchStart={this._onVidespaceTouchStart}>
+                        <Header mode={videoMode} translate={t} onSetModeClicked={this._onSetVideoModeClicked} />
+                        <div className="flex">
+                            {/* <LargeVideoWeb /> */}
+                            <VideoGalleryWrapper videoMode={videoMode} />
+                        </div>
                         {_showPrejoin || _showLobby || (
                             <>
-                                <StageFilmstrip />
-                                <ScreenshareFilmstrip />
-                                <MainFilmstrip />
+                                {/* <StageFilmstrip /> */}
+                                {/*  <ScreenshareFilmstrip />*/}
+                                {/* right screen tools component */}
+                                {/* <MainFilmstrip /> */}
                             </>
                         )}
                     </div>
@@ -236,12 +240,14 @@ class Conference extends AbstractConference<IProps, any> {
                     {_showPrejoin || _showLobby || (
                         <>
                             <span aria-level={1} className="sr-only" role="heading">
-                                {t("toolbar.accessibilityLabel.heading")}
+                                {t("toolbar.accessibilityLabel.heading") as string}
                             </span>
-                            <Toolbox />
+
+                            {/* <Toolbox /> */}
                         </>
                     )}
-
+                    {/* CONFERENCE MEDIA CONTROLS */}
+                    <ConferenceControlsWrapper />
                     {_notificationsVisible &&
                         !_isAnyOverlayVisible &&
                         (_overflowDrawer ? (
@@ -254,7 +260,7 @@ class Conference extends AbstractConference<IProps, any> {
 
                     <CalleeInfoContainer />
 
-                    {shouldShowPrejoin(this.props) && <Prejoin />}
+                    {_showPrejoin && <Prejoin />}
                     {_showLobby && <LobbyScreen />}
                 </div>
                 <ParticipantsPane />
@@ -298,7 +304,7 @@ class Conference extends AbstractConference<IProps, any> {
      * @private
      * @returns {void}
      */
-    _onVideospaceTouchStart() {
+    _onVidespaceTouchStart() {
         this.props.dispatch(toggleToolboxVisible());
     }
 
@@ -365,15 +371,15 @@ class Conference extends AbstractConference<IProps, any> {
      */
     _start() {
         APP.UI.start();
+
+        APP.UI.registerListeners();
         APP.UI.bindEvents();
 
         FULL_SCREEN_EVENTS.forEach((name) => document.addEventListener(name, this._onFullScreenChange));
 
         const { dispatch, t } = this.props;
 
-        // if we will be showing prejoin we don't want to call connect from init.
-        // Connect will be dispatched from prejoin screen.
-        dispatch(init(!shouldShowPrejoin(this.props)));
+        dispatch(init());
 
         maybeShowSuboptimalExperienceNotification(dispatch, t);
     }
@@ -401,8 +407,7 @@ function _mapStateToProps(state: IReduxState) {
         _roomName: getConferenceNameForTitle(state),
         _showLobby: getIsLobbyVisible(state),
         _showPrejoin: isPrejoinPageVisible(state),
-        // _showVisitorsQueue: showVisitorsQueue(state),
     };
 }
 
-export default reactReduxConnect(_mapStateToProps)(translate(Conference));
+export default translate(reactReduxConnect(_mapStateToProps)(Conference));
