036f045257608850fe309ba322bcd418f4b5ff56
Calin-Teodor <calin.chitu@8x8.com>
2025-03-18 17:26:26 +0200

    feat(base/ui): use lodash to merge tokens into one

--- react/features/base/ui/utils.ts
+++ react/features/base/ui/utils.ts
@@ -1,20 +1,20 @@
+import { merge } from 'lodash';
+
 import * as jitsiTokens from './jitsiTokens.json';
 import * as tokens from './tokens.json';
 
 /**
  * Creates the color tokens based on the color theme and the association map.
- * If a key is not found in tokens, it checks jitsiTokens, and if still not found,
- * defaults to the current value.
  *
  * @param {Object} colorMap - A map between the token name and the actual color value.
  * @returns {Object}
  */
 export function createColorTokens(colorMap: Object): any {
+    const allTokens = merge({}, tokens, jitsiTokens);
+
     return Object.entries(colorMap)
         .reduce((result, [ token, value ]: [any, string]) => {
-            const color = tokens[value as keyof typeof tokens]
-                || jitsiTokens[value as keyof typeof jitsiTokens]
-                || value;
+            const color = allTokens[value as keyof typeof allTokens];
 
             return Object.assign(result, { [token]: color });
         }, {});
