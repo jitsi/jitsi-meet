d1f967beb70f277e81e55836785d89f08d5a816c
Calin-Teodor <calin.chitu@8x8.com>
2025-03-28 17:23:42 +0200

    fix(mobile/external-api): check types for recording error, initiator, terminator

--- react/features/mobile/external-api/middleware.ts
+++ react/features/mobile/external-api/middleware.ts
@@ -298,17 +298,20 @@ externalAPIEnabled && MiddlewareRegistry.register(store => next => action => {
             timestamp
         } = action.sessionData;
 
+        const getId = (obj: any) => typeof obj === 'object' ? obj.getId() : obj;
+        const getError = (err: any) => typeof err === 'object' ? String(err) : err;
+
         sendEvent(
             store,
             RECORDING_STATUS_CHANGED,
             /* data */ {
-                error,
+                error: getError(error),
                 id,
-                initiator,
+                initiator: getId(initiator),
                 liveStreamViewURL,
                 mode,
                 status,
-                terminator,
+                terminator: getId(terminator),
                 timestamp
             });
         break;
