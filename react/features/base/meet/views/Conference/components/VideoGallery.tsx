300d6a6b85b4da8f1285d069142f37f6fb5f79eb
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2025-04-09 14:49:26 +0200

    Adapt gallery view to mobile screens

--- react/features/base/meet/views/Conference/components/VideoGallery.tsx
+++ react/features/base/meet/views/Conference/components/VideoGallery.tsx
@@ -1,8 +1,7 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { VideoParticipantType } from "../types";
 import VideoParticipant from "./VideoParticipant";
 import { useAspectRatio } from "../../../general/hooks/useAspectRatio";
-
 
 export interface VideoGalleryProps {
     participants: VideoParticipantType[];
@@ -30,34 +29,37 @@ const VideoGallery = ({ participants, flipX, translate }: VideoGalleryProps) => 
         let heightClass = "";
 
         if (participantsNumber === 1) {
-            return "relative aspect-video max-h-[75%] max-w-full";
+            return "relative aspect-square sm:aspect-video max-h-[75%] max-w-full";
         } else if (participantsNumber === 2) {
             widthClass = "w-[calc(50%-5px)]";
-            heightClass = "max-h-[75%]";
+            heightClass = "sm:max-h-[75%]";
         } else if (participantsNumber === 3) {
-            widthClass = "w-[calc(33%-10px)]";
-            heightClass = "max-h-[36%]";
+            widthClass = "w-[calc(33.333%-5px)] sm:w-[calc(33%-10px)]";
+            heightClass = "sm:max-h-[36%]";
         } else if (participantsNumber === 4) {
             widthClass = "w-[calc(50%-5px)]";
-            heightClass = "max-h-[36%]";
+            heightClass = "sm:max-h-[36%]";
         } else if (participantsNumber > 4 && participantsNumber < 10) {
-            widthClass = "w-[calc(33.333%-7px)]";
-            heightClass = "max-h-[24%]";
+            widthClass = "w-[calc(50%-5px)] sm:w-[calc(33.333%-7px)]";
+            heightClass = "sm:max-h-[24%]";
         } else {
-            widthClass = "w-[calc(25%-7px)]";
-            heightClass = "max-h-[18%]";
+            widthClass = "w-[calc(50%-5px)] sm:w-[calc(25%-7px)]";
+            heightClass = "sm:max-h-[18%]";
         }
 
-        return `relative ${widthClass} ${heightClass} aspect-video`;
+
+        const mobileHeightClass = participantsNumber > 4 ? "max-h-[120px]" : "";
+
+        return `relative ${widthClass} ${heightClass} ${mobileHeightClass} aspect-square sm:aspect-video`;
     };
 
     return (
         <div className="h-full w-full flex items-center justify-center overflow-hidden bg-gray-950">
             <div
-                className="h-[90%] w-[90%] flex justify-center items-center overflow-hidden"
+                className={`max-h-[85vh] sm:h-[90%] w-[95%] sm:w-[90%] flex justify-center items-center`}
                 style={containerStyle}
             >
-                <div className="max-h-full w-full flex flex-wrap justify-center items-center content-center gap-2.5 overflow-hidden">
+                <div className="w-full flex flex-wrap justify-center items-start content-start gap-2.5">
                     {sortedParticipants.map((participant) => (
                         <VideoParticipant
                             key={participant.id}
