f9927e4cd76f23d3690bcb8f10b4f7787da98057
Дамян Минков <damencho@jitsi.org>
2025-05-02 09:41:48 -0500

    feat(tests): Adds invite test. (#15986)
    * feat(tests): Adds invite test.
    
    Tests dial-in, dial-out and inviting sip-jibri.
    
    * squash: Extract duplicate code in a function.
    
    * squash: Fixes comments.

--- tests/specs/alone/dialInAudio.spec.ts
+++ tests/specs/alone/dialInAudio.spec.ts
@@ -1,8 +1,7 @@
-import https from 'node:https';
 import process from 'node:process';
 
 import { ensureOneParticipant } from '../../helpers/participants';
-import { cleanup, isDialInEnabled, waitForAudioFromDialInParticipant } from '../helpers/DialIn';
+import { cleanup, dialIn, isDialInEnabled, retrievePin, waitForAudioFromDialInParticipant } from '../helpers/DialIn';
 
 describe('Dial-In', () => {
     it('join participant', async () => {
@@ -22,59 +21,25 @@ describe('Dial-In', () => {
     });
 
     it('retrieve pin', async () => {
-        let dialInPin;
-
         try {
-            dialInPin = await ctx.p1.getInviteDialog().getPinNumber();
+            await retrievePin(ctx.p1);
         } catch (e) {
             console.error('dial-in.test.no-pin');
             ctx.skipSuiteTests = true;
             throw e;
         }
 
-        await ctx.p1.getInviteDialog().clickCloseButton();
-
-        if (dialInPin.length === 0) {
+        if (ctx.data.dialInPin === 0) {
             console.error('dial-in.test.no-pin');
             ctx.skipSuiteTests = true;
             throw new Error('no pin');
         }
 
-        expect(dialInPin.length >= 8).toBe(true);
-
-        ctx.data.dialInPin = dialInPin;
+        expect(ctx.data.dialInPin.length >= 8).toBe(true);
     });
 
     it('invite dial-in participant', async () => {
-        if (!await ctx.p1.isInMuc()) {
-            // local participant did not join abort
-            return;
-        }
-
-        const restUrl = process.env.DIAL_IN_REST_URL?.replace('{0}', ctx.data.dialInPin);
-
-        // we have already checked in the first test that DIAL_IN_REST_URL exist so restUrl cannot be ''
-        const responseData: string = await new Promise((resolve, reject) => {
-            https.get(restUrl || '', res => {
-                let data = '';
-
-                res.on('data', chunk => {
-                    data += chunk;
-                });
-
-                res.on('end', () => {
-                    ctx.times.restAPIExecutionTS = performance.now();
-
-                    resolve(data);
-                });
-            }).on('error', err => {
-                console.error('dial-in.test.restAPI.request.fail');
-                console.error(err);
-                reject(err);
-            });
-        });
-
-        console.log(`dial-in.test.call_session_history_id:${JSON.parse(responseData).call_session_history_id}`);
+        await dialIn(ctx.p1);
     });
 
     it('wait for audio from dial-in participant', async () => {
