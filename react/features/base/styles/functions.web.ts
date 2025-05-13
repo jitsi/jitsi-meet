c1fcac41bb880bb898046e18e910e96f8f0b6c56
raduanastase8x8 <radu.anastase@8x8.com>
2025-05-13 17:25:53 +0300

    Fixed px calculations

--- react/features/base/styles/functions.web.ts
+++ react/features/base/styles/functions.web.ts
@@ -2,6 +2,8 @@ import { StyleType } from './functions.any';
 
 export * from './functions.any';
 
+const BASE_FONT_SIZE = 16;
+
 /**
  * Fixes the style prop that is passed to a platform generic component based on platform specific
  * format requirements.
@@ -31,8 +33,24 @@ export function getFixedPlatformStyle(style?: StyleType | StyleType[]) {
  * @returns {Object}
  */
 export function withPixelLineHeight(base: any) {
+    let lineHeight = base.lineHeight;
+
+    if (typeof lineHeight === 'string') {
+        lineHeight = remToPixels(base.lineHeight);
+    }
+
     return {
         ...base,
-        lineHeight: `${base.lineHeight}px`
+        lineHeight: `${lineHeight}px`
     };
+}
+
+/**
+ * Converts a rem value to pixels.
+ *
+ * @param {string | number} rem - The rem value to convert.
+ * @returns {number} - The converted pixel value.
+ */
+export function remToPixels(rem: string | number): number {
+    return Math.round(parseFloat(String(rem)) * BASE_FONT_SIZE);
 }
