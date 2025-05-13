0897db3efcf0adaf3571e1a053d31159947bf0be
damencho <damencho@jitsi.org>
2025-05-13 18:34:05 -0500

    feat(tests): Adds visitors go live test.

--- tests/pageobjects/Visitors.ts
+++ tests/pageobjects/Visitors.ts
@@ -31,4 +31,33 @@ export default class Visitors extends BasePageObject {
 
         return this.participant.driver.$('#visitor-list-header').getText();
     }
+
+    /**
+     * Whether the visitors queue UI is shown.
+     */
+    isVisitorsQueueUIShown() {
+        return this.participant.driver.$('#visitors-waiting-queue').isDisplayed();
+    }
+
+    /**
+     * Returns the name of the knocking participant (the only one) that is displayed on the notification.
+     */
+    async getWaitingVisitorsInQueue() {
+        const goLiveNotification
+            = this.participant.driver.$('//div[@data-testid="notify.waitingVisitors"]');
+
+        await goLiveNotification.waitForDisplayed({
+            timeout: 3000,
+            timeoutMsg: 'Go live notification not displayed'
+        });
+
+        return await goLiveNotification.getText();
+    }
+
+    /**
+     * Clicks the go live button in the visitors notification.
+     */
+    async goLive() {
+        return this.participant.driver.$('//button[@data-testid="participantsPane.actions.goLive"]').click();
+    }
 }
