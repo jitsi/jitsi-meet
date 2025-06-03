32dd3e9857806fce2fe8f08880cc12fae5308259
damencho <damencho@jitsi.org>
2025-06-03 13:00:57 -0500

    feat(jwt): Delay loading of avatar.
    Only load avatar when jwt has been validated server-side (after connected). The drawback is not loading the avatar on pre-join page only the first time.

--- react/features/base/jwt/actions.ts
+++ react/features/base/jwt/actions.ts
@@ -1,11 +1,11 @@
-import { SET_JWT } from './actionTypes';
+import { SET_DELAYED_LOAD_OF_AVATAR_URL, SET_JWT, SET_KNOWN_AVATAR_URL } from './actionTypes';
 
 /**
  * Stores a specific JSON Web Token (JWT) into the redux store.
  *
  * @param {string} [jwt] - The JSON Web Token (JWT) to store.
  * @returns {{
- *     type: SET_TOKEN_DATA,
+ *     type: SET_JWT,
  *     jwt: (string|undefined)
  * }}
  */
@@ -13,5 +13,37 @@ export function setJWT(jwt?: string) {
     return {
         type: SET_JWT,
         jwt
+    };
+}
+
+/**
+ * Sets an avatar URL for delayed loading.
+ *
+ * @param {string} avatarUrl - The avatar URL to set for delayed loading.
+ * @returns {{
+ *     type: SET_DELAYED_LOAD_OF_AVATAR_URL,
+ *     avatarUrl: string
+ * }}
+ */
+export function setDelayedLoadOfAvatarUrl(avatarUrl?: string) {
+    return {
+        type: SET_DELAYED_LOAD_OF_AVATAR_URL,
+        avatarUrl
+    };
+}
+
+/**
+ * Sets a known avatar URL.
+ *
+ * @param {string} avatarUrl - The avatar URL to set as known.
+ * @returns {{
+ *     type: SET_KNOWN_AVATAR_URL,
+ *     avatarUrl: string
+ * }}
+ */
+export function setKnownAvatarUrl(avatarUrl: string) {
+    return {
+        type: SET_KNOWN_AVATAR_URL,
+        avatarUrl
     };
 }
