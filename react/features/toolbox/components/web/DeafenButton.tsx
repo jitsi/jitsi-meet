4aced629c16d3ebc0e15daceb39f1b94a5b40931
Kota Yatagai <kota@yatagai.com>
2025-06-06 15:23:33 +0900

    add translation

--- react/features/toolbox/components/web/DeafenButton.tsx
+++ react/features/toolbox/components/web/DeafenButton.tsx
@@ -1,21 +1,37 @@
-import AbstractButton, { IProps as AbstractButtonProps } from '../../../base/toolbox/components/AbstractButton';
+import { connect } from 'react-redux';
+import { withStyles } from 'tss-react/mui';
+import AbstractDeafenButton, {
+    IProps as AbstractDeafenButtonProps,
+    mapStateToProps as abstractMapStateToProps
+} from '../AbstractDeafenButton';
+import { translate } from '../../../base/i18n/functions';
+
+/**
+ * The type of the React {@code Component} props of {@link DeafenButton}.
+ */
+interface IProps extends AbstractDeafenButtonProps {
+    /**
+     * Whether the deafen action is pending.
+     */
+    _pending?: boolean;
+}
 
 /**
  * Component that renders a toolbar button for toggling deafen.
  *
  * @augments AbstractDeafenButton
  */
-class DeafenButton extends AbstractButton<AbstractButtonProps> {
+class DeafenButton extends AbstractDeafenButton<IProps> {
     /**
      * Initializes a new {@code DeafenButton} instance.
      *
      * @param {AbstractButtonProps} props - The read-only React {@code Component} props with
      * which the new instance is to be initialized.
      */
-    constructor(props: AbstractButtonProps) {
+    constructor(props: IProps) {
         super(props);
         this._getTooltip = this._getLabel;
     }
 }
 
-export default DeafenButton;
+export default translate(connect(abstractMapStateToProps)(DeafenButton));
