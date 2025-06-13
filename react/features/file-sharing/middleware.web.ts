e5187de9c3d0f757816a963d4eb778ee0705cb99
damencho <damencho@jitsi.org>
2025-06-13 15:12:48 -0500

    feat(file-sharing): Uses a filesharing management from ljm.
    squash: Change the way we pass the list of files from the backend.
    
    squash: Remove actions to sync local state with remote.

--- react/features/file-sharing/middleware.web.ts
+++ react/features/file-sharing/middleware.web.ts
@@ -2,17 +2,46 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { IStore } from '../app/types';
 import { getCurrentConference } from '../base/conference/functions';
+import { JitsiConferenceEvents } from '../base/lib-jitsi-meet';
 import { getLocalParticipant, getParticipantDisplayName } from '../base/participants/functions';
 import MiddlewareRegistry from '../base/redux/MiddlewareRegistry';
+import StateListenerRegistry from '../base/redux/StateListenerRegistry';
 import { showErrorNotification } from '../notifications/actions';
 import { NOTIFICATION_TIMEOUT_TYPE, NOTIFICATION_TYPE } from '../notifications/constants';
 
-import { DOWNLOAD_FILE, REMOVE_FILE, UPLOAD_FILES } from './actionTypes';
+import { DOWNLOAD_FILE, REMOVE_FILE, UPLOAD_FILES, _FILE_LIST_RECEIVED, _FILE_REMOVED } from './actionTypes';
 import { addFile, removeFile, updateFileProgress } from './actions';
-import { FILE_SHARING_PREFIX, FILE_SHARING_SERVICE } from './constants';
 import { getFileExtension } from './functions.any';
 import logger from './logger';
 import { IFileMetadata } from './types';
+
+
+/**
+ * Registers a change handler for state['features/base/conference'].conference to
+ * set the event listeners needed for the file sharing feature to operate.
+ */
+StateListenerRegistry.register(
+    state => state['features/base/conference'].conference,
+    (conference, { dispatch }, previousConference) => {
+        if (conference && !previousConference) {
+            conference.on(JitsiConferenceEvents.FILE_SHARING_FILE_ADDED, (file: IFileMetadata) => {
+                dispatch(addFile(file));
+            });
+            conference.on(JitsiConferenceEvents.FILE_SHARING_FILE_REMOVED, (fileId: string) => {
+                dispatch({
+                    type: _FILE_REMOVED,
+                    fileId
+                });
+            });
+
+            conference.on(JitsiConferenceEvents.FILE_SHARING_FILES_RECEIVED, (files: object) => {
+                dispatch({
+                    type: _FILE_LIST_RECEIVED,
+                    files
+                });
+            });
+        }
+    });
 
 /**
  * Middleware that handles file sharing actions.
@@ -26,7 +55,7 @@ MiddlewareRegistry.register(store => next => action => {
         const state = store.getState();
         const conference = getCurrentConference(state);
 
-        conference?.getShortTermCredentials(FILE_SHARING_SERVICE).then((token: string) => {
+        conference?.getShortTermCredentials(conference?.getFileSharing()?.getIdentityType()).then((token: string) => {
             for (const file of action.files) {
                 uploadFile(file, store, token);
             }
@@ -38,29 +67,30 @@ MiddlewareRegistry.register(store => next => action => {
     case REMOVE_FILE: {
         const state = store.getState();
         const conference = getCurrentConference(state);
-        const { fileSharing } = state['features/base/config'];
-        const sessionId = conference?.getMeetingUniqueId();
-        let doDelete = false;
+        const { files } = state['features/file-sharing'];
+        const fileId = action.fileId;
+        const existingMetadata = files.get(fileId);
 
-        // First remove the file metadata so others won't attempt to download it anymore.
-        const metadataHandler = conference?.getMetadataHandler();
-
-        if (metadataHandler) {
-            const metadataId = `${FILE_SHARING_PREFIX}.${action.fileId}`;
-            const existingMetadata = metadataHandler.getMetadata()[metadataId] ?? {};
-
-            doDelete = (existingMetadata?.process ?? 100) === 100;
-
-            metadataHandler.setMetadata(metadataId, {});
-        }
-
-        if (!doDelete) {
+        // ignore remove a file till the file is actually uploaded
+        if (!conference || (existingMetadata?.progress ?? 100) !== 100) {
             return next(action);
         }
 
+        // First, remove the file metadata so others won't attempt to download it anymore.
+        conference.getFileSharing().removeFile(fileId);
+
+        // remove it from local state
+        store.dispatch({
+            type: _FILE_REMOVED,
+            fileId
+        });
+
+        const { fileSharing } = state['features/base/config'];
+        const sessionId = conference.getMeetingUniqueId();
+
         // Now delete it from the server.
-        conference?.getShortTermCredentials(FILE_SHARING_SERVICE)
-        .then((token: string) => fetch(`${fileSharing!.apiUrl!}/sessions/${sessionId}/files/${action.fileId}`, {
+        conference.getShortTermCredentials(conference.getFileSharing().getIdentityType())
+        .then((token: string) => fetch(`${fileSharing!.apiUrl!}/sessions/${sessionId}/files/${fileId}`, {
             method: 'DELETE',
             headers: {
                 'Authorization': `Bearer ${token}`
@@ -84,7 +114,7 @@ MiddlewareRegistry.register(store => next => action => {
         const conference = getCurrentConference(state);
         const sessionId = conference?.getMeetingUniqueId();
 
-        conference?.getShortTermCredentials(FILE_SHARING_SERVICE)
+        conference?.getShortTermCredentials(conference?.getFileSharing()?.getIdentityType())
         .then((token: string) => fetch(`${fileSharing!.apiUrl!}/sessions/${sessionId}/files/${action.fileId}`, {
             method: 'GET',
             headers: {
@@ -195,9 +225,9 @@ function uploadFile(file: File, store: IStore, token: string): void {
         if (xhr.status >= 200 && xhr.status < 300) {
             store.dispatch(updateFileProgress(fileId, 100));
 
-            const metadataHandler = conference?.getMetadataHandler();
+            const fileSharingHandler = conference?.getFileSharing();
 
-            metadataHandler?.setMetadata(`${FILE_SHARING_PREFIX}.${fileId}`, fileMetadata);
+            fileSharingHandler.addFile(fileMetadata);
         } else {
             handleError();
         }
