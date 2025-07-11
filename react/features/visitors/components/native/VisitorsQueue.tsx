4188802491c54039a65bc93d6920a1b935d90651
Calin-Teodor <calin.chitu@8x8.com>
2025-07-11 17:18:28 +0300

    feat(visitors): style adjustments

--- react/features/visitors/components/native/VisitorsQueue.tsx
+++ react/features/visitors/components/native/VisitorsQueue.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Text, View } from 'react-native';
+import { Text, View, ViewStyle } from 'react-native';
 import { useDispatch } from 'react-redux';
 
 import { hangup } from '../../../base/connection/actions.native';
@@ -8,7 +8,9 @@ import LoadingIndicator from '../../../base/react/components/native/LoadingIndic
 import BaseTheme from '../../../base/ui/components/BaseTheme.native';
 import Button from '../../../base/ui/components/native/Button';
 import { BUTTON_TYPES } from '../../../base/ui/constants.any';
-import styles from '../../../lobby/components/native/styles';
+import lobbyStyles from '../../../lobby/components/native/styles';
+
+import styles from './styles';
 
 /**
  * The component that renders visitors queue UI.
@@ -23,13 +25,13 @@ export default function VisitorsQueue() {
     }, []);
 
     return (
-        <View style = { styles.lobbyWaitingFragmentContainer }>
-            <Text style = { styles.lobbyTitle }>
+        <View style = { styles.visitorsQueue as ViewStyle }>
+            <Text style = { styles.visitorsQueueTitle }>
                 { t('visitors.waitingMessage') }
             </Text>
             <LoadingIndicator
                 color = { BaseTheme.palette.icon01 }
-                style = { styles.loadingIndicator } />
+                style = { lobbyStyles.loadingIndicator } />
             <Button
                 accessibilityLabel = 'toolbar.accessibilityLabel.leaveConference'
                 labelKey = 'toolbar.accessibilityLabel.leaveConference'
