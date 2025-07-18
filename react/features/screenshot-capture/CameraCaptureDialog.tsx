c5e7f27190c0ee7e353de12097ec5f4a18e48c9b
Horatiu Muresan <horatiu.muresan@8x8.com>
2025-07-18 09:55:54 +0300

    fix lint

--- react/features/screenshot-capture/CameraCaptureDialog.tsx
+++ react/features/screenshot-capture/CameraCaptureDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { WithTranslation } from 'react-i18next';
 import { connect, useDispatch } from 'react-redux';
 import { makeStyles } from 'tss-react/mui';
