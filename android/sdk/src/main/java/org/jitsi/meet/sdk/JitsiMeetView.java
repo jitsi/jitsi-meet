17c50a6420831af0188640336c5db7bbee814183
Calin-Teodor <calin.chitu@8x8.com>
2025-07-22 12:07:40 +0300

    feat(android/sdk): remove comment

--- android/sdk/src/main/java/org/jitsi/meet/sdk/JitsiMeetView.java
+++ android/sdk/src/main/java/org/jitsi/meet/sdk/JitsiMeetView.java
@@ -197,10 +197,6 @@ public class JitsiMeetView extends FrameLayout {
         }
 
         setBackgroundColor(BACKGROUND_COLOR);
-
-        // ReactInstanceManagerHolder.initReactInstanceManager((Application) context.getApplicationContext());
-        // ReactInstanceManager is now initialized by JitsiInitializer during application startup
-        // No need to initialize it here anymore
     }
 
     /**
