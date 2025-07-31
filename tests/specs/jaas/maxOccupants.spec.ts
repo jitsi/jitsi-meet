b0491d7d2b08d7fecab3766954659921920a6e8b
Boris Grozev <boris@jitsi.org>
2025-07-31 09:59:25 -0500

    test: Add a maxOccupants jaas test.

---
+++ tests/specs/jaas/maxOccupants.spec.ts
@@ -0,0 +1,29 @@
+import { getToken } from '../../helpers/participants';
+import { setTestProperties } from "../../helpers/TestProperties";
+import { joinMuc } from "../helpers/jaas";
+
+setTestProperties(__filename, {
+    useJaas: true,
+    useWebhookProxy: true
+});
+
+describe('MaxOccupants limit enforcement', () => {
+    it('test maxOccupants limit', async () => {
+        ctx.webhooksProxy.defaultMeetingSettings = {
+            maxOccupants: 2
+        };
+
+        const p1 = await joinMuc(ctx.roomName, 'p1', getToken(ctx, "p1", { room: ctx.roomName }));
+        const p2 = await joinMuc(ctx.roomName, 'p2', getToken(ctx, "p2", { room: ctx.roomName }));
+        expect(await p1.isInMuc()).toBe(true);
+        expect(await p2.isInMuc()).toBe(true);
+
+        // Third participant should be rejected (exceeding maxOccupants), even if it's a moderator
+        let p3 = await joinMuc(ctx.roomName, 'p3', getToken(ctx, "p3", { room: ctx.roomName, moderator: true }));
+        expect(Boolean(await p3.isInMuc())).toBe(false);
+
+        await p1.hangup()
+        p3 = await joinMuc(ctx.roomName, 'p3', getToken(ctx, "p3", { room: ctx.roomName }));
+        expect(await p3.isInMuc()).toBe(true);
+    });
+});
