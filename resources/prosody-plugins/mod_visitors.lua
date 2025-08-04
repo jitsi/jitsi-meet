8269b8879677071612dbe16a67f7b5254dc75b9e
Дамян Минков <damencho@jitsi.org>
2025-08-04 22:56:21 +0300

    feat(prosody): Adds docs for added room fields. (#16299)
    * feat(prosody): Adds docs for added room fields.
    
    * squash: Drop comment.

--- resources/prosody-plugins/mod_visitors.lua
+++ resources/prosody-plugins/mod_visitors.lua
@@ -102,11 +102,8 @@ local function send_visitors_iq(conference_service, room, type)
 end
 
 -- Filter out identity information (nick name, email, etc) from a presence stanza,
--- if the hideDisplayNameForGuests option for the room is set (note that the
--- hideDisplayNameForAll option is implemented in a diffrent way and does not
--- require filtering here)
--- This is applied to presence of main room participants before it is sent out to
--- vnodes.
+-- if the hideDisplayNameForGuests option for the room is set.
+-- This is applied to presence of main room participants before it is sent out to vnodes.
 local function filter_stanza_nick_if_needed(stanza, room)
     if not stanza or stanza.name ~= 'presence' or stanza.attr.type == 'error' or stanza.attr.type == 'unavailable' then
         return stanza;
