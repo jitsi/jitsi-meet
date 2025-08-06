1c4b5841e2f170840ece857ea228f65f69b2b85f
Hugo Lavernhe <hugo.lavernhe@gmail.com>
2025-08-06 20:35:16 +0200

    Move previewAudioTrack to redux

--- react/features/settings/actionTypes.ts
+++ react/features/settings/actionTypes.ts
@@ -7,3 +7,8 @@ export const SET_AUDIO_SETTINGS_VISIBILITY = 'SET_AUDIO_SETTINGS_VISIBILITY';
  * The type of (redux) action which sets the visibility of the video settings popup.
  */
 export const SET_VIDEO_SETTINGS_VISIBILITY = 'SET_VIDEO_SETTINGS_VISIBILITY';
+
+/**
+ * The type of (redux) action which sets the preview audio track settings.
+ */
+export const SET_PREVIEW_AUDIO_TRACK = 'SET_PREVIEW_AUDIO_TRACK';
