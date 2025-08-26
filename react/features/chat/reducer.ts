e98c6e14c2d331fe0e7f78a2893a19164880d118
damencho <damencho@jitsi.org>
2025-08-26 12:36:26 -0500

    feat(display-name): Handles new display-name extension in messages.
    The display name is used in messages when messages are coming from visitors or from the history. The display name is used only when the participant is not available in the meeting to get its name.

--- react/features/chat/reducer.ts
+++ react/features/chat/reducer.ts
@@ -68,6 +68,7 @@ ReducerRegistry.register<IChatState>('features/chat', (state = DEFAULT_STATE, ac
         const newMessage: IMessage = {
             displayName: action.displayName,
             error: action.error,
+            isFromGuest: Boolean(action.isFromGuest),
             isFromVisitor: Boolean(action.isFromVisitor),
             participantId: action.participantId,
             isReaction: action.isReaction,
--- react/features/visitors/functions.ts
+++ react/features/visitors/functions.ts
@@ -141,19 +141,13 @@ export function shouldDisplayCurrentVisitorsList(stateful: IStateful): boolean {
 }
 
 /**
- *
- * @param state
- * @param displayName
- * @returns
- */
-/**
- * Returns visitor's display name, falling back to the default remote display name
+ * Returns display name, falling back to the default remote display name
  * from config, or 'Fellow Jitster' if neither is available.
  *
  * @param {IReduxState} state - The Redux state.
  * @param {string} [displayName] - Optional display name to use if available.
- * @returns {string} - The display name for a visitor.
+ * @returns {string} - The display name.
  */
-export function getVisitorDisplayName(state: IReduxState, displayName?: string): string {
+export function getDisplayName(state: IReduxState, displayName?: string): string {
     return displayName || state['features/base/config'].defaultRemoteDisplayName || 'Fellow Jitster';
 }
