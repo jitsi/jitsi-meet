9ae97bfb506e16754a9edfed86965cfb06c00c44
sandunsameera <sandunsameera25@gmail.com>
2025-09-21 10:22:51 +0530

    fix: add MeetingParticipants component to ParticipantsPane

--- react/features/deep-linking/components/DeepLinkingDesktopPage.web.tsx
+++ react/features/deep-linking/components/DeepLinkingDesktopPage.web.tsx
@@ -127,7 +127,7 @@ const DeepLinkingDesktopPage: React.FC<WithTranslation> = ({ t }) => {
                             && <img
                                 alt = { t('welcomepage.logo.logoDeepLinking') }
                                 className = { styles.logo }
-                                src = 'images/logo-deep-linking.png' />
+                                src = 'images/heyjoe-logo.png' />
                     }
                 </div>
                 <div className = { styles.launchingMeetingLabel }>
