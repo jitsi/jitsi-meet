b78d59a4de6503f5a3c0635cdff217f785a97a65
ibrahim ali <41322490+catFurr@users.noreply.github.com>
2025-09-23 10:11:46 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -201,7 +201,7 @@ function getConfig(options = {}) {
                     vendor: {
                         test: /[\\/]node_modules[\\/]/,
                         name: 'vendor',
-                        chunks: 'all',
+                        chunks: 'initial', // Only bundle initial chunks, not async chunks
                         priority: 10,
                         enforce: true
                     },
