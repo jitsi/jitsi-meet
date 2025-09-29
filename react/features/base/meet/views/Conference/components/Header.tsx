7b689ced4b484ff30092aa2723b836a315f30ec1
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2025-09-29 14:58:49 +0200

    Removed beta icon

--- react/features/base/meet/views/Conference/components/Header.tsx
+++ react/features/base/meet/views/Conference/components/Header.tsx
@@ -14,7 +14,6 @@ const LeftContent = React.memo(
                 <span className="text-lg font-semibold text-white" style={{ fontWeight: 600 }}>
                     Meet
                 </span>
-                <img src={"images/beta.png"} alt="logo" className="h-6" style={{ margin: "0px" }} />
             </div>
             <div className="border-l-[1px] border-white/15 px-3">
                 <div className="w-12">
