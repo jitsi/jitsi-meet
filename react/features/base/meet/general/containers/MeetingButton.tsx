3dd054524dc082b26d686d271366d99393741ff7
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2025-09-29 12:31:14 +0200

    Added tooltip to upgrade button

--- react/features/base/meet/general/containers/MeetingButton.tsx
+++ react/features/base/meet/general/containers/MeetingButton.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@internxt/ui";
-import React from "react";
+import { Info } from "@phosphor-icons/react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useUserData } from "../../views/PreMeeting/hooks/useUserData";
 import { isMeetingEnabled } from "../store/meeting/selectors";
@@ -44,6 +45,7 @@ const MeetingButton: React.FC<MeetingButtonProps> = ({
 }) => {
     const isMeetEnabled = useSelector(isMeetingEnabled);
     const isLogged = !!useUserData();
+    const [showTooltip, setShowTooltip] = useState(false);
 
     if (!isLogged) {
         return null;
@@ -57,15 +59,35 @@ const MeetingButton: React.FC<MeetingButtonProps> = ({
         );
     } else {
         return (
-            <Button
-                variant={variant}
-                onClick={() => window.open("https://internxt.com/es/pricing", "_blank")}
-                disabled={loading}
-                loading={loading}
-                className={className}
-            >
-                {translate("meet.preMeeting.upgrade")}
-            </Button>
+            <div className="flex flex-row items-center space-x-3">
+                <Button
+                    variant={variant}
+                    onClick={() => window.open("https://internxt.com/es/pricing", "_blank")}
+                    disabled={loading}
+                    loading={loading}
+                    className={className}
+                >
+                    {translate("meet.preMeeting.upgrade")}
+                </Button>
+                <div
+                    className="relative flex items-center"
+                    onMouseEnter={() => setShowTooltip(true)}
+                    onMouseLeave={() => setShowTooltip(false)}
+                >
+                    <Info size={20} className="text-white cursor-pointer" />
+
+                    {showTooltip && (
+                        <div className="absolute left-full ml-3 top-1/2 -translate-y-1/2 z-50 w-max max-w-sm">
+                            <div className="relative bg-white rounded-lg shadow-lg px-4 py-3">
+                                <div className="absolute right-full top-1/2 -translate-y-1/2 w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent border-r-8 border-r-white" />
+                                <p className="text-sm text-black leading-tight whitespace-pre-line">
+                                    {translate("meet.preMeeting.upgradeMessage")}
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
         );
     }
 };
