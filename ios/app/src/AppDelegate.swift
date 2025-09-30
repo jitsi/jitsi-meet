47d459e80d837ec9b2a5362defc4fdebb32d41f2
Calin-Teodor <calin.chitu@8x8.com>
2025-09-30 10:37:17 +0300

    chore(ios): enable fabric and remove new arch disabling

--- ios/app/src/AppDelegate.swift
+++ ios/app/src/AppDelegate.swift
@@ -3,13 +3,12 @@ import Firebase
 import JitsiMeetSDK
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: RCTAppDelegate {
 
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
-
+        self.dependencyProvider = RCTAppDependencyProvider()
+        
         let jitsiMeet = JitsiMeet.sharedInstance()
 
         // jitsiMeet.webRtcLoggingSeverity = .verbose
@@ -36,28 +35,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         let vc = ViewController()
-        self.window?.rootViewController = vc
+        self.window.rootViewController = vc
         jitsiMeet.showSplashScreen()
 
-        self.window?.makeKeyAndVisible()
+        self.window.makeKeyAndVisible()
 
         return true
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
+    override func applicationWillTerminate(_ application: UIApplication) {
         print("Application will terminate!")
-        if let rootController = self.window?.rootViewController as? ViewController {
+        if let rootController = self.window.rootViewController as? ViewController {
             rootController.terminate()
         }
     }
 
     // MARK: Linking delegate methods
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         return JitsiMeet.sharedInstance().application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         if url.absoluteString.contains("google/link/?dismiss=1&is_weak_match=1") {
             return false
         }
@@ -65,9 +64,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return JitsiMeet.sharedInstance().application(app, open: url, options: options)
     }
 
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+    override func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return JitsiMeet.sharedInstance().application(application, supportedInterfaceOrientationsFor: window)
     }
+
+    override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
+ 
+  override func bundleURL() -> URL? {
+#if DEBUG
+    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
 }
 
 // Firebase utilities
