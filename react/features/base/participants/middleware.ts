33d607757f1f3cb5b6fc5539b2c00fb93ecc9840
ibrahim ali <41322490+catFurr@users.noreply.github.com>
2025-09-30 14:38:36 +0300

    update lockfile

--- react/features/base/participants/middleware.ts
+++ react/features/base/participants/middleware.ts
@@ -30,8 +30,8 @@ import {
 import { open as openParticipantsPane } from '../../participants-pane/actions';
 import { CALLING, INVITED } from '../../presence-status/constants';
 import { RAISE_HAND_SOUND_ID } from '../../reactions/constants';
-import { iAmVisitor } from '../../visitors/functions';
 import { RECORDING_OFF_SOUND, RECORDING_ON_SOUND } from '../../recording/sounds';
+import { iAmVisitor } from '../../visitors/functions';
 import { APP_WILL_MOUNT, APP_WILL_UNMOUNT } from '../app/actionTypes';
 import { CONFERENCE_JOINED, CONFERENCE_WILL_JOIN } from '../conference/actionTypes';
 import { forEachConference, getCurrentConference } from '../conference/functions';
