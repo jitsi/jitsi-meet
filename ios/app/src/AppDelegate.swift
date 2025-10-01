06a5f3a02058273530eb161d3f08eef038fecdbd
Calin-Teodor <calin.chitu@8x8.com>
2025-10-01 15:10:18 +0300

    chore(ios): create wrapper for RTCAppDelegate

--- ios/app/src/AppDelegate.swift
+++ ios/app/src/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.main.bounds)       
+        self.window = UIWindow(frame: UIScreen.main.bounds)
         
         let jitsiMeet = JitsiMeet.sharedInstance()
 
