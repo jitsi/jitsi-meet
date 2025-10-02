d1d20410b86dfe2291b4edabf5fad2904f9bddfb
Boris Grozev <boris@jitsi.org>
2025-10-02 10:58:47 -0500

    Add JaaS unauthenticatedJoins expectation.

--- tests/helpers/expectations.ts
+++ tests/helpers/expectations.ts
@@ -13,6 +13,12 @@ const defaultExpectations = {
          */
         enabled: null,
     },
+    jaas: {
+        /**
+         * Whether the jaas account is configured with the account-level setting to allow unauthenticated users to join.
+         */
+        unauthenticatedJoins: false
+    },
     moderation: {
         // Everyone is a moderator.
         allModerators: false,
