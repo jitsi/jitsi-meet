7275a29d53505008f9a443fd2ba3cd895de33aaa
sandunsameera <sandunsameera25@gmail.com>
2025-10-04 10:16:25 +0530

    fix: add MeetingParticipants component to ParticipantsPane

---
+++ react/features/notifications/constants.js
@@ -0,0 +1,13 @@
+export const NOTIFICATION_TYPE = {
+    NORMAL: 'normal',
+    ERROR: 'error',
+    WARNING: 'warning',
+    SUCCESS: 'success'
+};
+
+export const NOTIFICATION_TIMEOUT_TYPE = {
+    SHORT: 'short',
+    MEDIUM: 'medium',
+    LONG: 'long'
+};
+
