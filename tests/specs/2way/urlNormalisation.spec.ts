325a4c48541a706b12109521d7a04c7286bfeb28
Boris Grozev <boris@jitsi.org>
2025-10-07 10:23:46 -0500

    test: Add more URL normalization test cases.

--- tests/specs/2way/urlNormalisation.spec.ts
+++ tests/specs/2way/urlNormalisation.spec.ts
@@ -1,42 +1,75 @@
-import { multiremotebrowser } from '@wdio/globals';
+import { setTestProperties } from '../../helpers/TestProperties';
+import { config as testsConfig } from '../../helpers/TestsConfig';
+import { joinMuc } from '../../helpers/joinMuc';
 
-import { config } from '../../helpers/TestsConfig';
-import { ensureTwoParticipants } from '../../helpers/participants';
+setTestProperties(__filename, {
+    usesBrowsers: [ 'p1' ]
+});
 
 describe('URL Normalisation', () => {
-    it('joining the meeting', async () => {
-
-        // if we are running with token this becomes ugly to match the URL
-        if (config.jwt.preconfiguredToken) {
-            // TODO: just don't use the token...
-            ctx.skipSuiteTests = 'This test does not support running with a preconfigured token';
-
-            return;
-        }
-
-        // a hack to extract the baseUrl that the test will use
-        const baseUrl = multiremotebrowser.getInstance('p1').options.baseUrl;
-
-        if (!baseUrl) {
-            throw new Error('baseUrl is not set');
-        }
-
-        await ensureTwoParticipants({
+    const tests = [
+        {
+            hint: '@ sign and .',
+            // Room as entered in the URL
+            room: '@example.com',
+            // Room as normalized in the URL
+            roomUrl: 'example.com',
+            // The room part of the MUC JID
+            roomJid: 'example.com',
+            // Tenant as entered in the URL
             tenant: 'tenant@example.com',
-            roomName: `${ctx.roomName}@example.com`
+            // Tenant as normalized in the URL
+            tenantUrl: 'tenantexample.com',
+            // The tenant part of the MUC JID
+            tenantJid: 'tenantexample_com'
+        },
+        {
+            hint: 'Dashes',
+            room: 'foo-bar',
+            roomUrl: 'foo-bar',
+            roomJid: 'foo-bar',
+            tenant: 'tenant-example.com',
+            tenantUrl: 'tenant-example.com',
+            tenantJid: 'tenant-example_com'
+        },
+        {
+            hint: 'Cyrillic',
+            room: 'фоо-бар',
+            roomUrl: '%D1%84%D0%BE%D0%BE-%D0%B1%D0%B0%D1%80',
+            roomJid: '%d1%84%d0%be%d0%be-%d0%b1%d0%b0%d1%80',
+            tenant: 'обитател',
+            tenantUrl: '%D0%BE%D0%B1%D0%B8%D1%82%D0%B0%D1%82%D0%B5%D0%BB',
+            tenantJid: '%d0%be%d0%b1%d0%b8%d1%82%d0%b0%d1%82%d0%b5%d0%bb',
+        }
+    ];
+
+    for (const test of tests) {
+        it(test.hint, async () => {
+            const fullRoom = `${test.room}${ctx.roomName}`;
+            const fullRoomUrl = `${test.roomUrl}${ctx.roomName}`;
+
+            const p = await joinMuc({
+                name: 'p1',
+                token: testsConfig.jwt.preconfiguredToken,
+            }, {
+                tenant: test.tenant,
+                roomName: fullRoom
+            });
+
+            const currentUrlStr = await p.driver.getUrl();
+            const currentUrl = new URL(currentUrlStr);
+            const path = currentUrl.pathname;
+            const parts = path.split('/');
+
+            expect(parts[1]).toBe(test.tenantUrl);
+            expect(parts[2]).toBe(fullRoomUrl);
+
+            const mucJid = (await p.execute(() => APP.conference._room.room.roomjid)).split('@');
+            const roomJid = mucJid[0];
+            const domain = mucJid[1];
+
+            expect(roomJid).toBe(`${test.roomJid}${ctx.roomName}`);
+            expect(domain.startsWith(`conference.${test.tenantJid}.`)).toBe(true);
         });
-    });
-
-    it('check', async () => {
-        const currentUrlStr = await ctx.p1.driver.getUrl();
-        const currentUrl = new URL(currentUrlStr);
-        const path = currentUrl.pathname;
-
-        const parts = path.split('/');
-
-        expect(parts[1]).toBe('tenantexample.com');
-
-        // @ts-ignore
-        expect(parts[2]).toBe(`${ctx.roomName}example.com`);
-    });
+    }
 });
