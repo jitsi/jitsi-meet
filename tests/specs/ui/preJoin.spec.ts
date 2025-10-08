bbe4e45d2abc0f6113de978ca0cae0dc3735aa7a
Boris Grozev <boris@jitsi.org>
2025-10-08 08:46:07 -0500

    test: Move preJoin to ui/.

--- tests/specs/ui/preJoin.spec.ts
+++ tests/specs/ui/preJoin.spec.ts
@@ -1,4 +1,9 @@
+import { setTestProperties } from '../../helpers/TestProperties';
 import { ensureOneParticipant, joinFirstParticipant, joinSecondParticipant } from '../../helpers/participants';
+
+setTestProperties(__filename, {
+    usesBrowsers: [ 'p1', 'p2' ]
+});
 
 describe('PreJoin', () => {
     it('display name required', async () => {
