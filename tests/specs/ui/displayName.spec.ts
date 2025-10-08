7c93184d18adb183c2bce73803da70292d54b4bb
Boris Grozev <boris@jitsi.org>
2025-10-08 08:43:47 -0500

    test: Move displayName to ui/.

--- tests/specs/ui/displayName.spec.ts
+++ tests/specs/ui/displayName.spec.ts
@@ -1,4 +1,9 @@
+import { setTestProperties } from '../../helpers/TestProperties';
 import { ensureTwoParticipants } from '../../helpers/participants';
+
+setTestProperties(__filename, {
+    usesBrowsers: [ 'p1', 'p2' ]
+});
 
 describe('DisplayName', () => {
     it('joining the meeting', () => ensureTwoParticipants({ skipDisplayName: true }));
