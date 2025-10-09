6c04b553ead9bef2c81ac3d99c1e42995cb09113
Saúl Ibarra Corretgé <saghul@jitsi.org>
2025-10-09 14:24:40 +0200

    feat(ios) remove Apple Watch app
    It has been unmaintained for years.

--- react/features/app/reducers.native.ts
+++ react/features/app/reducers.native.ts
@@ -2,7 +2,6 @@ import '../mobile/audio-mode/reducer';
 import '../mobile/background/reducer';
 import '../mobile/call-integration/reducer';
 import '../mobile/external-api/reducer';
-import '../mobile/watchos/reducer';
 import '../share-room/reducer';
 
 import './reducer.native';
