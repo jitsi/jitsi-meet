c6ea0fcf43c3334845b97228dc62805623e8cc6c
Calin-Teodor <calin.chitu@8x8.com>
2025-10-29 14:52:24 +0200

    chore(android/sdk): invoke permissionListener immediatly

--- android/sdk/src/main/java/org/jitsi/meet/sdk/JitsiMeetActivityDelegate.java
+++ android/sdk/src/main/java/org/jitsi/meet/sdk/JitsiMeetActivityDelegate.java
@@ -37,7 +37,6 @@ public class JitsiMeetActivityDelegate {
      * React Native module.
      */
     private static PermissionListener permissionListener;
-    private static Callback permissionsCallback;
 
     /**
      * Tells whether or not the permissions request is currently in progress.
@@ -142,11 +141,6 @@ public class JitsiMeetActivityDelegate {
         if (reactInstanceManager != null) {
             reactInstanceManager.onHostResume(activity, new DefaultHardwareBackBtnHandlerImpl(activity));
         }
-
-        if (permissionsCallback != null) {
-            permissionsCallback.invoke();
-            permissionsCallback = null;
-        }
     }
 
     /**
@@ -169,15 +163,10 @@ public class JitsiMeetActivityDelegate {
 
     public static void onRequestPermissionsResult(
             final int requestCode, final String[] permissions, final int[] grantResults) {
-        permissionsCallback = new Callback() {
-            @Override
-            public void invoke(Object... args) {
-                if (permissionListener != null
-                        && permissionListener.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-                    permissionListener = null;
-                }
-            }
-        };
+        // Invoke the callback immediately
+        if (permissionListener != null && permissionListener.onRequestPermissionsResult(requestCode, permissions, grantResults)) {  
+            permissionListener = null;
+        }
     }
 
     public static void requestPermissions(Activity activity, String[] permissions, int requestCode, PermissionListener listener) {
