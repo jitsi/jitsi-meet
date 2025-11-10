ba55ac55e60fa434b5e8f55766c6251e98e2bb40
Calin-Teodor <calin.chitu@8x8.com>
2025-11-10 13:30:11 +0200

    chore(ios/sdk): remove RNRootView and RCTBridgeWrapper, handle bridge creation through rootViewFactory property

--- ios/sdk/src/JitsiMeet+Private.h
+++ ios/sdk/src/JitsiMeet+Private.h
@@ -15,6 +15,7 @@
  */
 
 #import <React/RCTBridge.h>
+#import <RCTReactNativeFactory.h>
 
 #import "ExternalAPI.h"
 #import "JitsiMeet.h"
@@ -23,6 +24,7 @@
 
 - (NSDictionary *)getDefaultProps;
 - (RCTBridge *)getReactBridge;
+- (RCTReactNativeFactory *)getReactNativeFactory;
 - (ExternalAPI *)getExternalAPI;
 
 @end
