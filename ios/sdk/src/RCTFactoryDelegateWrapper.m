8e2f6080052ff2549960ac9339502b487a8b49ab
Calin-Teodor <calin.chitu@8x8.com>
2025-11-11 12:54:44 +0200

    chore(ios/sdk): check once for NSBundle and remove log emojis

--- ios/sdk/src/RCTFactoryDelegateWrapper.m
+++ ios/sdk/src/RCTFactoryDelegateWrapper.m
@@ -5,7 +5,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTBridge.h>
 
-static NSString *JMReadMetroHostFromBundle(NSBundle *bundle) {
+static NSString *RCTReadMetroHostFromBundle(NSBundle *bundle) {
     NSString *ipPath = [bundle pathForResource:@"ip" ofType:@"txt"];
     if (ipPath.length == 0) {
         return nil;
@@ -14,7 +14,7 @@ static NSString *JMReadMetroHostFromBundle(NSBundle *bundle) {
     NSError *error = nil;
     NSString *host = [NSString stringWithContentsOfFile:ipPath encoding:NSUTF8StringEncoding error:&error];
     if (error) {
-        NSLog(@"⚠️ Failed to read ip.txt: %@", error);
+        NSLog(@"Failed to read ip.txt: %@", error);
         return nil;
     }
 
@@ -25,7 +25,7 @@ static NSString *JMReadMetroHostFromBundle(NSBundle *bundle) {
 
 - (instancetype)init {
     if (self = [super init]) {
-        NSLog(@"🔵 RCTFactoryDelegateWrapper init called");
+        NSLog(@"RCTFactoryDelegateWrapper init called");
     }
     return self;
 }
@@ -40,52 +40,56 @@ static NSString *JMReadMetroHostFromBundle(NSBundle *bundle) {
     NSURL *bundleURL = [provider jsBundleURLForBundleRoot:@"index"];
 
     if (!bundleURL) {
+        // In the SDK the ip.txt used by RN's auto-detection lives inside the framework,
+        // not the host app bundle, so copy the value into jsLocation manually.
         NSBundle *sdkBundle = [NSBundle bundleForClass:[JitsiMeet class]];
-        NSString *host = JMReadMetroHostFromBundle(sdkBundle);
+        NSString *host = RCTReadMetroHostFromBundle(sdkBundle);
         if (host.length > 0) {
-            NSString *port = [[[NSProcessInfo processInfo] environment] objectForKey:@"RCT_METRO_PORT"];
-            if (port.length == 0) {
-                port = [NSString stringWithFormat:@"%lu", (unsigned long)kRCTBundleURLProviderDefaultPort];
-            }
+            NSString *port = [[[NSProcessInfo processInfo] environment] objectForKey:@"RCT_METRO_PORT"]
+                ?: [NSString stringWithFormat:@"%lu", (unsigned long)kRCTBundleURLProviderDefaultPort];
 
             NSString *hostPort = [host containsString:@":"] ? host : [NSString stringWithFormat:@"%@:%@", host, port];
             provider.jsLocation = hostPort;
 
             bundleURL = [provider jsBundleURLForBundleRoot:@"index"];
         }
-    }
-
-    if (bundleURL) {
-        return bundleURL;
-    }
-#else
-    NSURL *bundleURL = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-    if (bundleURL) {
+    } else {
         return bundleURL;
     }
 #endif
 
-    NSBundle *sdkBundle = [NSBundle bundleForClass:[JitsiMeet class]];
-    return [sdkBundle URLForResource:@"main" withExtension:@"jsbundle"];
+    NSArray<NSBundle *> *candidateBundles = @[
+        [NSBundle mainBundle],              // App bundle
+        [NSBundle bundleForClass:[JitsiMeet class]] // SDK framework
+    ];
+
+    for (NSBundle *bundle in candidateBundles) {
+        NSURL *url = [bundle URLForResource:@"main" withExtension:@"jsbundle"];
+        if (url) {
+            return url;
+        }
+    }
+
+    return nil;
 }
 
 - (BOOL)fabricEnabled {
-    NSLog(@"🔵 fabricEnabled called, returning NO");
+    NSLog(@"fabricEnabled called, returning NO");
     return NO;
-}
+}       
 
 - (BOOL)newArchEnabled {
-    NSLog(@"🔵 newArchEnabled called, returning NO");
+    NSLog(@"newArchEnabled called, returning NO");
     return NO;
 }
 
 - (BOOL)turboModuleEnabled {
-    NSLog(@"🔵 turboModuleEnabled called, returning NO");
+    NSLog(@"turboModuleEnabled called, returning NO");
     return NO;
 }
 
 - (BOOL)bridgelessEnabled {
-    NSLog(@"🔵 bridgelessEnabled called, returning NO");
+    NSLog(@"bridgelessEnabled called, returning NO");
     return NO;
 }
 
