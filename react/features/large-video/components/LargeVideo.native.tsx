aaa6f8da02c0abb1b6f58174dfa874c05d569489
Srijan <srijan.mart@gmail.com>
2025-11-26 10:58:27 +0530

    refactor(large-video): migrate LargeVideo.native to function component with useTrackStreamingStatus hook

--- react/features/large-video/components/LargeVideo.native.tsx
+++ react/features/large-video/components/LargeVideo.native.tsx
@@ -1,13 +1,12 @@
-import React, { PureComponent } from 'react';
+import React, { useMemo } from 'react';
 import { connect } from 'react-redux';
 
 import { IReduxState, IStore } from '../../app/types';
-import { JitsiTrackEvents } from '../../base/lib-jitsi-meet';
 import ParticipantView from '../../base/participants/components/ParticipantView.native';
 import { getParticipantById, isLocalScreenshareParticipant } from '../../base/participants/functions';
-import { trackStreamingStatusChanged } from '../../base/tracks/actions.native';
 import { getVideoTrackByParticipant, isLocalVideoTrackDesktop } from '../../base/tracks/functions.native';
 import { ITrack } from '../../base/tracks/types';
+import { useTrackStreamingStatus } from '../hooks/useTrackStreamingStatus';
 
 import { AVATAR_SIZE } from './styles';
 
@@ -55,61 +54,25 @@ interface IProps {
 }
 
 /**
- * The type of the React {@link Component} state of {@link LargeVideo}.
- */
-interface IState {
-
-    /**
-     * Size for the Avatar. It will be dynamically adjusted based on the
-     * available size.
-     */
-    avatarSize: number;
-
-    /**
-     * Whether the connectivity indicator will be shown or not. It will be true
-     * by default, but it may be turned off if there is not enough space.
-     */
-    useConnectivityInfoLabel: boolean;
-}
-
-const DEFAULT_STATE = {
-    avatarSize: AVATAR_SIZE,
-    useConnectivityInfoLabel: true
-};
-
-/** .
- * Implements a React {@link Component} which represents the large video (a.k.a.
+ * Implements a React function component which represents the large video (a.k.a.
  * The conference participant who is on the local stage) on mobile/React Native.
  *
- * @augments Component
+ * @param {IProps} props - The component props.
+ * @returns {JSX.Element} The LargeVideo component.
  */
-class LargeVideo extends PureComponent<IProps, IState> {
-    /**
-     * Creates new LargeVideo component.
-     *
-     * @param {IProps} props - The props of the component.
-     * @returns {LargeVideo}
-     */
-    constructor(props: IProps) {
-        super(props);
+const LargeVideo: React.FC<IProps> = ({
+    _disableVideo,
+    _height,
+    _participantId,
+    _videoTrack,
+    _width,
+    onClick
+}) => {
+    // Use custom hook to manage track streaming status
+    useTrackStreamingStatus(_videoTrack);
 
-        this.handleTrackStreamingStatusChanged = this.handleTrackStreamingStatusChanged.bind(this);
-    }
-
-    state = {
-        ...DEFAULT_STATE
-    };
-
-    /**
-     * Handles dimension changes. In case we deem it's too
-     * small, the connectivity indicator won't be rendered and the avatar
-     * will occupy the entirety of the available screen state.
-     *
-     * @inheritdoc
-     */
-    static getDerivedStateFromProps(props: IProps) {
-        const { _height, _width } = props;
-
+    // Calculate avatar size and connectivity label based on available dimensions
+    const { avatarSize, useConnectivityInfoLabel } = useMemo(() => {
         // Get the size, rounded to the nearest even number.
         const size = 2 * Math.round(Math.min(_height, _width) / 2);
 
@@ -120,119 +83,24 @@ class LargeVideo extends PureComponent<IProps, IState> {
             };
         }
 
-        return DEFAULT_STATE;
+        return {
+            avatarSize: AVATAR_SIZE,
+            useConnectivityInfoLabel: true
+        };
+    }, [ _height, _width ]);
 
-    }
-
-    /**
-     * Starts listening for track streaming status updates after the initial render.
-     *
-     * @inheritdoc
-     * @returns {void}
-     */
-    override componentDidMount() {
-        // Listen to track streaming status changed event to keep it updated.
-        // TODO: after converting this component to a react function component,
-        // use a custom hook to update local track streaming status.
-        const { _videoTrack, dispatch } = this.props;
-
-        if (_videoTrack && !_videoTrack.local) {
-            _videoTrack.jitsiTrack.on(JitsiTrackEvents.TRACK_STREAMING_STATUS_CHANGED,
-                this.handleTrackStreamingStatusChanged);
-            dispatch(trackStreamingStatusChanged(_videoTrack.jitsiTrack,
-                _videoTrack.jitsiTrack.getTrackStreamingStatus()));
-        }
-    }
-
-    /**
-     * Stops listening for track streaming status updates on the old track and starts listening instead on the new
-     * track.
-     *
-     * @inheritdoc
-     * @returns {void}
-     */
-    override componentDidUpdate(prevProps: IProps) {
-        // TODO: after converting this component to a react function component,
-        // use a custom hook to update local track streaming status.
-        const { _videoTrack, dispatch } = this.props;
-
-        if (prevProps._videoTrack?.jitsiTrack?.getSourceName() !== _videoTrack?.jitsiTrack?.getSourceName()) {
-            if (prevProps._videoTrack && !prevProps._videoTrack.local) {
-                prevProps._videoTrack.jitsiTrack.off(JitsiTrackEvents.TRACK_STREAMING_STATUS_CHANGED,
-                    this.handleTrackStreamingStatusChanged);
-                dispatch(trackStreamingStatusChanged(prevProps._videoTrack.jitsiTrack,
-                    prevProps._videoTrack.jitsiTrack.getTrackStreamingStatus()));
-            }
-            if (_videoTrack && !_videoTrack.local) {
-                _videoTrack.jitsiTrack.on(JitsiTrackEvents.TRACK_STREAMING_STATUS_CHANGED,
-                    this.handleTrackStreamingStatusChanged);
-                dispatch(trackStreamingStatusChanged(_videoTrack.jitsiTrack,
-                    _videoTrack.jitsiTrack.getTrackStreamingStatus()));
-            }
-        }
-    }
-
-    /**
-     * Remove listeners for track streaming status update.
-     *
-     * @inheritdoc
-     * @returns {void}
-     */
-    override componentWillUnmount() {
-        // TODO: after converting this component to a react function component,
-        // use a custom hook to update local track streaming status.
-        const { _videoTrack, dispatch } = this.props;
-
-        if (_videoTrack && !_videoTrack.local) {
-            _videoTrack.jitsiTrack.off(JitsiTrackEvents.TRACK_STREAMING_STATUS_CHANGED,
-                this.handleTrackStreamingStatusChanged);
-            dispatch(trackStreamingStatusChanged(_videoTrack.jitsiTrack,
-                _videoTrack.jitsiTrack.getTrackStreamingStatus()));
-        }
-    }
-
-    /**
-     * Handle track streaming status change event by by dispatching an action to update track streaming status for the
-     * given track in app state.
-     *
-     * @param {JitsiTrack} jitsiTrack - The track with streaming status updated.
-     * @param {JitsiTrackStreamingStatus} streamingStatus - The updated track streaming status.
-     * @returns {void}
-     */
-    handleTrackStreamingStatusChanged(jitsiTrack: any, streamingStatus: string) {
-        this.props.dispatch(trackStreamingStatusChanged(jitsiTrack, streamingStatus));
-    }
-
-    /**
-     * Implements React's {@link Component#render()}.
-     *
-     * @inheritdoc
-     * @returns {ReactElement}
-     */
-    override render() {
-        const {
-            avatarSize,
-            useConnectivityInfoLabel
-        } = this.state;
-        const {
-            _disableVideo,
-            _participantId,
-            onClick
-        } = this.props;
-
-        return (
-            <ParticipantView
-                avatarSize = { avatarSize }
-                disableVideo = { _disableVideo }
-                onPress = { onClick }
-                participantId = { _participantId }
-                testHintId = 'org.jitsi.meet.LargeVideo'
-                useConnectivityInfoLabel = { useConnectivityInfoLabel }
-                zOrder = { 0 }
-                zoomEnabled = { true } />
-        );
-    }
-}
+    return (
+        <ParticipantView
+            avatarSize = { avatarSize }
+            disableVideo = { _disableVideo }
+            onPress = { onClick }
+            participantId = { _participantId }
+            testHintId = 'org.jitsi.meet.LargeVideo'
+            useConnectivityInfoLabel = { useConnectivityInfoLabel }
+            zOrder = { 0 }
+            zoomEnabled = { true } />
+    );
+};
 
 /**
  * Maps (parts of) the Redux state to the associated LargeVideo's props.
