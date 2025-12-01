55e96238f15ff42562a0045d4d1d7fb781537a67
damencho <damencho@jitsi.org>
2025-11-30 22:44:18 -0700

    squash: More dependencies.

--- react/features/video-quality/components/VideoQualityLabel.web.tsx
+++ react/features/video-quality/components/VideoQualityLabel.web.tsx
@@ -43,7 +43,8 @@ const VideoQualityLabel = () => {
         tooltipKey = 'videoStatus.performanceSettings';
     }
 
-    const onClick = useCallback(() => dispatch(openDialog('VideoQualityDialog', VideoQualityDialog)), [ dispatch ]);
+    const onClick = useCallback(() =>
+        dispatch(openDialog('VideoQualityDialog', VideoQualityDialog)), [ dispatch, openDialog ]);
 
     return (
         <Tooltip
