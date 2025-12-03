6ef63cc1b7df8aaf473cdc82225480f0e1a126d6
Calin-Teodor <calin.chitu@8x8.com>
2025-12-03 13:35:43 +0200

    feat(chat): increase bottom padding for android input bar

--- react/features/chat/components/native/styles.ts
+++ react/features/chat/components/native/styles.ts
@@ -137,7 +137,7 @@ export default {
      * A special padding to avoid issues on some devices (such as Android devices with custom suggestions bar).
      */
     extraBarPadding: {
-        paddingBottom: 30
+        paddingBottom: BaseTheme.spacing[8]
     },
 
     inputBar: {
