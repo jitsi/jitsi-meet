026fe4a1ab26b9f739b4ba1033c602c3861de5ac
tannviiiii1756 <tanvitoocool170506@gmail.com>
2025-12-08 10:49:06 +0530

    Update reducer.ts

--- react/features/notepad/reducer.ts
+++ react/features/notepad/reducer.ts
@@ -1,19 +1,34 @@
-import { TOGGLE_NOTEPAD } from './actions';
+import { TOGGLE_NOTEPAD, SET_NOTEPAD_CONTENT, CLEAR_NOTEPAD } from './actions';
 
 export interface INotepadState {
     isOpen: boolean;
+    content: string;
 }
 
 const initialState: INotepadState = {
-    isOpen: false
+    isOpen: false,
+    content: ''
 };
 
 export default function(state = initialState, action: any) {
     switch (action.type) {
+
         case TOGGLE_NOTEPAD:
             return {
                 ...state,
                 isOpen: !state.isOpen
+            };
+
+        case SET_NOTEPAD_CONTENT:
+            return {
+                ...state,
+                content: action.payload
+            };
+
+        case CLEAR_NOTEPAD:
+            return {
+                ...state,
+                content: ''
             };
 
         default:
