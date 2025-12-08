2e12dcb02cd960f0b2cb7fb8fa23945793b10079
tannviiiii1756 <tanvitoocool170506@gmail.com>
2025-12-08 10:49:29 +0530

    Update action.ts

--- react/features/notepad/action.ts
+++ react/features/notepad/action.ts
@@ -1,7 +1,16 @@
 export const TOGGLE_NOTEPAD = 'TOGGLE_NOTEPAD';
+export const SET_NOTEPAD_CONTENT = 'SET_NOTEPAD_CONTENT';
+export const CLEAR_NOTEPAD = 'CLEAR_NOTEPAD';
 
-export const toggleNotepad = () => {
-    return {
-        type: TOGGLE_NOTEPAD
-    };
-};
+export const toggleNotepad = () => ({
+    type: TOGGLE_NOTEPAD
+});
+
+export const setNotepadContent = (text: string) => ({
+    type: SET_NOTEPAD_CONTENT,
+    payload: text
+});
+
+export const clearNotepad = () => ({
+    type: CLEAR_NOTEPAD
+});
