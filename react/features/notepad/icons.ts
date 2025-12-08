b2f330f67e6345e244799b0d2fbf4a51439d6c24
tannviiiii1756 <tanvitoocool170506@gmail.com>
2025-12-08 10:47:02 +0530

    Create icons.ts

---
+++ react/features/notepad/icons.ts
@@ -0,0 +1,3 @@
+export const NOTEPAD_ICON = {
+    src: 'data:image/svg+xml;utf8,<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><rect width="18" height="22" x="3" y="1" rx="2" ry="2" fill="white" stroke="black" stroke-width="2"/><line x1="6" y1="6" x2="18" y2="6" stroke="black" stroke-width="2"/><line x1="6" y1="10" x2="18" y2="10" stroke="black" stroke-width="2"/><line x1="6" y1="14" x2="18" y2="14" stroke="black" stroke-width="2"/></svg>'
+};
