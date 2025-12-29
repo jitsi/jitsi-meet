45f7fcc3d51888765738f26ae30dafa7bab20677
Calin-Teodor <calin.chitu@8x8.com>
2025-12-29 13:15:24 +0200

    feat(chat): create AbstractClosedCaptions wrapper component

--- react/features/chat/components/native/ClosedCaptions.tsx
+++ react/features/chat/components/native/ClosedCaptions.tsx
@@ -1,12 +1,11 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { View, ViewStyle } from 'react-native';
 import { Text } from 'react-native-paper';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { IReduxState } from '../../../app/types';
-import { openDialog } from '../../../base/dialog/actions';
 import Icon from '../../../base/icons/components/Icon';
 import { IconSubtitles } from '../../../base/icons/svg';
 import JitsiScreen from '../../../base/modal/components/JitsiScreen';
@@ -15,12 +14,8 @@ import BaseTheme from '../../../base/ui/components/BaseTheme.native';
 import Button from '../../../base/ui/components/native/Button';
 import { BUTTON_TYPES } from '../../../base/ui/constants.native';
 import { TabBarLabelCounter } from '../../../mobile/navigation/components/TabBarLabelCounter';
-// @ts-ignore
-import { StartRecordingDialog } from '../../../recording/components/Recording';
-import { setRequestingSubtitles } from '../../../subtitles/actions.any';
-import { canStartSubtitles } from '../../../subtitles/functions.any';
-import { isTranscribing } from '../../../transcribing/functions';
 import { ChatTabs } from '../../constants';
+import AbstractClosedCaptions, { AbstractProps } from '../AbstractClosedCaptions';
 
 import { closedCaptionsStyles } from './styles';
 
@@ -29,16 +24,14 @@ import { closedCaptionsStyles } from './styles';
  *
  * @returns {JSX.Element} - The ClosedCaptions component.
  */
-export default function ClosedCaptions() {
-    const dispatch = useDispatch();
+const ClosedCaptions = ({
+    canStartSubtitles,
+    isButtonPressed,
+    isTranscribing,
+    startClosedCaptions
+}: AbstractProps): JSX.Element => {
     const navigation = useNavigation();
     const { t } = useTranslation();
-    const _isTranscribing = useSelector(isTranscribing);
-    const _canStartSubtitles = useSelector(canStartSubtitles);
-    const [ isButtonPressed, setButtonPressed ] = useState(false);
-    const subtitlesError = useSelector((state: IReduxState) => state['features/subtitles']._hasError);
-    const isAsyncTranscriptionEnabled = useSelector((state: IReduxState) =>
-        state['features/base/conference'].conference?.getMetadataHandler()?.getMetadata()?.asyncTranscription);
     const isCCTabFocused = useSelector((state: IReduxState) => state['features/chat'].focusedTab === ChatTabs.CLOSED_CAPTIONS);
 
     useEffect(() => {
@@ -49,36 +42,10 @@ export default function ClosedCaptions() {
                     label = { t('chat.tabs.closedCaptions') } />
             )
         });
-    }, [ isCCTabFocused ]);
-
-    const startClosedCaptions = useCallback(() => {
-        if (isAsyncTranscriptionEnabled) {
-            dispatch(openDialog('StartRecordingDialog', StartRecordingDialog, {
-                recordAudioAndVideo: false
-            }));
-        } else {
-            if (isButtonPressed) {
-                return;
-            }
-            dispatch(setRequestingSubtitles(true, false, null));
-            setButtonPressed(true);
-        }
-    }, [ isAsyncTranscriptionEnabled, dispatch, isButtonPressed ]);
-
-    useEffect(() => {
-        if (subtitlesError && isButtonPressed && !isAsyncTranscriptionEnabled) {
-            setButtonPressed(false);
-        }
-    }, [ subtitlesError, isButtonPressed, isAsyncTranscriptionEnabled ]);
-
-    useEffect(() => {
-        if (isButtonPressed && !isAsyncTranscriptionEnabled && !_isTranscribing) {
-            setButtonPressed(false);
-        }
-    }, [ isButtonPressed, isAsyncTranscriptionEnabled, _isTranscribing ]);
+    }, [ isCCTabFocused, navigation, t ]);
 
     const getContentContainerStyle = () => {
-        if (_isTranscribing) {
+        if (isTranscribing) {
             return closedCaptionsStyles.transcribingContainer as StyleType;
         }
 
@@ -86,7 +53,7 @@ export default function ClosedCaptions() {
     };
 
     const renderContent = () => {
-        if (_isTranscribing) {
+        if (isTranscribing) {
             return (
                 <View style = { closedCaptionsStyles.placeholderContainer as ViewStyle }>
                     { /* Placeholder for future LanguageSelector and SubtitlesMessagesContainer */ }
@@ -94,7 +61,7 @@ export default function ClosedCaptions() {
             );
         }
 
-        if (_canStartSubtitles) {
+        if (canStartSubtitles) {
             return (
                 <View style = { closedCaptionsStyles.emptyContent as ViewStyle }>
                     <Button
@@ -129,4 +96,6 @@ export default function ClosedCaptions() {
             { renderContent() }
         </JitsiScreen>
     );
-}
+};
+
+export default AbstractClosedCaptions(ClosedCaptions);
