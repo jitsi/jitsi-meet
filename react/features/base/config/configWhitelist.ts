3acc546398314cf29ea2115fc027621dddab8ad7
damencho <damencho@jitsi.org>
2026-01-05 13:41:04 -0600

    squash: Whitelist the config.

--- react/features/base/config/configWhitelist.ts
+++ react/features/base/config/configWhitelist.ts
@@ -220,6 +220,7 @@ export default [
     'resolution',
     'screenshotCapture',
     'securityUi',
+    'showChatPermissionsModeratorSetting',
     'speakerStats',
     'startAudioMuted',
     'startAudioOnly',
