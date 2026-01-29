1f0b6cfc74c210ed4da5b137b5ef8815e4e1ee8b
Calin-Teodor <calin.chitu@8x8.com>
2026-01-29 12:26:12 +0200

    dep(react-native): updates related to version 0.79.7

--- react-native.config.js
+++ react-native.config.js
@@ -8,7 +8,11 @@ module.exports = {
     dependencies: {
         '@giphy/react-native-sdk': {
             platforms: {
-                ios: null
+                ios: {
+                    // Disable auto-linking but keep codegen.
+                    // We need it to generate GiphyReactNativeSDKSpec.
+                    scriptPhases: []
+                }
             }
         },
         '@react-native-google-signin/google-signin': {
