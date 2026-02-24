2c24109e2882814e340870d73ae9383b5c69d650
Boris Grozev <boris@jitsi.org>
2026-02-23 18:20:45 -0600

    squash: Switch to iframe.

--- tests/specs/jaas/transcriptions.spec.ts
+++ tests/specs/jaas/transcriptions.spec.ts
@@ -44,9 +44,11 @@ for (const asyncTranscriptions of asyncTranscriptionValues) {
                 if (!expectedOn && !asyncTranscriptions) {
                     // The "stopped" event is sometimes fired before the jigasi participant leaves. If we re-start
                     // transcription before jigasi has left, jicofo will reject the request.
+                    await p.switchToIFrame()
                     await p.waitForParticipants(
                         1,
                         'Unexpected number of participants. Jigasi failed to leave?');
+                    await p.switchToMainFrame()
                 }
             }
         }
