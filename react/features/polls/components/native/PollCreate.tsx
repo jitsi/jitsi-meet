b6d89b0939f282eb09bcf509bf12509ee216c445
Calinteodor <calin.chitu@8x8.com>
2026-03-02 15:54:41 +0200

    feat(modal): fixes around JitsiScreen footer (#17041)
    *Add extra px for Jitsi screen footer when native keyboard is visible.

--- react/features/polls/components/native/PollCreate.tsx
+++ react/features/polls/components/native/PollCreate.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { FlatList, SafeAreaView, TextInput, View, ViewStyle } from 'react-native';
+import { FlatList, Platform, SafeAreaView, TextInput, View, ViewStyle } from 'react-native';
 import { Divider } from 'react-native-paper';
 import { useDispatch } from 'react-redux';
 
+import { useKeyboardVisible } from '../../../base/modal/hooks.native';
+import BaseTheme from '../../../base/ui/components/BaseTheme.native';
 import Button from '../../../base/ui/components/native/Button';
 import Input from '../../../base/ui/components/native/Input';
 import { BUTTON_TYPES } from '../../../base/ui/constants.native';
@@ -30,6 +32,7 @@ const PollCreate = (props: AbstractProps) => {
 
     const answerListRef = useRef<FlatList>(null);
     const dispatch = useDispatch();
+    const keyboardVisible = Platform.OS === 'android' && useKeyboardVisible();
 
     /*
      * This ref stores the Array of answer input fields, allowing us to focus on them.
@@ -166,7 +169,11 @@ const PollCreate = (props: AbstractProps) => {
                     keyExtractor = { (item, index) => index.toString() }
                     ref = { answerListRef }
                     renderItem = { renderListItem } />
-                <View style = { pollsStyles.pollCreateButtonsContainer as ViewStyle }>
+                <View
+                    style = { [
+                        pollsStyles.pollCreateButtonsContainer,
+                        keyboardVisible && { marginBottom: BaseTheme.spacing[8] }
+                    ] as ViewStyle[] }>
                     <Button
                         accessibilityLabel = 'polls.create.addOption'
                         disabled = { answers.length >= ANSWERS_LIMIT }
