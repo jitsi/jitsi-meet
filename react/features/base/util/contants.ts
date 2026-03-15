6fb92ae9f683ef53139f9771c99e073fd79e45ca
Dipanshu Rawat <dv451197@gmail.com>
2026-03-15 14:18:07 +0530

    chore: updated comment message

--- react/features/base/util/contants.ts
+++ react/features/base/util/contants.ts
@@ -1,2 +1,2 @@
-// This file is now empty - constants moved to config/constants.ts for consistency
+// Constants moved to config/constants.ts for consistency
 // Import security functions from config/constants instead
