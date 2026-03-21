c91d11a20bd2da5b6b12d60a0915f28d99503990
Sanskar Gunde <gundesanskar71@gmail.com>
2026-03-21 12:08:35 +0530

    fix(external-api): replay dataChannelOpened for late listeners

--- modules/API/external/external_api.js
+++ modules/API/external/external_api.js
@@ -341,6 +341,7 @@ export default class JitsiMeetExternalAPI extends EventEmitter {
         this._onStageParticipant = undefined;
         this._iAmvisitor = undefined;
         this._pipConfig = configOverwrite?.pip;
+        this._dataChannelOpened = false;
         this._setupListeners();
         id++;
     }
@@ -641,6 +642,12 @@ export default class JitsiMeetExternalAPI extends EventEmitter {
             const eventName = events[name];
 
             if (eventName) {
+                // Cache dataChannelOpened so late-registering listeners still fire.
+                // Must be set before emit so addListener replay works synchronously.
+                if (eventName === 'dataChannelOpened') {
+                    this._dataChannelOpened = true;
+                }
+
                 this.emit(eventName, data);
 
                 return true;
@@ -662,6 +669,25 @@ export default class JitsiMeetExternalAPI extends EventEmitter {
         });
 
         this._setupIntersectionObserver();
+    }
+
+    /**
+     * Overrides EventEmitter.addListener to fix a race condition where
+     * dataChannelOpened fires before the parent page can register a listener.
+     * If the event already fired, the callback is invoked on the next tick.
+     *
+     * @param {string} event - The event name.
+     * @param {Function} listener - The callback function.
+     * @returns {JitsiMeetExternalAPI} This.
+     */
+    addListener(event, listener) {
+        super.addListener(event, listener);
+
+        if (event === 'dataChannelOpened' && this._dataChannelOpened) {
+            setTimeout(() => listener({}), 0);
+        }
+
+        return this;
     }
 
     /**
