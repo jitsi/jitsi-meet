31245b334f1200edaa8ce7922377ffa645667ea6
Alexey Matveev <malex@1forma.ru>
2026-03-21 17:26:09 +0300

    feat(conf-destroy): remove unneeded reason on conf destroy on session termination

--- react/features/base/conference/middleware.native.ts
+++ react/features/base/conference/middleware.native.ts
@@ -35,11 +35,15 @@ MiddlewareRegistry.register(store => next => action => {
 
         const [ reason ] = error.params;
 
-        const reasonKey = Object.keys(TRIGGER_READY_TO_CLOSE_REASONS)[
+        let localizedReason = Object.keys(TRIGGER_READY_TO_CLOSE_REASONS)[
             Object.values(TRIGGER_READY_TO_CLOSE_REASONS).indexOf(reason)
         ];
 
-        dispatch(notifyConferenceFailed(reasonKey, () => {
+        if (reason === TRIGGER_READY_TO_CLOSE_REASONS['dialog.sessTerminatedReason']) {
+            localizedReason = '';
+        }
+
+        dispatch(notifyConferenceFailed(localizedReason, () => {
             dispatch(conferenceLeft(action.conference));
             dispatch(appNavigate(undefined));
         }));
