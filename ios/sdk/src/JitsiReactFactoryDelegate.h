69affb86f05745032d11431a5065296b86e5e05e
Calin-Teodor <calin.chitu@8x8.com>
2026-04-06 18:06:07 +0300

    dep(react-native): updates related to version 0.81.5

--- ios/sdk/src/JitsiReactFactoryDelegate.h
+++ ios/sdk/src/JitsiReactFactoryDelegate.h
@@ -4,6 +4,10 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface JitsiReactFactoryDelegate : RCTDefaultReactNativeFactoryDelegate
+- (BOOL)fabricEnabled;
+- (BOOL)newArchEnabled;
+- (BOOL)turboModuleEnabled;
+- (BOOL)bridgelessEnabled;
 @end
 
 NS_ASSUME_NONNULL_END
