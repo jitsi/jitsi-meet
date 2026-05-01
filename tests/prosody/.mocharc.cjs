d376f62d054609d069dfc833538dbba1251105e7
Boris Grozev <boris@jitsi.org>
2026-05-01 13:54:01 -0500

    fix: Tests hanging.

--- tests/prosody/.mocharc.cjs
+++ tests/prosody/.mocharc.cjs
@@ -2,6 +2,7 @@ module.exports = {
     require: ['./setup.js'],
     spec: './**/*_spec.js',
     timeout: 120000,
+    exit: true, // force-exit after tests: @xmpp/reconnect leaves open handles
     reporter: 'allure-mocha',
     reporterOptions: 'resultsDir=./allure-results,extraReporters=spec',
 };
