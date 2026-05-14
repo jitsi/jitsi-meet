04876da9c46644aafe8319be9643ad98ac72a56a
Calin-Teodor <calin.chitu@8x8.com>
2026-05-14 16:57:10 +0300

    center text inside inputs only for ios

--- react/features/welcome/components/styles.native.ts
+++ react/features/welcome/components/styles.native.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 import { BoxModel } from '../../base/styles/components/styles/BoxModel';
 import BaseTheme from '../../base/ui/components/BaseTheme.native';
@@ -206,7 +206,7 @@ export default {
     customInput: {
         fontSize: 18,
         letterSpacing: 0,
-        textAlign: 'center'
+        ...Platform.select({ ios: { textAlign: 'center' } })
     },
 
     recentList: {
