8a55bd40efda6634afb1c5542787ef104070f592
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2026-07-23 18:08:18 +0200

    format

--- resources/prosody-plugins/mod_muc_trace.lua
+++ resources/prosody-plugins/mod_muc_trace.lua
@@ -7,17 +7,20 @@ local tracer = otel.Tracer.new(processor, "prosody", "muc")
 
 
 local function find_tag(tags, name)
-	for _, tag in ipairs(tags) do
-		if tag.name == name then
-			return tag
-		end
-	end
-	return nil
+    for _, tag in ipairs(tags) do
+        if tag.name == name then
+            return tag
+        end
+    end
+    return nil
 end
 
 module:hook("muc-room-created", function(event)
-	local traceparent = find_tag(event.stanza.tags, "traceparent")
-	local span = tracer:start_span("muc.room-created", traceparent and traceparent.attr)
-			:set_attribute("room.name", otel.Attribute.string(jid.node(event.room.jid)))
-	span:end_span()
+    -- If the presence that created the room contains a traceparent extension,
+    -- it will be used as parent for current span.
+    local traceparent = find_tag(event.stanza.tags, "traceparent")
+
+    tracer:start_span("muc.room-created", traceparent and traceparent.attr)
+        :set_attribute("room.name", otel.Attribute.string(jid.node(event.room.jid)))
+        :end_span()
 end, -1)
--- resources/prosody-plugins/otel.lib.lua
+++ resources/prosody-plugins/otel.lib.lua
@@ -16,10 +16,10 @@ local m = {}
 --------------------------------------------------
 
 local function random_hex(n)
-	return hex.to(random.bytes(n))
+    return hex.to(random.bytes(n))
 end
 local function to_nanoseconds(t)
-	return tostring(math.floor(t * 1e9))
+    return tostring(math.floor(t * 1e9))
 end
 
 
@@ -31,9 +31,9 @@ local Attribute = {}
 m.Attribute = Attribute
 
 function Attribute.string(value)
-	return {
-		string_value = value
-	}
+    return {
+        string_value = value
+    }
 end
 
 --------------------------------------------------
@@ -41,18 +41,18 @@ end
 --------------------------------------------------
 
 local SpanStatus = {
-	UNSET = 0,
-	OK    = 1,
-	ERROR = 2,
+    UNSET = 0,
+    OK    = 1,
+    ERROR = 2,
 }
 
 local SpanKind = {
-	UNSPECIFIED = 0,
-	INTERNAL    = 1,
-	SERVER      = 2,
-	CLIENT      = 3,
-	PRODUCER    = 4,
-	CONSUMER    = 5,
+    UNSPECIFIED = 0,
+    INTERNAL    = 1,
+    SERVER      = 2,
+    CLIENT      = 3,
+    PRODUCER    = 4,
+    CONSUMER    = 5,
 }
 
 local Span = {}
@@ -60,43 +60,43 @@ Span.__index = Span
 m.Span = Span
 
 function Span.new(name, tracer, opts)
-	local self = {
-		name = name,
-		tracer = tracer,
-		parent = opts.parent,
-		trace_id = opts.parent and opts.parent.trace_id or random_hex(16),
-		span_id = random_hex(8),
-		start_time = time.now(),
-		attributes = array {},
-	}
+    local self = {
+        name = name,
+        tracer = tracer,
+        parent = opts.parent,
+        trace_id = opts.parent and opts.parent.trace_id or random_hex(16),
+        span_id = random_hex(8),
+        start_time = time.now(),
+        attributes = array {},
+    }
 
-	return setmetatable(self, Span)
+    return setmetatable(self, Span)
 end
 
 function Span:set_attribute(k, v)
-	table.insert(self.attributes, { key = k, value = v })
-	return self
+    table.insert(self.attributes, { key = k, value = v })
+    return self
 end
 
 function Span:end_span()
-	self.end_time = time.now()
-	self.tracer.processor:add(self)
+    self.end_time = time.now()
+    self.tracer.processor:add(self)
 end
 
 function Span:encode()
-	return {
-		trace_id = self.trace_id,
-		span_id = self.span_id,
-		parent_span_id = self.parent and self.parent.parent_id,
-		name = self.name,
-		start_time_unix_nano = to_nanoseconds(self.start_time),
-		end_time_unix_nano = to_nanoseconds(self.end_time),
-		attributes = self.attributes,
-		kind = self.kind or SpanKind.INTERNAL,
-		status = {
-			code = self.status or SpanStatus.OK
-		}
-	}
+    return {
+        trace_id = self.trace_id,
+        span_id = self.span_id,
+        parent_span_id = self.parent and self.parent.parent_id,
+        name = self.name,
+        start_time_unix_nano = to_nanoseconds(self.start_time),
+        end_time_unix_nano = to_nanoseconds(self.end_time),
+        attributes = self.attributes,
+        kind = self.kind or SpanKind.INTERNAL,
+        status = {
+            code = self.status or SpanStatus.OK
+        }
+    }
 end
 
 --------------------------------------------------
@@ -108,14 +108,14 @@ Processor.__index = Processor
 m.Processor = Processor
 
 function Processor.new(exporter)
-	local self = {
-		exporter = exporter
-	}
-	return setmetatable(self, Processor)
+    local self = {
+        exporter = exporter
+    }
+    return setmetatable(self, Processor)
 end
 
 function Processor:add(span)
-	self.exporter:export({ span })
+    self.exporter:export({ span })
 end
 
 --------------------------------------------------
@@ -127,51 +127,51 @@ Exporter.__index = Exporter
 m.Exporter = Exporter
 
 function Exporter.new(endpoint)
-	local self = {
-		endpoint = endpoint,
-	}
-	return setmetatable(self, Exporter)
+    local self = {
+        endpoint = endpoint,
+    }
+    return setmetatable(self, Exporter)
 end
 
 function Exporter:export(spans)
-	local body = {
-		resource_spans = {
-			{
-				resource = spans[1].tracer.resource,
-				scope_spans = {
-					{
-						scope = {
-							name = spans[1].tracer.name,
-						},
-						spans = {}
-					},
-				},
-			}
-		}
-	}
+    local body = {
+        resource_spans = {
+            {
+                resource = spans[1].tracer.resource,
+                scope_spans = {
+                    {
+                        scope = {
+                            name = spans[1].tracer.name,
+                        },
+                        spans = {}
+                    },
+                },
+            }
+        }
+    }
 
 
-	for _, span in ipairs(spans) do
-		table.insert(
-			body.resource_spans[1].scope_spans[1].spans,
-			span:encode()
-		)
-	end
+    for _, span in ipairs(spans) do
+        table.insert(
+            body.resource_spans[1].scope_spans[1].spans,
+            span:encode()
+        )
+    end
 
-	http.request(
-		self.endpoint, {
-			method = "POST",
-			headers = {
-				["Content-Type"] = "application/json",
-			},
-			body = json.encode(body),
-		},
-		function(body, code, response)
-			if code ~= 200 then
-				module:log("error", json.encode(body))
-			end
-		end
-	)
+    http.request(
+        self.endpoint, {
+            method = "POST",
+            headers = {
+                ["Content-Type"] = "application/json",
+            },
+            body = json.encode(body),
+        },
+        function(body, code, response)
+            if code ~= 200 then
+                module:log("error", json.encode(body))
+            end
+        end
+    )
 end
 
 --------------------------------------------------
@@ -183,22 +183,22 @@ Tracer.__index = Tracer
 m.Tracer = Tracer
 
 function Tracer.new(processor, service, name)
-	return setmetatable({
-		processor = processor,
-		resource = {
-			attributes = {
-				{
-					key = "service.name",
-					value = Attribute.string(service)
-				}
-			}
-		},
-		name = name,
-	}, Tracer)
+    return setmetatable({
+        processor = processor,
+        resource = {
+            attributes = {
+                {
+                    key = "service.name",
+                    value = Attribute.string(service)
+                }
+            }
+        },
+        name = name,
+    }, Tracer)
 end
 
 function Tracer:start_span(name, parent)
-	return Span.new(name, self, { parent = parent })
+    return Span.new(name, self, { parent = parent })
 end
 
 return m
