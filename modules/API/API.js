bd63ef57bf0b51c561aeab2854c1d49f4111328d
Joshua Irmer <62890337+joshuai96@users.noreply.github.com>
2026-07-28 14:29:34 +0200

    fix(external-api): regression in virtual background toggle command (#17657)

--- modules/API/API.js
+++ modules/API/API.js
@@ -953,7 +953,7 @@ function initCommands() {
             APP.store.dispatch(overwriteConfig(whitelistedConfig));
         },
         'toggle-virtual-background': () => {
-            APP.store.dispatch(toggleDialog(SettingsDialog, {
+            APP.store.dispatch(toggleDialog('SettingsDialog', SettingsDialog, {
                 defaultTab: SETTINGS_TABS.VIRTUAL_BACKGROUND }));
         },
         'end-conference': () => {
