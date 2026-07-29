9d0e30532048755b4cf9b337152b8f6748548acf
Bandhan Majumder <133476557+bandhan-majumder@users.noreply.github.com>
2026-07-29 21:38:45 +0530

    feat(documentPiP): auto open on tab switch and trigger button (#17450)
    * feat(documentPiP): auto open on tab switch and trigger button
    
    * suggestions
    
    * lint:fix
    
    * file and class name updates
    
    * restore comment
    
    * suggestions
    
    * add: suggestions and webkit for safari auto open
    
    * add: comment

---
+++ react/features/pip/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Default WIDTH for the Document Picture-in-Picture window.
+ */
+export const DEFAULT_DOCUMENT_PIP_WIDTH = 284;
+
+/**
+ * Default HEIGHT for the Document Picture-in-Picture window.
+ */
+export const DEFAULT_DOCUMENT_PIP_HEIGHT = 160;
+
+/**
+ * Relay check delay in PiP video element.
+ */
+export const FOCUS_CHECK_DELAY_MS = 100;
