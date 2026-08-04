e01cf09d02a3e0656be21902c29fe03c6630ee03
Bandhan Majumder <bandhanmajumder16@gmail.com>
2026-08-05 03:58:52 +0530

    fix(safari): PiP Video content

--- react/features/pip/constants.ts
+++ react/features/pip/constants.ts
@@ -12,3 +12,8 @@ export const DEFAULT_DOCUMENT_PIP_HEIGHT = 160;
  * Relay check delay in PiP video element.
  */
 export const FOCUS_CHECK_DELAY_MS = 100;
+
+/**
+ * Webkit PiP playback recovery timeout in PiP Video element.
+ */
+export const WEBKIT_PIP_PLAYBACK_RECOVERY_TIMEOUT_MS = 300;
