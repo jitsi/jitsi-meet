e75921d9231d8a5daab6fb6ae6270afc097931f4
Bandhan Majumder <bandhanmajumder16@gmail.com>
2026-08-06 01:57:04 +0530

    feat(config): gate browser pip behind config flag

--- config.js
+++ config.js
@@ -821,6 +821,8 @@ var config = {
 
     // Picture-in-Picture configuration.
     // pip: {
+    //     // Disable Picture-in-Picture for browser meetings entirely. Defaults to true.
+    //     disableBrowserPiP: true,
     //     // Disable Picture-in-Picture entirely. Defaults to false.
     //     disabled: false,
     //     // Allow Picture-in-Picture on the prejoin page. Defaults to false.
