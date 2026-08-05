db361cacc6575da9222286d9dd7d5b73fdb3964b
Bandhan Majumder <bandhanmajumder16@gmail.com>
2026-08-06 01:57:04 +0530

    feat(config): gate browser pip behind config flag

--- react/features/pip/functions.ts
+++ react/features/pip/functions.ts
@@ -78,9 +78,15 @@ export function getPiPVideoTrack(state: IReduxState, participant: IParticipant |
  */
 export function shouldShowPiP(state: IReduxState): boolean {
     const pipConfig = state['features/base/config'].pip;
+    const isBrowserPiPDisabled = pipConfig?.disableBrowserPiP;
 
     // Check if PiP is enabled at all.
     if (!isPiPEnabled(pipConfig)) {
+        return false;
+    }
+
+    // Check if PiP is enabled for browser meetings.
+    if (!browser.isElectron() && isBrowserPiPDisabled) {
         return false;
     }
 
